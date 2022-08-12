3beff10076171f31071546a6dc3b4895d4409221
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-08-12 10:36:33 -0500

    Time: 6 ms (40.98%), Space: 127.4 MB (48.60%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,50 +1,27 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int n = nums.length;
-
-        // Base case.
-        int contains = 0;
-        for (int i = 0; i < n; i++)
-            if (nums[i] == 1) {
-                contains++;
+        //step 1: check for 1
+        boolean hasOne = false;
+        for(int i = 0; i < nums.length; i++){ 
+            if(nums[i] == 1){
+                hasOne = true;
                 break;
             }
-
-        if (contains == 0)
-            return 1;
-
-        // Replace negative numbers, zeros,
-        // and numbers larger than n by 1s.
-        // After this convertion nums will contain 
-        // only positive numbers.
-        for (int i = 0; i < n; i++)
-            if ((nums[i] <= 0) || (nums[i] > n))
-                nums[i] = 1;
-
-        // Use index as a hash key and number sign as a presence detector.
-        // For example, if nums[1] is negative that means that number `1`
-        // is present in the array. 
-        // If nums[2] is positive - number 2 is missing.
-        for (int i = 0; i < n; i++) {
-            int a = Math.abs(nums[i]);
-            // If you meet number a in the array - change the sign of a-th element.
-            // Be careful with duplicates : do it only once.
-            if (a == n)
-                nums[0] = - Math.abs(nums[0]);
-            else
-                nums[a] = - Math.abs(nums[a]);
         }
-
-        // Now the index of the first positive number 
-        // is equal to first missing positive.
-        for (int i = 1; i < n; i++) {
-            if (nums[i] > 0)
-                return i;
+        if(!hasOne) return 1;
+        //step 2: change any nums[i] <= 0 || nums[i] > nums.length to 1
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] <= 0 || nums[i] > nums.length) nums[i] = 1;
         }
-
-        if (nums[0] > 0)
-            return n;
-
-        return n + 1;
+        
+        //step 3: change ath element to negative if present in array
+        for(int i = 0; i < nums.length; i++){
+            int index = Math.abs(nums[i]) - 1;
+            if(nums[index] > 0) nums[index]*=-1;
+        }
+        
+        //step 4: return first positive index >= 1
+        for(int i = 0; i < nums.length; i++) if(nums[i] > 0) return i+1;
+        return nums.length+1;
     }
 }
