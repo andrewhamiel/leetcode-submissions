266848ea2da8f3b9ed02750538ebbe20d2f0e73a
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-08-13 17:47:37 -0500

    Time: 9 ms (98.59%), Space: 43.3 MB (85.80%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
@@ -1,21 +1,22 @@
 class Solution {
+    private int n, numWords, wordLength, maxSubstringLength;
     private Map<String, Integer> wordCount = new HashMap();
-    private int n, wordLength, maxSubstringLength, numWords;
-    private List<Integer> answer = new ArrayList();
+    private List<Integer> ans = new ArrayList();
     
     public List<Integer> findSubstring(String s, String[] words) {
         n = s.length();
         numWords = words.length;
-        wordLength = words[0].length();   
-        maxSubstringLength = wordLength * numWords;
+        wordLength = words[0].length();
+        maxSubstringLength = numWords * wordLength;
         
-        for(String word : words) 
+        //Populate wordCount map
+        for(String word : words)
             wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
         
-        //Iterate up to wordLength starting characters in case of offset
+        //Iterate up to wordLength characters in case of offset
         for(int i = 0; i < wordLength; i++)
             slidingWindow(i, s);
-        return answer;
+        return ans;
     }
     
     private void slidingWindow(int left, String s){
@@ -25,34 +26,33 @@ class Solution {
         
         for(int right = left; right <= n - wordLength; right+=wordLength){
             String sub = s.substring(right, right + wordLength);
-            //Case 1: Word not in array, clear and shift left wordLength chars
+            //Case 1: mismatching word, reset window and clear
             if(!wordCount.containsKey(sub)){
                 wordsSeen.clear();
-                wordsUsed = 0;
-                excessWord = false;   
                 left = right + wordLength;
+                excessWord = false;
+                wordsUsed = 0;
             }else{
-                //Case 2: Max Window Size or excess word present
-                while(right-left == maxSubstringLength || excessWord){
-                    String leftmostWord = s.substring(left, left+wordLength);
+                //Case 2: Max window size or excess word
+                while(right - left == maxSubstringLength || excessWord){
+                    String leftmostWord = s.substring(left, left + wordLength);
                     left+=wordLength;
-                    wordsSeen.put(leftmostWord, wordsSeen.get(leftmostWord) -1);
-                    //Case 2a. excess word removed
+                    wordsSeen.put(leftmostWord, wordsSeen.get(leftmostWord) - 1);
+                    //Case 2a. Found an excess word
                     if(wordsSeen.get(leftmostWord) >= wordCount.get(leftmostWord)) excessWord = false;
-                    //Case 2b. word needed, remove from words used
+                    //Case 2b. We actually needed this one, decrement words used
                     else wordsUsed--;
                 }
                 
-                //Add occurrence of word seen
+                //Add to wordsSeen map
                 wordsSeen.put(sub, wordsSeen.getOrDefault(sub, 0) + 1);
-                //If not past limit increment wordsUsed
+                //Make sure this doesn't go past word limit
                 if(wordsSeen.get(sub) <= wordCount.get(sub)) wordsUsed++;
-                //Otherwise flag excessWord
                 else excessWord = true;
                 
-                //Case 3: Have reached conditions for substring
-                if(wordsUsed == numWords && !excessWord) answer.add(left);
+                //Case 3: valid substring found, add to ans
+                if(wordsUsed == numWords && !excessWord) ans.add(left);
             }
         }
-    }      
+    }
 }
