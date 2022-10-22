a1c304b5385d44f413a5405f457bfa47294edec9
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-10-22 08:41:58 -0500

    Time: 41 ms (35.16%), Space: 54.7 MB (10.66%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.java
@@ -1,20 +1,21 @@
 class Solution {
+    //Tabulation
+    //Time: O(nlog(n) + O(n^2)) ~ O(n^2)
     public int findLongestChain(int[][] pairs) {
-        Arrays.sort(pairs, (a, b) -> a[0] - b[0]);
+        Arrays.sort(pairs, (a,b) -> a[0] - b[0]);
         int[] dp = new int[pairs.length];
         Arrays.fill(dp, 1);
         int ans = 1;
-        for (int j = 1; j < pairs.length; j++) {
+        for(int j = 1; j < pairs.length; j++){
             int curr = 1;
-            for (int i = 0; i < j; i++) {
-                if (pairs[i][1] < pairs[j][0]){
+            for(int i = 0; i < j; i++){
+                if(pairs[i][1] < pairs[j][0]){
                     dp[j] = Math.max(dp[j], dp[i] + 1);
                     curr = Math.max(curr, dp[j]);
                 }
             }
             ans = Math.max(ans, curr);
         }
-
         return ans;
     }
 }
