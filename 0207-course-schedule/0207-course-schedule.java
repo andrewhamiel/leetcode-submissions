03606feca75bf2684c1c118553dd2164bbbd4087
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-10-25 13:16:01 -0500

    Time: 11 ms (59.13%), Space: 49.2 MB (28.33%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,31 @@
+class Solution {
+    Map<Integer, List<Integer>> map = new HashMap();
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        for(int[] course : prerequisites){
+            map.putIfAbsent(course[1], new ArrayList());
+            map.get(course[1]).add(course[0]);
+        }
+        
+        boolean[] path = new boolean[numCourses], checked = new boolean[numCourses];
+        for(int[] course : prerequisites) if(isCyclic(course[1], path, checked)) return false;
+        return true;
+    }
+    
+    private boolean isCyclic(int course, boolean[] path, boolean[] checked){
+        //if we already checked this node, or if key isn't present, no cycle
+        if(checked[course] || !map.containsKey(course)) return false;
+        //if we have repeated along same path, cycle found
+        if(path[course]) return true;
+        
+        //mark before backtracking
+        path[course] = true;
+        boolean isCycle = false;
+        for(Integer prereq : map.get(course)){
+            isCycle = isCyclic(prereq, path, checked);
+            if(isCycle) break;
+        }
+        path[course] = false;
+        checked[course] = true;
+        return isCycle;
+    }
+}
