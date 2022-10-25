23b3fbb2a3d8159da7011890a185edaa46e3e6d9
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-10-25 15:16:37 -0500

    Time: 846 ms (5.33%), Space: 181.5 MB (5.02%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        // boolean[][] visited = new boolean[board.length][board[0].length];
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
                 if(board[i][j] == word.charAt(0) && dfs(i, j, board, word)) return true;
@@ -10,17 +9,14 @@ class Solution {
     }
     
     private boolean dfs(int i, int j, char[][] board, String word){
-        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(0)) return false;
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || word.charAt(0) != board[i][j]) return false;
         if(word.length() == 1) return true;
         board[i][j] = ' ';
-        boolean hasWord = false;
-        if(dfs(i, j-1, board, word.substring(1))
+        boolean isWord = dfs(i, j-1, board, word.substring(1))
         || dfs(i, j+1, board, word.substring(1))
         || dfs(i-1, j, board, word.substring(1))
-        || dfs(i+1, j, board, word.substring(1))){
-            hasWord = true;
-        }
+        || dfs(i+1, j, board, word.substring(1));
         board[i][j] = word.charAt(0);
-        return hasWord;
+        return isWord;
     }
 }
