6a2f901f24405fc8c33111f769ca6b4a8e046063
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-10-26 10:13:35 -0500

    Time: 35 ms (27.35%), Space: 55 MB (17.51%) - LeetHub

--- 1062-longest-repeating-substring/1062-longest-repeating-substring.java
+++ 1062-longest-repeating-substring/1062-longest-repeating-substring.java
@@ -1,16 +1,13 @@
 class Solution {
     //naive O(n^2)
     public int longestRepeatingSubstring(String s) {
-        // char[] str = s.toCharArray();
-        int max=0;
-        // int len = str.length;
-        int[][] dp = new int[s.length()+1][s.length()+1];
-        
-        for(int i=0; i<s.length(); i++){
-            for(int j=i+1; j<s.length(); j++){
-                if(s.charAt(i)==s.charAt(j)){
-                    dp[i+1][j+1] = dp[i][j]+1;
-                    max = Math.max(dp[i+1][j+1],max);
+        int[][] dp = new int[s.length() + 1][s.length() + 1];
+        int max = 0;
+        for(int i = 0; i < s.length(); i++){
+            for(int j = i + 1; j < s.length(); j++){
+                if(s.charAt(i) == s.charAt(j)){
+                    dp[i+1][j+1] = dp[i][j] + 1;
+                    max = Math.max(max, dp[i+1][j+1]);
                 }
             }
         }
