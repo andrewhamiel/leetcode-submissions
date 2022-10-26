35288ffcdd07523a5f5bdcb0ea48de683920bb5d
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-10-26 10:25:39 -0500

    Time: 13 ms (78.77%), Space: 48.3 MB (42.67%) - LeetHub

--- 1062-longest-repeating-substring/1062-longest-repeating-substring.java
+++ 1062-longest-repeating-substring/1062-longest-repeating-substring.java
@@ -1,25 +1,21 @@
 class Solution {
-  
-    //Binary Search + HashSet of seen strings. T: O(nlog(n))
-  public int longestRepeatingSubstring(String S) {
-    int left = 1, right = S.length();
-    while (left <= right) {
-      int mid = left + (right - left) / 2;
-      if (search(mid, S.length(), S) != -1) left = mid + 1;
-      else right = mid - 1;
+    public int longestRepeatingSubstring(String s) {
+        int left = 0, right = s.length();
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            if(search(mid, s.length(), s) != -1) left = mid + 1;
+            else right = mid - 1;
+        }
+        return left - 1;
     }
-
-    return left - 1;
-  }
     
-  public int search(int L, int n, String S) {
-    HashSet<String> seen = new HashSet();
-    String tmp;
-    for(int start = 0; start < n - L + 1; ++start) {
-      tmp = S.substring(start, start + L);
-      if (seen.contains(tmp)) return start;
-      seen.add(tmp);
+    private int search(int length, int n, String s){
+        Set<String> seen = new HashSet();
+        for(int start = 0; start < n - length + 1; start++){
+            String tmp = s.substring(start, start+length);
+            if(seen.contains(tmp)) return start;
+            seen.add(tmp);
+        }
+        return -1;
     }
-    return -1;
-  }
 }
