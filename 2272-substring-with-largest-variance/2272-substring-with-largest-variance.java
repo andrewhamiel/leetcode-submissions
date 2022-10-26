fe3a7366887474715e70738dabe392d6be9097ae
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-10-26 09:44:00 -0500

    Time: 260 ms (72.46%), Space: 41.6 MB (98.78%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.java
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.java
@@ -1,39 +1,53 @@
 class Solution {
-    //Kadanes: Time O(26*26*n)
+    // Time Complexity:- O(26*26*N) -> Generalized to O(N)
+    // Space Complexity:- O(26) -> Generalized to O(1)
     public int largestVariance(String s) {
-        int [] freq = new int[26];
-        for(int i = 0 ; i < s.length() ; i++)
-            freq[(int)(s.charAt(i) - 'a')]++;
-        
-        int maxVariance = 0;
-        for(int a = 0 ; a < 26 ; a++){
-            for(int b = 0 ; b < 26 ; b++){
-                int remainingA = freq[a];
-                int remainingB = freq[b];
-                if(a == b || remainingA == 0 || remainingB == 0) continue;
-                
-				// run kadanes on each possible character pairs (A & B)
-                int currBFreq = 0, currAFreq = 0;
-                for(int i = 0 ; i < s.length() ; i++){
-                    int c =  (int)(s.charAt(i) - 'a');
-                    
-                    if(c == b) currBFreq++;
-                    if(c == a) {
-                        currAFreq++;
-                        remainingA--;
+        if (s == null || s.length() == 0) return 0;
+
+        // Init int array and increment char counts per s
+        int[] frequency = new int[26];
+        for (int i = 0; i < s.length(); i++){
+            frequency[s.charAt(i) - 'a']++;
+        }
+
+        int max = 0;
+
+        // Loop through every possible pair of chars in the initial string (charA, charB),
+        // comparing them to find max variance in substrings
+        for (int charA = 0; charA < 26; charA++){
+            for (int charB = 0; charB < 26; charB++){
+                int remainA = frequency[charA], remainB = frequency[charB];
+
+                // When i and j are equal or either or both i and j are 0, continue to next iteration of loop
+                if (charA == charB || remainA == 0 || remainB == 0) continue;
+
+                // Custom Kadanes Algorithm - Dynamic Programming custom implementation to find the substring
+                // with the max variance between the frequency of charA and charB
+                int charAFreq = 0, charBFreq = 0;
+                // Compare charA and charB by iterating the string s and comparing the count of charA with charB
+                for (int k = 0; k < s.length(); k++) {
+                    int currChar = s.charAt(k) - 'a';   // Get the current char in the string
+                    // Current char in String is charB
+                    if (currChar == charB) charBFreq++;                    // Increment frequency for charB
+                    else if (currChar == charA){
+                        charAFreq++;                    // Increment frequency of charA
+                        remainA--;                      // Decrement charA's remaining in the array
+                        
                     }
-                    
-                    if(currAFreq > 0)
-                        maxVariance = Math.max(maxVariance, currBFreq - currAFreq);
-                    
-                    if(currBFreq < currAFreq &&  remainingA >= 1){
-                        currBFreq = 0;
-                        currAFreq = 0;
+
+                    // Assuming charAFreq is larger than charBFreq, get max variance
+                    if (charAFreq > 0) max = Math.max(max, charBFreq - charAFreq);
+
+                    // charBFreq should always be larger than charAFreq
+                    // Reset the frequency counts for charA and charB so charB frequency can be maximized as long as
+                    // there's at least 1 charA remaining on the right side of the pointer
+                    if (charBFreq < charAFreq && remainA >= 1){
+                        charBFreq = 0;
+                        charAFreq = 0;
                     }
                 }
             }
         }
-        
-        return maxVariance;
+        return max;
     }
 }
