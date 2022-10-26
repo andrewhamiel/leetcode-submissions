9745c70131e8cc0185ac511f53fba5747d1cff2e
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-10-26 09:46:11 -0500

    Time: 368 ms (33.99%), Space: 42.8 MB (67.12%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.java
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.java
@@ -1,49 +1,25 @@
 class Solution {
-    // Time Complexity:- O(26*26*N) -> Generalized to O(N)
-    // Space Complexity:- O(26) -> Generalized to O(1)
     public int largestVariance(String s) {
-        if (s == null || s.length() == 0) return 0;
-
-        // Init int array and increment char counts per s
-        int[] frequency = new int[26];
-        for (int i = 0; i < s.length(); i++){
-            frequency[s.charAt(i) - 'a']++;
-        }
-
+        int[] freq = new int[26];
+        for(Character ch : s.toCharArray()) freq[ch - 'a']++;
+        
         int max = 0;
-
-        // Loop through every possible pair of chars in the initial string (charA, charB),
-        // comparing them to find max variance in substrings
-        for (int charA = 0; charA < 26; charA++){
-            for (int charB = 0; charB < 26; charB++){
-                int remainA = frequency[charA], remainB = frequency[charB];
-
-                // When i and j are equal or either or both i and j are 0, continue to next iteration of loop
-                if (charA == charB || remainA == 0 || remainB == 0) continue;
-
-                // Custom Kadanes Algorithm - Dynamic Programming custom implementation to find the substring
-                // with the max variance between the frequency of charA and charB
-                int charAFreq = 0, charBFreq = 0;
-                // Compare charA and charB by iterating the string s and comparing the count of charA with charB
-                for (int k = 0; k < s.length(); k++) {
-                    int currChar = s.charAt(k) - 'a';   // Get the current char in the string
-                    // Current char in String is charB
-                    if (currChar == charB) charBFreq++;                    // Increment frequency for charB
-                    else if (currChar == charA){
-                        charAFreq++;                    // Increment frequency of charA
-                        remainA--;                      // Decrement charA's remaining in the array
-                        
+        for(int charA = 0; charA < 26; charA++){
+            for(int charB = 0; charB < 26; charB++){
+                int remainA = freq[charA], remainB = freq[charB];
+                if(charA == charB || remainA == 0 || remainB == 0) continue;
+                int aFreq = 0, bFreq = 0;
+                for(int k = 0; k < s.length(); k++){
+                    int currChar = s.charAt(k) - 'a';
+                    if(currChar == charB) bFreq++;
+                    if(currChar == charA){
+                        remainA--;
+                        aFreq++;
                     }
-
-                    // Assuming charAFreq is larger than charBFreq, get max variance
-                    if (charAFreq > 0) max = Math.max(max, charBFreq - charAFreq);
-
-                    // charBFreq should always be larger than charAFreq
-                    // Reset the frequency counts for charA and charB so charB frequency can be maximized as long as
-                    // there's at least 1 charA remaining on the right side of the pointer
-                    if (charBFreq < charAFreq && remainA >= 1){
-                        charBFreq = 0;
-                        charAFreq = 0;
+                    if(aFreq > 0) max = Math.max(max, bFreq - aFreq);
+                    if(remainA >= 1 && bFreq < aFreq){
+                        aFreq = 0;
+                        bFreq = 0;
                     }
                 }
             }
