fdf139d78d5d18d3f494bd1dd3118f6455126819
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-10-27 14:23:00 -0500

    Time: 11 ms, Memory: 46.6 MB - LeetHub

--- roman-to-integer/roman-to-integer.java
+++ roman-to-integer/roman-to-integer.java
@@ -1,6 +1,8 @@
 class Solution {
     private String[] symbols = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
     private int[] values = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    
+    //Greedy. Time: O(n), Space: O(1)
     public int romanToInt(String s) {
         int num = 0;
         for(int i = 0; i < symbols.length && s.length() > 0; i++){
