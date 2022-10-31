b27319652c03881099dea6807347ff0b4d0423a4
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-10-31 12:44:51 -0500

    Time: 43 ms (35.32%), Space: 59.3 MB (28.68%) - LeetHub

--- 0562-longest-line-of-consecutive-one-in-matrix/0562-longest-line-of-consecutive-one-in-matrix.java
+++ 0562-longest-line-of-consecutive-one-in-matrix/0562-longest-line-of-consecutive-one-in-matrix.java
@@ -1,13 +1,12 @@
 class Solution {
-    
-    //3D Dynamic Programming
+    //3D Dynamic Programming problem
     public int longestLine(int[][] mat) {
-        int longestLine = 0;
         int[][][] dp = new int[mat.length][mat[0].length][4];
+        int longestLine = 0;
         for(int i = 0; i < mat.length; i++){
             for(int j = 0; j < mat[0].length; j++){
                 if(mat[i][j] != 0){
-                    dp[i][j][0] = (i > 0) && dp[i-1][j][0] != 0 ? dp[i-1][j][0]+1 : 1;
+                    dp[i][j][0] = (i > 0) && dp[i-1][j][0] != 0 ? dp[i-1][j][0] + 1 : 1;
                     dp[i][j][1] = (j > 0) && dp[i][j-1][1] != 0 ? dp[i][j-1][1] + 1 : 1;
                     dp[i][j][2] = (i > 0) && (j > 0) && dp[i-1][j-1][2] != 0 ? dp[i-1][j-1][2] + 1 : 1;
                     dp[i][j][3] = (i > 0) && (j+1) < mat[0].length && dp[i-1][j+1][3] != 0 ? dp[i-1][j+1][3] + 1 : 1;
