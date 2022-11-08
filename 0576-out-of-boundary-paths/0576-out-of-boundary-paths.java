e74c7c4f44d8392077037b6c55e826f44d6c7e28
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-11-08 07:15:26 -0600

    Time: 9 ms (66.67%), Space: 42.8 MB (45.74%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.java
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.java
@@ -1,25 +1,23 @@
 class Solution {
-  public int findPaths(int m, int n, int N, int x, int y) {
-    int M = 1000000000 + 7;
-    int dp[][] = new int[m][n];
-    dp[x][y] = 1;
-    int count = 0;
-    for (int moves = 1; moves <= N; moves++) {
-      int[][] temp = new int[m][n];
-      for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-          if (i == m - 1) count = (count + dp[i][j]) % M;
-          if (j == n - 1) count = (count + dp[i][j]) % M;
-          if (i == 0) count = (count + dp[i][j]) % M;
-          if (j == 0) count = (count + dp[i][j]) % M;
-          temp[i][j] = (
-              ((i > 0 ? dp[i - 1][j] : 0) + (i < m - 1 ? dp[i + 1][j] : 0)) % M +
-              ((j > 0 ? dp[i][j - 1] : 0) + (j < n - 1 ? dp[i][j + 1] : 0)) % M
-          ) % M;
-        }
-      }
-      dp = temp;
+    int mod = 1000000007, m = 0, n = 0;
+    int[][][] memo;
+    public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        this.m = m;
+        this.n = n;
+        memo = new int[m][n][maxMove + 1];
+        for(int[][] mat : memo)
+            for(int[] row : mat) Arrays.fill(row, -1);
+        return dp(startRow, startColumn, maxMove);
     }
-    return count;
-  }
+    
+    private int dp(int i, int j, int moves){
+        if(i < 0 || i >= m || j < 0 || j >= n) return 1;
+        if(moves == 0) return 0;
+        if(memo[i][j][moves] != -1) return memo[i][j][moves];
+        memo[i][j][moves] = (
+        (dp(i-1, j, moves-1) + dp(i+1, j, moves - 1)) % mod
+            + (dp(i, j-1, moves-1) + dp(i, j+1, moves-1)) % mod
+        ) % mod;
+        return memo[i][j][moves];
+    }
 }
