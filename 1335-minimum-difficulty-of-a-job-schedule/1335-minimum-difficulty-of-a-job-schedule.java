7d0461cbee6fdf7ceb3a3ae1f29dd09167d2e745
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-11-10 06:16:33 -0600

    Time: 22 ms (53.16%), Space: 42.3 MB (28.49%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.java
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.java
@@ -1,27 +1,34 @@
 class Solution {
-    //2D DP. Time: O(n^2 * d)
-    //Space: O(n*d)
+    private int[] jobDifficulty, hardestRemainingJob;
+    private int[][] memo;
+    private int d = 0;;
+    
     public int minDifficulty(int[] jobDifficulty, int d) {
-        if(d > jobDifficulty.length) return -1;
-        
-        int[] hardestRemaining = new int[jobDifficulty.length];
-        
-        int[][] dp = new int[d + 1][jobDifficulty.length + 1];
-        for(int day = 0; day <= d; day++){
-            for(int j = 0; j < jobDifficulty.length; j++) dp[day][j] = Integer.MAX_VALUE;
+        this.d = d;
+        this.jobDifficulty = jobDifficulty;
+        if(jobDifficulty.length < d) return -1;
+        memo = new int[jobDifficulty.length][d+1];
+        for(int[] row : memo) Arrays.fill(row, -1);
+        hardestRemainingJob = new int[jobDifficulty.length];
+        int hardest = 0;
+        for(int i = jobDifficulty.length - 1; i >= 0; i--){
+            hardest = Math.max(hardest, jobDifficulty[i]);
+            hardestRemainingJob[i] = hardest;
         }
+        return dp(0, 1);
+    }
+    
+    private int dp(int i, int day){
+        if(day == d) return hardestRemainingJob[i];
         
-        for(int daysRemain = 1; daysRemain <= d; daysRemain++){
-            for(int i = 0; i < jobDifficulty.length - daysRemain + 1; i++){
-                int dailyMaxDiff = 0;
-                for(int j = i + 1; j < jobDifficulty.length - daysRemain + 2; j++){
-                    dailyMaxDiff = Math.max(dailyMaxDiff, jobDifficulty[j-1]);
-                    if(dp[daysRemain-1][j] != Integer.MAX_VALUE){
-                        dp[daysRemain][i] = Math.min(dp[daysRemain][i], dailyMaxDiff + dp[daysRemain-1][j]);
-                    }
-                }
+        if(memo[i][day] == -1){
+            int best = Integer.MAX_VALUE, hardest = 0;
+            for(int j = i; j < jobDifficulty.length - (d - day); j++){
+                hardest = Math.max(hardest, jobDifficulty[j]);
+                best = Math.min(best, hardest + dp(j+1, day+1));
             }
+            memo[i][day] = best;
         }
-        return dp[d][0] < Integer.MAX_VALUE ? dp[d][0] : -1;
+        return memo[i][day];
     }
 }
