3ae76dac2dc37ff0145307028e03e973fee3f451
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-11-14 07:48:08 -0600

    Time: 25 ms (65.71%), Space: 144 MB (21.29%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.java
+++ 2055-plates-between-candles/2055-plates-between-candles.java
@@ -1,51 +1,34 @@
 class Solution {
     public int[] platesBetweenCandles(String s, int[][] queries) {
-        int n= s.length();
-        int sum=0;
-        int[] psum= new int[n];
+        int n = s.length(), sum = 0;
+        int[] psum = new int[n];
         //1. psum
-        for(int i=0;i<n;++i){
-            if(s.charAt(i)=='|'){
-                psum[i]=sum;
-            }else{
-                sum+=1;
-            }
-            psum[i]=sum;
+        for(int i = 0; i < n; i++){
+            if(s.charAt(i) != '|') sum++;
+            psum[i] = sum;
         }
         //2. left sum
-        int[] leftCand= new int[n];
-        Arrays.fill(leftCand,-1);
-        for(int i=0;i<n;++i){
-            if(s.charAt(i)=='|')
-                leftCand[i]=i;
-            else{
-                leftCand[i]=i-1>=0?leftCand[i-1]:-1;
-                }
+        int[] leftCand = new int[n];
+        Arrays.fill(leftCand, -1);
+        for(int i = 0; i < n; i++){
+            if(s.charAt(i) == '|') leftCand[i] = i;
+            else leftCand[i] = i-1 >= 0 ? leftCand[i-1] : -1;
         }
         //3. right sum
-        int[] rightCand= new int[n];
-        Arrays.fill(rightCand,-1);
-        for(int i=n-1;i>=0;--i){
-            if(s.charAt(i)=='|')
-                rightCand[i]=i;
-            else{
-                rightCand[i]=i+1<n?rightCand[i+1]:-1;
-            }
+        int[] rightCand = new int[n];
+        Arrays.fill(rightCand, -1);
+        for(int i = n-1; i >= 0; i--){
+            if(s.charAt(i) == '|') rightCand[i] = i;
+            else rightCand[i] = i+1 < n ? rightCand[i+1] : -1;
         }
         //4. ans
-        int[] ans= new int[queries.length];
-        for(int i=0;i<queries.length;++i){
-            int l= queries[i][0];
-            int r= queries[i][1];
-            
-            int lCand= rightCand[l];
-            int rCand= leftCand[r];
-            if(lCand==-1 || rCand==-1 || lCand>=rCand)
-                ans[i]=0;
-            else
-            ans[i]=psum[rCand]-psum[lCand];
+        int[] ans = new int[queries.length];
+        for(int i = 0; i < queries.length; i++){
+            int leftQuery = queries[i][0], rightQuery = queries[i][1];
+            int left = rightCand[leftQuery], right = leftCand[rightQuery];
+            if(left == -1 || right == -1 || left >= right) ans[i] = 0;
+            else ans[i] = psum[right] - psum[left];
         }
-        
         return ans;
     }
 }
