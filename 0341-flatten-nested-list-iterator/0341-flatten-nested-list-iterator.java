7eeccee76dc842fe368feb75f20ef1b6a142fdcd
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-11-15 07:48:59 -0600

    Time: 10 ms (8.73%), Space: 46.2 MB (71.29%) - LeetHub

--- 0341-flatten-nested-list-iterator/0341-flatten-nested-list-iterator.java
+++ 0341-flatten-nested-list-iterator/0341-flatten-nested-list-iterator.java
@@ -1,50 +1,51 @@
 import java.util.NoSuchElementException;
-/**
- * // This is the interface that allows for creating nested lists.
- * // You should not implement it, or speculate about its implementation
- * public interface NestedInteger {
- *
- *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
- *     public boolean isInteger();
- *
- *     // @return the single integer that this NestedInteger holds, if it holds a single integer
- *     // Return null if this NestedInteger holds a nested list
- *     public Integer getInteger();
- *
- *     // @return the nested list that this NestedInteger holds, if it holds a nested list
- *     // Return empty list if this NestedInteger holds a single integer
- *     public List<NestedInteger> getList();
- * }
- */
+
 public class NestedIterator implements Iterator<Integer> {
-    Deque<NestedInteger> stack;
-
+    
+    private Deque<List<NestedInteger>> listStack = new ArrayDeque();
+    private Deque<Integer> indexStack = new ArrayDeque();
+    
     public NestedIterator(List<NestedInteger> nestedList) {
-        stack = new ArrayDeque(nestedList);
+        listStack.push(nestedList);
+        indexStack.push(0);
     }
-
+        
     @Override
     public Integer next() {
-        if(!hasNext()) throw new NoSuchElementException();
-        return stack.pop().getInteger(); 
+        if (!hasNext()) throw new NoSuchElementException();
+        int currentPosition = indexStack.pop();
+        indexStack.push(currentPosition + 1);
+        Integer ans = listStack.peek().get(currentPosition).getInteger();
+        return ans;
     }
 
+    
     @Override
     public boolean hasNext() {
-        moveToTopOfStack();
-        return !stack.isEmpty();
+        makeStackTopAnInteger();
+        return !indexStack.isEmpty();
     }
-    
-    private void moveToTopOfStack(){
-        while(!stack.isEmpty() && !stack.peek().isInteger()){
-            List<NestedInteger> list = stack.pop().getList();
-            for(int i = list.size() - 1; i >= 0; i--) stack.push(list.get(i));
+
+
+    private void makeStackTopAnInteger() {  
+        while (!indexStack.isEmpty()) {
+            Integer ind = indexStack.peek();
+            // If the top list is used up, pop it and its index.
+            if (ind >= listStack.peek().size()) {
+                indexStack.pop();
+                listStack.pop();
+                continue;
+            }
+
+            // Otherwise, if it's already an integer, we don't need to do anything.
+            NestedInteger nestedInteger = listStack.peek().get(ind);
+            if (nestedInteger.isInteger()) break;
+
+            // Otherwise, it must be a list. We need to update the previous index
+            // and then add the new list with an index of 0.
+            listStack.push(nestedInteger.getList());
+            indexStack.push(indexStack.pop() + 1);
+            indexStack.push(0);
         }
     }
 }
-
-/**
- * Your NestedIterator object will be instantiated and called as such:
- * NestedIterator i = new NestedIterator(nestedList);
- * while (i.hasNext()) v[f()] = i.next();
- */
