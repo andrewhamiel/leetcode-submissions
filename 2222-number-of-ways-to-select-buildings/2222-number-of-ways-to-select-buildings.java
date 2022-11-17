7a84a38cbe34f016a895a8eea13cd95d413968f7
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-11-17 07:13:36 -0600

    Time: 33 ms (88.37%), Space: 61.8 MB (67.49%) - LeetHub

---
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long numberOfWays(String s) {
+        long[] endsWithZero = new long[2], endsWithOne = new long[2];
+        long ans = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '1'){
+                ans+=endsWithZero[1];
+                endsWithOne[0]++;
+                endsWithOne[1]+=endsWithZero[0];
+                
+            }else{
+                ans+=endsWithOne[1];
+                endsWithZero[0]++;
+                endsWithZero[1]+=endsWithOne[0];
+            }
+        }
+        return ans;
+    }
+}
