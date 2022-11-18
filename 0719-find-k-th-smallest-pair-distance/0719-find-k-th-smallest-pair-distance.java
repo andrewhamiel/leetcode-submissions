e014594d54b2d73e8789ff5d090294524371c7cc
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-11-18 16:03:48 -0600

    Time: 5 ms (100.00%), Space: 42.7 MB (85.68%) - LeetHub

---
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int smallestDistancePair(int[] nums, int k) {
+        //sort the array & initialize variable
+        Arrays.sort(nums); 
+        int n = nums.length, l = 0, r = nums[n-1] - nums[0];         
+        //simple binary search
+        //check whether value is greater or equal to k (and execute binary search function)
+        while(l < r){
+            int m = l+ (r-l) / 2; 
+            
+            //just assume that condition function excute and give us the answer
+            if(numPairsWithDistanceLessThanOrEqualTo(nums, m)>=k) r = m; 
+            else l = m +1;       
+        }
+        return l;             
+    }
+    
+    
+    //simple sliding window
+    //remember this is sorted array
+    private int numPairsWithDistanceLessThanOrEqualTo(int [] nums, int mid){
+        
+        int total = 0; 
+        int left = 0;
+        for(int right =0 ; right < nums.length; right++){
+            
+            //check if distance between two values " <= mid "
+            
+            //if anytime our distance becomes greater so we need to increment left pointer (to minimize distance) upto when condition not becomes true
+            while(nums[right] - nums[left] > mid){
+                left++; 
+            }
+            
+            //store  " right - left " index (as this is sorted array)
+            //  for left to right index values's all must satisfy (<=mid) condition
+            total += right - left; 
+            
+        }
+        
+        return total;
+    }
+}
