06d97c0888efb5377f145d655b613877a8db5eac
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-11-20 07:01:15 -0600

    Time: 17 ms (57.32%), Space: 45.3 MB (13.20%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
@@ -1,29 +1,22 @@
 class Solution {
-    //Binary Search + Sliding Window. n = nums.length, w = Math.max(nums[i])
-    //Time: O(nlog(n) + nlog(w))
-    //nlog(n) from Arrays.sort
-    //binary search is O(log(w)), sliding window is O(n)
-    //Space: O(1)
     public int smallestDistancePair(int[] nums, int k) {
-        //sort the array & initialize variable
         Arrays.sort(nums); //O(nlog(n))
         int w = nums[nums.length - 1] - nums[0], left = 0, right = w;
-        //O(n*log(w))
+        //binary search is O(log(w)) with nested O(n) to numOfPairDistancesSmallerThan method
+        //Total loop time complexity = O(nlog(w))
         while(left < right){
             int mid = left + (right - left)/2;
-            if(numPairsWithDistanceLessThanOrEqualTo(nums, mid) >= k) right = mid; //O(n)
+            if(numOfPairDistancesSmallerThan(mid, nums) >= k) right = mid;
             else left = mid + 1;
         }
         return left;
     }
     
-    
-    //simple sliding window
-    //remember this is sorted array
-    private int numPairsWithDistanceLessThanOrEqualTo(int [] nums, int mid){
+    //O(n)
+    private int numOfPairDistancesSmallerThan(int rangeDist, int[] nums){
         int left = 0, total = 0;
         for(int right = 0; right < nums.length; right++){
-            while(nums[right] - nums[left] > mid) left++;
+            while(nums[right] - nums[left] > rangeDist) left++;
             total+=right - left;
         }
         return total;
