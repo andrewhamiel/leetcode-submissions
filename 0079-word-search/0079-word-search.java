43c06cfebc59a2b6b99708ac22e0d9f088faa522
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-11-24 15:58:47 -0600

    Time: 713 ms (6.80%), Space: 144.6 MB (5.78%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,4 +1,7 @@
 class Solution {
+    
+    //DFS + Backtracking. Time: O(N*3^L), N = num cells, L = Length of word to be matched
+    //Space: O(L)
     public boolean exist(char[][] board, String word) {
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
