f25d2762e623107b71fb9c69d078ac4679b86ad6
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-11-28 10:24:04 -0600

    Time: 11 ms (97.73%), Space: 42.8 MB (95.02%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,16 +1,15 @@
 class Solution {
-    
-  public int longestCommonSubsequence(String text1, String text2) {    
-      if(text2.length() < text1.length()) return longestCommonSubsequence(text2, text1);
-      int[] previous = new int[text1.length() + 1];
-      for(int col = text2.length() - 1; col >= 0; col--){
-          int[] current = new int[text1.length() + 1];
-          for(int row = text1.length() - 1; row >= 0; row--){
-              if(text1.charAt(row) == text2.charAt(col)) current[row] = previous[row+1] + 1;
-              else current[row] = Math.max(previous[row], current[row + 1]);
-          }
-          previous = current;
-      }
-      return previous[0];
-  }    
+    public int longestCommonSubsequence(String text1, String text2) {
+        if(text2.length() < text1.length()) return longestCommonSubsequence(text2, text1);
+        int[] previous = new int[text1.length() + 1];
+        for(int col = text2.length() - 1; col >= 0; col--){
+            int[] current = new int[text1.length() + 1];
+            for(int row = text1.length() - 1; row >= 0; row--){
+                if(text2.charAt(col) == text1.charAt(row)) current[row] = 1 + previous[row+1];
+                else current[row] = Math.max(current[row+1], previous[row]);
+            }
+            previous = current;
+        }
+        return previous[0];
+    }
 }
