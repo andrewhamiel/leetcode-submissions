0a51769f9a1525c4980afb5ccbd4bedbd2509e9d
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-11-29 07:04:23 -0600

    Time: 65 ms (40.49%), Space: 113 MB (23.19%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,33 +1,41 @@
 class RandomizedSet {
-    Map<Integer, Integer> map;
-    List<Integer> list;
-    Random rand;
-    
-    public RandomizedSet(){
+    private Map<Integer, Integer> map;
+    private List<Integer> list;
+    private Random rand;
+
+    public RandomizedSet() {
         map = new HashMap();
         list = new ArrayList();
         rand = new Random();
     }
     
-    public boolean insert(int val){
+    public boolean insert(int val) {
         if(map.containsKey(val)) return false;
         map.put(val, list.size());
-        list.add(list.size(), val);
+        list.add(val);
         return true;
     }
     
-    public boolean remove(int val){
+    public boolean remove(int val) {
         if(!map.containsKey(val)) return false;
+        int lastElement = list.get(list.size() - 1);
         int ind = map.get(val);
-        Integer last = list.get(list.size() - 1);
-        list.set(ind, last);
-        map.put(last, ind);
+        map.put(lastElement, ind);
+        list.set(ind, lastElement);
         list.remove(list.size() - 1);
         map.remove(val);
         return true;
     }
     
-    public int getRandom(){
+    public int getRandom() {
         return list.get(rand.nextInt(list.size()));
     }
 }
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
