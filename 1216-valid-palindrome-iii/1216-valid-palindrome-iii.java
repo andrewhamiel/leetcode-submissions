21aaaf40f5d776aad4643002763fbb921b1693cd
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2022-11-29 07:03:24 -0600

    Time: 13 ms (95.31%), Space: 44.4 MB (82.50%) - LeetHub

---
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.java
@@ -0,0 +1,12 @@
+class Solution {
+public boolean isValidPalindrome(String s, int k) {
+        int dp[][] = new int[s.length()][s.length()];
+
+        for (int i = s.length() - 2; i >= 0; i--)
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) dp[i][j] = dp[i + 1][j - 1];
+                else dp[i][j] = 1 + Math.min(dp[i + 1][j], dp[i][j - 1]);
+            }
+        return dp[0][s.length() - 1] <= k;
+    }
+};
