04b19d59ef22a717cec7aa115004b9e01c654931
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-05-20 15:23:19 -0500

    Time: 1 ms (90.42%) | Memory: 41.7 MB (5.50%) - LeetSync

---
+++ evaluate-division/evaluate-division.java
@@ -0,0 +1,66 @@
+class Solution {
+
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        HashMap<String, Pair<String, Double>> gidWeight = new HashMap<>();
+
+        // Step 1). build the union groups
+        for (int i = 0; i < equations.size(); i++) {
+            List<String> equation = equations.get(i);
+            String dividend = equation.get(0), divisor = equation.get(1);
+            double quotient = values[i];
+
+            union(gidWeight, dividend, divisor, quotient);
+        }
+
+        // Step 2). run the evaluation, with "lazy" updates in find() function
+        double[] results = new double[queries.size()];
+        for (int i = 0; i < queries.size(); i++) {
+            List<String> query = queries.get(i);
+            String dividend = query.get(0), divisor = query.get(1);
+            // case 1). at least one variable did not appear before
+            if (!gidWeight.containsKey(dividend) || !gidWeight.containsKey(divisor)) results[i] = -1.0;
+            else {
+                Pair<String, Double> dividendEntry = find(gidWeight, dividend);
+                Pair<String, Double> divisorEntry = find(gidWeight, divisor);
+
+                String dividendGid = dividendEntry.getKey();
+                String divisorGid = divisorEntry.getKey();
+                Double dividendWeight = dividendEntry.getValue();
+                Double divisorWeight = divisorEntry.getValue();
+                // case 2). the variables do not belong to the same chain/group
+                if (!dividendGid.equals(divisorGid)) results[i] = -1.0;
+                // case 3). there is a chain/path between the variables
+                else results[i] = dividendWeight / divisorWeight;
+            }
+        }
+
+        return results;
+    }
+
+    private Pair<String, Double> find(HashMap<String, Pair<String, Double>> gidWeight, String nodeId) {
+        if (!gidWeight.containsKey(nodeId)) gidWeight.put(nodeId, new Pair<String, Double>(nodeId, 1.0));
+
+        Pair<String, Double> entry = gidWeight.get(nodeId);
+        // found inconsistency, trigger chain update
+        if (!entry.getKey().equals(nodeId)) {
+            Pair<String, Double> newEntry = find(gidWeight, entry.getKey());
+            gidWeight.put(nodeId, new Pair(newEntry.getKey(), entry.getValue() * newEntry.getValue()));
+        }
+
+        return gidWeight.get(nodeId);
+    }
+
+    private void union(HashMap<String, Pair<String, Double>> gidWeight, String dividend, String divisor, Double value) {
+        Pair<String, Double> dividendEntry = find(gidWeight, dividend);
+        Pair<String, Double> divisorEntry = find(gidWeight, divisor);
+
+        String dividendGid = dividendEntry.getKey();
+        String divisorGid = divisorEntry.getKey();
+        Double dividendWeight = dividendEntry.getValue();
+        Double divisorWeight = divisorEntry.getValue();
+
+        // merge the two groups together,
+        // by attaching the dividend group to the one of divisor
+        if (!dividendGid.equals(divisorGid)) gidWeight.put(dividendGid, new Pair(divisorGid, divisorWeight * value / dividendWeight));
+    }
+}
