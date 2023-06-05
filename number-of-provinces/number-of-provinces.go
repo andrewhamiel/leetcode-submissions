13dcb0a9a696231e5cfb29dd1ddad3df1ab26c0a
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-06-04 19:25:44 -0500

    Time: 18 ms (87.65%) | Memory: 6.9 MB (65.43%) - LeetSync

--- number-of-provinces/number-of-provinces.go
+++ number-of-provinces/number-of-provinces.go
@@ -1,21 +1,57 @@
-func dfs(curr int, visited []bool, isConnected [][]int) {
-    visited[curr] = true
-    for next := 0; next < len(isConnected); next++ {
-        if isConnected[curr][next] == 0 {continue} 
-        if !visited[next] {
-            dfs(next, visited, isConnected)
-        }
-    } 
+type UnionFind struct {
+    group []int
+    rank []int
+    count int
+}
+
+func NewUnionFind(n int) *UnionFind{
+    uf := new(UnionFind)
+    uf.group = make([]int, n)
+    uf.rank = make([]int, n)
+    for i := 0; i < n; i++ {
+        uf.group[i] = i
+    }
+    uf.count = n
+    return uf
+}
+
+func (uf *UnionFind) find(node int) int {
+    if uf.group[node] != node {
+        uf.group[node] = uf.find(uf.group[node])
+    }
+    return uf.group[node]
+}
+
+func (uf *UnionFind) union(node1, node2 int) bool {
+    group1 := uf.find(node1)
+    group2 := uf.find(node2)
+    if group1 == group2 {
+        return false
+    }
+
+    if uf.rank[group1] > uf.rank[group2] {
+        uf.group[group2] = uf.group[group1]
+    }else if uf.rank[group2] > uf.rank[group1] {
+        uf.group[group1] = uf.group[group2]
+    }else{
+        uf.group[group2] = uf.group[group1]
+        uf.count--
+    }
+    return true
+}
+
+func (uf *UnionFind) getCount() int {
+    return uf.count
 }
 
 func findCircleNum(isConnected [][]int) int {
-    var count int = 0
-    visited := make([]bool, len(isConnected))
+    uf := NewUnionFind(len(isConnected))
     for i := 0; i < len(isConnected); i++ {
-        if !visited[i] {
-            count++
-            dfs(i, visited, isConnected)
+        for j := 0; j < len(isConnected[0]); j++ {
+            if isConnected[i][j] == 1 {
+                uf.union(i, j)
+            }
         }
     }
-    return count
+    return uf.getCount()
 }
