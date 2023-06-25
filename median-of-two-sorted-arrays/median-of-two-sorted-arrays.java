4ebdd1c253d8ff2c438e09bffe254efef65e7bc7
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-06-25 14:59:16 -0500

    Time: 2 ms (100.00%) | Memory: 44 MB (64.96%) - LeetSync

--- median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,31 +1,28 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
+        if(nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
 
-        int m = nums1.length, n = nums2.length;
-        int left = 0, right = m;
-        
-        while (left <= right) {
-            int partitionA = (left + right) / 2;
-            int partitionB = (m + n + 1) / 2 - partitionA;
+        int left = 0, right = nums1.length;
+        while(left <= right){
+            int partitionA = left + (right - left)/2;
+            int partitionB = (nums1.length + nums2.length + 1)/2 - partitionA;        
+
+            int maxLeftA = partitionA == 0 ? Integer.MIN_VALUE : nums1[partitionA - 1];
+            int minRightA = partitionA == nums1.length ? Integer.MAX_VALUE : nums1[partitionA];
+            int maxLeftB = partitionB == 0 ? Integer.MIN_VALUE : nums2[partitionB - 1];
+            int minRightB = partitionB == nums2.length ? Integer.MAX_VALUE :nums2[partitionB];
             
-            int maxLeftA = (partitionA == 0) ? Integer.MIN_VALUE : nums1[partitionA - 1];
-            int minRightA = (partitionA == m) ? Integer.MAX_VALUE : nums1[partitionA];
-            int maxLeftB = (partitionB == 0) ? Integer.MIN_VALUE : nums2[partitionB - 1];
-            int minRightB = (partitionB == n) ? Integer.MAX_VALUE : nums2[partitionB];
-            
-            if (maxLeftA <= minRightB && maxLeftB <= minRightA) {
-                if ((m + n) % 2 == 0) {
-                    return (Math.max(maxLeftA, maxLeftB) + Math.min(minRightA, minRightB)) / 2.0;
-                } else {
-                    return Math.max(maxLeftA, maxLeftB);
-                }
-            } else if (maxLeftA > minRightB) {
-                right = partitionA - 1;
-            } else {
-                left = partitionA + 1;
+            //Case 1: maxLeftA <= minRightB: Correct Partition
+            if(maxLeftA <= minRightB && maxLeftB <= minRightA){
+                if((nums1.length + nums2.length) % 2 == 0) return (double)(Math.max(maxLeftA, maxLeftB) + Math.min(minRightA, minRightB)) / 2;
+                else return (double) Math.max(maxLeftA, maxLeftB);
             }
+            //Case 2: maxLeftA > minRightB: minLeftA too large for partition A, need smaller partition
+            else if(maxLeftA > minRightB) right = partitionA - 1;
+            //Case 3: maxLeftB < minRightA: maxLeftA too small to be in larger half, need larger partition
+            else left = partitionA + 1;
         }
         return 0.0;
+        
     }
 }
