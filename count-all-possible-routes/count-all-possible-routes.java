abbac9c474f07185364bd4f85046b90b5331d764
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-06-25 12:27:58 -0500

    Time: 83 ms (69.33%) | Memory: 43 MB (57.33%) - LeetSync

---
+++ count-all-possible-routes/count-all-possible-routes.java
@@ -0,0 +1,27 @@
+class Solution {
+    private int solve(int[] locations, int currCity, int finish, int remainingFuel, int memo[][]) {
+        if (remainingFuel < 0) return 0;
+
+        if (memo[currCity][remainingFuel] != -1) return memo[currCity][remainingFuel];
+
+        int ans = currCity == finish ? 1 : 0;
+        for (int nextCity = 0; nextCity < locations.length; nextCity++) {
+            if (nextCity != currCity) {
+                ans = (ans + solve(locations, nextCity, finish,
+                remainingFuel - Math.abs(locations[currCity] - locations[nextCity]),
+                                   memo)) % 1000000007;
+            }
+        }
+
+        return memo[currCity][remainingFuel] = ans;
+    }
+
+    public int countRoutes(int[] locations, int start, int finish, int fuel) {
+        int memo[][] = new int[locations.length][fuel + 1];
+        for (int i = 0; i < locations.length; i++) {
+            Arrays.fill(memo[i], -1);
+        }
+
+        return solve(locations, start, finish, fuel, memo);
+    }
+}
