6c85870c6b3f245e8e2e34323b46c5927af17414
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-06-30 16:45:02 -0500

    Time: 7 ms (81.08%) | Memory: 43.4 MB (45.34%) - LeetSync

--- longest-increasing-subsequence/longest-increasing-subsequence.java
+++ longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -4,12 +4,19 @@ class Solution {
         subArr.add(nums[0]);
         for(int i = 1; i < nums.length; i++){
             if(nums[i] > subArr.get(subArr.size() - 1)) subArr.add(nums[i]);
-            else{
-                int j = 0;
-                while(subArr.get(j) < nums[i]) j++;
-                subArr.set(j, nums[i]);
-            }
+            else subArr.set(binarySearch(subArr, nums[i]), nums[i]);
         }
         return subArr.size();
+    }
+
+    private int binarySearch(List<Integer> subArr, int target){
+        int left = 0, right = subArr.size() - 1;
+        while(left < right){
+            int mid = left + (right - left)/2;
+            if(subArr.get(mid) == target) return mid;
+            else if(subArr.get(mid) < target) left = mid + 1;
+            else right = mid;
+        }
+        return left;
     }
 }
