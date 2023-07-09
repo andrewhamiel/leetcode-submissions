1e88e9bf7581eb069a3b388599713544c4973b1c
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-07-09 14:11:43 -0500

    Time: 10 ms (51.20%) | Memory: 44 MB (8.71%) - LeetSync

--- longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.java
+++ longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.java
@@ -1,41 +1,21 @@
 class Solution {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
-        if (k >= s.length()) return s.length();
         
-        int left = k, right = s.length();
-        while (left < right) {
-            int mid = (left + right + 1) / 2;
-            
-            if (isValid(s, mid, k)) left = mid;
-            else right = mid - 1;
-        }
-        
-        return left;
-    }
-    
-    private boolean isValid(String s, int size, int k) {
         Map<Character, Integer> counter = new HashMap<>();
-        
-        for (int i = 0; i < size; i++) {
-            char c = s.charAt(i);
-            counter.put(c, counter.getOrDefault(c, 0) + 1);
-        }
-        
-        if (counter.size() <= k) return true;
-        
-        for (int i = size; i < s.length(); i++) {
-            //move right side of window
-            char c1 = s.charAt(i);
-            counter.put(c1, counter.getOrDefault(c1, 0) + 1);
+        int left = 0, maxSize = 0;
+        for (int right = 0; right < s.length(); right++) {
+            counter.put(s.charAt(right), counter.getOrDefault(s.charAt(right), 0) + 1);
             
-            //move left side of window
-            char c2 = s.charAt(i - size);
-            counter.put(c2, counter.getOrDefault(c2, 0) - 1);
-            if (counter.get(c2) == 0) counter.remove(c2);
-
-            if (counter.size() <= k) return true;
+            while (counter.size() > k) {
+                counter.put(s.charAt(left), counter.get(s.charAt(left)) - 1);
+                if (counter.get(s.charAt(left)) == 0) counter.remove(s.charAt(left));
+                
+                left++;
+            }
+            
+            maxSize = Math.max(maxSize, right - left + 1);
         }
-        
-        return false;
+                    
+        return maxSize;  
     }
 }
