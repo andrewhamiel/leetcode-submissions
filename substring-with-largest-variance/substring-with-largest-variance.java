2991bfb382c2dfa0071f9b63e016cafc88c2e179
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-07-09 13:35:28 -0500

    Time: 269 ms (31.06%) | Memory: 41.3 MB (77.64%) - LeetSync

---
+++ substring-with-largest-variance/substring-with-largest-variance.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int largestVariance(String s) {
+        int[] freq = new int[26];
+        for(Character ch : s.toCharArray()) freq[ch - 'a']++;
+        
+        int max = 0;
+        for(int charA = 0; charA < 26; charA++){
+            for(int charB = 0; charB < 26; charB++){
+                int remainA = freq[charA], remainB = freq[charB];
+                if(charA == charB || remainA == 0 || remainB == 0) continue;
+                
+                int aFreq = 0, bFreq = 0;
+                for(int k = 0; k < s.length(); k++){
+                    int currChar = s.charAt(k) - 'a';
+                    if(currChar == charB) bFreq++;
+                    if(currChar == charA){
+                        remainA--;
+                        aFreq++;
+                    }
+                    
+                    if(aFreq > 0) max = Math.max(max, bFreq - aFreq);
+                    if(bFreq < aFreq && remainA >= 1){
+                        aFreq = 0;
+                        bFreq = 0;
+                    }
+                }
+            }
+        }
+        return max;
+    }
+}
