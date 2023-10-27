662ed9b67d916f9262d0080b2390a70ee66116ba
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-10-26 21:10:59 -0500

    Time: 5 ms (95.56%) | Memory: 44.1 MB (86.37%) - LeetSync

---
+++ 332-reconstruct-itinerary/reconstruct-itinerary.java
@@ -0,0 +1,40 @@
+class Solution {
+
+  public List<String> findItinerary(List<List<String>> tickets) {
+
+        Map<String, PriorityQueue<String>> map = new HashMap<>();
+
+        for(int i=0; i<tickets.size(); i++){
+
+            map.putIfAbsent(tickets.get(i).get(0), new PriorityQueue<String>());
+
+            map.get(tickets.get(i).get(0)).add(tickets.get(i).get(1));
+
+        } 
+
+        Deque<String> deq = new ArrayDeque<>();
+
+        dfs("JFK", map, deq);
+
+        return new ArrayList<>(deq);
+    }
+
+    private void dfs(String s, Map<String, PriorityQueue<String>> map, Deque<String> list){
+
+        PriorityQueue<String> pq = map.get(s);
+
+        while(pq!=null && !pq.isEmpty()){
+
+            dfs(pq.poll(), map, list);
+
+        }
+
+        list.addFirst(s);
+
+    }
+
+    
+
+}
+
+        
