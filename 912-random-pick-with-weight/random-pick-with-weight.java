6d52857f76f21bdaea85a3a7fd1e92e6e21f3a1f
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-10-30 15:29:00 -0500

    Time: 23 ms (84.44%) | Memory: 47.2 MB (78.58%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.java
+++ 912-random-pick-with-weight/random-pick-with-weight.java
@@ -1,23 +1,22 @@
 class Solution {
     int[] w;
-
+    int sum = 0;
     public Solution(int[] w) {
+        for(int i = 1; i < w.length; i++) w[i] += w[i-1];
         this.w = w;
-        for(int i = 1; i < w.length; i++){
-            w[i]+= w[i-1];
-        }
+        sum = w[w.length - 1];
     }
     
-    public int pickIndex() {
-        int right = w.length-1;
-        int left = 0;
-        double target = w[w.length - 1] * Math.random();
+    public int pickIndex() {   
+        double target = sum * Math.random();
+        
+        int left = 0, right = w.length;
         while(left <= right){
-            int mid = left + (right - left/2);
+            int mid = left + (right - left)/2;
             if(w[mid] < target) left = mid + 1;
             else right = mid - 1;
         }
-        return left;
+        return left;     
     }
 }
 
