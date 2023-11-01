7b49a6c8fbded206ab79be42bc1e7a2888d024ee
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-01 18:00:55 -0500

    Time: 143 ms (20.22%) | Memory: 41.2 MB (80.85%) - LeetSync

--- 301-remove-invalid-parentheses/remove-invalid-parentheses.java
+++ 301-remove-invalid-parentheses/remove-invalid-parentheses.java
@@ -1,51 +1,44 @@
 class Solution {
     Set<String> validStrings = new HashSet<>();
     int minRemoved = Integer.MAX_VALUE;
-    
+
     public List<String> removeInvalidParentheses(String s) {
         helper(s, new StringBuilder(), 0, 0, 0, 0);
         return new ArrayList<>(validStrings);
     }
 
-    private void helper(String s, StringBuilder expression, int ind, int removed, int leftCount, int rightCount){
+    private void helper(String s, StringBuilder expr, int ind, int removed, int leftCount, int rightCount){
         if(ind == s.length()){
             if(removed <= minRemoved && leftCount == rightCount){
                 if(removed < minRemoved){
-                    minRemoved = removed;
                     validStrings = new HashSet<>();
+                    minRemoved = removed;
                 }
-                validStrings.add(expression.toString());
+                validStrings.add(expr.toString());
             }
         }else{
-            //Case 1: '('
             char c = s.charAt(ind);
-            int currLength = expression.length();
+            int currLength = expr.length();
+            //Case 1: '(' - try adding and removing
             if(c == '('){
-                //Try both adding and removing
-                expression.append(c);
-                helper(s, expression, ind+1, removed, leftCount+1, rightCount); //adding
-                expression.deleteCharAt(currLength);
-                helper(s, expression, ind+1, removed+1, leftCount, rightCount); //removing
-            }
-            //Case 2: ')'
-            else if(c == ')'){
-                //if not enough leftCount, must remove
-                if(leftCount <= rightCount){
-                    helper(s, expression, ind+1, removed+1, leftCount, rightCount);
-                }else{
-                    //try adding and removing
-                    expression.append(c);
-                    helper(s, expression, ind+1, removed, leftCount, rightCount+1); //adding
-                    expression.deleteCharAt(currLength);
-                    helper(s, expression, ind+1, removed+1, leftCount, rightCount); //removing
+                expr.append(c);
+                helper(s, expr, ind + 1, removed, leftCount + 1, rightCount); //add
+                expr.deleteCharAt(currLength);
+                helper(s, expr, ind + 1, removed + 1, leftCount, rightCount); //remove
+            }else if(c == ')'){ //Case 2: ')' - check to see if enough left count. add and remove if enough, just remove if not
+                if(leftCount > rightCount){
+                    expr.append(c);
+                    helper(s, expr, ind + 1, removed, leftCount, rightCount + 1); //add
+                    expr.deleteCharAt(currLength);
                 }
+                helper(s, expr, ind + 1, removed + 1, leftCount, rightCount); //remove
+            }else{ //Case 3: Everything else - add
+                expr.append(c);
+                helper(s, expr, ind + 1, removed, leftCount, rightCount);
+                expr.deleteCharAt(currLength);
             }
-            //Case 3: Anything else: add
-            else{
-                expression.append(c);
-                helper(s, expression, ind+1, removed, leftCount, rightCount);
-                expression.deleteCharAt(currLength);
-            }
+            
+            
         }
     }
 }
