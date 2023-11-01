0d69513eaba0d375a15547d9d3ac9e8669910621
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-01 16:04:48 -0500

    Time: 0 ms (100.00%) | Memory: 43.1 MB (20.01%) - LeetSync

--- 850-insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.java
+++ 850-insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.java
@@ -19,21 +19,20 @@ class Node {
 
 class Solution {
     public Node insert(Node head, int insertVal) {
-        //Case 0: Empty List
+        //Case 0: head is null
         if(head == null){
             head = new Node(insertVal);
             head.next = head;
             return head;
         }
-
         Node curr = head, nextNode = head.next, original = head;
         boolean isInserted = false;
         while(!isInserted){
             //3 Cases
-            if((curr.val != nextNode.val) && 
-            (curr.val <= insertVal && nextNode.val >= insertVal && curr.val <= nextNode.val //1. In Between
-            || curr.val <= insertVal && nextNode.val <= insertVal && curr.val >= nextNode.val //2. Max at end
-            || curr.val >= insertVal && nextNode.val >= insertVal && curr.val >= nextNode.val)){ // Min at beginning 
+            if((curr.val != nextNode.val) &&
+            (curr.val <= insertVal && nextNode.val >= insertVal && curr.val <= nextNode.val
+            || curr.val <= insertVal && nextNode.val <= insertVal && curr.val >= nextNode.val
+            || curr.val >= insertVal && nextNode.val >= insertVal && curr.val >= nextNode.val)){
                 Node ans = new Node(insertVal);
                 ans.next = curr.next;
                 curr.next = ans;
@@ -41,9 +40,9 @@ class Solution {
             }
             curr = curr.next;
             nextNode = nextNode.next;
-            if(curr == original) isInserted = true; 
+            if(curr == original) isInserted = true;
         }
-        //If all elements being inserted are the same, can add after first element in beginning
+        //Case 4: All duplicate values, can insert after first node
         Node ans = new Node(insertVal);
         ans.next = original.next;
         original.next = ans;
