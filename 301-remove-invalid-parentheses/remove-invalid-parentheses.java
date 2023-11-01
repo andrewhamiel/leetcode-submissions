34a47b21c96f75304923a6929ac9c1b367219fe9
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-01 17:55:30 -0500

    Time: 147 ms (17.85%) | Memory: 41.8 MB (56.24%) - LeetSync

--- 301-remove-invalid-parentheses/remove-invalid-parentheses.java
+++ 301-remove-invalid-parentheses/remove-invalid-parentheses.java
@@ -1,69 +1,51 @@
 class Solution {
-  private Set<String> validExpressions = new HashSet<>();
-  private int minimumRemoved;
-
-  public List<String> removeInvalidParentheses(String s) {
-
-    this.reset();
-    this.recurse(s, 0, 0, 0, new StringBuilder(), 0);
-    return new ArrayList(this.validExpressions);
-  }
-
-  private void reset() {
-    this.validExpressions.clear();
-    this.minimumRemoved = Integer.MAX_VALUE;
-  }
-
-  private void recurse(String s,int index,int leftCount,int rightCount,StringBuilder expression,
-      int removedCount) {
-
-    // If we have reached the end of string.
-    if (index == s.length()) {
-
-      // If the current expression is valid.
-      if (leftCount == rightCount) {
-
-        // If the current count of removed parentheses is <= the current minimum count
-        if (removedCount <= this.minimumRemoved) {
-
-          // Convert StringBuilder to a String. This is an expensive operation.
-          // So we only perform this when needed.
-          String possibleAnswer = expression.toString();
-
-          // If the current count beats the overall minimum we have till now
-          if (removedCount < this.minimumRemoved) {
-            this.validExpressions.clear();
-            this.minimumRemoved = removedCount;
-          }
-          this.validExpressions.add(possibleAnswer);
-        }
-      }
-    } else {
-      char currentCharacter = s.charAt(index);
-      int length = expression.length();
-
-      // If the current character is neither an opening bracket nor a closing one,
-      // simply recurse further by adding it to the expression StringBuilder
-      if (currentCharacter != '(' && currentCharacter != ')') {
-        expression.append(currentCharacter);
-        this.recurse(s, index + 1, leftCount, rightCount, expression, removedCount);
-        expression.deleteCharAt(length);
-      } else {
-        // Recursion where we delete the current character and move forward
-        this.recurse(s, index + 1, leftCount, rightCount, expression, removedCount + 1);
-        expression.append(currentCharacter);
-
-        // If it's an opening parenthesis, consider it and recurse
-        if (currentCharacter == '(') {
-          this.recurse(s, index + 1, leftCount + 1, rightCount, expression, removedCount);
-        } else if (rightCount < leftCount) {
-          // For a closing parenthesis, only recurse if right < left
-          this.recurse(s, index + 1, leftCount, rightCount + 1, expression, removedCount);
-        }
-
-        // Undoing the append operation for other recursions.
-        expression.deleteCharAt(length);
-      }
+    Set<String> validStrings = new HashSet<>();
+    int minRemoved = Integer.MAX_VALUE;
+    
+    public List<String> removeInvalidParentheses(String s) {
+        helper(s, new StringBuilder(), 0, 0, 0, 0);
+        return new ArrayList<>(validStrings);
     }
-  }
+
+    private void helper(String s, StringBuilder expression, int ind, int removed, int leftCount, int rightCount){
+        if(ind == s.length()){
+            if(removed <= minRemoved && leftCount == rightCount){
+                if(removed < minRemoved){
+                    minRemoved = removed;
+                    validStrings = new HashSet<>();
+                }
+                validStrings.add(expression.toString());
+            }
+        }else{
+            //Case 1: '('
+            char c = s.charAt(ind);
+            int currLength = expression.length();
+            if(c == '('){
+                //Try both adding and removing
+                expression.append(c);
+                helper(s, expression, ind+1, removed, leftCount+1, rightCount); //adding
+                expression.deleteCharAt(currLength);
+                helper(s, expression, ind+1, removed+1, leftCount, rightCount); //removing
+            }
+            //Case 2: ')'
+            else if(c == ')'){
+                //if not enough leftCount, must remove
+                if(leftCount <= rightCount){
+                    helper(s, expression, ind+1, removed+1, leftCount, rightCount);
+                }else{
+                    //try adding and removing
+                    expression.append(c);
+                    helper(s, expression, ind+1, removed, leftCount, rightCount+1); //adding
+                    expression.deleteCharAt(currLength);
+                    helper(s, expression, ind+1, removed+1, leftCount, rightCount); //removing
+                }
+            }
+            //Case 3: Anything else: add
+            else{
+                expression.append(c);
+                helper(s, expression, ind+1, removed, leftCount, rightCount);
+                expression.deleteCharAt(currLength);
+            }
+        }
+    }
 }
