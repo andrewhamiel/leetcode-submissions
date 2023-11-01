a835fdf3262334fd9a9d36198e8534793a20adfb
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-01 15:31:31 -0500

    Time: 0 ms (100.00%) | Memory: 41.3 MB (17.88%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,6 +1,6 @@
 class Solution {
     public double myPow(double x, int n) {
-        return binaryExp(x, (long) n);        
+        return binaryExp(x, (long) n);
     }
 
     private double binaryExp(double x, long n){
@@ -10,13 +10,14 @@ class Solution {
             n*= -1;
             x = 1/x;
         }
-        double result = 1;
+
+        double result = 1.0;
         while(n != 0){
             if(n % 2 != 0){
                 result*= x;
                 n--;
             }
-            x*=x;
+            x*= x;
             n/=2;
         }
         return result;
