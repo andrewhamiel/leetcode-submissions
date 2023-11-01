4cd178802ab47408951947b72ebbbdd0c97aa53a
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-01 15:30:11 -0500

    Time: 0 ms (100.00%) | Memory: 41.1 MB (23.44%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,20 +1,24 @@
 class Solution {
-        
     public double myPow(double x, int n) {
-        long N = n;
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
-        }
+        return binaryExp(x, (long) n);        
+    }
 
-        return fastPow(x, N);
-    }
-    
-    private double fastPow(double x, long n) {
+    private double binaryExp(double x, long n){
         if(n == 0) return 1.0;
-        
-        double half = fastPow(x, n/2);
-        if(n % 2 == 0) return half * half;
-        else return half * half * x;
+
+        if(n < 0){
+            n*= -1;
+            x = 1/x;
+        }
+        double result = 1;
+        while(n != 0){
+            if(n % 2 != 0){
+                result*= x;
+                n--;
+            }
+            x*=x;
+            n/=2;
+        }
+        return result;
     }
-};
+}
