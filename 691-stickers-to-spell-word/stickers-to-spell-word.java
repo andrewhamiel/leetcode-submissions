3a87ed2a4c2cefb24a0063521155b1b9f8798aa8
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-01 10:24:05 -0500

    Time: 125 ms (43.70%) | Memory: 44.4 MB (38.15%) - LeetSync

--- 691-stickers-to-spell-word/stickers-to-spell-word.java
+++ 691-stickers-to-spell-word/stickers-to-spell-word.java
@@ -1,45 +1,42 @@
 class Solution {
-    List<int[]> stickerFreqs = new ArrayList<>();
+    List<int[]> freqs = new ArrayList<>();
     Map<String, Integer> memo = new HashMap<>();
+
     public int minStickers(String[] stickers, String target) {
-        //Add frequencies for each sticker first
         for(String sticker : stickers){
             int[] freq = new int[26];
-            for(int i = 0; i < sticker.length(); i++){
-                freq[(int)(sticker.charAt(i) - 'a')]++;
-            }
-            stickerFreqs.add(freq);
+            for(Character c : sticker.toCharArray()) freq[c - 'a']++;
+            freqs.add(freq);
         }
-
-        int result = dp(target, null);
-        return result == Integer.MAX_VALUE ? -1 : result;
+        int ans = helper(target, null);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 
-    private int dp(String remainingTarget, int[] currentSticker){
-        if(remainingTarget.isEmpty()) return 0;
-        if(memo.containsKey(remainingTarget)) return memo.get(remainingTarget);
-
-        //If we have a current sticker, try to extract as much as we can
-        StringBuilder remain = new StringBuilder();
-        int stickersUsed = currentSticker != null ? 1 : 0;
-        if(currentSticker != null){
-            for(Character c : remainingTarget.toCharArray()){
-                if(currentSticker[c - 'a'] > 0) currentSticker[c- 'a']--;
-                else remain.append(c);
+    private int helper(String target, int[] currSticker){
+        if(target.length() == 0) return 0;
+        if(memo.containsKey(target)) return memo.get(target);
+        int minStickers = 0;
+        StringBuilder remainingTarget = new StringBuilder();
+        if(currSticker != null){
+            for(int i = 0; i < target.length(); i++){
+                if(currSticker[target.charAt(i) - 'a'] > 0){
+                    currSticker[target.charAt(i) - 'a']--;
+                    minStickers = 1;
+                }else remainingTarget.append(target.charAt(i));
             }
-        }else remain.append(remainingTarget);
-        
-        if(!remain.isEmpty()){
-            int used = Integer.MAX_VALUE;
-            for(int[] freq : stickerFreqs){
-                if(freq[remain.charAt(0) - 'a'] > 0){
-                    used = Math.min(used, dp(remain.toString(), freq.clone()));
+        }else remainingTarget = new StringBuilder(target);
+
+        if(!remainingTarget.isEmpty()){
+            int minAdditional = Integer.MAX_VALUE;
+            for(int[] freq : freqs){
+                if(freq[remainingTarget.charAt(0) - 'a'] > 0){
+                    minAdditional = Math.min(minAdditional, helper(remainingTarget.toString(), freq.clone()));
                 }
             }
-            memo.put(remain.toString(), used);
-            stickersUsed = used == Integer.MAX_VALUE ? used : stickersUsed + used;
+            memo.put(remainingTarget.toString(), minAdditional);
+            minStickers = minAdditional == Integer.MAX_VALUE ? Integer.MAX_VALUE : minStickers + minAdditional;
+
         }
-        
-        return stickersUsed;
+        return minStickers;
     }
 }
