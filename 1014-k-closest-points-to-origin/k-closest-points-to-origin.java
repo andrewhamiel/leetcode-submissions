148f5859dade95cc81324adb237310017df37d55
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-02 18:52:38 -0500

    Time: 6 ms (98.11%) | Memory: 49.6 MB (56.28%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        quickselect(0, points.length - 1, points, k);
+        int[][] ans = new int[k][2];
+        for(int i = 0; i < ans.length; i++) ans[i] = points[i];
+        return ans;
+    }
+
+    private void quickselect(int left, int right, int[][] points, int k){
+        if(left >= right) return;
+
+        int pivotIndex = new Random().nextInt(right - left + 1) + left;
+        pivotIndex = partition(left, right, points, pivotIndex);
+        if(pivotIndex == k) return;
+        else if (pivotIndex < k) quickselect(pivotIndex + 1, right, points, k);
+        else quickselect(left, pivotIndex - 1, points, k);
+    }
+
+    private int partition(int left, int right, int[][] points, int pivotIndex){
+        int pivotDistance = getDistance(points[pivotIndex]);
+        swap(pivotIndex, right, points);
+        pivotIndex = left;
+        for(int i = left; i <= right; i++){
+            if(getDistance(points[i]) <= pivotDistance) swap(i, pivotIndex++, points);
+        }
+        return pivotIndex - 1;
+    }
+
+    private void swap(int i, int j, int[][] nums){
+        int[] tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+    private int getDistance(int[] point){
+        return (point[0] * point[0]) + (point[1] * point[1]);
+    }
+}
