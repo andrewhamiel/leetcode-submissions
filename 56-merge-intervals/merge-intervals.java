f4a24e67bb4e2ada072b4e61e0b3c04509974452
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-01 19:10:04 -0500

    Time: 10 ms (29.43%) | Memory: 46.3 MB (8.12%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.java
@@ -0,0 +1,69 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+		Itnode root=new Itnode(intervals[0][0], intervals[0][1]);
+		
+		for(int i=1;i<intervals.length;i++) {
+			add(root, intervals[i]);
+		}
+		
+		List<int[]> list=merge(root);
+		int[][] ans=new int[list.size()][2];
+		for(int i=0;i<ans.length;i++) {
+			ans[i]=list.get(i);
+		}
+		return ans;
+	}
+
+    class Itnode{
+		int low,high,max;
+		Itnode left,right;
+		Itnode(int l,int h){
+			this.low=l;this.high=h;this.max=h;
+			this.left=this.right=null;
+		}
+	}
+	
+	private List<int[]> merge(Itnode root){
+		List<int[]> res=new ArrayList<>();
+		if(root==null) return res;
+		
+		List<int[]> left=merge(root.left);
+		List<int[]> right=merge(root.right);
+		
+		boolean inserted=false;
+		for(int[] i:left) {
+			if(i[1]>=root.low) {
+				inserted=true;
+				i[0]=Math.min(i[0], root.low);
+				i[1]=Math.max(i[1], root.high);
+				root=new Itnode(i[0], i[1]);
+			}
+			res.add(i);
+		}
+		
+		if(!inserted) res.add(new int[] {root.low,root.high});
+		
+		for(int[] i:right) {
+			if(i[0]<=root.high) {
+				inserted=true;
+				i[0]=Math.min(i[0], root.low);
+				i[1]=Math.max(i[1], root.high);
+				res.remove(res.size()-1);
+			}
+			res.add(i);
+		}
+		
+		return res;
+		
+	}
+	
+	private Itnode add(Itnode root,int[] i) {
+		if(root==null) return new Itnode(i[0], i[1]);
+		
+		if(i[0]<root.low)
+			root.left=add(root.left, i);
+		else
+			root.right=add(root.right,i);
+		return root;
+	}
+}
