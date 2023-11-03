bad41571444d3ff7ec157f54a4356174033f5b87
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-03 10:10:53 -0500

    Time: 6 ms (98.11%) | Memory: 49 MB (63.35%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,31 +1,32 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        quickselect(points, 0, points.length - 1, k);
+        quickselect(0, points.length - 1, points, k);
         int[][] ans = new int[k][2];
         for(int i = 0; i < ans.length; i++) ans[i] = points[i];
         return ans;
     }
 
-    private void quickselect(int[][] points, int left, int right, int k){
+    private void quickselect(int left, int right, int[][] points, int k){
         if(left >= right) return;
+
         int pivotIndex = new Random().nextInt(right - left + 1) + left;
-        pivotIndex = partition(points, left, right, pivotIndex);
+        pivotIndex = partition(left, right, points, pivotIndex);
         if(pivotIndex == k) return;
-        else if(pivotIndex < k) quickselect(points, pivotIndex + 1, right, k);
-        else quickselect(points, left, pivotIndex - 1, k);
+        else if(pivotIndex < k) quickselect(pivotIndex + 1, right, points, k);
+        else quickselect(left, pivotIndex - 1, points, k);
     }
 
-    private int partition(int[][] points, int left, int right, int pivotIndex){
-        int pivotDist = getDistance(points[pivotIndex]);
-        swap(points, pivotIndex, right);
+    private int partition(int left, int right, int[][] points, int pivotIndex){
+        int pivotDistance = getDistance(points[pivotIndex]);
+        swap(pivotIndex, right, points);
         pivotIndex = left;
         for(int i = left; i <= right; i++){
-            if(getDistance(points[i]) <= pivotDist) swap(points, i, pivotIndex++);
+            if(getDistance(points[i]) <= pivotDistance) swap(i, pivotIndex++, points);
         }
         return pivotIndex - 1;
     }
 
-    private void swap(int[][] points, int i, int j){
+    private void swap(int i, int j, int[][] points){
         int[] tmp = points[i];
         points[i] = points[j];
         points[j] = tmp;
