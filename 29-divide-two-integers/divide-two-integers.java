7f2dd04d9677d842651a00a91e5a76bfc215791f
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-04 17:39:46 -0500

    Time: 1 ms (85.82%) | Memory: 39.7 MB (42.51%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.java
+++ 29-divide-two-integers/divide-two-integers.java
@@ -1,5 +1,5 @@
 class Solution {
-        private static int HALF_INT_MIN = -1073741824;
+    private static int HALF_INT_MIN = -1073741824;
 
     public int divide(int dividend, int divisor) {
         if(dividend == Integer.MIN_VALUE && divisor == -1) return Integer.MAX_VALUE;
@@ -16,8 +16,7 @@ class Solution {
 
         int quotient = 0;
         while(divisor >= dividend){
-            int powerOfTwo = -1;
-            int value = divisor;
+            int powerOfTwo = -1, value = divisor;
             while(value >= HALF_INT_MIN && value + value >= dividend){
                 value+= value;
                 powerOfTwo+= powerOfTwo;
@@ -28,6 +27,5 @@ class Solution {
 
         if(negatives != 1) return -quotient;
         return quotient;
-
     }
 }
