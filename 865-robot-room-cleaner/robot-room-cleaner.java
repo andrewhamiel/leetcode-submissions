72a8c71fb06886dc2b971b3aae7f8cc5608a7361
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-04 15:44:28 -0500

    Time: 4 ms (95.91%) | Memory: 43 MB (84.21%) - LeetSync

---
+++ 865-robot-room-cleaner/robot-room-cleaner.java
@@ -0,0 +1,52 @@
+/**
+ * // This is the robot's control interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface Robot {
+ *     // Returns true if the cell in front is open and robot moves into the cell.
+ *     // Returns false if the cell in front is blocked and robot stays in the current cell.
+ *     public boolean move();
+ *
+ *     // Robot will stay in the same cell after calling turnLeft/turnRight.
+ *     // Each turn will be 90 degrees.
+ *     public void turnLeft();
+ *     public void turnRight();
+ *
+ *     // Clean the current cell.
+ *     public void clean();
+ * }
+ */
+
+class Solution {
+    //right-hand rule
+    private int[][] dirs = new int[][]{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}; //up/right/down/left
+    private Set<Pair<Integer, Integer>> visited = new HashSet<>();
+    private Robot robot;
+
+    public void cleanRoom(Robot robot) {
+        this.robot = robot;
+        backtrack(0, 0, 0);
+    }
+
+    private void backtrack(int row, int col, int dir){
+      visited.add(new Pair<>(row, col));
+      robot.clean();
+      for(int i = 0; i < 4; i++){
+        int newDir = (dir + i) % 4;
+        int newRow = row + dirs[newDir][0], newCol = col + dirs[newDir][1];
+        if(!visited.contains(new Pair<>(newRow, newCol)) && robot.move()) {
+          backtrack(newRow, newCol, newDir);
+          goBack();
+        }
+        //turn robot following chosen direciton: clockwise
+        robot.turnRight();
+      }
+    }
+
+    private void goBack(){
+      robot.turnRight();
+      robot.turnRight();
+      robot.move();
+      robot.turnRight();
+      robot.turnRight();
+    }
+}
