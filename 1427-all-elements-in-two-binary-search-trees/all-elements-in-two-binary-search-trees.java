1070fb3644bdda960650ecfe162a900db4fbc45c
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-05 17:15:53 -0600

    Time: 8 ms (99.16%) | Memory: 44.4 MB (98.42%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
@@ -15,63 +15,63 @@
  */
 class Solution {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        TreeNode curr1 = root1, curr2 = root2;
         List<Integer> result = new ArrayList<>();
+        TreeNode curr1 = root1, curr2 = root2;
         while(curr1 != null && curr2 != null){
-          //Setup temp cycle for curr1/curr2
-          while(curr1.left != null){
-            TreeNode tmp = curr1.left;
-            while(tmp.right != null && tmp.right != curr1) tmp = tmp.right;
+            //Condition morris
+            while(curr1.left != null){
+                TreeNode tmp = curr1.left;
+                while(tmp.right != null && tmp.right != curr1) tmp = tmp.right;
 
-            if(tmp.right == null){
-              tmp.right = curr1;
-              curr1 = curr1.left;
-            }else break; //connection already established
-          }
-          while(curr2.left != null){
-            TreeNode tmp = curr2.left;
-            while(tmp.right != null && tmp.right != curr2) tmp = tmp.right;
+                if(tmp.right == null){
+                    tmp.right = curr1;
+                    curr1 = curr1.left;
+                }else break; //Connection already established
+            }
+            while(curr2.left != null){
+                TreeNode tmp = curr2.left;
+                while(tmp.right != null && tmp.right != curr2) tmp = tmp.right;
 
-            if(tmp.right == null){
-              tmp.right = curr2;
-              curr2 = curr2.left;
-            }else break;
-          }
+                if(tmp.right == null){
+                    tmp.right = curr2;
+                    curr2 = curr2.left;
+                }else break;
+            }
 
-          //Compare
-          if(curr1.val <= curr2.val){
-            result.add(curr1.val);
-            curr1 = curr1.right;
-          }else{
-            result.add(curr2.val);
-            curr2 = curr2.right;
-          }
+            //Compare
+            if(curr1.val <= curr2.val) {
+                result.add(curr1.val);
+                curr1 = curr1.right;
+            }else{
+                result.add(curr2.val);
+                curr2 = curr2.right;
+            }
         }
-        //Any remaining nodes if one tree larger
+        //If one tree is larger than the other, add elements in those trees as well
         inorderTraversalUsingMorris(curr1, result);
         inorderTraversalUsingMorris(curr2, result);
         return result;
     }
 
     private void inorderTraversalUsingMorris(TreeNode root, List<Integer> result){
-      TreeNode curr = root;
-      while(curr != null){
-        if(curr.left != null){
-          TreeNode tmp = curr.left;
-          while(tmp.right != null && tmp.right != curr) tmp = tmp.right;
+        TreeNode curr = root;
+        while(curr != null){
+            if(curr.left != null){
+                TreeNode tmp = curr.left;
+                while(tmp.right != null && tmp.right != curr) tmp = tmp.right;
 
-          if(tmp.right == null){
-            tmp.right = curr;
-            curr = curr.left;
-          }else{
-            tmp.right = null;
-            result.add(curr.val);
-            curr = curr.right;
-          }
-        }else{
-          result.add(curr.val);
-          curr = curr.right;
+                if(tmp.right == null){
+                    tmp.right = curr;
+                    curr = curr.left;
+                }else{
+                    tmp.right = null;
+                    result.add(curr.val);
+                    curr = curr.right;
+                }
+            }else{
+                result.add(curr.val);
+                curr = curr.right;
+            }
         }
-      }
     }
 }
