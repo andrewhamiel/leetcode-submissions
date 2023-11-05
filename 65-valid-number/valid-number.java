cb2d6a0683205f9d754b03b0af20874a5f4d72be
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-05 14:04:31 -0600

    Time: 1 ms (100.00%) | Memory: 41.3 MB (74.43%) - LeetSync

--- 65-valid-number/valid-number.java
+++ 65-valid-number/valid-number.java
@@ -1,4 +1,5 @@
 class Solution {
+    //4 Cases:
     //1. Digit
     //2. Exponent
     //3. Sign
@@ -6,18 +7,20 @@ class Solution {
     public boolean isNumber(String s) {
         boolean isDigit = false, isExponent = false, isSign = false, isDot = false;
         for(int i = 0; i < s.length(); i++){
-            if(Character.isDigit(s.charAt(i))) isDigit = true;
-            else if(s.charAt(i) == 'e' || s.charAt(i) == 'E'){
-                if(i == 0 || isExponent || !isDigit) return false;
+            Character c = s.charAt(i);
+            if(Character.isDigit(c)) isDigit = true;
+            else if(c == 'e' || c == 'E'){
+                if(i == 0 || !isDigit || isExponent) return false;
                 isExponent = true;
                 isDigit = false;
-            }else if(s.charAt(i) == '+' || s.charAt(i) == '-'){
+            }else if(c == '+' || c == '-'){
                 if(i > 0 && s.charAt(i-1) != 'e' && s.charAt(i-1) != 'E') return false;
                 isSign = true;
                 isDigit = false;
-            }else if(s.charAt(i) == '.'){
+            }else if(c == '.'){
                 if(isDot || isExponent) return false;
                 isDot = true;
+
             }else return false;
         }
         return isDigit;
