7e01339b7ffc3c40ef617e746ea6ab18400f343f
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-05 16:10:20 -0600

    Time: 12 ms (96.84%) | Memory: 44.7 MB (95.17%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
@@ -15,28 +15,28 @@
  */
 class Solution {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        Deque<TreeNode> stack1 = new ArrayDeque<>(), stack2 = new ArrayDeque<>();
-        List<Integer> output = new ArrayList<>();
-        
-        while(root1 != null || root2 != null || !stack1.isEmpty() || !stack2.isEmpty()){
-          while(root1 != null){
-            stack1.addFirst(root1);
-            root1 = root1.left;
-          }
-          while(root2 != null){
-            stack2.addFirst(root2);
-            root2 = root2.left;
-          }
-          if(stack2.isEmpty() || !stack1.isEmpty() && stack1.peekFirst().val <= stack2.peekFirst().val){
-            root1 = stack1.removeFirst();
-            output.add(root1.val);
-            root1 = root1.right;
-          }else{
-            root2 = stack2.removeFirst();
-            output.add(root2.val);
-            root2 = root2.right;
-          }
+      Deque<TreeNode> stack1 = new ArrayDeque<>(), stack2 = new ArrayDeque<>();
+      List<Integer> output = new ArrayList<>();
+      while(root1 != null || root2 != null || !stack1.isEmpty() || !stack2.isEmpty()){
+        while(root1 != null){
+          stack1.addFirst(root1);
+          root1 = root1.left;
         }
-        return output;
+        while(root2 != null){
+          stack2.addFirst(root2);
+          root2 = root2.left;
+        }
+        if(stack2.isEmpty() || !stack1.isEmpty() && stack1.peekFirst().val <= stack2.peekFirst().val){
+          root1 = stack1.removeFirst();
+          output.add(root1.val);
+          root1 = root1.right;
+        }else{
+          root2 = stack2.removeFirst();
+          output.add(root2.val);
+          root2 = root2.right;
+        }
+      }
+      return output;
+        
     }
 }
