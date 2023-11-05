ef5d3c2e73a57707cc005a78d51a9b01f7e6254c
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-05 16:53:53 -0600

    Time: 7 ms (99.26%) | Memory: 44.8 MB (86.44%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
@@ -14,123 +14,76 @@
  * }
  */
 class Solution {
-    /** 
-        2. Lets dicsuss Morris law inorder traversal
-            1. First we will go to the most left of tree1 and tree2
-            2. compare both value, consider the smaller one, move to the right whhich value has been considered
-            3. do step 1 and 2 till both are not null
-            4. one of them null, break the loop
-            5. Do morris inorder traversal for the one which is remaining
-
-        Time complexity: O(n + m), as we need to traversal both the tree
-        Space complexity: O(1), Note: Here we are not considering the output array
-    */
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        return getAllElementsUsingMorrisLaw(root1, root2);
+        return getAllElementsWithMorrisLaw(root1, root2);
     }
 
-    private List<Integer> getAllElementsUsingMorrisLaw(TreeNode root1, TreeNode root2) {
-        TreeNode current1 = root1;
-        TreeNode current2 = root2;
+    private List<Integer> getAllElementsWithMorrisLaw(TreeNode root1, TreeNode root2){
+      TreeNode current1 = root1, current2 = root2;
+      List<Integer> result = new ArrayList<>();
 
-        List<Integer> result = new ArrayList<>();
-
-        // if both are not null
-        while(current1 != null && current2 != null) {
-            // go to left most
-            // while going make the connection 
-            while(current1.left != null) {
-                TreeNode temp = current1.left;
-
-                // go to most right 
-                while(temp.right != null && temp.right != current1) {
-                    temp = temp.right;
-                }
-                // if the connection is not there
-                // make the connection
-                // move current to current left
-                if(temp.right == null) {
-                    temp.right = current1;
-                    current1 = current1.left;
-                }
-                // if the connection is already present
-                // means we have already considered
-                // do nothing
-                else {
-                    // temp.right = null;
-                    break;
-                }
-            }
-            // go to left most
-            // while going make the connetion
-            while(current2.left != null) {
-                TreeNode temp = current2.left;
-
-                // go to most right
-                while(temp.right != null && temp.right != current2) {
-                    temp = temp.right;
-                }
-
-                // if the connection is not present
-                // make the connection
-                // move current to current left
-                if(temp.right == null) {
-                    temp.right = current2;
-                    current2 = current2.left;
-                }
-
-                // if the connection is already present
-                // means we have already considered 
-                // do nothing
-                else {
-                    // temp.right = null;
-                    break;
-                }
-            }
-            // compare both the value
-            // add to the list
-            // move the pointer to the right whichever have considered
-            if(current1.val <= current2.val) {
-                result.add(current1.val);
-                current1 = current1.right;
-            } else {
-                result.add(current2.val);
-                current2 = current2.right;
-            }
+      while(current1 != null && current2 != null){
+        //Leftmost
+        while(current1.left != null){
+          TreeNode leftmost = current1.left;
+          while(leftmost.right != null && leftmost.right != current1){
+            leftmost = leftmost.right;
+          }
+          if(leftmost.right == null){
+            leftmost.right = current1;
+            current1 = current1.left;
+          }else break;
         }
 
-        // if first tree node are remaining
-        // traverse the tree and collect all values
-        inorderTraversalUsingMorris(current1, result);
-        // if second tree node are remaining, 
-        // traverse the tree and collect all values
-        inorderTraversalUsingMorris(current2, result);
+        while(current2.left != null){
+          TreeNode leftmost = current2.left;
+          while(leftmost.right != null && leftmost.right != current2){
+            leftmost = leftmost.right;
+          }
+          
+          if(leftmost.right == null){
+            leftmost.right = current2;
+            current2 = current2.left;
+          }else break;
+        }
 
-        return result;
+        //Both current1 and current2 have been conditioned, now compare
+        if(current1.val <= current2.val){
+          result.add(current1.val);
+          current1 = current1.right;
+        }else{
+          result.add(current2.val);
+          current2 = current2.right;
+        }
+      }
+      //If there are remaining nodes in one tree
+      inorderTraversalUsingMorris(current1, result);
+      inorderTraversalUsingMorris(current2, result);
+
+      return result;
     }
 
+    private void inorderTraversalUsingMorris(TreeNode root, List<Integer> result){
+      TreeNode current = root;
+      while(current != null){
+        if(current.left == null){
+          result.add(current.val);
+          current = current.right;
+        }else{
+          TreeNode leftmost = current.left;
+          while(leftmost.right != null && leftmost.right != current){
+            leftmost = leftmost.right;
+          }
 
-    private void inorderTraversalUsingMorris(TreeNode root, List<Integer> inorder) {
-        TreeNode current = root;
-        while(current != null) {
-            if(current.left == null) {
-                inorder.add(current.val);
-                current = current.right;
-            } else {
-                TreeNode temp = current.left;
-                while(temp.right != null && temp.right != current) {
-                    temp = temp.right;
-                }
-
-                if(temp.right == null) {
-                    temp.right = current;
-                    current = current.left;
-                } else {
-                    temp.right = null;
-                    inorder.add(current.val);
-                    current = current.right;
-                }
-            }
+          if(leftmost.right == null){
+            leftmost.right = current;
+            current = current.left;
+          }else{
+            leftmost.right = null;
+            result.add(current.val);
+            current = current.right;
+          }
         }
+      }
     }
 }
