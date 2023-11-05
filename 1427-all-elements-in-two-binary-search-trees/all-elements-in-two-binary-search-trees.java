94710300cde16edc20bf2afc6106f5479603275f
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-05 17:02:58 -0600

    Time: 9 ms (99.07%) | Memory: 44.3 MB (99.26%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
@@ -15,74 +15,66 @@
  */
 class Solution {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        return getAllElementsWithMorrisLaw(root1, root2);
-    }
+        TreeNode current1 = root1, current2 = root2;
+        List<Integer> result = new ArrayList<>();
 
-    private List<Integer> getAllElementsWithMorrisLaw(TreeNode root1, TreeNode root2){
-      TreeNode current1 = root1, current2 = root2;
-      List<Integer> result = new ArrayList<>();
+        while(current1 != null && current2 != null){
+          //Get predecessor for current1 and current2
+          while(current1.left != null){
+            TreeNode predecessor = current1.left;
+            while(predecessor.right != null && predecessor.right != current1) predecessor = predecessor.right;
 
-      while(current1 != null && current2 != null){
-        //Leftmost
-        while(current1.left != null){
-          TreeNode leftmost = current1.left;
-          while(leftmost.right != null && leftmost.right != current1){
-            leftmost = leftmost.right;
+            if(predecessor.right == null){
+              predecessor.right = current1;
+              current1 = current1.left;
+            }else break;
           }
-          if(leftmost.right == null){
-            leftmost.right = current1;
-            current1 = current1.left;
-          }else break;
-        }
+          while(current2.left != null){
+            TreeNode predecessor = current2.left;
+            while(predecessor.right != null && predecessor.right != current2) predecessor = predecessor.right;
 
-        while(current2.left != null){
-          TreeNode leftmost = current2.left;
-          while(leftmost.right != null && leftmost.right != current2){
-            leftmost = leftmost.right;
+            if(predecessor.right == null){
+              predecessor.right = current2;
+              current2 = current2.left;
+            }else break;
           }
-          
-          if(leftmost.right == null){
-            leftmost.right = current2;
-            current2 = current2.left;
-          }else break;
+
+          //Compare
+          if(current1.val <= current2.val){
+            result.add(current1.val);
+            current1 = current1.right;
+          }else{
+            result.add(current2.val);
+            current2 = current2.right;
+          }
         }
 
-        //Both current1 and current2 have been conditioned, now compare
-        if(current1.val <= current2.val){
-          result.add(current1.val);
-          current1 = current1.right;
-        }else{
-          result.add(current2.val);
-          current2 = current2.right;
-        }
-      }
-      //If there are remaining nodes in one tree
-      inorderTraversalUsingMorris(current1, result);
-      inorderTraversalUsingMorris(current2, result);
+        //Handle any remaining nodes if one not null
+        inorderTraversalUsingMorris(current1, result);
+        inorderTraversalUsingMorris(current2, result);
 
-      return result;
+        return result;
     }
 
     private void inorderTraversalUsingMorris(TreeNode root, List<Integer> result){
       TreeNode current = root;
       while(current != null){
-        if(current.left == null){
-          result.add(current.val);
-          current = current.right;
-        }else{
-          TreeNode leftmost = current.left;
-          while(leftmost.right != null && leftmost.right != current){
-            leftmost = leftmost.right;
-          }
+        //Find predecessor
+        if(current.left != null){
+          TreeNode predecessor = current.left;
+          while(predecessor.right != null && predecessor.right != current) predecessor = predecessor.right;
 
-          if(leftmost.right == null){
-            leftmost.right = current;
+          if(predecessor.right == null){
+            predecessor.right = current;
             current = current.left;
           }else{
-            leftmost.right = null;
+            predecessor.right = null;
             result.add(current.val);
             current = current.right;
           }
+        }else{
+          result.add(current.val);
+          current = current.right;
         }
       }
     }
