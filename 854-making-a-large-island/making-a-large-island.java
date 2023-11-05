495c871b264a3f4226d030193ba90331d89fbc4c
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-05 13:50:21 -0600

    Time: 93 ms (73.16%) | Memory: 77.3 MB (48.70%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.java
+++ 854-making-a-large-island/making-a-large-island.java
@@ -2,14 +2,14 @@ class Solution {
     private int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
 
     public int largestIsland(int[][] grid) {
-        int maxSize = 0, islandId = 2;
+        int islandId = 2, maxIslandSize = 0;
         Map<Integer, Integer> islandSizes = new HashMap<>();
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == 1){
                     int size = getIslandSize(i, j, grid, islandId);
                     islandSizes.put(islandId++, size);
-                    maxSize = Math.max(maxSize, size);
+                    maxIslandSize = Math.max(maxIslandSize, size);
                 }
             }
         }
@@ -19,17 +19,18 @@ class Solution {
                 if(grid[i][j] == 0){
                     Set<Integer> nums = new HashSet<>();
                     for(int[] dir : dirs){
-                        int newRow = i + dir[0], newCol = j + dir[1];
-                        if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length
-                            && grid[newRow][newCol] != 0) nums.add(grid[newRow][newCol]);
+                        int row = i + dir[0], col = j + dir[1];
+                        if(row >= 0 && row < grid.length && col >= 0 && col < grid[0].length && grid[row][col] != 0){
+                            nums.add(grid[row][col]);
+                        }
                     }
                     int sum = 1;
                     for(int num : nums) sum+= islandSizes.get(num);
-                    maxSize = Math.max(maxSize, sum);
+                    maxIslandSize = Math.max(maxIslandSize, sum);
                 }
             }
         }
-        return maxSize;
+        return maxIslandSize;
     }
 
     private int getIslandSize(int i, int j, int[][] grid, int islandId){
