56902336014e9fde0234bb87d49a9c6c433101da
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-05 18:20:35 -0600

    Time: 37 ms (94.86%) | Memory: 120.6 MB (41.21%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,74 +1,72 @@
 class LRUCache {
+    class Node {
+        Node prev;
+        Node next;
+        int key;
+        int value;
+    }
 
-  class Node {
-    Node next;
-    Node prev;
-    int key;
-    int val;
-  }
-
-  Node head, tail;
-  int capacity;
-  Map<Integer, Node> map = new HashMap<>();
+    Node head, tail;
+    Map<Integer, Node> cache = new HashMap<>();
+    int capacity = 0;
 
     public LRUCache(int capacity) {
         head = new Node();
-        tail = new Node();     
+        tail = new Node();
         head.next = tail;
         tail.prev = head;
         this.capacity = capacity;
     }
-    
 
     private void add(Node node){
-      node.next = head.next;
-      node.prev = head;
-      head.next.prev = node; 
-      head.next = node;
+        node.next = head.next;
+        node.prev = head;
+        
+        head.next.prev = node;
+        head.next = node;
     }
 
     private void remove(Node node){
-      Node nextNode = node.next;
-      Node prevNode = node.prev;
-      nextNode.prev = prevNode;
-      prevNode.next = nextNode;
+        Node nextNode = node.next;
+        Node prevNode = node.prev;
+        nextNode.prev = prevNode;
+        prevNode.next = nextNode;
     }
 
-    private void moveToHead(Node node){
-      remove(node);
-      add(node);
+    private void moveToFront(Node node){
+        remove(node);
+        add(node);
     }
 
     private Node popTail(){
-        Node res = tail.prev;
-        remove(res);
-        return res;    
+        Node poppedTail = tail.prev;
+        remove(poppedTail);
+        return poppedTail;
     }
     
     public int get(int key) {
-        if(!map.containsKey(key)) return -1;
-        Node node = map.get(key);
-        moveToHead(node);
-        return node.val;      
+        if(!cache.containsKey(key)) return -1;
+        Node node = cache.get(key);
+        moveToFront(node);
+        return node.value;
     }
     
     public void put(int key, int value) {
-        if(map.containsKey(key)){
-          Node node = map.get(key);
-          moveToHead(node);
-          node.val = value;
+        if(!cache.containsKey(key)){
+            Node node = new Node();
+            node.key = key;
+            node.value = value;
+            add(node);
+            cache.put(key, node);
+            if(cache.size() > capacity){
+                Node poppedTail = popTail();
+                cache.remove(poppedTail.key);
+            }
         }else{
-          Node node = new Node();
-          node.key = key;
-          node.val = value;
-          add(node);
-          map.put(key, node);
-          if(map.size() > capacity){
-            Node toBeRemoved = popTail();
-            map.remove(toBeRemoved.key);
-          }
+            Node node = cache.get(key);
+            node.value = value;
+            moveToFront(node);
         }
-        
     }
 }
 
