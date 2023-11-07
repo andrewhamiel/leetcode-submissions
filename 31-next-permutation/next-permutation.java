51a858f8adc017705317bc2d33d5d341193ae80e
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-06 21:17:18 -0600

    Time: 0 ms (100.00%) | Memory: 42.3 MB (17.53%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,7 +1,7 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int i = nums.length - 2;
-        while(i >= 0 && nums[i] >= nums[i+1]) i--;
+        while(i>= 0 && nums[i] >= nums[i+1]) i--;
         if(i >= 0){
             int j = nums.length - 1;
             while(j >= 0 && nums[j] <= nums[i]) j--;
@@ -15,7 +15,7 @@ class Solution {
         nums[i] = nums[j];
         nums[j] = tmp;
     }
-
+    
     private void reverse(int i, int[] nums){
         int j = nums.length - 1;
         while(i < j) swap(i++, j--, nums);
