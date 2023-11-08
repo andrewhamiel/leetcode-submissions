9fe4e5493e25b3c6cde2fec207a084d2a255f84e
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-08 12:35:08 -0600

    Time: 40 ms (38.41%) | Memory: 55.9 MB (95.91%) - LeetSync

--- 2483-task-scheduler-ii/task-scheduler-ii.java
+++ 2483-task-scheduler-ii/task-scheduler-ii.java
@@ -1,16 +1,17 @@
 class Solution {
     public long taskSchedulerII(int[] tasks, int space) {
-        Map<Integer, Long> last = new HashMap<>();
+        Map<Integer, Long> map = new HashMap<>();
         long days = 0;
-        for (int taskType : tasks)
-            if (last.containsKey(taskType)){
-                days = Math.max(days, last.get(taskType) + space) + 1;
-                last.put(taskType, days);
-            } 
-            else {
+        for(int taskType : tasks){
+            if(map.containsKey(taskType)){
+                days = Math.max(days, map.get(taskType) + space);
                 days++;
-                last.put(taskType, days);
+                map.put(taskType, days);
+            }else{
+                days++;
+                map.put(taskType, days);
             }
+        }
         return days;
     }
 }
