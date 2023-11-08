337f52a96d44c331869d2701c4207de4243231f3
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-07 21:55:20 -0600

    Time: 1 ms (26.68%) | Memory: 40.4 MB (17.09%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -1,25 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-  public int sumNumbers(TreeNode root) {
-    int rootToLeaf = 0, currNumber = 0;
-    Deque<Pair<TreeNode, Integer>> stack = new ArrayDeque();
-    stack.push(new Pair(root, 0));
+    public int sumNumbers(TreeNode root) {
+        int rootToLeaf = 0, curr = 0;
+        Deque<Pair<TreeNode, Integer>> stack = new ArrayDeque<>();
+        stack.addFirst(new Pair(root, 0));
 
-    while (!stack.isEmpty()) {
-      Pair<TreeNode, Integer> p = stack.pop();
-      root = p.getKey();
-      currNumber = p.getValue();
+        while(!stack.isEmpty()){
+            Pair<TreeNode, Integer> p = stack.removeFirst();
+            root = p.getKey();
+            curr = p.getValue();
 
-      if (root != null) {
-        currNumber = currNumber * 10 + root.val;
-        // if it's a leaf, update root-to-leaf sum
-        if (root.left == null && root.right == null) {
-          rootToLeaf += currNumber;
-        } else {
-          stack.push(new Pair(root.right, currNumber));
-          stack.push(new Pair(root.left, currNumber));
+            if(root != null){
+                curr*= 10;
+                curr+= root.val;
+                if(root.left == null && root.right == null) rootToLeaf+= curr; //Leaf node
+                else{
+                    stack.addFirst(new Pair(root.right, curr));
+                    stack.addFirst(new Pair(root.left, curr));
+                }
+            }
         }
-      }
+        return rootToLeaf;
     }
-    return rootToLeaf;
-  }
 }
