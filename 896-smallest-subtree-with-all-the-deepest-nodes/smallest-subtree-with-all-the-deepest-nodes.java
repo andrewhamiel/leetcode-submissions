9fc42e497c5748af790ebd93c2f50275d4b303bf
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-08 09:40:26 -0600

    Time: 0 ms (100.00%) | Memory: 40.3 MB (66.31%) - LeetSync

--- 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.java
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.java
@@ -1,39 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public TreeNode subtreeWithAllDeepest(TreeNode root) {
-        Queue<TreeNode> q = new ArrayDeque<TreeNode>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
         TreeNode first = null, last = null;
-        q.offer(root);
-        
-        while(!q.isEmpty())
-        {
+        while(!q.isEmpty()){
             int size = q.size();
-            for(int i = 0; i < size; i++)
-            {
-                TreeNode temp = q.poll();
-                if(i == 0) first = temp;
-                if(i == size - 1) last = temp;
-                
-                if(temp.left != null) q.offer(temp.left);
-                
-                if(temp.right != null) q.offer(temp.right);
+            for(int i = 0; i < size; i++){
+                TreeNode curr = q.poll();
+                if(i == 0) first = curr;
+                if(i == size - 1) last = curr;
+
+                if(curr.left != null) q.add(curr.left);
+                if(curr.right != null) q.add(curr.right);
             }
         }
-        
-        TreeNode SubTreeRoot = findLCA(root, first, last);
-        
-        return SubTreeRoot;
+        TreeNode ans = lca(root, first, last);
+        return ans;
     }
-    
-    private TreeNode findLCA(TreeNode root, TreeNode first, TreeNode last) {
-        if(root == null) return null;
-        
+
+    private TreeNode lca(TreeNode root, TreeNode first, TreeNode last){
+        if(root == null) return root;
+
         if(root == first || root == last) return root;
-        
-        TreeNode left = findLCA(root.left, first, last);
-        TreeNode right = findLCA(root.right, first, last);
+        TreeNode left = lca(root.left, first, last);
+        TreeNode right = lca(root.right, first, last);
         
         if(left != null && right != null) return root;
-        
-        return left != null ? left :  right;
+
+        return left != null ? left : right;
     }
 }
