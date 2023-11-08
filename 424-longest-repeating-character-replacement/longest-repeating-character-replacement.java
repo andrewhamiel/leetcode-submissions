9bbebe7508325ea2a9890cb83d237146d327728b
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-08 15:15:16 -0600

    Time: 9 ms (70.54%) | Memory: 41.3 MB (85.65%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -3,7 +3,7 @@ class Solution {
         if(s.length() == 1) return 1;
         int[] count = new int[26];
 
-        int left = 0, maxCount = 0, ans = 0;
+        int left = 0, maxCount = 0, result = 0;
         for(int right = 0; right < s.length(); right++){
             char c = s.charAt(right);
             count[c - 'A']++;
@@ -11,8 +11,8 @@ class Solution {
             if(right - left + 1 - maxCount > k){
                 count[s.charAt(left++) - 'A']--;
             }
-            ans = Math.max(ans, right - left + 1);
+            result = Math.max(result, right - left + 1);
         }
-        return ans;
+        return result;
     }
 }
