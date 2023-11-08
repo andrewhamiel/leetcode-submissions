3534d8175902b659e82ac8e2ac90462445f7d519
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-07 21:48:23 -0600

    Time: 0 ms (100.00%) | Memory: 39.7 MB (83.34%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -1,18 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     private int rootToLeaf = 0;
-    
+
     public int sumNumbers(TreeNode root) {
         preorder(root, 0);
         return rootToLeaf;
     }
 
-    private void preorder(TreeNode r, int curr) {
-        if (r == null) return;
-        curr = curr * 10 + r.val;
-        // if it's a leaf, update root-to-leaf sum
-        if (r.left == null && r.right == null) rootToLeaf += curr;
+    private void preorder(TreeNode root, int curr){
+        if(root == null) return;
+        curr*= 10;
+        curr+= root.val;
+        //leaf node
+        if(root.left == null && root.right == null) rootToLeaf+= curr;
         
-        preorder(r.left, curr);
-        preorder(r.right, curr) ;
+        preorder(root.left, curr);
+        preorder(root.right, curr);
     }
 }
