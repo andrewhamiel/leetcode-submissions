d77b942bf08ce7997d21541a165db20805a3066f
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-08 11:11:44 -0600

    Time: 2 ms (21.13%) | Memory: 42.6 MB (93.70%) - LeetSync

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        TreeNode first = null, last = null;
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                TreeNode curr = q.poll();
+                if(i == 0) first = curr;
+                if(i == size - 1) last = curr;
+
+                if(curr.left != null) q.add(curr.left);
+                if(curr.right != null) q.add(curr.right);
+            }
+        }
+        return lca(root, first, last);
+    }
+
+    private TreeNode lca(TreeNode root, TreeNode first, TreeNode last){
+        if(root == null) return root;
+
+        if(root == first || root == last) return root;
+
+        TreeNode left = lca(root.left, first, last);
+        TreeNode right = lca(root.right, first, last);
+
+        if(left != null && right != null) return root;
+        return left != null ? left : right;
+    }
+}
