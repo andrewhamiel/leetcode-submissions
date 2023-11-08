941c6dfe6d10cddbbb585613fcc1e063be162f3b
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-07 21:52:22 -0600

    Time: 1 ms (26.68%) | Memory: 39.6 MB (90.76%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -1,36 +1,25 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    private int rootToLeaf = 0;
+  public int sumNumbers(TreeNode root) {
+    int rootToLeaf = 0, currNumber = 0;
+    Deque<Pair<TreeNode, Integer>> stack = new ArrayDeque();
+    stack.push(new Pair(root, 0));
 
-    public int sumNumbers(TreeNode root) {
-        preorder(root, 0);
-        return rootToLeaf;
+    while (!stack.isEmpty()) {
+      Pair<TreeNode, Integer> p = stack.pop();
+      root = p.getKey();
+      currNumber = p.getValue();
+
+      if (root != null) {
+        currNumber = currNumber * 10 + root.val;
+        // if it's a leaf, update root-to-leaf sum
+        if (root.left == null && root.right == null) {
+          rootToLeaf += currNumber;
+        } else {
+          stack.push(new Pair(root.right, currNumber));
+          stack.push(new Pair(root.left, currNumber));
+        }
+      }
     }
-
-    private void preorder(TreeNode root, int curr){
-        if(root == null) return;
-
-        curr*= 10;
-        curr+= root.val;
-
-        //Leaf node
-        if(root.left == null && root.right == null) rootToLeaf+= curr;
-
-        preorder(root.left, curr);
-        preorder(root.right, curr);
-    }
+    return rootToLeaf;
+  }
 }
