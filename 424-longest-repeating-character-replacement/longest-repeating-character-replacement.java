b544e2e96031438697aa8ce359468d5e43be689a
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-08 15:13:20 -0600

    Time: 9 ms (70.54%) | Memory: 41.5 MB (67.17%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,17 +1,17 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        if(s.length()==1) return 1;
-        int[] array=new int[26];
-        int left=0, maxCount=0, ans=0;
+        if(s.length() == 1) return 1;
+        int[] count = new int[26];
 
+        int left = 0, maxCount = 0, ans = 0;
         for(int right = 0; right < s.length(); right++){
-            array[s.charAt(right)-'A']++;
-            maxCount=Math.max(array[s.charAt(right)-'A'],maxCount);
-            if((right-left+1) - maxCount > k){
-                array[s.charAt(left)-'A']--;
-                left++;
+            char c = s.charAt(right);
+            count[c - 'A']++;
+            maxCount = Math.max(maxCount, count[c - 'A']);
+            if(right - left + 1 - maxCount > k){
+                count[s.charAt(left++) - 'A']--;
             }
-            ans=Math.max(ans,right-left+1);
+            ans = Math.max(ans, right - left + 1);
         }
         return ans;
     }
