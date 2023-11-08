00b19b663b424a97243b5070f4b19ecd47b923b5
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-08 13:34:31 -0600

    Time: 12 ms (52.85%) | Memory: 44 MB (67.64%) - LeetSync

--- 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.java
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.java
@@ -1,21 +1,21 @@
 class Solution {
     public int minInsertions(String s) {
-        int res = 0, right = 0;
-        for (int i = 0; i < s.length(); ++i) {
-            if (s.charAt(i) == '(') {
-                if (right % 2 > 0) {
-                    right--;
-                    res++;
+        int rightCount = 0, result = 0;
+        for(Character c : s.toCharArray()){
+            if(c == '('){
+                if(rightCount % 2 > 0){
+                    rightCount--;
+                    result++;
                 }
-                right += 2;
-            } else {
-                right--;
-                if (right < 0) {
-                    right += 2;
-                    res++;
+                rightCount+= 2;
+            }else{
+                rightCount--;
+                if(rightCount < 0){
+                    result++;
+                    rightCount+= 2;
                 }
             }
         }
-        return right + res;
+        return result + rightCount;
     }
 }
