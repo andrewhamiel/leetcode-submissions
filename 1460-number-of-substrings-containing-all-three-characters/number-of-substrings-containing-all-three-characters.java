e73b302914ef58055c6e2f1a238949c47fd065ac
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-08 14:07:34 -0600

    Time: 13 ms (68.22%) | Memory: 44.2 MB (11.81%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,23 +1,20 @@
 class Solution {
     public int numberOfSubstrings(String s) {
         int[] count = new int[3];
-        int left = 0; int right = 0;
-        int result = 0;
-        while (right < s.length()) {
-            char ch=s.charAt(right);
-            count[ch-'a']++;
-            while (isValidSubstring(count)) {
-                result += s.length() - right;
-                 // Update count for the current and future substrings
-                count[s.charAt(left) - 'a']--; 
-                // Reduce the count for the character at the start
-                left++; // Move the start of the window forward
-            }   
+        int left = 0, right = 0, result = 0;
+        while(right < s.length()){
+            char c = s.charAt(right);
+            count[c - 'a']++;
+            while(isValidSubstring(count)){
+                result+= s.length() - right;
+                count[s.charAt(left++) - 'a']--;
+            }
             right++;
-        }   
+        }
         return result;
     }
-    public boolean isValidSubstring(int[] count) {
-        return count[0] != 0 && count[1] != 0 && count[2] != 0;
+
+    private boolean isValidSubstring(int[] count){
+        return count[0] > 0 && count[1] > 0 && count[2] > 0;
     }
 }
