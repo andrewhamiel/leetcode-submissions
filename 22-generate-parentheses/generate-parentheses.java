56009f660c6774f98ae5b547c94529e9ae16d315
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-08 14:47:44 -0600

    Time: 0 ms (100.00%) | Memory: 41.2 MB (99.45%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -3,12 +3,12 @@ class Solution {
         if(n == 0) return new ArrayList<>();
 
         List<String> result = new ArrayList<>();
-        dfs(n, n, new StringBuilder(), result, 0);
+        helper(n, n, new StringBuilder(), result, 0);
         return result;
     }
 
-    private void dfs(int leftCount, int rightCount, StringBuilder sb, List<String> result, int start){
-        if(leftCount == 0 && rightCount == 0){
+    private void helper(int leftCount, int rightCount, StringBuilder sb, List<String> result, int start){
+        if(leftCount == 0 && rightCount == 0) {
             result.add(sb.toString());
             return;
         }
@@ -16,12 +16,12 @@ class Solution {
         int currLength = sb.length();
         if(leftCount > 0){
             sb.append("(");
-            dfs(leftCount - 1, rightCount, sb, result, start + 1);
+            helper(leftCount - 1, rightCount, sb, result, start + 1);
             sb.deleteCharAt(currLength);
         }
         if(rightCount > leftCount){
             sb.append(")");
-            dfs(leftCount, rightCount - 1, sb, result, start + 1);
+            helper(leftCount, rightCount - 1, sb, result, start + 1);
             sb.deleteCharAt(currLength);
         }
     }
