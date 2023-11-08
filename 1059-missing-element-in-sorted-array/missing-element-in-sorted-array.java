6661d7223ab5840e0559f1a40f04b3e7e2f435df
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-07 22:59:32 -0600

    Time: 0 ms (100.00%) | Memory: 46.7 MB (70.85%) - LeetSync

--- 1059-missing-element-in-sorted-array/missing-element-in-sorted-array.java
+++ 1059-missing-element-in-sorted-array/missing-element-in-sorted-array.java
@@ -2,7 +2,7 @@ class Solution {
     public int missingElement(int[] nums, int k) {
         int left = 0, right = nums.length - 1;
         while(left < right){
-            int mid = left + (right - left + 1)/2;
+            int mid = right - (right - left)/2;
             if(nums[mid] - nums[0] - mid < k) left = mid;
             else right = mid - 1;
         }
