80d25531ef4cc5e3e2efcb9f981013781bf582cf
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-08 11:20:50 -0600

    Time: 1 ms (41.80%) | Memory: 43.2 MB (23.10%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.java
@@ -1,25 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    int count = 0;
+    private int count = 0;
 
     public int averageOfSubtree(TreeNode root) {
-        postOrder(root);
+        postorder(root);
         return count;
     }
-    
-    private Pair<Integer, Integer> postOrder(TreeNode root) {
-        if (root == null) return new Pair(0, 0);
-        
-        // First iterate over left and right subtrees.
-        Pair<Integer, Integer> left = postOrder(root.left);
-        Pair<Integer, Integer> right = postOrder(root.right);
-        
-        int nodeSum = left.getKey() + right.getKey() + root.val;
-        int nodeCount = left.getValue() + right.getValue() + 1;
 
-        // Check if the average of the subtree is equal to the node value.
-        if (root.val == nodeSum / (nodeCount)) count++;
-        
-        // Return the sum of nodes and the count in the subtree.
-        return new Pair(nodeSum, nodeCount);
+    private Pair<Integer, Integer> postorder(TreeNode root){
+        if(root == null) return new Pair<>(0, 0);
+
+        Pair<Integer, Integer> left= postorder(root.left);
+        Pair<Integer, Integer> right = postorder(root.right);
+
+        int sum = left.getKey() + right.getKey() + root.val;
+        int numNodes = left.getValue() + right.getValue() + 1;
+
+        if(root.val == sum/numNodes) count++;
+        return new Pair<>(sum, numNodes);
     }
 }
