00076e74e254a2be878fb89d524553ec973a0074
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-08 16:02:36 -0600

    Time: 26 ms (81.26%) | Memory: 68.5 MB (69.86%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.java
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.java
@@ -1,27 +1,20 @@
 class Solution {
     public int[] findDiagonalOrder(List<List<Integer>> nums) {
-        Queue<Pair<Integer, Integer>> q = new LinkedList();
-        q.offer(new Pair(0, 0));
-        List<Integer> ans = new ArrayList();
-        
-        while (!q.isEmpty()) {
+        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+        q.add(new Pair(0, 0));
+        List<Integer> ans = new ArrayList<>();
+
+        while(!q.isEmpty()){
             Pair<Integer, Integer> p = q.poll();
-            int row = p.getKey();
-            int col = p.getValue();
+            int row = p.getKey(), col = p.getValue();
             ans.add(nums.get(row).get(col));
-            
-            if (col == 0 && row + 1 < nums.size()) q.offer(new Pair(row + 1, col));         
-            if (col + 1 < nums.get(row).size()) q.offer(new Pair(row, col + 1));
+
+            if(col == 0 && row + 1 < nums.size()) q.add(new Pair<>(row + 1, col));
+            if(col + 1 < nums.get(row).size()) q.add(new Pair<>(row, col + 1));
         }
-        
-        // Java needs conversion
+
         int[] result = new int[ans.size()];
-        int i = 0;
-        for (int num : ans) {
-            result[i] = num;
-            i++;
-        }
-        
+        for(int i = 0; i < result.length; i++) result[i] = ans.get(i);
         return result;
     }
 }
