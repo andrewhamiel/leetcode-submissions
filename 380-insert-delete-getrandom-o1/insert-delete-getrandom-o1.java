2092ca03a3fc080f62957a83b385fa08038e11b8
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-12 17:15:36 -0600

    Time: 23 ms (77.99%) | Memory: 89 MB (81.80%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
@@ -20,8 +20,8 @@ class RandomizedSet {
         if(!map.containsKey(val)) return false;
         int ind = map.get(val);
         int lastElement = list.get(list.size() - 1);
-        map.put(lastElement, ind);
         list.set(ind, lastElement);
+        map.put(lastElement, ind);
         map.remove(val);
         list.remove(list.size() - 1);
         return true;
