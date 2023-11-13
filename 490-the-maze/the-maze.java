6a42dd6031a6f3e20ebd60799ab6c1393ee70e94
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-12 19:12:27 -0600

    Time: 5 ms (60.46%) | Memory: 44.8 MB (48.08%) - LeetSync

--- 490-the-maze/the-maze.java
+++ 490-the-maze/the-maze.java
@@ -1,31 +1,28 @@
 class Solution {
-    int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+    private int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
 
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
-        boolean[][] visit = new boolean[maze.length][maze[0].length];
-
+        boolean[][] visited = new boolean[maze.length][maze[0].length];
         Queue<int[]> q = new LinkedList<>();
-        q.offer(start);
-        visit[start[0]][start[1]] = true;
-
-        while (!q.isEmpty()) {
-            int[] curr = q.poll();
-            if (curr[0] == destination[0] && curr[1] == destination[1]) return true;
-
+        q.add(start);
+        visited[start[0]][start[1]] = true;
+        while(!q.isEmpty()){
+            int[]curr = q.poll();
+            if(curr[0] == destination[0] && curr[1] == destination[1]) return true;
             for(int[] dir : dirs){
-                int row = curr[0], col  = curr[1];
+                int row = curr[0], col = curr[1];
                 while(row >= 0 && row < maze.length && col >= 0 && col < maze[0].length && maze[row][col] == 0){
                     row+= dir[0];
                     col+= dir[1];
                 }
                 row-= dir[0];
                 col-= dir[1];
-                if(!visit[row][col]){
-                    visit[row][col] = true;
+                if(!visited[row][col]){
+                    visited[row][col] = true;
                     q.add(new int[]{row, col});
                 }
             }
         }
-        return false;
+        return false;    
     }
 }
