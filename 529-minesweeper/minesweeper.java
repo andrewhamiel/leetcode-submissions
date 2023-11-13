6b161efe9572c42e1f97ea2aee11acb86b188c22
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-12 19:29:14 -0600

    Time: 3 ms (34.09%) | Memory: 44.3 MB (9.61%) - LeetSync

--- 529-minesweeper/minesweeper.java
+++ 529-minesweeper/minesweeper.java
@@ -1,39 +1,37 @@
 class Solution {
-    private int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}, {-1, -1}, {1, -1}, {-1, 1}, {1, 1}};
+    private int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+
     public char[][] updateBoard(char[][] board, int[] click) {
         Queue<int[]> q = new LinkedList<>();
         q.add(click);
         while(!q.isEmpty()){
             int[] cell = q.poll();
             int row = cell[0], col = cell[1];
-            //Mine
             if(board[row][col] == 'M') board[row][col] = 'X';
             else{
-                //Get mines
                 int mines = getMines(row, col, board);
                 if(mines > 0) board[row][col] = (char)(mines + '0');
                 else{
                     board[row][col] = 'B';
                     for(int[] dir : dirs){
-                        int i = row + dir[0], j = col + dir[1];
-                        if(i >= 0 && i < board.length && j >= 0 && j < board[0].length && board[i][j] == 'E') {
-                            q.add(new int[]{i, j});
-                            board[i][j] = 'B';
+                        int newRow = row + dir[0], newCol = col + dir[1];
+                        if(newRow >= 0 && newRow < board.length && newCol >= 0 && newCol < board[0].length && board[newRow][newCol] == 'E'){
+                            board[newRow][newCol] = 'B';
+                            q.add(new int[]{newRow, newCol});
                         }
                     }
                 }
-                
             }
         }
-        return board;
+        return board;    
     }
 
     private int getMines(int row, int col, char[][] board){
         int mines = 0;
         for(int[] dir : dirs){
-            int i = row + dir[0], j = col + dir[1];
-            if(i >= 0 && i < board.length && j >= 0 && j < board[0].length 
-            && (board[i][j] == 'M' || board[i][j] == 'X')) mines++;
+            int newRow = row + dir[0], newCol = col + dir[1];
+            if(newRow >= 0 && newRow < board.length && newCol >= 0 && newCol < board[0].length 
+                && (board[newRow][newCol] == 'M' || board[newRow][newCol] == 'X')) mines++; 
         }
         return mines;
     }
