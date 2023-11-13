82b511e54682aa6a07d44ae57beba709398b8d2e
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-12 21:01:23 -0600

    Time: 95 ms (78.75%) | Memory: 64.4 MB (78.80%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -2,19 +2,18 @@ class NumMatrix {
     private int[][] mat;
 
     public NumMatrix(int[][] matrix) {
-        if(matrix.length == 0 || matrix[0].length == 0) return;
         mat = new int[matrix.length + 1][matrix[0].length + 1];
         for(int row = 0; row < matrix.length; row++){
             int rowSum = 0;
             for(int col = 0; col < matrix[0].length; col++){
                 rowSum+= matrix[row][col];
-                mat[row+1][col+1] = mat[row][col + 1] + rowSum;
+                mat[row+1][col+1] = mat[row][col+1] + rowSum;
             }
         }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        return mat[row2 + 1][col2 + 1] - mat[row1][col2 + 1] - mat[row2 + 1][col1] + mat[row1][col1];
+        return mat[row2+1][col2+1] - mat[row1][col2+1] - mat[row2+1][col1] + mat[row1][col1];
     }
 }
 
