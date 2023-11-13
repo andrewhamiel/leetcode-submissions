995ce9a6fded0f4f96952ba16e8f6ad4dc6b6aa0
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-12 19:57:28 -0600

    Time: 4 ms (62.64%) | Memory: 46.2 MB (82.11%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -24,7 +24,7 @@ class Solution {
         count1 = 0; count2 = 0;
 
         for(int num : nums){
-            if(candidate1 != null && num == candidate1) count1++;
+            if(candidate1 != null && candidate1 == num) count1++;
             if(candidate2 != null && num == candidate2) count2++;
         }
 
