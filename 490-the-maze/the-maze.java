a0d3ba21baf1303c5a29b3ff853eab9cce38a503
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-12 19:09:05 -0600

    Time: 5 ms (60.46%) | Memory: 45.5 MB (7.48%) - LeetSync

--- 490-the-maze/the-maze.java
+++ 490-the-maze/the-maze.java
@@ -1,36 +1,31 @@
 class Solution {
-    public boolean dfs(int m, int n, int[][] maze, int[] curr, int[] destination,
-                boolean[][] visit) {
-        if (visit[curr[0]][curr[1]]) {
-            return false;
-        }
-        if (curr[0] == destination[0] && curr[1] == destination[1]) {
-            return true;
-        }
+    int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
 
-        visit[curr[0]][curr[1]] = true;
-        int[] dirX = {0, 1, 0, -1};
-        int[] dirY = {-1, 0, 1, 0};
+    public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+        boolean[][] visit = new boolean[maze.length][maze[0].length];
 
-        for (int i = 0; i < 4; i++) {
-            int r = curr[0], c = curr[1];
-            // Move the ball in the chosen direction until it can.
-            while (r >= 0 && r < m && c >= 0 && c < n && maze[r][c] == 0) {
-                r += dirX[i];
-                c += dirY[i];
-            }
-            // Revert the last move to get the cell to which the ball rolls.
-            if (dfs(m, n, maze, new int[]{r - dirX[i], c - dirY[i]}, destination, visit)) {
-                return true;
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(start);
+        visit[start[0]][start[1]] = true;
+
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            if (curr[0] == destination[0] && curr[1] == destination[1]) return true;
+
+            for(int[] dir : dirs){
+                int row = curr[0], col  = curr[1];
+                while(row >= 0 && row < maze.length && col >= 0 && col < maze[0].length && maze[row][col] == 0){
+                    row+= dir[0];
+                    col+= dir[1];
+                }
+                row-= dir[0];
+                col-= dir[1];
+                if(!visit[row][col]){
+                    visit[row][col] = true;
+                    q.add(new int[]{row, col});
+                }
             }
         }
         return false;
-    }
-
-    public boolean hasPath(int[][] maze, int[] start, int[] destination) {
-        int m = maze.length;
-        int n = maze[0].length;
-        boolean[][] visit = new boolean[m][n];
-        return dfs(m, n, maze, start, destination, visit);
     }
 }
