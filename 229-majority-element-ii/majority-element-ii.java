2048a2602c2bd9830dbe6606da4340be3650c745
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-12 20:02:05 -0600

    Time: 3 ms (69.94%) | Memory: 46.4 MB (60.15%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -2,7 +2,6 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int count1 = 0, count2 = 0;
         Integer candidate1 = null, candidate2 = null;
-
         //First pass
         for(int num : nums){
             if(candidate1 != null && num == candidate1) count1++;
