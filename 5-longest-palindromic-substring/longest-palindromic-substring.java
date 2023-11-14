207ba5bbd1b9de4c6be7b85e875a0f765345aae7
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-13 21:39:24 -0600

    Time: 16 ms (73.78%) | Memory: 41 MB (81.46%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -3,19 +3,19 @@ class Solution {
         int longest = 0, left = 0, right = 0;
         for(int i = 0; i < s.length(); i++){
             int oddLength = middleOut(i, i, s);
-            int evenLength = middleOut(i, i+1, s);
+            int evenLength = middleOut(i, i + 1, s);
             if(oddLength > longest || evenLength > longest){
                 longest = Math.max(oddLength, evenLength);
                 left = i - (longest - 1)/2;
-                right = i + (longest/2);
+                right = i + longest/2;
             }
         }
         return s.substring(left, right + 1);
     }
 
     private int middleOut(int left, int right, String s){
-        int length = 0;
-        while(left >= 0 && right < s.length() && s.charAt(left--) == s.charAt(right++)) length = right - left - 1;
-        return length;
+        int longest = 0;
+        while(left >= 0 && right < s.length() && s.charAt(left--) == s.charAt(right++)) longest = right - left - 1;
+        return longest;
     }
 }
