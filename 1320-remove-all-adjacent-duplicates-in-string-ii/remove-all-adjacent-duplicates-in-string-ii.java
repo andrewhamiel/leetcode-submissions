54776804e3b9ae8785c4a1cd2ab73044d4f46ca1
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-13 22:39:53 -0600

    Time: 30 ms (56.49%) | Memory: 45.2 MB (33.53%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
@@ -2,22 +2,17 @@ class Solution {
     public String removeDuplicates(String s, int k) {
         Deque<Pair<Character, Integer>> stack = new ArrayDeque<>();
         for(int i = 0; i < s.length(); i++){
-            if(stack.isEmpty() || s.charAt(i) != stack.peekFirst().getKey()){
-                stack.push(new Pair<Character, Integer>(s.charAt(i), 1));
-            }else{
+            if(stack.isEmpty() || s.charAt(i) != stack.peekFirst().getKey()) stack.push(new Pair<>(s.charAt(i), 1));
+            else{
                 Pair<Character, Integer> p = stack.removeFirst();
-                if(p.getValue() + 1 < k){
-                    stack.addFirst(new Pair(p.getKey(), p.getValue() + 1));
-                }
+                if(p.getValue() + 1 < k) stack.addFirst(new Pair<>(p.getKey(), p.getValue() + 1));
             }
         }
         StringBuilder sb = new StringBuilder();
         while(!stack.isEmpty()){
             Pair<Character, Integer> p = stack.removeLast();
             int count = p.getValue();
-            while(count-- != 0){
-                sb.append(p.getKey());
-            }
+            while(count-- > 0) sb.append(p.getKey());
         }
         return sb.toString();
     }
