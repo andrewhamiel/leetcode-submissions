ac3ea016a5371e3185dfc9c063f681cb6acec716
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-13 19:22:19 -0600

    Time: 1 ms (89.88%) | Memory: 44.8 MB (20.52%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,7 +2,7 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         boolean isCol = false;
 
-        //mark first row/col if 0 found
+        //mark first row/col if zero found
         for(int i = 0; i < matrix.length; i++){
             if(matrix[i][0] == 0) isCol = true;
             for(int j = 1; j < matrix[0].length; j++){
@@ -13,7 +13,7 @@ class Solution {
             }
         }
 
-        //update rows/columns that have been marked
+        //zero out rows/columns that have been marked
         for(int i = 1; i < matrix.length; i++){
             for(int j = 1; j < matrix[0].length; j++){
                 if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
@@ -22,7 +22,7 @@ class Solution {
 
         //check first row
         if(matrix[0][0] == 0){
-            for(int j = 0; j < matrix[0].length; j++) matrix[0][j] = 0;
+            for(int j = 1; j < matrix[0].length; j++) matrix[0][j] = 0;
         }
         //check first column
         if(isCol){
