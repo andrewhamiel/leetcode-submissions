563b24715fe6d266d065c5c69b1e0d9eb8981c7f
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-13 19:08:20 -0600

    Time: 1 ms (89.88%) | Memory: 44.6 MB (31.56%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,8 +1,8 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
         boolean isCol = false;
-        
-        //set first row/col as 0
+
+        //mark first row/col as 0
         for(int i = 0; i < matrix.length; i++){
             if(matrix[i][0] == 0) isCol = true;
             for(int j = 1; j < matrix[0].length; j++){
@@ -12,17 +12,21 @@ class Solution {
                 }
             }
         }
-        
-        //iterate through, zero out rows/cols flagged
+
+        //zero out rows/cols that were marked
         for(int i = 1; i < matrix.length; i++){
             for(int j = 1; j < matrix[0].length; j++){
                 if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
             }
         }
-        
-        //Check first row
-        if(matrix[0][0] == 0) for(int j = 0; j < matrix[0].length; j++) matrix[0][j] = 0;
-        //check first column as well
-        if(isCol) for(int i = 0; i < matrix.length; i++) matrix[i][0] = 0;
+
+        //check first row
+        if(matrix[0][0] == 0){
+            for(int j = 0; j < matrix[0].length; j++) matrix[0][j] = 0;
+        }
+        //check first col
+        if(isCol){
+            for(int i = 0; i < matrix.length; i++) matrix[i][0] = 0;
+        }
     }
 }
