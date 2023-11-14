f06aedb7a892c9621035e748bac37599d73dcc5d
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-13 18:36:02 -0600

    Time: 2 ms (100.00%) | Memory: 40.2 MB (66.52%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -2,8 +2,9 @@ class Solution {
     public int change(int amount, int[] coins) {
         int[] sums = new int[amount + 1];
         sums[0] = 1;
+
         for(int coin : coins){
-            for(int i = coin; i <= amount; i++) sums[i]+= sums[i-coin];
+            for(int i = coin; i <= amount; i++) sums[i]+= sums[i - coin];
         }
         return sums[amount];
     }
