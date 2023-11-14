80f85a7748065b1debe4f2fca2da4fc1fcda5d00
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-13 18:03:34 -0600

    Time: 23 ms (86.95%) | Memory: 59.8 MB (11.12%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        Deque<Integer> stack = new ArrayDeque<>();
         int[] result = new int[temperatures.length];
-
+        Deque<Integer> stack = new ArrayDeque<>();
+        
         for(int currDay = 0; currDay < temperatures.length; currDay++){
-            int currentTemp = temperatures[currDay];
-            while(!stack.isEmpty() && temperatures[stack.peekFirst()] < currentTemp){
+            int currTemp = temperatures[currDay];
+            while(!stack.isEmpty() && temperatures[stack.peekFirst()] < currTemp){
                 int prevDay = stack.removeFirst();
                 result[prevDay] = currDay - prevDay;
             }
