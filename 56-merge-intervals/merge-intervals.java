473e4441d1d4db467db8dec1cc0a342e4c8df051
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-15 14:22:36 -0600

    Time: 12 ms (11.79%) | Memory: 48.7 MB (5.25%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,13 +1,16 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         if(intervals.length == 0) return new int[0][0];
-		List<int[]> list = new ArrayList<>();
+
+		//1. Populate Ordered Map
 		TreeMap<Integer, Integer> map = new TreeMap<>();
 		for(int[] interval : intervals){
 			map.put(interval[0], map.getOrDefault(interval[0], 0) + 1);
 			map.put(interval[1], map.getOrDefault(interval[1], 0) - 1);
 		}
 
+		//2. Merge intervals
+		List<int[]> list = new ArrayList<>();
 		int count = 0, start = Integer.MAX_VALUE, end = Integer.MIN_VALUE;
 		for(int k : map.keySet()){
 			count+= map.get(k);
@@ -20,6 +23,7 @@ class Solution {
 			}
 		}
 
+		//3. Build result
 		int[][] result = new int[list.size()][2];
 		for(int i = 0; i < result.length; i++) result[i] = list.get(i);
 		return result;
