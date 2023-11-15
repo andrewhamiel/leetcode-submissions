83ad8897357f17ebdb83cabdd518bd9b8bbd7e77
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-15 14:14:47 -0600

    Time: 12 ms (11.79%) | Memory: 47.7 MB (5.25%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,69 +1,24 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-		IntervalNode root=new IntervalNode(intervals[0][0], intervals[0][1]);
+        if (intervals.length == 0) return new int[0][0];
+        List<int[]> list = new ArrayList<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for(int[] itv : intervals){
+            map.put(itv[0], map.getOrDefault(itv[0], 0)+1);
+            map.put(itv[1], map.getOrDefault(itv[1], 0)-1);
+        }
 		
-		for(int i=1;i<intervals.length;i++) {
-			add(root, intervals[i]);
-		}
-		
-		List<int[]> list=merge(root);
-		int[][] ans=new int[list.size()][2];
-		for(int i=0;i<ans.length;i++) {
-			ans[i]=list.get(i);
-		}
-		return ans;
-	}
-
-    class IntervalNode{
-		int low,high,max;
-		IntervalNode left,right;
-		IntervalNode(int l,int h){
-			this.low=l;this.high=h;this.max=h;
-			this.left=this.right=null;
-		}
-	}
-	
-	private List<int[]> merge(IntervalNode root){
-		List<int[]> res=new ArrayList<>();
-		if(root==null) return res;
-		
-		List<int[]> left=merge(root.left);
-		List<int[]> right=merge(root.right);
-		
-		boolean inserted=false;
-		for(int[] i:left) {
-			if(i[1]>=root.low) {
-				inserted=true;
-				i[0]=Math.min(i[0], root.low);
-				i[1]=Math.max(i[1], root.high);
-				root=new IntervalNode(i[0], i[1]);
-			}
-			res.add(i);
-		}
-		
-		if(!inserted) res.add(new int[] {root.low,root.high});
-		
-		for(int[] i:right) {
-			if(i[0]<=root.high) {
-				inserted=true;
-				i[0]=Math.min(i[0], root.low);
-				i[1]=Math.max(i[1], root.high);
-				res.remove(res.size()-1);
-			}
-			res.add(i);
-		}
-		
-		return res;
-		
-	}
-	
-	private IntervalNode add(IntervalNode root,int[] i) {
-		if(root==null) return new IntervalNode(i[0], i[1]);
-		
-		if(i[0]<root.low)
-			root.left=add(root.left, i);
-		else
-			root.right=add(root.right,i);
-		return root;
-	}
+        int count = 0, start = Integer.MAX_VALUE, end = Integer.MIN_VALUE;
+        for (int k : map.keySet()){
+            count+=map.get(k);
+            start = Math.min(start, k);
+            end = Math.max(end, k);
+            if (count==0){
+                list.add(new int[]{start, end});
+                start = Integer.MAX_VALUE;
+                end = Integer.MIN_VALUE;
+            }
+        }
+        return list.toArray(new int[list.size()][2]);
+    }
 }
