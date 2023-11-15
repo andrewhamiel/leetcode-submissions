9ad3a6b212a982ec9568ee489d3d9c95d227b3f8
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-14 21:22:13 -0600

    Time: 1 ms (63.89%) | Memory: 40.4 MB (61.11%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.java
+++ 807-custom-sort-string/custom-sort-string.java
@@ -1,25 +1,22 @@
 class Solution {
     public String customSortString(String order, String s) {
         int[] sFreq = new int[26];
-        for(Character c : s.toCharArray()){
-            sFreq[(int)(c - 'a')]++;
-        }
+        for(Character c : s.toCharArray()) sFreq[c - 'a']++;
 
-        StringBuilder sb = new StringBuilder();
-        for(Character c : order.toCharArray()){
-            while(sFreq[c - 'a'] > 0){
-                sb.append(c);
-                sFreq[c - 'a']--;
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < order.length(); i++){
+            Character c = order.charAt(i);
+            while(sFreq[c - 'a']-- > 0){
+                result.append(c);
             }
         }
 
         for(int i = 0; i < 26; i++){
-            while(sFreq[i] > 0){
-                sb.append((char)(i + 'a'));
-                sFreq[i]--;
+            while(sFreq[i]-- > 0) {
+                result.append((char)(i + 'a'));
             }
         }
 
-        return sb.toString();
+        return result.toString();
     }
 }
