4c26e89259211f75f30b656a7877c7dffcbcd271
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-15 11:15:56 -0600

    Time: 0 ms (100.00%) | Memory: 43.1 MB (13.67%) - LeetSync

--- 850-insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.java
+++ 850-insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.java
@@ -19,28 +19,26 @@ class Node {
 
 class Solution {
     public Node insert(Node head, int insertVal) {
-        if(head == null) {
+        if(head == null){
             head = new Node(insertVal);
             head.next = head;
             return head;
         }
-        Node original = head, curr = head;
+        Node curr = head;
         boolean isInserted = false;
         while(!isInserted){
-            //3 cases
-            if((curr.val != curr.next.val) &&
-            ((insertVal >= curr.val && insertVal <= curr.next.val && curr.val <= curr.next.val)
-            || (insertVal >= curr.val && insertVal >= curr.next.val && curr.val >= curr.next.val)
-            || (insertVal <= curr.val && insertVal <= curr.next.val && curr.val >= curr.next.val))){
+            if((curr.val != curr.next.val) && 
+            ((curr.val <= insertVal && insertVal <= curr.next.val && curr.val <= curr.next.val)
+            || (curr.val <= insertVal && curr.next.val <= insertVal && curr.val >= curr.next.val)
+            || (curr.val >= insertVal && curr.next.val >= insertVal && curr.val >= curr.next.val))){
                 Node node = new Node(insertVal);
                 node.next = curr.next;
                 curr.next = node;
                 return head;
             }
             curr = curr.next;
-            if(curr == original) isInserted = true;
+            if(curr == head) isInserted = true;
         }
-
         //case 4: all duplicates
         curr = head;
         Node node = new Node(insertVal);
