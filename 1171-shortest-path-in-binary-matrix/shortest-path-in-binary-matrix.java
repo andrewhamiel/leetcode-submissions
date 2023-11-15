edb71d05c058fdf055a71056145fd755ec9b708e
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-15 15:40:17 -0600

    Time: 12 ms (94.31%) | Memory: 45.3 MB (16.11%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,27 +1,24 @@
 class Solution {
-    int[][] dirs = new int[][]{{-1, -1}, {-1, 1}, {1, -1}, {1, 1}, {0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+    private int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+    
     public int shortestPathBinaryMatrix(int[][] grid) {
-        if(grid.length == 0 || grid[0][0] != 0) return -1;
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
-        int shortestPath = Integer.MAX_VALUE;
-        visited[0][0] = true;
+        if(grid == null || grid.length == 0 || grid[0][0] != 0) return -1;
+
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0, 0});
-        int steps = 0;
+        grid[0][0] = 1;
+        q.add(new int[]{0, 0, 1});
+
         while(!q.isEmpty()){
-            int size = q.size();
-            steps++;
-            while(size-- > 0){
-                int[] cell = q.poll();
-                int row = cell[0], col = cell[1];
-                if(row == grid.length - 1 && col == grid[0].length - 1 && grid[row][col] == 0) return steps;
-                for(int[] dir : dirs){
-                    int newRow = row + dir[0], newCol = col + dir[1];
-                    if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length 
-                        && !visited[newRow][newCol] && grid[newRow][newCol] == 0){
-                            visited[newRow][newCol] = true;
-                            q.add(new int[]{newRow, newCol});
-                        }
+            int[] entry = q.poll();
+            int row = entry[0], col = entry[1], steps = entry[2];
+
+            if(row == grid.length - 1 && col == grid[0].length - 1) return steps;
+
+            for(int[] dir : dirs){
+                int newRow = row + dir[0], newCol = col + dir[1];
+                if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length && grid[newRow][newCol] == 0){
+                    grid[newRow][newCol] = 1;
+                    q.add(new int[]{newRow, newCol, steps + 1});
                 }
             }
         }
