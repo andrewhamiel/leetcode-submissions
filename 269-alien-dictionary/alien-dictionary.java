c0b12b7604a249a9edd9ae1260cb330492413c14
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-16 17:15:22 -0600

    Time: 5 ms (36.97%) | Memory: 41.8 MB (6.72%) - LeetSync

--- 269-alien-dictionary/alien-dictionary.java
+++ 269-alien-dictionary/alien-dictionary.java
@@ -1,44 +1,44 @@
 class Solution {
     public String alienOrder(String[] words) {
-        //Step 0: Build frequencies and adjacency graph
+        //Step 0: Build adjacency graph
         Map<Character, List<Character>> adj = new HashMap<>();
-        Map<Character, Integer> freq = new HashMap<>();
+        Map<Character, Integer> deps = new HashMap<>();
         for(String word : words){
-            for(Character c : word.toCharArray()){
-                adj.put(c, new ArrayList<>());
-                freq.put(c, 0);
+            for(char c : word.toCharArray()){
+                adj.putIfAbsent(c, new ArrayList<>());
+                deps.putIfAbsent(c, 0);
             }
         }
-        //Step 1: Find relations between characters, validate correctness
+
+        //Step 1: Find adjacencies and dependencies
         for(int i = 0; i < words.length - 1; i++){
-            String word1 = words[i], word2 = words[i+1];
+            String word1 = words[i], word2 = words[i + 1];
             for(int j = 0; j < Math.min(word1.length(), word2.length()); j++){
-                //check to se word2 not substring of word1
+                //Make sure word2 not substring of word1
                 if(word1.length() > word2.length() && word1.startsWith(word2)) return "";
                 if(word1.charAt(j) != word2.charAt(j)){
                     adj.get(word1.charAt(j)).add(word2.charAt(j));
-                    freq.put(word2.charAt(j), freq.get(word2.charAt(j)) + 1);
-                    break;   
+                    deps.put(word2.charAt(j), deps.get(word2.charAt(j)) + 1);
+                    break;
                 }
             }
         }
         //Step 2: BFS
-        StringBuilder sb = new StringBuilder();
         Queue<Character> q = new LinkedList<>();
-        for(Character key : freq.keySet()) if(freq.get(key) == 0) q.add(key);
+        //Seed with characters that do not have dependencies
+        for(char c : deps.keySet()) if(deps.get(c) == 0) q.add(c);
+        StringBuilder sb = new StringBuilder();
+
         while(!q.isEmpty()){
-            Character curr = q.poll();
+            char curr = q.poll();
             sb.append(curr);
-            List<Character> rels = adj.get(curr);
-            for(Character nextChar : rels){
-                freq.put(nextChar, freq.get(nextChar) - 1);
-                if(freq.get(nextChar) == 0) q.add(nextChar);
+            for(Character nextChar : adj.get(curr)){
+                deps.put(nextChar, deps.get(nextChar) - 1);
+                if(deps.get(nextChar) == 0) q.add(nextChar);
             }
         }
-        //Step 3: Validate each character in returning order
-        if(sb.length() != freq.size()) return "";
+        //Step 3: Validate sb length same as number of characters
+        if(sb.length() != deps.size()) return "";
         return sb.toString();
-
-        
     }
 }
