0c79260c499e9d8fb0e6accf5cf13e99d5cb71c3
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-17 09:19:37 -0600

    Time: 0 ms (100.00%) | Memory: 43.1 MB (39.27%) - LeetSync

--- 1665-diameter-of-n-ary-tree/diameter-of-n-ary-tree.java
+++ 1665-diameter-of-n-ary-tree/diameter-of-n-ary-tree.java
@@ -1,3 +1,26 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    
+    public Node() {
+        children = new ArrayList<Node>();
+    }
+    
+    public Node(int _val) {
+        val = _val;
+        children = new ArrayList<Node>();
+    }
+    
+    public Node(int _val,ArrayList<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
 class Solution {
     private int diameter = 0;
 
@@ -6,29 +29,23 @@ class Solution {
         return diameter;
     }
 
-    /**
-     * return the height of the node
-     */
-    private int height(Node node) {
-        if (node.children.size() == 0) return 0;
+    private int height(Node root){
+        if(root == null) return 0;
 
-        // select the top two largest heights
         int maxHeight1 = 0, maxHeight2 = 0;
-        for (Node child : node.children) {
+        for(Node child : root.children){
             int parentHeight = height(child) + 1;
-            //1. Determine if parentHeight top 2
-            if (parentHeight > maxHeight1) {
+
+            //determine if parent height top 2
+            if(parentHeight > maxHeight1){
                 maxHeight2 = maxHeight1;
                 maxHeight1 = parentHeight;
-            } else if (parentHeight > maxHeight2) maxHeight2 = parentHeight;
+            }else if(parentHeight > maxHeight2) maxHeight2 = parentHeight;
 
-            // calculate the distance between the two farthest leaves nodes.
+            //determine longest path
             int distance = maxHeight1 + maxHeight2;
             diameter = Math.max(diameter, distance);
         }
-
         return maxHeight1;
     }
-
-    
 }
