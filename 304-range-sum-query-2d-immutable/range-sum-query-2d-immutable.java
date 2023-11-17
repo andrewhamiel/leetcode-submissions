20a9a9fd07462c23292ec98847683f115b21e4fd
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-17 08:59:30 -0600

    Time: 93 ms (93.84%) | Memory: 70.7 MB (5.52%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,26 +1,23 @@
 class NumMatrix {
     int[][] mat;
-    //Time: O(mn) pre-computation, O(1) query
-    //Space: O(mn)
+
     public NumMatrix(int[][] matrix) {
+        //cumulative region sum
         mat = new int[matrix.length + 1][matrix[0].length + 1];
-        int[][] tmp = mat;
-        //cumulative region sum with respect to origin at (0, 0)
         for(int row = 0; row < matrix.length; row++){
             int rowSum = 0;
             for(int col = 0; col < matrix[0].length; col++){
                 rowSum+= matrix[row][col];
                 int colSum = mat[row][col + 1];
-                mat[row + 1][col + 1] = colSum + rowSum;
+                mat[row + 1][col + 1] = rowSum + colSum;
             }
         }
     }
     
-    //sum(oa) covered twice by both sum(ob) and sum(oc)
     //principle of inclusion-exclusion
-    //sum(abcd) = sum(od) - sum(ob) - sum(oc) + sum(oa)
+    //sum(oa) covered twice in sum(ob) and sum(oc)
+    //sum(abcd) = sum(ad) - sum(ob) - sum(oc) + sum(oa)
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int[][] tmp = mat;
         return mat[row2 + 1][col2 + 1] - mat[row1][col2 + 1] - mat[row2 + 1][col1] + mat[row1][col1];
     }
 }
