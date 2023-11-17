1ce21564149603be944fdf46385c4884681984c1
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-17 09:32:26 -0600

    Time: 0 ms (100.00%) | Memory: 42.8 MB (81.19%) - LeetSync

--- 1665-diameter-of-n-ary-tree/diameter-of-n-ary-tree.java
+++ 1665-diameter-of-n-ary-tree/diameter-of-n-ary-tree.java
@@ -1,36 +1,51 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    
+    public Node() {
+        children = new ArrayList<Node>();
+    }
+    
+    public Node(int _val) {
+        val = _val;
+        children = new ArrayList<Node>();
+    }
+    
+    public Node(int _val,ArrayList<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
 class Solution {
     private int diameter = 0;
 
     public int diameter(Node root) {
-        this.diameter = 0;
-        maxDepth(root, 0);
+        depth(root, 0);
         return diameter;
     }
 
-    /**
-     * return the maximum depth of leaves nodes descending from the given node
-     */
-    private int maxDepth(Node node, int currDepth) {
-        if (node.children.size() == 0) return currDepth;
+    private int depth(Node root, int currDepth){
+        if(root == null) return currDepth;
 
-        // select the top two largest depths
         int maxDepth1 = currDepth, maxDepth2 = 0;
-        for (Node child : node.children) {
-            int depth = maxDepth(child, currDepth + 1);
-            //determine if top 2 depths
-            if (depth > maxDepth1) {
-                maxDepth2 = maxDepth1;
-                maxDepth1 = depth;
-            } else if (depth > maxDepth2) {
-                maxDepth2 = depth;
-            }
-            // calculate the distance between the two farthest leaves nodes.
-            int distance = maxDepth1 + maxDepth2 - 2 * currDepth;
-            this.diameter = Math.max(this.diameter, distance);
-        }
+        for(Node child : root.children){
+            int newDepth = depth(child, currDepth + 1);
 
+            //see if new depth in top two
+            if(newDepth > maxDepth1){
+                maxDepth2 = maxDepth1;
+                maxDepth1 = newDepth;
+            }else if(newDepth > maxDepth2) maxDepth2 = newDepth;
+
+            //calculate longest path
+            int distance = maxDepth1 + maxDepth2 - 2 * currDepth;
+            diameter = Math.max(diameter, distance);
+        }
         return maxDepth1;
     }
-
-    
 }
