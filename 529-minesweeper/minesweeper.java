d20d8be7ba1f262b2d1d3d8af43b38d9e6e5ad75
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-17 11:08:11 -0600

    Time: 2 ms (40.68%) | Memory: 44.2 MB (19.77%) - LeetSync

--- 529-minesweeper/minesweeper.java
+++ 529-minesweeper/minesweeper.java
@@ -4,23 +4,25 @@ class Solution {
     public char[][] updateBoard(char[][] board, int[] click) {
         Queue<int[]> q = new LinkedList<>();
         q.add(click);
+
         while(!q.isEmpty()){
             int[] cell = q.poll();
             int row = cell[0], col = cell[1];
             if(board[row][col] == 'M') board[row][col] = 'X';
             else{
+                board[row][col] = 'B';
                 int mines = getMines(row, col, board);
                 if(mines > 0) board[row][col] = (char)(mines + '0');
                 else{
-                    board[row][col] = 'B';
                     for(int[] dir : dirs){
                         int newRow = row + dir[0], newCol = col + dir[1];
-                        if(newRow >= 0 && newRow < board.length && newCol >= 0 && newCol < board[0].length && board[newRow][newCol] == 'E'){
+                        if(newRow >= 0 && newRow < board.length && newCol >= 0 && newCol < board[0].length && (board[newRow][newCol] == 'E')){
                             board[newRow][newCol] = 'B';
                             q.add(new int[]{newRow, newCol});
                         }
                     }
                 }
+                
             }
         }
         return board;
