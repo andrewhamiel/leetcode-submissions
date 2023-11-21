a3a91ca145afef1d5427749f74cac2cf5f668b1c
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-21 15:45:50 -0600

    Time: 1 ms (38.79%) | Memory: 39.5 MB (26.65%) - LeetSync

--- 670-maximum-swap/maximum-swap.java
+++ 670-maximum-swap/maximum-swap.java
@@ -1,21 +1,18 @@
 class Solution {
     public int maximumSwap(int num) {
-        char[] arr = String.valueOf(num).toCharArray();
-
+        char[] nums = String.valueOf(num).toCharArray();
         int[] rightmost = new int[10];
-        for(int i = 0; i < arr.length; i++) rightmost[arr[i] - '0'] = i;
+        for(int i = 0; i < nums.length; i++) rightmost[nums[i] - '0'] = i;
 
-        for(int i = 0; i < arr.length; i++){
-            int currDigit = arr[i] - '0';
-            for(int j = 9; j > currDigit; j--){
-                if(rightmost[j] > i){
-                    swap(i, rightmost[j], arr);
-                    return Integer.parseInt(new String(arr));
+        for(int i = 0; i < nums.length; i++){
+            for(char c = '9'; c > nums[i]; c--){
+                if(rightmost[c - '0'] > i){
+                    swap(i, rightmost[c - '0'], nums);
+                    return Integer.parseInt(new String(nums));
                 }
-                
             }
         }
-        return num;
+        return Integer.parseInt(new String(nums));
     }
 
     private void swap(int i, int j, char[] nums){
