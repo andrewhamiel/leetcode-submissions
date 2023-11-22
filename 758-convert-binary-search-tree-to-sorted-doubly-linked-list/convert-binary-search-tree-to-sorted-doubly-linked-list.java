0c49d3ef1b842d9abafaae65a07094c1f4759517
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-22 06:43:02 -0600

    Time: 1 ms (17.28%) | Memory: 41 MB (28.17%) - LeetSync

--- 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.java
+++ 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.java
@@ -1,45 +1,50 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val,Node _left,Node _right) {
-        val = _val;
-        left = _left;
-        right = _right;
-    }
-};
-*/
-
 class Solution {
-    Node first = null, last = null;
-
     public Node treeToDoublyList(Node root) {
-        if(root == null) return root;
-        helper(root);
-        first.left = last;
-        last.right = first;
-        return first;
-    }
-
-    private void helper(Node root){
-        if(root == null) return;
-        
-        helper(root.left);
-
-        if(last != null){
-            last.right = root;
-            root.left = last;
-        }else first = root;
-        last = root;
-        helper(root.right);
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        Node cur = root, head = null, tail = null, tmp;
+        // morris' algo
+        while (cur != null) {
+            head = cur.val < min ? cur : head;
+            min = Math.min(min, cur.val);
+            tail = cur.val > max ? cur : tail;
+            max = Math.max(max, cur.val);
+            // right most on left subtree
+            Node rlmost = cur.left;
+            while (rlmost != null
+                   && rlmost.right != null && rlmost != cur) {
+                rlmost = rlmost.right;
+            }
+            tmp = cur.left;
+            if (rlmost != null && rlmost != cur) {
+                // link rlmost node and cur
+				// if still can go left then go
+                rlmost.right = cur;
+                cur.left = rlmost;
+                cur = tmp;
+                continue;
+            }
+            // no left substree or linked before, go right
+			// find the left most node of right subtree
+            Node lrmost = cur.right;
+            while (lrmost != null
+                   && lrmost.left != null && lrmost.left != cur) {
+                // note that if lrmost.left (lrmost.pre) is cur
+                // which means cur and lrmost are already in linked list
+                // then just skip
+                lrmost = lrmost.left;
+            }
+            tmp = cur.right;
+            if (lrmost != null && lrmost.left != cur) {
+                lrmost.left = cur;
+                cur.right = lrmost;
+            }
+            cur = tmp;
+        }
+        // cyclic linking
+        if (head != null) {
+            tail.right = head;
+            head.left = tail;
+        }
+        return head;
     }
 }
