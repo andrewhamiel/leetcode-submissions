25e77e7f8f62531b9b6d85f93c0f4c86db461ea1
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-22 10:00:03 -0600

    Time: 0 ms (100.00%) | Memory: 40.6 MB (12.85%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -1,47 +1,34 @@
 class Solution {
   public int sumNumbers(TreeNode root) {
     int rootToLeaf = 0, currNumber = 0;
-    int steps;
-    TreeNode predecessor;
 
     while (root != null) {
-      // If there is a left child,
-      // then compute the predecessor.
-      // If there is no link predecessor.right = root --> set it.
-      // If there is a link predecessor.right = root --> break it.
       if (root.left != null) {
-        predecessor = root.left;
-        steps = 1;
+        TreeNode predecessor = root.left;
+        int steps = 1;
         while (predecessor.right != null && predecessor.right != root) {
           predecessor = predecessor.right;
-          ++steps;
+          steps++;
         }
-
-        // Set link predecessor.right = root
-        // and go to explore the left subtree
+        // Set link predecessor.right = root and go to explore the left subtree
         if (predecessor.right == null) {
           currNumber = currNumber * 10 + root.val;
           predecessor.right = root;
           root = root.left;
-        }
-        // Break the link predecessor.right = root
-        // Once the link is broken,
-        // it's time to change subtree and go to the right
-        else {
+        } else {
+          // Break the link predecessor.right = root
+          // Once the link is broken, it's time to change subtree and go to the right
           // If you're on the leaf, update the sum
           if (predecessor.left == null) rootToLeaf += currNumber;
           // This part of tree is explored, backtrack
-          for(int i = 0; i < steps; ++i) {
+          for(int i = 0; i < steps; i++) {
             currNumber /= 10;
           }
           predecessor.right = null;
           root = root.right;
         }
-      }
-
-      // If there is no left child
-      // then just go right.
-      else {
+      } else {
+        // If there is no left child, then just go right.
         currNumber = currNumber * 10 + root.val;
         // if you're on the leaf, update the sum
         if (root.right == null) rootToLeaf += currNumber;
@@ -49,11 +36,5 @@ class Solution {
       }
     }
     return rootToLeaf;
-  }
-
-  private TreeNode predecessor(TreeNode root){
-      TreeNode rightmost = root.left;
-      while(rightmost.right != null) rightmost = rightmost.right;
-      return rightmost;
   }
 }
