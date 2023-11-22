58dd4858b78e54b39d662ed7a371bca504fd0535
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-22 16:58:06 -0600

    Time: 0 ms (100.00%) | Memory: 39.9 MB (50.68%) - LeetSync

--- 1038-number-of-squareful-arrays/number-of-squareful-arrays.java
+++ 1038-number-of-squareful-arrays/number-of-squareful-arrays.java
@@ -1,23 +1,24 @@
 class Solution {
     public int numSquarefulPerms(int[] nums) {
         Arrays.sort(nums);
-        int[] result = new int[]{0};
-        dfs(nums, 0, result);
-        return result[0];
+        return backtrack(0, nums);
     }
 
-    private void dfs(int[] nums, int level, int[] count){
-        if(level == nums.length) count[0]++;
+    private int backtrack(int level, int[] nums){
+        if(level == nums.length) return 1;
+
+        int count = 0;
 
         for(int i = level; i < nums.length; i++){
-            if(level != 0 && nums[i] == nums[level] && i != level) continue;
+            if(i != level && nums[i] == nums[level]) continue;
             if(level == 0 || isSquareful(nums[level - 1] + nums[i])){
                 swap(i, level, nums);
-                dfs(nums, level + 1, count);
+                count+= backtrack(level + 1, nums);
                 swap(i, level, nums);
-                while(i + 1 < nums.length && nums[i] == nums[i + 1]) i++; //move past duplicates
-            } 
+                while(i + 1 < nums.length && nums[i] == nums[i + 1]) i++; //filter out duplicates
+            }
         }
+        return count;
     }
 
     private void swap(int i, int j, int[] nums){
