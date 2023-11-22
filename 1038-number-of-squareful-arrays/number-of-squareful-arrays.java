7daa7bdad223556c8c15704fbd695ef8c49ceab1
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-22 17:04:42 -0600

    Time: 0 ms (100.00%) | Memory: 40.4 MB (19.73%) - LeetSync

--- 1038-number-of-squareful-arrays/number-of-squareful-arrays.java
+++ 1038-number-of-squareful-arrays/number-of-squareful-arrays.java
@@ -9,12 +9,12 @@ class Solution {
 
         int count = 0;
         for(int i = level; i < nums.length; i++){
-            if(i != level && nums[i] == nums[level]) continue;
+            if(i != level && nums[i] == nums[level]) continue; //keep permutations unique
             if(level == 0 || isSquareful(nums[level - 1] + nums[i])){
                 swap(i, level, nums);
                 count+= backtrack(level + 1, nums);
                 swap(i, level, nums);
-                while(i + 1 < nums.length && nums[i] == nums[i + 1]) i++;
+                while(i + 1 < nums.length && nums[i] == nums[i + 1]) i++; //remove duplicates
             }
         }
         return count;
