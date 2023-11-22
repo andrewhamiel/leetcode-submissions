c00c62e5ec1f56ae0bd7bb8328e2efd505f80499
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-22 06:08:09 -0600

    Time: 27 ms (75.85%) | Memory: 68.8 MB (64.19%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.java
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.java
@@ -3,18 +3,16 @@ class Solution {
         Queue<Pair<Integer, Integer>> q = new LinkedList<>();
         q.add(new Pair<>(0, 0));
         List<Integer> list = new ArrayList<>();
-
         while(!q.isEmpty()){
-            Pair<Integer, Integer> p  = q.poll();
+            Pair<Integer, Integer> p = q.poll();
             int row = p.getKey(), col = p.getValue();
             list.add(nums.get(row).get(col));
 
             if(col == 0 && row + 1 < nums.size()) q.add(new Pair<>(row + 1, col));
-            if(col + 1 < nums.get(row).size()) q.add(new Pair<>(row, col + 1));
+            if(row < nums.size() && col < nums.get(row).size() - 1) q.add(new Pair<>(row, col + 1));
         }
-
         int[] result = new int[list.size()];
-        for(int i = 0; i < list.size(); i++) result[i] = list.get(i);
+        for(int i = 0; i < result.length; i++) result[i] = list.get(i);
         return result;
     }
 }
