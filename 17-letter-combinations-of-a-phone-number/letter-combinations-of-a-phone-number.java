d59cdf5a00b09a87b1457f953ded763aebb49ca3
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-23 13:43:50 -0600

    Time: 1 ms (80.54%) | Memory: 41.4 MB (27.71%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,38 +1,35 @@
 class Solution {
-    List<String> combs = new ArrayList<>();
+    Map<Character, List<Character>> map = new HashMap<>();
+    List<String> result = new ArrayList<>();
 
     public List<String> letterCombinations(String digits) {
-        if(digits.length() == 0) return combs;
-        Map<Character, List<Character>> map = getChars();
-        helper(0, digits, new StringBuilder(), map);
-        return new ArrayList<>(combs);
+        if(digits == null || digits.length() == 0) return result;
+        populateMap();
+        backtrack(0, digits, new StringBuilder());
+        return result;
     }
 
-    private void helper(int ind, String digits, StringBuilder result, Map<Character, List<Character>> map){
-        if(result.length() == digits.length()){
-            combs.add(result.toString());
+    private void backtrack(int ind, String digits, StringBuilder sb){
+        if(ind == digits.length()){
+            result.add(sb.toString());
             return;
         }
 
-        char digit = digits.charAt(ind);
-        List<Character> list = map.get(digit);
-        int currLength = result.length();
-        for(int i = 0; i < list.size(); i++){
-            result.append(list.get(i));
-            helper(ind + 1, digits, result, map);
-            result.deleteCharAt(result.length() - 1);
+        List<Character> list = map.get(digits.charAt(ind));
+        for(char c : list){
+            sb.append(c);
+            backtrack(ind + 1, digits, sb);
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
 
-    private Map<Character, List<Character>> getChars(){
-        Map<Character, List<Character>> map = new HashMap<>();
+    private void populateMap(){
         char c = 'a';
-        for(char i = '2'; i <= '9'; i++){
+        for(char digit = '2'; digit <= '9'; digit++){
             List<Character> letters = new ArrayList<>();
-            for(int j = 0; j < 3; j++) letters.add(c++);
-            if(i == '7' || i == '9') letters.add(c++);
-            map.put(i, letters);
+            for(int i = 0; i < 3; i++) letters.add(c++);
+            if(digit == '7' || digit == '9') letters.add(c++);
+            map.put(digit, letters);
         }
-        return map;
     }
 }
