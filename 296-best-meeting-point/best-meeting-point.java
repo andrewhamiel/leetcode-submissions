dc3ff711c4abf909ce131750437c05a8c463f763
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-23 15:16:16 -0600

    Time: 8 ms (63.35%) | Memory: 45.3 MB (39.22%) - LeetSync

--- 296-best-meeting-point/best-meeting-point.java
+++ 296-best-meeting-point/best-meeting-point.java
@@ -1,39 +1,34 @@
 class Solution {
     public int minTotalDistance(int[][] grid) {
-    List<Integer> rows = collectRows(grid);
-    List<Integer> cols = collectCols(grid);
-    return get1DDistance(rows) + get1DDistance(cols);
-}
+        List<Integer> rows = getRows(grid);
+        List<Integer> cols = getCols(grid);
 
-private List<Integer> collectRows(int[][] grid) {
-    List<Integer> rows = new ArrayList<>();
-    for (int row = 0; row < grid.length; row++) {
-        for (int col = 0; col < grid[0].length; col++) {
-            if (grid[row][col] == 1) {
-                rows.add(row);
+        return get1DDistance(rows) + get1DDistance(cols);
+    }
+
+    private List<Integer> getRows(int[][] grid){
+        List<Integer> rows = new ArrayList<>();
+        for(int row = 0; row < grid.length; row++){
+            for(int col = 0; col < grid[0].length; col++){
+                if(grid[row][col] == 1) rows.add(row);
             }
         }
+        return rows;
     }
-    return rows;
-}
 
-private List<Integer> collectCols(int[][] grid) {
-    List<Integer> cols = new ArrayList<>();
-    for (int col = 0; col < grid[0].length; col++) {
-        for (int row = 0; row < grid.length; row++) {
-            if (grid[row][col] == 1) {
-                cols.add(col);
+    private List<Integer> getCols(int[][] grid){
+        List<Integer> cols = new ArrayList<>();
+        for(int col = 0; col < grid[0].length; col++){
+            for(int row = 0; row < grid.length; row++){
+                if(grid[row][col] == 1) cols.add(col);
             }
         }
+        return cols;
     }
-    return cols;
-}
 
-private int get1DDistance(List<Integer> points){
-    int left = 0, right = points.size() - 1, distance = 0;
-    while(left < right){
-        distance+= points.get(right--) - points.get(left++);
+    private int get1DDistance(List<Integer> points){
+        int left = 0, right = points.size() - 1, distance = 0;
+        while(left < right) distance+= points.get(right--) - points.get(left++);
+        return distance;
     }
-    return distance;
-}
 }
