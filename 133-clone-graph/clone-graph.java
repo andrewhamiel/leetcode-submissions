44cb98f73bc261ac2dbe029993e7ac45a1d6f6f8
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-23 14:11:25 -0600

    Time: 28 ms (24.99%) | Memory: 41.7 MB (72.17%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -3,36 +3,40 @@
 class Node {
     public int val;
     public List<Node> neighbors;
-
-    public Node() {}
-
-    public Node(int _val,List<Node> _neighbors) {
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
         val = _val;
         neighbors = _neighbors;
     }
-};
+}
 */
+
 class Solution {
     public Node cloneGraph(Node node) {
-        if (node == null) return node;
-  
-        Map<Node, Node> visited = new HashMap();
-        visited.put(node, new Node(node.val, new ArrayList()));
-        LinkedList<Node> queue = new LinkedList<Node> ();
-        queue.add(node);
+        if(node == null) return node;
 
-        while (!queue.isEmpty()) {
-            Node curr = queue.remove();
-            for (Node neighbor: curr.neighbors) {
-                if (!visited.containsKey(neighbor)) {
-                    visited.put(neighbor, new Node(neighbor.val, new ArrayList()));
-                    queue.add(neighbor);
+        Queue<Node> q = new LinkedList<>();
+        q.add(node);
+        Map<Node, Node> visited = new HashMap<>();
+        visited.put(node, new Node(node.val));
+
+        while(!q.isEmpty()){
+            Node curr = q.poll();
+            for(Node neighbor : curr.neighbors){
+                if(!visited.containsKey(neighbor)){
+                    visited.put(neighbor, new Node(neighbor.val));
+                    q.add(neighbor);
                 }
-                // Add the clone of the neighbor to the neighbors of the clone node 
                 visited.get(curr).neighbors.add(visited.get(neighbor));
             }
         }
-
         return visited.get(node);
     }
 }
