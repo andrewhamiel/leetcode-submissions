4b7ecf51872a568e9d3030629dca0547f61cde3e
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-23 07:38:46 -0600

    Time: 68 ms (14.30%) | Memory: 51.3 MB (48.26%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,45 +1,41 @@
 class Solution {
-    private int[][] points;
-
     public int[][] kClosest(int[][] points, int k) {
-        this.points = points;
-        quickselect(0, points.length - 1, k);
-
+        quickselect(0, points.length - 1, points, k);
         int[][] result = new int[k][2];
         for(int i = 0; i < result.length; i++) result[i] = points[i];
         return result;
     }
 
-    private void quickselect(int left, int right, int k){
+    private void quickselect(int left, int right, int[][] points, int k){
         if(left >= right) return;
 
         int pivotIndex = new Random().nextInt(right - left) + left;
-        pivotIndex = partition(left, right, pivotIndex);
+        pivotIndex = partition(left, right, points, pivotIndex);
 
         if(pivotIndex == k) return;
-        else if(pivotIndex < k) quickselect(pivotIndex + 1, right, k);
-        else quickselect(left, pivotIndex - 1, k);
+        else if(pivotIndex < k) quickselect(pivotIndex + 1, right, points, k);
+        else quickselect(left, pivotIndex - 1, points, k);
     }
 
-    private int partition(int left, int right, int pivotIndex){
-        int pivotDistance = getDistance(pivotIndex);
-        swap(pivotIndex, right);
+    private int partition(int left, int right, int[][] points, int pivotIndex){
+        int pivotDistance = getDistance(points, pivotIndex);
+        swap(pivotIndex, right, points);
         int swapIndex = left;
 
         for(int i = left; i <= right; i++){
-            if(getDistance(i) < pivotDistance) swap(swapIndex++, i);
+            if(getDistance(points, i) < pivotDistance) swap(i, swapIndex++, points);
         }
-        swap(swapIndex, right);
+        swap(swapIndex, right, points);
         return swapIndex;
     }
 
-    private void swap(int i, int j){
+    private void swap(int i, int j, int[][] points){
         int[] tmp = points[i];
         points[i] = points[j];
         points[j] = tmp;
     }
 
-    private int getDistance(int pivotIndex){
-        return (points[pivotIndex][0] * points[pivotIndex][0]) + (points[pivotIndex][1] * points[pivotIndex][1]);
+    private int getDistance(int[][] points, int p){
+        return (points[p][0] * points[p][0]) + (points[p][1] * points[p][1]);
     }
 }
