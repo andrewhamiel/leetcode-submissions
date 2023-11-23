979032a57d34b6244e6169e38a6b182ee9fc729e
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-23 07:48:40 -0600

    Time: 0 ms (100.00%) | Memory: 43 MB (75.65%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -16,7 +16,8 @@ class Node {
 class Solution {
     public Node copyRandomList(Node head) {
         if(head == null) return head;
-        
+
+        //1. Create copies
         Node curr = head;
         while(curr != null){
             Node nextNode = new Node(curr.val);
@@ -25,19 +26,22 @@ class Solution {
             curr = nextNode.next;
         }
 
+        //2. Interweave
         curr = head;
         while(curr != null){
             curr.next.random = curr.random != null ? curr.random.next : null;
             curr = curr.next.next;
         }
 
-        Node oldPtr = head, newPtr = head.next, newHead = newPtr;
+        //3. Outerweave
+        Node oldPtr = head, newPtr = head.next, newHead = head.next;
         while(oldPtr != null){
             oldPtr.next = oldPtr.next.next;
             newPtr.next = oldPtr.next != null ? newPtr.next.next : null;
             oldPtr = oldPtr.next;
             newPtr = newPtr.next;
         }
+
         return newHead;
     }
 }
