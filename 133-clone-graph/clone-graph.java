82becf41c3b4964d1f251ee57ad1ad9fb745b199
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-23 14:08:37 -0600

    Time: 30 ms (7.08%) | Memory: 42.1 MB (25.82%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -3,34 +3,36 @@
 class Node {
     public int val;
     public List<Node> neighbors;
-    public Node() {
-        val = 0;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-    public Node(int _val, ArrayList<Node> _neighbors) {
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _neighbors) {
         val = _val;
         neighbors = _neighbors;
     }
-}
+};
 */
-
 class Solution {
-    Map<Node, Node> visited = new HashMap<>();
-
     public Node cloneGraph(Node node) {
-        if(node == null) return node;
-        if(visited.containsKey(node)) return visited.get(node);
+        if (node == null) return node;
+  
+        Map<Node, Node> visited = new HashMap();
+        visited.put(node, new Node(node.val, new ArrayList()));
+        LinkedList<Node> queue = new LinkedList<Node> ();
+        queue.add(node);
 
-        Node newNode = new Node(node.val);
-        visited.put(node, newNode);
-
-        for(Node oldNeighbor : node.neighbors){
-            newNode.neighbors.add(cloneGraph(oldNeighbor));
+        while (!queue.isEmpty()) {
+            Node curr = queue.remove();
+            for (Node neighbor: curr.neighbors) {
+                if (!visited.containsKey(neighbor)) {
+                    visited.put(neighbor, new Node(neighbor.val, new ArrayList()));
+                    queue.add(neighbor);
+                }
+                // Add the clone of the neighbor to the neighbors of the clone node 
+                visited.get(curr).neighbors.add(visited.get(neighbor));
+            }
         }
-        return newNode;
+
+        return visited.get(node);
     }
 }
