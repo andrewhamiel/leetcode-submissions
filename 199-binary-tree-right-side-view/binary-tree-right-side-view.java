793467e0b42cb76a42478dbd590f5c53e2ea9e18
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-23 08:04:15 -0600

    Time: 1 ms (70.52%) | Memory: 41.4 MB (18.46%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,38 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        
-        int masH = 1, minH = 0;
-        TreeNode temp = null;
-        while(root!=null) {
-            if(root.right == null) {
-                //check
-                if(list.size()<masH) list.add(root.val);
-                masH++;
-                root = root.left;
-            }
-            else {
-                temp = root.right;
-                minH = 1;
-                while(temp.left!=null && temp.left!=root) {
-                    temp = temp.left;
-                    minH++;
+        List<Integer> result = new ArrayList<>();
+        int level = 0;
+
+        while(root != null){
+            if(root.right != null){
+                TreeNode successor = root.right;
+                int backDepth = 1;
+                while(successor.left != null  && successor.left != root){
+                    successor = successor.left;
+                    backDepth++;
                 }
-                if(temp.left == null) {
-                    //check
-                    if(list.size()<masH) list.add(root.val);
-                    temp.left = root;
-                    masH++;
-                    root = root.right;
-                }
-                else {
-                    temp.left = null;
-                    //subtract from height as we are climbing back up
-                    masH-=minH;
+
+                if(successor.left == null){
+                    //Unexplored
+                    if(result.size() == level) result.add(root.val);
+                    level++;
+                    successor.left = root;
+                    root = root.right; 
+                }else{
+                    //Already visited, time to unlink
+                    level-= backDepth;
+                    successor.left = null;
                     root = root.left;
                 }
+            }else{
+                //preorder visit
+                if(result.size() == level) result.add(root.val);
+                root = root.left;
+                level++;
             }
         }
-        return list;
+        return result;
     }
 }
