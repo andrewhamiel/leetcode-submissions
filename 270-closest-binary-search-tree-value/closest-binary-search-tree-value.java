6270fb9458cb4d9e7f24f1dec796dfdd3ce49135
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-24 12:57:54 -0600

    Time: 0 ms (100.00%) | Memory: 44.1 MB (32.64%) - LeetSync

--- 270-closest-binary-search-tree-value/closest-binary-search-tree-value.java
+++ 270-closest-binary-search-tree-value/closest-binary-search-tree-value.java
@@ -1,33 +1,15 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public int closestValue(TreeNode root, double target) {
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        int closest = root.val;
+  //Time: O(H) -> Height of tree
+  //Space: O(1)
+  public int closestValue(TreeNode root, double target) {
+    int closest = root.val;
+    while (root != null) {
+      int val = root.val;
+      if((Math.abs(val - target) < Math.abs(closest - target))
+        || (Math.abs(val - target) == Math.abs(closest - target) && val < closest)) closest = val;
 
-        while(!q.isEmpty()){
-          TreeNode curr = q.poll();
-          if(curr.val == target) return curr.val;
-          if(Math.abs(target - curr.val) < Math.abs(target - closest)||
-          (Math.abs(target - curr.val) == Math.abs(target - closest) && curr.val < closest)) closest = curr.val;
-
-          if(curr.left != null) q.add(curr.left);
-          if(curr.right != null) q.add(curr.right);
-        }
-        return closest;
+      root =  target < root.val ? root.left : root.right;
     }
+    return closest;
+  }
 }
