f25c3b9351af61d209e42690ee9fa383bc711bff
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-24 13:32:49 -0600

    Time: 5 ms (38.22%) | Memory: 41.8 MB (7.29%) - LeetSync

--- 269-alien-dictionary/alien-dictionary.java
+++ 269-alien-dictionary/alien-dictionary.java
@@ -1,43 +1,43 @@
 class Solution {
     public String alienOrder(String[] words) {
-        //Step 0: Build adjacency graph
+        //Step 0: Initialize adjacency graphs
         Map<Character, List<Character>> adj = new HashMap<>();
-        Map<Character, Integer> deps = new HashMap<>();
+        Map<Character, Integer> dependencies = new HashMap<>();
         for(String word : words){
             for(char c : word.toCharArray()){
                 adj.putIfAbsent(c, new ArrayList<>());
-                deps.putIfAbsent(c, 0);
+                dependencies.putIfAbsent(c, 0);
             }
         }
 
-        //Step 1: Connect adjacent edges
-        for(int wordInd = 0; wordInd < words.length - 1; wordInd++){
-            String word1 = words[wordInd], word2 = words[wordInd + 1];
-            for(int j = 0; j < Math.min(word1.length(), word2.length()); j++){
-                //Check to ensure not a substring
+        //Step 1: Connect components
+        for(int i = 0; i < words.length - 1; i++){
+            String word1 = words[i], word2 = words[i + 1];
+            for(int wordInd = 0; wordInd < Math.min(word1.length(), word2.length()); wordInd++){
+                //Ensure not a substring
                 if(word1.length() > word2.length() && word1.startsWith(word2)) return "";
-                if(word1.charAt(j) != word2.charAt(j)){
-                    adj.get(word1.charAt(j)).add(word2.charAt(j));
-                    deps.put(word2.charAt(j), deps.get(word2.charAt(j)) + 1);
+                if(word1.charAt(wordInd) != word2.charAt(wordInd)){
+                    adj.get(word1.charAt(wordInd)).add(word2.charAt(wordInd));
+                    dependencies.put(word2.charAt(wordInd), dependencies.get(word2.charAt(wordInd)) + 1);
                     break;
                 }
             }
         }
 
         //Step 2: BFS
-        Queue<Character> q = new LinkedList<>();
-        for(Character key : deps.keySet()) if(deps.get(key) == 0) q.add(key);
-        StringBuilder sb = new StringBuilder();
+        Queue<Character> noDeps = new LinkedList<>();
+        for(char c : dependencies.keySet()) if(dependencies.get(c) == 0) noDeps.add(c);
+        StringBuilder result = new StringBuilder();
 
-        while(!q.isEmpty()){
-            char curr = q.poll();
-            sb.append(curr);
-            for(Character nextChar : adj.get(curr)){
-                deps.put(nextChar, deps.get(nextChar) - 1);
-                if(deps.get(nextChar) == 0) q.add(nextChar);
+        while(!noDeps.isEmpty()){
+            char c = noDeps.poll();
+            result.append(c);
+            for(char dep : adj.get(c)){
+                dependencies.put(dep, dependencies.get(dep) - 1);
+                if(dependencies.get(dep) == 0) noDeps.add(dep);
             }
         }
-        //Make sure all characters added
-        return sb.length() != deps.size() ? "" : sb.toString();
+        //Step 3: Validate all characters collected
+        return result.length() == dependencies.size() ? result.toString() : "";
     }
 }
