cc9707704c4a0036d25e69bc80f10aa67bfb465a
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-24 10:49:23 -0600

    Time: 29 ms (41.57%) | Memory: 44.6 MB (38.89%) - LeetSync

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.java
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.java
@@ -14,22 +14,21 @@ class Solution {
         return lca(root, p, q);
     }
 
-    private Node findParent(Node node){
-        Node root = node;
+    private Node findParent(Node p){
+        Node root = p;
         while(root.parent != null) root = root.parent;
         return root;
     }
 
     private Node lca(Node root, Node p, Node q){
-        if(root == null) return null;
+        if(root == null) return root;
 
         if(root == p || root == q) return root;
 
         Node left = lca(root.left, p, q);
         Node right = lca(root.right, p, q);
-
+        
         if(left != null && right != null) return root;
-
         return left != null ? left : right;
     }
 }
