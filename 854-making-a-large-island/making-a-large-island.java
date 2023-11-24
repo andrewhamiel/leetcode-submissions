276362013a5a9198ba00957f1dc971bdbc3158b9
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-24 07:42:48 -0600

    Time: 94 ms (71.59%) | Memory: 79.5 MB (29.12%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.java
+++ 854-making-a-large-island/making-a-large-island.java
@@ -1,15 +1,15 @@
 class Solution {
     private int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
-    private Map<Integer, Integer> islandSizes = new HashMap<>();
 
     public int largestIsland(int[][] grid) {
+        Map<Integer, Integer> islandSizes = new HashMap<>();
         int islandId = 2, maxIslandSize = 0;
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == 1){
-                    int size = getIslandSize(i, j, grid, islandId);
-                    islandSizes.put(islandId++, size);
-                    maxIslandSize = Math.max(maxIslandSize, size);
+                    int islandSize = getIslandSize(i, j, grid, islandId);
+                    islandSizes.put(islandId++, islandSize);
+                    maxIslandSize = Math.max(maxIslandSize, islandSize);
                 }
             }
         }
@@ -17,13 +17,17 @@ class Solution {
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == 0){
-                    Set<Integer> nums = new HashSet<>();
+                    Set<Integer> seen = new HashSet<>();
                     for(int[] dir : dirs){
                         int row = i + dir[0], col = j + dir[1];
-                        if(row >= 0 && row < grid.length && col >= 0 && col < grid[0].length && grid[row][col] != 0) nums.add(grid[row][col]);
+                        if(row >= 0 && row < grid.length && col >= 0 && col < grid[0].length && grid[row][col] != 0){
+                            seen.add(grid[row][col]);
+                        }
                     }
                     int sum = 1;
-                    for(int num : nums) sum+= islandSizes.get(num);
+                    for(int num : seen){
+                        sum+= islandSizes.get(num);
+                    }
                     maxIslandSize = Math.max(maxIslandSize, sum);
                 }
             }
@@ -34,10 +38,7 @@ class Solution {
     private int getIslandSize(int i, int j, int[][] grid, int islandId){
         if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 1) return 0;
         grid[i][j] = islandId;
-        int left = getIslandSize(i, j-1, grid, islandId);
-        int right = getIslandSize(i, j+1, grid, islandId);
-        int up = getIslandSize(i-1, j, grid, islandId);
-        int down = getIslandSize(i+1, j, grid, islandId);
-        return left + right + up + down + 1;
+        return 1 + getIslandSize(i, j-1, grid, islandId) + getIslandSize(i, j + 1, grid, islandId)
+            + getIslandSize(i - 1, j, grid, islandId) + getIslandSize(i + 1, j, grid, islandId);
     }
 }
