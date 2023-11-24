ada45040024d211ac9d3b38ed19a1918cc05dd88
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-24 07:59:14 -0600

    Time: 1 ms (100.00%) | Memory: 41.7 MB (55.76%) - LeetSync

--- 415-add-strings/add-strings.java
+++ 415-add-strings/add-strings.java
@@ -1,23 +1,28 @@
 class Solution {
     public String addStrings(String num1, String num2) {
-        StringBuilder res = new StringBuilder();
-
+        int ptr1 = num1.length() - 1, ptr2 = num2.length() - 1;
         int carry = 0;
-        int p1 = num1.length() - 1;
-        int p2 = num2.length() - 1;
-        while (p1 >= 0 || p2 >= 0) {
-            int x1 = p1 >= 0 ? num1.charAt(p1) - '0' : 0;
-            int x2 = p2 >= 0 ? num2.charAt(p2) - '0' : 0;
-            int value = (x1 + x2 + carry) % 10;
-            carry = (x1 + x2 + carry) / 10;
-            res.append(value);
-            p1--;
-            p2--;    
+        StringBuilder result = new StringBuilder();
+        while(ptr1 >= 0 || ptr2 >= 0){
+            int sum = 0;
+            if(ptr1 >= 0 && ptr2 >= 0){
+                int c1 = (int)(num1.charAt(ptr1--) - '0'), c2 = (int)(num2.charAt(ptr2--) - '0');
+                sum = c1 + c2 + carry;
+                result.append(sum % 10);
+                carry = sum/10;
+            }else if(ptr1 >= 0){
+                int c1 = (int)(num1.charAt(ptr1--) - '0');
+                sum = c1 + carry;
+                result.append(sum % 10);
+                carry = sum/10;
+            }else if(ptr2 >= 0){
+                int c2 = (int)(num2.charAt(ptr2--) - '0');
+                sum = c2 + carry;
+                result.append(sum % 10);
+                carry = sum/10;
+            }
         }
-        
-        if (carry != 0)
-            res.append(carry);
-        
-        return res.reverse().toString();
+        if(carry != 0) result.append(carry);
+        return result.reverse().toString();
     }
 }
