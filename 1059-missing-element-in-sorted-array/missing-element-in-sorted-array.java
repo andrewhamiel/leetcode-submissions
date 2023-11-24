03a6ec11e2ba22e6350aa1dd79b36d0a2a45e892
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-24 12:13:03 -0600

    Time: 0 ms (100.00%) | Memory: 46.6 MB (73.73%) - LeetSync

--- 1059-missing-element-in-sorted-array/missing-element-in-sorted-array.java
+++ 1059-missing-element-in-sorted-array/missing-element-in-sorted-array.java
@@ -1,19 +1,19 @@
 class Solution {
-    public int missingElement(int[] nums, int k) {
-        int left = 0, right = nums.length - 1;
-        while(left < right){
-            int mid = right - (right - left)/2;
-            //total number of integers within range [nums[0], nums[mid]] = nums[mid] - nums[0] + 1
-            //there are mid + 1 existing elements, so number of missing elements is:
-            // (nums[mid] - nums[0] + 1) - (mid + 1) = nums[mid] - nums[0] - mid
-            if(nums[mid] - nums[0] - mid < k) left = mid;
-            else right = mid - 1;
+    public int missingElement(int[] nums, int k) {  
+        //prevent overflow
+        int missingNumAmt = nums[nums.length - 1] - nums[0] - (nums.length-1);
+        if (missingNumAmt < k) return nums[nums.length - 1] + k - missingNumAmt;
+
+        int left = 0 , right = nums.length - 1;  
+        while(left<right) {
+            int mid = (left)+(right-left)/2;
+            int missing = nums[mid]-nums[0]-mid;
+            if(missing < k) left = mid+1;
+            else right = mid;
         }
-        //total number of elements in range [nums[0], nums[left]] = left + 1
-        //total number of integers in that range, because we have k missing elements = left + 1 + k
-        //size of range is answer - nums[0] + 1
-        //setting both equal, we have: answer - nums[0] + 1 = left + 1 + k
-        //answer = nums[0] + left + k
-        return nums[0] + left + k;
+        int integersInRange = left + 1 + k;
+        // kthMissing - nums[0] + 1 = integersInRange
+        //kthMissing = integersInRange + nums[0] + 1
+        return nums[0]+left+k-1;
     }
 }
