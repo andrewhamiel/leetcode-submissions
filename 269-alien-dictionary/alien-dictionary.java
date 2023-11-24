9372365595d2148ccdddef523cf4215bfeb21299
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-24 13:45:32 -0600

    Time: 4 ms (78.93%) | Memory: 41.4 MB (20.35%) - LeetSync

--- 269-alien-dictionary/alien-dictionary.java
+++ 269-alien-dictionary/alien-dictionary.java
@@ -1,6 +1,6 @@
 class Solution {
     public String alienOrder(String[] words) {
-        //Step 0: Build adjacency graphs
+        //Step 0: Build adjacency graph
         Map<Character, List<Character>> adj = new HashMap<>();
         Map<Character, Integer> deps = new HashMap<>();
         for(String word : words){
@@ -13,9 +13,9 @@ class Solution {
         //Step 1: Connect components
         for(int i = 0; i < words.length - 1; i++){
             String word1 = words[i], word2 = words[i + 1];
-            //Ensure not a substring
+            //Ensure this is not a substring
             if(word1.length() > word2.length() && word1.startsWith(word2)) return "";
-            for(int wordInd = 0; wordInd < Math.min(word1.length(), word2.length()); wordInd++){             
+            for(int wordInd = 0; wordInd < Math.min(word1.length(), word2.length()); wordInd++){
                 char c1 = word1.charAt(wordInd), c2 = word2.charAt(wordInd);
                 if(c1 != c2){
                     adj.get(c1).add(c2);
@@ -24,22 +24,23 @@ class Solution {
                 }
             }
         }
-        
-        //Step 2: BFS
+
+        //Step 3: BFS
         Queue<Character> q = new LinkedList<>();
-        for(char key : deps.keySet()) if(deps.get(key) == 0) q.add(key);
+        for(char c : deps.keySet()) if(deps.get(c) == 0) q.add(c);
         StringBuilder result = new StringBuilder();
 
         while(!q.isEmpty()){
             char curr = q.poll();
             result.append(curr);
+
             for(char nextChar : adj.get(curr)){
                 deps.put(nextChar, deps.get(nextChar) - 1);
                 if(deps.get(nextChar) == 0) q.add(nextChar);
             }
         }
 
-        //Step 4: Validate all characters present
+        //Step 4: Validate all characters have been collected
         return result.length() == deps.size() ? result.toString() : "";
     }
 }
