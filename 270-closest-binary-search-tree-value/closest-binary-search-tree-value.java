5efec15471e1448c9ef21fd63b6e6cc51f5c6b5c
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-24 13:09:52 -0600

    Time: 0 ms (100.00%) | Memory: 44.5 MB (8.73%) - LeetSync

--- 270-closest-binary-search-tree-value/closest-binary-search-tree-value.java
+++ 270-closest-binary-search-tree-value/closest-binary-search-tree-value.java
@@ -19,7 +19,7 @@ class Solution {
         while(root != null){
           if((Math.abs(target - root.val) < Math.abs(target - closest))
             || (Math.abs(target - root.val) == Math.abs(target - closest) && root.val < closest)) closest = root.val;
-          
+
           root = root.val < target ? root.right : root.left;
         }
         return closest;
