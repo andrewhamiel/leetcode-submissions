75b10101962e25b565955d31be442d08d8085d77
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-24 08:07:38 -0600

    Time: 25 ms (66.97%) | Memory: 61 MB (25.63%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,12 +1,10 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        int[] copied = Arrays.copyOf(arr, arr.length);
-        Arrays.sort(copied);
+        int[] result = Arrays.copyOf(arr, arr.length);
+        Arrays.sort(result);
         Map<Integer, Integer> rank = new HashMap<>();
-        for(int i = 0; i < copied.length; i++){
-            rank.putIfAbsent(copied[i], rank.size() + 1);
-        }
-        for(int i = 0; i < arr.length; i++) copied[i] = rank.get(arr[i]);
-        return copied;
+        for(int i = 0; i < result.length; i++) rank.putIfAbsent(result[i], rank.size() + 1);
+        for(int i = 0; i < result.length; i++) result[i] = rank.get(arr[i]);
+        return result;
     }
 }
