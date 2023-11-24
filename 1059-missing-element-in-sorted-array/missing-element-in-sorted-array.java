21eb80ae29cfc6d70a78470a1958fa0383bf2303
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-24 12:16:19 -0600

    Time: 0 ms (100.00%) | Memory: 46.5 MB (88.87%) - LeetSync

--- 1059-missing-element-in-sorted-array/missing-element-in-sorted-array.java
+++ 1059-missing-element-in-sorted-array/missing-element-in-sorted-array.java
@@ -1,19 +1,11 @@
 class Solution {
-    public int missingElement(int[] nums, int k) {  
-        //prevent overflow
-        int missingNumAmt = nums[nums.length - 1] - nums[0] - (nums.length-1);
-        if (missingNumAmt < k) return nums[nums.length - 1] + k - missingNumAmt;
-
-        int left = 0 , right = nums.length - 1;  
-        while(left<right) {
-            int mid = (left)+(right-left)/2;
-            int missing = nums[mid]-nums[0]-mid;
-            if(missing < k) left = mid+1;
-            else right = mid;
+    public int missingElement(int[] nums, int k) {
+        int left = 0, right = nums.length - 1;
+        while(left < right){
+            int mid = right - (right - left)/2;
+            if(nums[mid] - nums[0] - mid < k) left = mid;
+            else right = mid - 1;
         }
-        int integersInRange = left + 1 + k;
-        // kthMissing - nums[0] + 1 = integersInRange
-        //kthMissing = integersInRange + nums[0] + 1
-        return nums[0]+left+k-1;
+        return nums[0] + left + k;
     }
 }
