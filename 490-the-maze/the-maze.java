c868a2ee781df473419176e5b7d5ad557fe4710a
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-25 08:36:59 -0600

    Time: 6 ms (43.23%) | Memory: 45.3 MB (9.52%) - LeetSync

--- 490-the-maze/the-maze.java
+++ 490-the-maze/the-maze.java
@@ -13,7 +13,7 @@ class Solution {
             if(row == destination[0] && col == destination[1]) return true;
             for(int[] dir : dirs){
                 int newRow = row + dir[0], newCol = col + dir[1];
-                while(newRow >= 0 && newRow < maze.length && newCol >= 0 && newCol < maze[0].length 
+                while(newRow >= 0 && newRow < maze.length && newCol >= 0 && newCol < maze[0].length
                     && maze[newRow][newCol] == 0){
                         newRow+= dir[0];
                         newCol+= dir[1];
