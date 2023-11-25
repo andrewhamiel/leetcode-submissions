d3da73367ebfd324117bf8be3af220b7f0460536
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-25 08:34:04 -0600

    Time: 6 ms (43.23%) | Memory: 44.8 MB (39.49%) - LeetSync

--- 490-the-maze/the-maze.java
+++ 490-the-maze/the-maze.java
@@ -2,18 +2,18 @@ class Solution {
     private int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
 
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
-        boolean[][] visited = new boolean[maze.length][maze[0].length];
         Queue<int[]> q = new LinkedList<>();
         q.add(start);
+        boolean[][] visited = new boolean[maze.length][maze[0].length];
         visited[start[0]][start[1]] = true;
 
         while(!q.isEmpty()){
-            int[] curr = q.poll();
-            int row = curr[0], col = curr[1];
+            int[] cell = q.poll();
+            int row = cell[0], col = cell[1];
             if(row == destination[0] && col == destination[1]) return true;
             for(int[] dir : dirs){
                 int newRow = row + dir[0], newCol = col + dir[1];
-                while(newRow >= 0 && newRow < maze.length && newCol >= 0 && newCol < maze[0].length
+                while(newRow >= 0 && newRow < maze.length && newCol >= 0 && newCol < maze[0].length 
                     && maze[newRow][newCol] == 0){
                         newRow+= dir[0];
                         newCol+= dir[1];
