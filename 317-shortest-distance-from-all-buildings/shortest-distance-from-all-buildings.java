6bbb82f1052da9902f99a509bd5cb0b118bb6935
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-26 10:58:10 -0600

    Time: 93 ms (94.37%) | Memory: 43.9 MB (30.56%) - LeetSync

--- 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.java
+++ 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.java
@@ -2,8 +2,8 @@ class Solution {
     private int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
 
     public int shortestDistance(int[][] grid) {
-        int minDistance = Integer.MAX_VALUE, emptyLandValue = 0;
         int[][] total = new int[grid.length][grid[0].length];
+        int minDistance = Integer.MAX_VALUE, emptyLandValue = 0;
 
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
@@ -26,7 +26,7 @@ class Solution {
                 int row = cell[0], col = cell[1];
                 for(int[] dir : dirs){
                     int newRow = row + dir[0], newCol = col + dir[1];
-                    if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length 
+                    if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length
                         && grid[newRow][newCol] == emptyLandValue){
                             grid[newRow][newCol]--;
                             total[newRow][newCol]+= steps;
