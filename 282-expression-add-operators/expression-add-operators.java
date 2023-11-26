1c6547729f2846d9d70748ec645059c70d693689
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-26 16:42:22 -0600

    Time: 61 ms (86.68%) | Memory: 44.6 MB (44.33%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.java
+++ 282-expression-add-operators/expression-add-operators.java
@@ -1,56 +1,53 @@
 class Solution {
-    List<String> ans = new ArrayList<>();
-    String num;
+    List<String> result = new ArrayList<>();
+    String num = "";
     int target = 0;
 
     public List<String> addOperators(String num, int target) {
         this.num = num;
         this.target = target;
         helper(0, 0, 0, 0, new ArrayList<>());
-        return ans;
+        return result;
     }
 
-    private void helper(int index, long value, long prevOperand, long currOperand, List<String> expression){
-        //if index == nums.length and no curr ops to evaluate
-        if(index == num.length() ){
+    private void helper(int ind, long value, long prevOperand, long currOperand, List<String> expression){
+        //0. Exit condition
+        if(ind == num.length()){
             if(currOperand == 0 && value == target){
                 StringBuilder sb = new StringBuilder();
                 for(int i = 1; i < expression.size(); i++) sb.append(expression.get(i));
-                ans.add(sb.toString());
+                result.add(sb.toString());
             }
             return;
         }
 
-        //convert to correct digit
-        long correctDigit = (currOperand * 10) + Character.getNumericValue(num.charAt(index));
+        //1. convert to correct digit
+        currOperand*= 10;
+        currOperand+= Character.getNumericValue(num.charAt(ind));
 
-        //No Op. Need to make sure current operand not zero to avoid leading zero
-        if(correctDigit > 0){
-            helper(index + 1, value, prevOperand, correctDigit, expression);
-        }
-        
+        //2. No op. Make sure curr operand != 0 to avoid leading zeroes
+        if(currOperand > 0) helper(ind + 1, value, prevOperand, currOperand, expression);
 
-        //Addition
+        //3. Addition
         expression.add("+");
-        expression.add(Long.toString(correctDigit));
-        helper(index + 1, value + correctDigit, correctDigit, 0, expression);
+        expression.add(Long.toString(currOperand));
+        helper(ind + 1, value + currOperand, currOperand, 0, expression);
         expression.remove(expression.size() - 1);
         expression.remove(expression.size() - 1);
-        
+
         if(expression.size() > 0){
-            //Subtraction
+            //4. Subtraction
             expression.add("-");
-            expression.add(Long.toString(correctDigit));
-            helper(index + 1, value - correctDigit, -correctDigit, 0, expression);
+            expression.add(Long.toString(currOperand));
+            helper(ind + 1, value - currOperand, -currOperand, 0, expression);
             expression.remove(expression.size() - 1);
             expression.remove(expression.size() - 1);
-            //Multiplication
+            //5. Multiplication
             expression.add("*");
-            expression.add(Long.toString(correctDigit));
-            helper(index + 1, value - prevOperand + (correctDigit * prevOperand) ,correctDigit * prevOperand, 0, expression);
+            expression.add(Long.toString(currOperand));
+            helper(ind + 1, value - prevOperand + (prevOperand * currOperand), currOperand * prevOperand, 0, expression);
             expression.remove(expression.size() - 1);
             expression.remove(expression.size() - 1);
         }
-
     }
 }
