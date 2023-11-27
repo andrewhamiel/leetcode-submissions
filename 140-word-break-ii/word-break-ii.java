6e0f778bbd3587463799484b657b18aa07786ac8
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-26 23:07:10 -0600

    Time: 1 ms (97.68%) | Memory: 40.9 MB (48.15%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -1,52 +1,45 @@
 class Solution {
     class TrieNode {
-        boolean isWord;
-        Map<Character, TrieNode> children;
-        public TrieNode(){
-            children = new HashMap<>();
-            isWord = false;
-        }
+        Map<Character, TrieNode> children = new HashMap<>();
+        boolean isWord = false;
     }
+
     public List<String> wordBreak(String s, List<String> wordDict) {
-        //Construct TrieNode: O(m * k)
         TrieNode root = new TrieNode();
         for(String word : wordDict){
             TrieNode curr = root;
-            for(Character c : word.toCharArray()){
+            for(char c : word.toCharArray()){
                 curr.children.putIfAbsent(c, new TrieNode());
                 curr = curr.children.get(c);
             }
             curr.isWord = true;
         }
 
-        List<String> ans = new ArrayList<>();
-        dp(s, new StringBuilder(), root, ans);
-        return ans;
+        List<String> result = new ArrayList<>();
+        dp(s, new StringBuilder(), root, result);
+        return result;
     }
 
-    private void dp(String s, StringBuilder sb, TrieNode root, List<String> ans){
-        //memo condition
-        if(s.length() == 0 && !sb.isEmpty()) {
-            ans.add(sb.toString());
+    private void dp(String s, StringBuilder sb, TrieNode root, List<String> result){
+        if(s.length() == 0 && !sb.isEmpty()){
+            result.add(sb.toString());
             return;
         }
 
         TrieNode curr = root;
-
-        //check if is word
         for(int i = 0; i < s.length(); i++){
-            
-            if(!curr.children.containsKey(s.charAt(i))) return;
-            curr = curr.children.get(s.charAt(i));
-            sb.append(s.charAt(i));
+            char c = s.charAt(i);
+            if(!curr.children.containsKey(c)) return;
+
+            curr = curr.children.get(c);
+            sb.append(c);
             if(curr.isWord){
-                //two choices: use and don't use
+                //2 choices: use or don't use
                 //1. use
                 StringBuilder usingWord = new StringBuilder(sb);
-                
-                if(i != s.length() - 1)usingWord.append(" ");
-                dp(s.substring(i+1), usingWord, root, ans);
-                //2. don't use. Just continue
+                if(i != s.length() - 1) usingWord.append(" ");
+                dp(s.substring(i + 1), usingWord, root, result);
+                //2. Don't use. Just continue
             }
         }
     }
