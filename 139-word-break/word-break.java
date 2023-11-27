36dd8eeeaa384c7fd2e1c92e411799275fb78014
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-26 21:17:00 -0600

    Time: 3 ms (83.98%) | Memory: 41 MB (82.92%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -3,8 +3,7 @@ class Solution {
         boolean[] dp = new boolean[s.length()];
         for(int i = 0; i < s.length(); i++){
             for(String word : wordDict){
-                //Out of bounds case
-                if(i < word.length() - 1 ) continue;
+                if(i < word.length() - 1) continue;
 
                 if(i == word.length() - 1 || dp[i - word.length()]){
                     if(s.substring(i - word.length() + 1, i + 1).equals(word)){
