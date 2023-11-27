2aa7d733de0cc8653ce4d5c87cb8fa0b81bf30a5
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-26 20:38:40 -0600

    Time: 8 ms (36.10%) | Memory: 43.9 MB (16.84%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,24 +1,23 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> words = new HashSet<>(wordDict);
-        Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
         boolean[] seen = new boolean[s.length() + 1];
-        queue.add(0);
-        
-        while (!queue.isEmpty()) {
-            int start = queue.remove();
-            if (start == s.length()) return true;
-            
-            for (int end = start + 1; end <= s.length(); end++) {
-                if (seen[end]) continue;
-                
-                if (words.contains(s.substring(start, end))) {
-                    queue.add(end);
+
+        while(!q.isEmpty()){
+            int start = q.poll();
+            if(start == s.length()) return true;
+
+            for(int end = start + 1; end < seen.length; end++){
+                if(seen[end]) continue;
+
+                if(words.contains(s.substring(start, end))){
                     seen[end] = true;
+                    q.add(end);
                 }
             }
         }
-        
         return false;
     }
 }
