2f590fbf0a43e02010250ef3e028f95719e6e901
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-27 18:05:03 -0600

    Time: 11 ms (84.12%) | Memory: 43.5 MB (6.85%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -1,55 +1,45 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
+    Map<Integer, List<Integer>> graph;
+    List<Integer> answer;
+    Set<Integer> visited;
+    
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        List<Integer> res = new ArrayList<>();
-        getNodes(root, target, k, res);
-        return res;
-    }
-
-    private int getNodes(TreeNode root, TreeNode target, int k, List<Integer> res){
-        if(root == null) return -1;
+        graph = new HashMap<>();
+        buildGraph(root, null);
         
-        if(root == target){
-            getSub(root, 0, k, res);
-            return 1;
+        answer = new ArrayList<>();
+        visited = new HashSet<>();
+        visited.add(target.val);
+        
+        dfs(target.val, 0, k);
+        
+        return answer;
+    }
+    
+    // Recursively build the undirected graph from the given binary tree.
+    private void buildGraph(TreeNode curr, TreeNode parent) {
+        if (curr != null && parent != null) {
+            graph.putIfAbsent(curr.val, new ArrayList<>());
+            graph.putIfAbsent(parent.val, new ArrayList<>());
+            graph.get(curr.val).add(parent.val);
+            graph.get(parent.val).add(curr.val);
         }
 
-        int left = getNodes(root.left, target, k, res);
-        int right = getNodes(root.right, target, k, res);
-
-        if(left == k || right == k){
-            res.add(root.val);
-            return -1;
-        } 
-        
-        if(left != -1){
-            getSub(root.right, left + 1, k, res);
-            return left + 1;
-        } 
-        
-        if(right != -1){
-            getSub(root.left, right + 1, k, res);
-            return right + 1;
-        }
-        return -1;
+        if (curr.left != null) buildGraph(curr.left, curr);
+        if (curr.right != null) buildGraph(curr.right, curr);
     }
-
-    private void getSub(TreeNode root, int cur, int k, List<Integer> res){
-        if(root == null) return;
-
-        if(k == cur){
-            res.add(root.val);
+    
+    private void dfs(int cur, int distance, int k) {
+        if (distance == k) {
+            answer.add(cur);
             return;
         }
-        getSub(root.left, cur + 1, k, res);
-        getSub(root.right, cur + 1, k, res);
+
+        for (int neighbor : graph.getOrDefault(cur, new ArrayList<>())) {
+            if (!visited.contains(neighbor)) {
+                visited.add(neighbor);
+                dfs(neighbor, distance + 1, k);
+            }
+        }
     }
 }
