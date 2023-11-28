cc5bc213a648aed6fa01953d7efb1aa75822ef2b
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-27 18:21:27 -0600

    Time: 14 ms (15.03%) | Memory: 43.7 MB (5.26%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -1,47 +1,50 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
     Map<Integer, List<Integer>> graph = new HashMap<>();
-    Set<Integer> visited = new HashSet<>();
     List<Integer> result = new ArrayList<>();
+    Set<Integer> visited = new HashSet<>();
 
-    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        buildGraph(root, null);
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {  
+        buildGraph(root, null);  
+        
+        Queue<int[]> queue = new LinkedList<>();     
+        // Add the target node to the queue with a distance of 0
+        queue.add(new int[]{target.val, 0});
         visited.add(target.val);
-        dfs(target.val, 0, k);
-        return result;
-    }
-
-    private void dfs(int curr, int distance, int k){
-        if(distance == k){
-            result.add(curr);
-            return;
-        }
-
-        for(int neighbor : graph.getOrDefault(curr, new ArrayList<>())){
-            if(!visited.contains(neighbor)){
-                visited.add(neighbor);
-                dfs(neighbor, distance + 1, k);
+        
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int node = cur[0], distance = cur[1];
+            
+            // If the current node is at distance k from target,
+            // add it to the answer list and continue to the next node.
+            if (distance == k) {
+                result.add(node);
+                continue;
+            }
+            
+            // Add all unvisited neighbors of the current node to the queue.
+            for (int neighbor : graph.getOrDefault(node, new ArrayList<>())) {
+                if (!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    queue.add(new int[]{neighbor, distance + 1});
+                }
             }
         }
+        
+        return result;
     }
-
-    private void buildGraph(TreeNode root, TreeNode parent){
-        if(root != null && parent != null){
-            graph.putIfAbsent(root.val, new ArrayList<>());
-            graph.putIfAbsent(parent.val, new ArrayList<>());
-            graph.get(root.val).add(parent.val);
-            graph.get(parent.val).add(root.val);
+    
+    // Recursively build the undirected graph from the given binary tree.
+    private void buildGraph(TreeNode cur, TreeNode parent) {
+        if (cur != null && parent != null) {
+            int curVal = cur.val, parentVal = parent.val;
+            graph.putIfAbsent(curVal, new ArrayList<>());
+            graph.putIfAbsent(parentVal, new ArrayList<>());
+            graph.get(curVal).add(parentVal);
+            graph.get(parentVal).add(curVal);
         }
-
-        if(root.left != null) buildGraph(root.left, root);
-        if(root.right != null) buildGraph(root.right, root);
+        
+        if (cur != null && cur.left != null) buildGraph(cur.left, cur);
+        if (cur != null && cur.right != null) buildGraph(cur.right, cur);
     }
 }
