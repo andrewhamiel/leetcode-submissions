784efd23f6b9bc7e51d84763b37084ffea54657b
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-27 21:09:30 -0600

    Time: 0 ms (100.00%) | Memory: 41.1 MB (5.02%) - LeetSync

--- 163-missing-ranges/missing-ranges.java
+++ 163-missing-ranges/missing-ranges.java
@@ -1,24 +1,20 @@
 class Solution {
     public List<List<Integer>> findMissingRanges(int[] nums, int lower, int upper) {
-        List<List<Integer>> missingRanges = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
 
-        if (nums.length == 0) {
-            missingRanges.add(List.of(lower, upper));
-            return missingRanges;
+        if(nums == null || nums.length == 0){
+            result.add(List.of(lower, upper));
+            return result;
         }
 
-        // Check for any missing numbers between the lower bound and nums[0].
-        if (lower < nums[0]) missingRanges.add(List.of(lower, nums[0] - 1));
+        if(lower < nums[0]) result.add(List.of(lower, nums[0] - 1));
 
-        // Check for any missing numbers between successive elements of nums.
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i + 1] - nums[i] <= 1) continue;
-            missingRanges.add(List.of(nums[i] + 1, nums[i + 1] - 1));
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] - nums[i - 1] <= 1) continue;
+            result.add(List.of(nums[i - 1] + 1, nums[i] - 1));
         }
-        
-        // Check for any missing numbers between the last element of nums and the upper bound.
-        if (upper > nums[nums.length - 1]) missingRanges.add(List.of(nums[nums.length - 1] + 1, upper));
 
-        return missingRanges;
+        if(upper > nums[nums.length - 1]) result.add(List.of(nums[nums.length - 1] + 1, upper));
+        return result;
     }
 }
