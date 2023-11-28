9fc6c48ea027efb82cddd9be7147822fcb8e723e
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-27 20:35:49 -0600

    Time: 0 ms (100.00%) | Memory: 40.6 MB (13.09%) - LeetSync

--- 266-palindrome-permutation/palindrome-permutation.java
+++ 266-palindrome-permutation/palindrome-permutation.java
@@ -4,8 +4,7 @@ class Solution {
         int oddCount = 0;
         for(int i = 0; i < s.length(); i++){
             freq[s.charAt(i) - 'a']++;
-            if(freq[s.charAt(i) - 'a'] % 2 == 0) oddCount--;
-            else oddCount++;
+            oddCount+= freq[s.charAt(i) - 'a'] % 2 == 0 ? -1 : 1;
         }
         return oddCount <= 1;
     }
