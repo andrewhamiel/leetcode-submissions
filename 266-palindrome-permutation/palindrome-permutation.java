f2fd509164eb07c393552cd2ad358ad5e731b9ad
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-27 20:34:45 -0600

    Time: 0 ms (100.00%) | Memory: 40.3 MB (54.02%) - LeetSync

--- 266-palindrome-permutation/palindrome-permutation.java
+++ 266-palindrome-permutation/palindrome-permutation.java
@@ -1,13 +1,11 @@
-public class Solution {
+class Solution {
     public boolean canPermutePalindrome(String s) {
         int[] freq = new int[26];
         int oddCount = 0;
-        for (int i = 0; i < s.length(); i++) {
+        for(int i = 0; i < s.length(); i++){
             freq[s.charAt(i) - 'a']++;
-            if (freq[s.charAt(i) - 'a'] % 2 == 0)
-                oddCount--;
-            else
-                oddCount++;
+            if(freq[s.charAt(i) - 'a'] % 2 == 0) oddCount--;
+            else oddCount++;
         }
         return oddCount <= 1;
     }
