6cc68de29a25fe631cf1fe1db13d574fb4fe2bf2
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-27 22:54:25 -0600

    Time: 162 ms (17.72%) | Memory: 41 MB (86.95%) - LeetSync

--- 301-remove-invalid-parentheses/remove-invalid-parentheses.java
+++ 301-remove-invalid-parentheses/remove-invalid-parentheses.java
@@ -3,44 +3,39 @@ class Solution {
     int minRemoved = Integer.MAX_VALUE;
 
     public List<String> removeInvalidParentheses(String s) {
-        helper(0, 0, 0, 0, new StringBuilder(), s);
-        return new ArrayList<>(result);
+        helper(0, 0, 0, 0, s, new StringBuilder());
+        return new ArrayList<>(result);    
     }
 
-    private void helper(int ind, int leftCount, int rightCount, int removed, StringBuilder sb, String s){
-        if(ind >= s.length()){
+    private void helper(int ind, int leftCount, int rightCount, int removed, String s, StringBuilder sb){
+        if(ind == s.length()){
             if(leftCount == rightCount && removed <= minRemoved){
                 if(removed < minRemoved){
-                    minRemoved = removed;
                     result = new HashSet<>();
+                    minRemoved = removed;
                 }
                 result.add(sb.toString());
             }
             return;
         }
-
         int currLength = sb.length();
 
         if(s.charAt(ind) == '('){
-            //try add and remove
             sb.append(s.charAt(ind));
-            helper(ind + 1, leftCount + 1, rightCount, removed, sb, s);
+            helper(ind + 1, leftCount + 1, rightCount, removed, s, sb); //add
             sb.deleteCharAt(currLength);
-            helper(ind + 1, leftCount, rightCount, removed + 1, sb, s);
+            helper(ind + 1, leftCount, rightCount, removed + 1, s, sb); //remove
         }else if(s.charAt(ind) == ')'){
-            //try add and remove if enough leftCount, just remove otherwise
             if(leftCount > rightCount){
                 sb.append(s.charAt(ind));
-                helper(ind + 1, leftCount, rightCount + 1, removed, sb, s);
+                helper(ind + 1, leftCount, rightCount + 1, removed, s, sb); //add
                 sb.deleteCharAt(currLength);
             }
-            helper(ind + 1, leftCount, rightCount, removed + 1, sb, s); //remove
+            helper(ind + 1, leftCount, rightCount, removed + 1, s, sb); //remove
         }else{
-            //for everything else, just add
             sb.append(s.charAt(ind));
-            helper(ind + 1, leftCount, rightCount, removed, sb, s);
+            helper(ind + 1, leftCount, rightCount, removed, s, sb);
             sb.deleteCharAt(currLength);
         }
     }
-
 }
