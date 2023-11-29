c983f55ce20bc1a3f34d6dc7330c0a4c47e34977
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-29 10:57:02 -0600

    Time: 28 ms (69.33%) | Memory: 107.7 MB (5.04%) - LeetSync

--- 2019-product-of-two-run-length-encoded-arrays/product-of-two-run-length-encoded-arrays.java
+++ 2019-product-of-two-run-length-encoded-arrays/product-of-two-run-length-encoded-arrays.java
@@ -6,23 +6,22 @@ class Solution {
         while(e1 < encoded1.length){
             int[] first = encoded1[e1], second = encoded2[e2];
 
-            int common = Math.min(first[1], second[1]);
+            int commonFreq = Math.min(first[1], second[1]);
             int product = first[0] * second[0];
-
             if(!result.isEmpty() && result.get(result.size() - 1).get(0) == product){
                 List<Integer> prev = result.get(result.size() - 1);
                 int prevFreq = prev.get(1);
-                prev.set(1, prevFreq + common);
+                prev.set(1, prevFreq + commonFreq);
                 result.set(result.size() - 1, prev);
             }else{
                 List<Integer> curr = new ArrayList<>();
                 curr.add(product);
-                curr.add(common);
+                curr.add(commonFreq);
                 result.add(curr);
             }
 
-            first[1]-= common;
-            second[1]-= common;
+            first[1]-= commonFreq;
+            second[1]-= commonFreq;
 
             if(first[1] == 0) e1++;
             if(second[1] == 0) e2++;
