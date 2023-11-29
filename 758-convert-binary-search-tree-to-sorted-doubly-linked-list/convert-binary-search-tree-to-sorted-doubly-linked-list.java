5d724a0039045e32dba30e3af3a65be27e686761
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-28 21:09:49 -0600

    Time: 0 ms (100.00%) | Memory: 40.4 MB (92.12%) - LeetSync

--- 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.java
+++ 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.java
@@ -1,60 +1,34 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val,Node _left,Node _right) {
-        val = _val;
-        left = _left;
-        right = _right;
-    }
-};
-*/
-
 class Solution {
-    //better, morris traversal, O(1) space
     public Node treeToDoublyList(Node root) {
         if(root == null) return root;
         Node first = null, last = null;
-    
-        while(root != null){       
-            if(root.left == null){
-                if(first == null) first = root;
-                //do something
-                if(last != null) last.right = root;
-                root.left = last;
-                last = root;
-                //explore right subtree
-                root = root.right;           
-            }else {
+
+        while(root != null){
+            if(root.left != null){
                 Node predecessor = root.left;
                 while(predecessor.right != null && predecessor.right != root) predecessor = predecessor.right;
 
-                if(predecessor.right == root){
-                    //done exploration of left tree               
-                    //do something
-                    if(last != null) last.right = root;
-                    root.left = last;
-                    last = root;             
-                    //explore right subtree
-                    root = root.right;            
-                }else{
+                if(predecessor.right == null){
                     predecessor.right = root;
                     root = root.left;
-                }      
-            }        
+                }else{
+                    //already explored
+                    if(last != null) last.right = root;
+                    root.left = last;
+                    last = root;
+                    root = root.right;
+                }
+            }else{
+                if(first == null) first = root;
+                if(last != null) last.right = root;
+                root.left = last;
+                last = root;
+                root = root.right;
+            }
         }
 
         last.right = first;
-        first.left = last;     
-        return first; 
+        first.left = last;
+        return first;
     }
 }
