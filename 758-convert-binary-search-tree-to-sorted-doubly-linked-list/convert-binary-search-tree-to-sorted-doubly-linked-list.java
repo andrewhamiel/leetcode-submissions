54e3cd28f6fef3e2751632177d18944c3623f21c
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-28 20:58:17 -0600

    Time: 0 ms (100.00%) | Memory: 40.8 MB (41.98%) - LeetSync

--- 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.java
+++ 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.java
@@ -1,50 +1,70 @@
-class Solution {
-    public Node treeToDoublyList(Node root) {
-        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
-        Node cur = root, head = null, tail = null, tmp;
-        // morris' algo
-        while (cur != null) {
-            head = cur.val < min ? cur : head;
-            min = Math.min(min, cur.val);
-            tail = cur.val > max ? cur : tail;
-            max = Math.max(max, cur.val);
-            // right most on left subtree
-            Node rlmost = cur.left;
-            while (rlmost != null
-                   && rlmost.right != null && rlmost != cur) {
-                rlmost = rlmost.right;
-            }
-            tmp = cur.left;
-            if (rlmost != null && rlmost != cur) {
-                // link rlmost node and cur
-				// if still can go left then go
-                rlmost.right = cur;
-                cur.left = rlmost;
-                cur = tmp;
-                continue;
-            }
-            // no left substree or linked before, go right
-			// find the left most node of right subtree
-            Node lrmost = cur.right;
-            while (lrmost != null
-                   && lrmost.left != null && lrmost.left != cur) {
-                // note that if lrmost.left (lrmost.pre) is cur
-                // which means cur and lrmost are already in linked list
-                // then just skip
-                lrmost = lrmost.left;
-            }
-            tmp = cur.right;
-            if (lrmost != null && lrmost.left != cur) {
-                lrmost.left = cur;
-                cur.right = lrmost;
-            }
-            cur = tmp;
-        }
-        // cyclic linking
-        if (head != null) {
-            tail.right = head;
-            head.left = tail;
-        }
-        return head;
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
     }
+
+    public Node(int _val,Node _left,Node _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+*/
+
+class Solution {
+    //better, morris traversal, O(1) space
+    public Node treeToDoublyList(Node root) {
+        if(root==null) return root;
+        Node cur=root, head=null, prev=null;
+    
+        while(cur!=null){       
+            if(cur.left==null){
+                if(head==null)head=cur;
+                //do something
+                if(prev!=null) prev.right=cur;
+                cur.left=prev;
+                prev=cur;
+                //explore right subtree
+                cur=cur.right;           
+            }else {
+                Node pred= findPredecessor(cur);
+
+                if(pred.right==cur){
+                    //done exploration of left tree               
+                    //do something
+                    if(prev!=null) prev.right=cur;
+                    cur.left=prev;
+                    prev=cur;             
+                    //explore right subtree
+                    cur=cur.right;            
+                }else{
+                    pred.right=cur;
+                    cur=cur.left;
+                }      
+            }        
+        }
+        prev.right=head;
+        head.left=prev;
+        
+        return head; 
+    }
+
+    public Node findPredecessor(Node node){
+        Node cur=node;
+        if(cur.left==null) return null;
+        
+        cur=cur.left;
+        while(cur.right!=null&&cur.right!=node) cur=cur.right;
+
+        return cur;
+    }
+
 }
