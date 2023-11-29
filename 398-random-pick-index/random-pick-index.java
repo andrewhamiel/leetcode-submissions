a78e134bc80dfaa7d242db51ac56a53efc7cb2ec
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-29 16:32:35 -0600

    Time: 73 ms (85.41%) | Memory: 55.2 MB (42.15%) - LeetSync

--- 398-random-pick-index/random-pick-index.java
+++ 398-random-pick-index/random-pick-index.java
@@ -1,18 +1,17 @@
 class Solution {
-    private int[] nums;
-    private Random rand;
+    Map<Integer, List<Integer>> map = new HashMap<>();
+    Random rand = new Random();
 
     public Solution(int[] nums) {
-        this.nums = nums;
-        this.rand = new Random();
+        for(int i = 0; i < nums.length; i++){
+            map.putIfAbsent(nums[i], new ArrayList<>());
+            map.get(nums[i]).add(i);
+        }
     }
     
     public int pick(int target) {
-        int count = 1, index = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == target && rand.nextInt(count++) == 0) index = i;
-        }
-        return index;
+        List<Integer> indeces = map.get(target);
+        return indeces.get(rand.nextInt(indeces.size()));
     }
 }
 
