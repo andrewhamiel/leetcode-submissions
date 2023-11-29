35476bb9963f72d4197accae9535e320e902d119
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-29 10:53:55 -0600

    Time: 28 ms (69.33%) | Memory: 107.6 MB (5.04%) - LeetSync

--- 2019-product-of-two-run-length-encoded-arrays/product-of-two-run-length-encoded-arrays.java
+++ 2019-product-of-two-run-length-encoded-arrays/product-of-two-run-length-encoded-arrays.java
@@ -1,35 +1,32 @@
 class Solution {
-    public List<List<Integer>> findRLEArray(int[][] encoded1, int[][] encoded2) {    
+    public List<List<Integer>> findRLEArray(int[][] encoded1, int[][] encoded2) {
         List<List<Integer>> result = new ArrayList<>();
-        
-        int e1 = 0, e2 = 0;       
-        while(e1 < encoded1.length) {
-            int[] first = encoded1[e1];
-            int[] second = encoded2[e2];
-            
-            
+
+        int e1 = 0, e2 = 0;
+        while(e1 < encoded1.length){
+            int[] first = encoded1[e1], second = encoded2[e2];
+
             int common = Math.min(first[1], second[1]);
-            int mul = first[0] * second[0];
-            
-            if(!result.isEmpty() && result.get(result.size() - 1).get(0) == mul) {
+            int product = first[0] * second[0];
+
+            if(!result.isEmpty() && result.get(result.size() - 1).get(0) == product){
                 List<Integer> prev = result.get(result.size() - 1);
                 int prevFreq = prev.get(1);
                 prev.set(1, prevFreq + common);
                 result.set(result.size() - 1, prev);
-            } else {
-                List<Integer> current = new ArrayList<>();
-                current.add(mul);
-                current.add(common);
-                result.add(current);
+            }else{
+                List<Integer> curr = new ArrayList<>();
+                curr.add(product);
+                curr.add(common);
+                result.add(curr);
             }
-            
-            first[1] -= common;
-            second[1] -= common;
-            
+
+            first[1]-= common;
+            second[1]-= common;
+
             if(first[1] == 0) e1++;
             if(second[1] == 0) e2++;
         }
-        
         return result;
     }
 }
