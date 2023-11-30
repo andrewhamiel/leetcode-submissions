4fd4bea75799c0b71c5db462afa5bebdc6a1f170
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-30 17:06:25 -0600

    Time: 13 ms (32.48%) | Memory: 43.9 MB (5.68%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -13,27 +13,22 @@ class Solution {
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         buildGraph(root, null);
 
-        List<Integer> result = new ArrayList<>();
-        Set<Integer> visited = new HashSet<>();
-        visited.add(target.val);
-
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{target.val, 0});
+        Set<Integer> visited = new HashSet<>();
+        visited.add(target.val);
+        List<Integer> result = new ArrayList<>();
 
         while(!q.isEmpty()){
             int[] entry = q.poll();
             int curr = entry[0], distance = entry[1];
-
+            visited.add(curr);
             if(distance == k){
                 result.add(curr);
                 continue;
             }
-
             for(int neighbor : graph.getOrDefault(curr, new ArrayList<>())){
-                if(!visited.contains(neighbor)){
-                    visited.add(neighbor);
-                    q.add(new int[]{neighbor, distance + 1});
-                }
+                if(!visited.contains(neighbor)) q.add(new int[]{neighbor, distance + 1});
             }
         }
         return result;
@@ -46,7 +41,6 @@ class Solution {
             graph.get(root.val).add(parent.val);
             graph.get(parent.val).add(root.val);
         }
-
         if(root.left != null) buildGraph(root.left, root);
         if(root.right != null) buildGraph(root.right, root);
     }
