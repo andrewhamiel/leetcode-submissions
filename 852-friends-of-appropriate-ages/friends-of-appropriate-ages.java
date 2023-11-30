e1718fd95866e88cd00abddd8de19ecbef2c1c73
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-29 20:43:05 -0600

    Time: 3 ms (80.13%) | Memory: 45.9 MB (91.95%) - LeetSync

--- 852-friends-of-appropriate-ages/friends-of-appropriate-ages.java
+++ 852-friends-of-appropriate-ages/friends-of-appropriate-ages.java
@@ -3,14 +3,14 @@ class Solution {
         int[] freq = new int[121];
         for(int age : ages) freq[age]++;
 
-        for(int i = 1; i < freq.length; i++) freq[i]+= freq[i - 1];
+        for(int i = 1; i < 121; i++) freq[i]+= freq[i - 1];
 
-        int totalFriendRequests = 0;
+        int friendRequests = 0;
         for(int age : ages){
             int condition = age/2 + 7;
             if(condition >= age) continue;
-            totalFriendRequests+= freq[age] - freq[condition] - 1;
+            friendRequests+= freq[age] - freq[condition] - 1;
         }
-        return totalFriendRequests;
+        return friendRequests;
     }
 }
