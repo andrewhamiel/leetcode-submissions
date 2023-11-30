6ec34079a748f5b5cf0892da5244896af956f079
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-29 21:50:10 -0600

    Time: 10 ms (76.10%) | Memory: 45.4 MB (22.30%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -2,26 +2,26 @@ class Solution {
     public int shipWithinDays(int[] weights, int days) {
         int maxWeight = 0, totalWeight = 0;
         for(int weight : weights){
-            totalWeight+= weight;
             maxWeight = Math.max(maxWeight, weight);
+            totalWeight+= weight;
         }
 
         int left = maxWeight, right = totalWeight;
         while(left < right){
             int mid = left + (right - left)/2;
-            if(isFeasible(mid, weights, days)) right = mid;
+            if(isFeasible(mid, days, weights)) right = mid;
             else left = mid + 1;
         }
-        return right;
+        return left;
     }
 
-    private boolean isFeasible(int capacity, int[] weights, int days){
+    private boolean isFeasible(int capacity, int days, int[] weights){
         int currWeight = 0, currDays = 1;
         for(int weight : weights){
             currWeight+= weight;
             if(currWeight > capacity){
-                currDays++;
                 currWeight = weight;
+                currDays++;
             }
         }
         return currDays <= days;
