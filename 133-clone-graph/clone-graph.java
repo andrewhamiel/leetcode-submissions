e80ac3c3811c9a03a7348609e04990785c4d4ea5
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-29 22:12:15 -0600

    Time: 31 ms (7.19%) | Memory: 42 MB (39.78%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -19,20 +19,24 @@ class Node {
 */
 
 class Solution {
-    Map<Node, Node> visited = new HashMap<>();
-
     public Node cloneGraph(Node node) {
         if(node == null) return null;
-
-        if(visited.containsKey(node)) return visited.get(node);
-
         Node newNode = new Node(node.val);
+        Map<Node, Node> visited = new HashMap<>();
         visited.put(node, newNode);
+        Queue<Node> q = new LinkedList<>();
+        q.add(node);
 
-        for(Node neighbor : node.neighbors){
-            visited.putIfAbsent(neighbor, cloneGraph(neighbor));
-            newNode.neighbors.add(visited.get(neighbor));
-        }    
-        return newNode;
+        while(!q.isEmpty()){
+            Node curr = q.poll();
+            for(Node neighbor : curr.neighbors){
+                if(!visited.containsKey(neighbor)){
+                    visited.put(neighbor, new Node(neighbor.val));
+                    q.add(neighbor);
+                }
+                visited.get(curr).neighbors.add(visited.get(neighbor));
+            }
+        }
+        return visited.get(node);
     }
 }
