767abf5c9796f0330e718e0e3562c042c79c4343
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-29 18:18:26 -0600

    Time: 13 ms (86.81%) | Memory: 44.9 MB (46.32%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -2,30 +2,22 @@ class Solution {
     private int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
 
     public int shortestPathBinaryMatrix(int[][] grid) {
-        if(grid[0][0] == 1 || grid[grid.length - 1][grid[0].length - 1] == 1) return -1;
-
-        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
-        q.add(new Pair<>(0, 0));
-        grid[0][0] = 1;
-        int steps = 1;
+        if(grid[0][0] != 0 || grid[grid.length - 1][grid[0].length - 1] != 0) return -1;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0, 1});
 
         while(!q.isEmpty()){
-            int size = q.size();
-            while(size-- > 0){
-                Pair<Integer, Integer> p = q.poll();
-                int row = p.getKey(), col = p.getValue();
-                if(row == grid.length - 1 && col == grid[0].length - 1) return steps;
-
-                for(int[] dir : dirs){
-                    int newRow = row + dir[0], newCol = col + dir[1];
-                    if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length 
-                        && grid[newRow][newCol] == 0){
-                            grid[newRow][newCol] = 1;
-                            q.add(new Pair<>(newRow, newCol));
-                    }
+            int[] cell = q.poll();
+            int row = cell[0], col = cell[1], steps = cell[2];
+            if(row == grid.length - 1 && col == grid[0].length - 1) return steps;
+            for(int[] dir : dirs){
+                int newRow = row + dir[0], newCol = col + dir[1];
+                if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length
+                    && grid[newRow][newCol] == 0){
+                        grid[newRow][newCol] = 1;
+                        q.add(new int[]{newRow, newCol, steps + 1});
                 }
             }
-            steps++;
         }
         return -1;
     }
