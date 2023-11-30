05e2497332d78c27b3565609e18716a95266a5db
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-29 19:53:41 -0600

    Time: 0 ms (100.00%) | Memory: 40.4 MB (35.39%) - LeetSync

--- 246-strobogrammatic-number/strobogrammatic-number.java
+++ 246-strobogrammatic-number/strobogrammatic-number.java
@@ -1,23 +1,13 @@
 class Solution {
-
     public boolean isStrobogrammatic(String num) {
-        StringBuilder rotatedStringBuilder = new StringBuilder();
-        
-        // Remember that we want to loop backwards through the string
-        for (int i = num.length() - 1; i >= 0; i--) {
+        StringBuilder reversed = new StringBuilder();
+        for(int i = num.length() - 1; i >= 0; i--){
             char c = num.charAt(i);
-            if (c == '0' || c == '1' || c == '8') {
-                rotatedStringBuilder.append(c);
-            } else if (c == '6') {
-                rotatedStringBuilder.append('9');
-            } else if (c == '9') {
-                rotatedStringBuilder.append('6');
-            } else { // This must be an invalid digit.
-                return false;
-            }
+            if(c == '0' || c == '1' || c == '8') reversed.append(c);
+            else if(c == '6') reversed.append('9');
+            else if(c == '9') reversed.append('6');
+            else return false;
         }
-        
-        String rotatedString = rotatedStringBuilder.toString();
-        return num.equals(rotatedString);
+        return reversed.toString().equals(num);
     }
 }
