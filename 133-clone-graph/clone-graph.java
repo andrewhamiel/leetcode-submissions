6c975d4f00de5b853b376a926421a57b0755d37d
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-29 22:06:43 -0600

    Time: 25 ms (96.82%) | Memory: 42.3 MB (10.89%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -22,7 +22,7 @@ class Solution {
     Map<Node, Node> visited = new HashMap<>();
 
     public Node cloneGraph(Node node) {
-        if(node == null) return node;
+        if(node == null) return null;
 
         if(visited.containsKey(node)) return visited.get(node);
 
@@ -30,7 +30,7 @@ class Solution {
         visited.put(node, newNode);
 
         for(Node neighbor : node.neighbors){
-            if(!visited.containsKey(neighbor)) visited.put(neighbor, cloneGraph(neighbor));
+            visited.putIfAbsent(neighbor, cloneGraph(neighbor));
             newNode.neighbors.add(visited.get(neighbor));
         }    
         return newNode;
