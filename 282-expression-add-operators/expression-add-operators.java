a6f4919b7e466991d3b183c16e9f097bceb6e160
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-30 15:15:59 -0600

    Time: 58 ms (88.89%) | Memory: 44.5 MB (54.93%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.java
+++ 282-expression-add-operators/expression-add-operators.java
@@ -1,13 +1,13 @@
 class Solution {
-    List<String> result = new ArrayList<>();
     String num;
     int target = 0;
+    List<String> result = new ArrayList<>();
 
     public List<String> addOperators(String num, int target) {
         this.num = num;
         this.target = target;
         helper(0, 0, 0, 0, new ArrayList<>());
-        return result; 
+        return result;
     }
 
     private void helper(int ind, long value, long prev, long curr, List<String> expr){
@@ -21,29 +21,29 @@ class Solution {
             return;
         }
 
-        //1. Fix curr, add next digit shifting left base 10
+        //1. Fix curr value, shift base 10
         curr*= 10;
         curr+= (int)(num.charAt(ind) - '0');
 
         //2. No op if != 0 to avoid leading zeroes
         if(curr != 0) helper(ind + 1, value, prev, curr, expr);
 
-        //3. Add
+        //3. Addition
         expr.add("+");
         expr.add(Long.toString(curr));
         helper(ind + 1, value + curr, curr, 0, expr);
         expr.remove(expr.size() - 1);
         expr.remove(expr.size() - 1);
 
-        //if there is another value present, subtract/multiply
+        //if there are values to subtract/multiply from
         if(expr.size() > 0){
-            //4. subtract
+            //4. Subtraction
             expr.add("-");
             expr.add(Long.toString(curr));
             helper(ind + 1, value - curr, -curr, 0, expr);
             expr.remove(expr.size() - 1);
             expr.remove(expr.size() - 1);
-            //5. Multiply
+            //5. Multiplication
             expr.add("*");
             expr.add(Long.toString(curr));
             helper(ind + 1, value - prev + (prev * curr), prev * curr, 0, expr);
