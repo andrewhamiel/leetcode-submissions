1f7554d2575991972e788bb871a5715c4d39cb5c
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-30 19:49:32 -0600

    Time: 25 ms (60.93%) | Memory: 68.5 MB (55.58%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.java
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.java
@@ -1,16 +1,19 @@
 class Solution {
     public int[] findDiagonalOrder(List<List<Integer>> nums) {
+        List<Integer> list = new ArrayList<>();
         Queue<Pair<Integer, Integer>> q = new LinkedList<>();
         q.add(new Pair<>(0, 0));
-        List<Integer> list = new ArrayList<>();
+
         while(!q.isEmpty()){
             Pair<Integer, Integer> p = q.poll();
             int row = p.getKey(), col = p.getValue();
+
             list.add(nums.get(row).get(col));
 
             if(col == 0 && row + 1 < nums.size()) q.add(new Pair<>(row + 1, col));
-            if(row < nums.size() && col < nums.get(row).size() - 1) q.add(new Pair<>(row, col + 1));
+            if(row < nums.size() && col + 1 < nums.get(row).size()) q.add(new Pair<>(row, col + 1));
         }
+
         int[] result = new int[list.size()];
         for(int i = 0; i < result.length; i++) result[i] = list.get(i);
         return result;
