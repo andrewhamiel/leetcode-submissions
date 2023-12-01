d24ca4b04f2dcfbabfe34a4531bda179d8dd4074
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-12-01 09:44:00 -0600

    Time: 1 ms (98.10%) | Memory: 41.1 MB (21.47%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.java
+++ 408-valid-word-abbreviation/valid-word-abbreviation.java
@@ -1,23 +1,17 @@
 class Solution {
     public boolean validWordAbbreviation(String word, String abbr) {
-        int wordPtr = 0, abbrPtr = 0, digits = 0;
-        while(wordPtr < word.length() && abbrPtr < abbr.length()){
-            char c = abbr.charAt(abbrPtr);
-            if(Character.isDigit(c)){
-                if(digits == 0 && c == '0') return false;
-                digits*= 10;
-                digits+= (int)(c - '0');
-                abbrPtr++;
-            }else if(digits != 0){
-                    wordPtr+= digits;
-                    digits = 0;
-            }else{
-                if(word.charAt(wordPtr) != c) return false;
-                wordPtr++;
-                abbrPtr++;
-            }
+        int wordInd = 0, abbrInd = 0, currNum = 0;
+        while(abbrInd < abbr.length() && wordInd < word.length()){
+            if(Character.isDigit(abbr.charAt(abbrInd))){
+                if(currNum == 0 && abbr.charAt(abbrInd) == '0') return false;
+                currNum*= 10;
+                currNum+= (int)(abbr.charAt(abbrInd++) - '0');
+            }else if(currNum > 0){
+                    wordInd+= currNum;
+                    currNum = 0;
+            }else if(word.charAt(wordInd++) != abbr.charAt(abbrInd++)) return false;
         }
-        if(digits != 0) wordPtr+= digits;
-        return wordPtr == word.length() && abbrPtr == abbr.length();
+        if(currNum != 0) wordInd+= currNum;
+        return wordInd == word.length() && abbrInd == abbr.length();
     }
 }
