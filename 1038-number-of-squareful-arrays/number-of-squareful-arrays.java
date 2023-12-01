3b199638ad721fb7bc6f6a1941a491e2b0beeed6
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-12-01 08:46:49 -0600

    Time: 0 ms (100.00%) | Memory: 40.1 MB (47.98%) - LeetSync

--- 1038-number-of-squareful-arrays/number-of-squareful-arrays.java
+++ 1038-number-of-squareful-arrays/number-of-squareful-arrays.java
@@ -5,29 +5,29 @@ class Solution {
     }
 
     private int backtrack(int level, int[] nums){
-        if(level == nums.length)  return 1;
+        if(level == nums.length) return 1;
 
         int count = 0;
         for(int i = level; i < nums.length; i++){
-            if(i != level && nums[i] == nums[level]) continue;
+            if(i != level && nums[i] == nums[level]) continue; //avoid duplicates
             if(level == 0 || isSquareful(nums[level - 1] + nums[i])){
-                swap(i, level, nums);
+                swap(level, i, nums);
                 count+= backtrack(level + 1, nums);
-                swap(i, level, nums);
+                swap(level, i, nums);
                 while(i + 1 < nums.length && nums[i] == nums[i + 1]) i++;
             }
         }
         return count;
     }
 
+    private boolean isSquareful(int num){
+        int sqrt = (int) Math.sqrt(num);
+        return sqrt * sqrt == num;
+    }
+
     private void swap(int i, int j, int[] nums){
         int tmp = nums[i];
         nums[i] = nums[j];
         nums[j] = tmp;
-    }
-
-    private boolean isSquareful(int num){
-        int tmp = (int) Math.sqrt(num);
-        return tmp * tmp == num;
     }
 }
