8f8db42bbaad2647ea74c1cfce4705da17535e2c
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-12-01 07:15:57 -0600

    Time: 3 ms (100.00%) | Memory: 45.3 MB (39.16%) - LeetSync

--- 689-maximum-sum-of-3-non-overlapping-subarrays/maximum-sum-of-3-non-overlapping-subarrays.java
+++ 689-maximum-sum-of-3-non-overlapping-subarrays/maximum-sum-of-3-non-overlapping-subarrays.java
@@ -1,35 +1,34 @@
 class Solution {
     public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
-        // W is an array of sums of windows
-        int[] W = new int[nums.length - k + 1];
+        int[] windows = new int[nums.length - k + 1];
         int currSum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            currSum += nums[i];
-            if (i >= k) currSum -= nums[i - k];
-            if (i >= k - 1) W[i - k + 1] = currSum;
+        for(int i = 0; i < nums.length; i++){
+            currSum+= nums[i];
+            if(i >= k) currSum-= nums[i - k];
+            if(i >= k - 1) windows[i - k + 1] = currSum;
         }
 
-        int[] left = new int[W.length];
+        int[] left = new int[windows.length];
         int best = 0;
-        for (int i = 0; i < W.length; i++) {
-            if (W[i] > W[best]) best = i;
+        for(int i = 0; i < left.length; i++){
+            if(windows[i] > windows[best]) best = i;
             left[i] = best;
         }
 
-        int[] right = new int[W.length];
-        best = W.length - 1;
-        for (int i = W.length - 1; i >= 0; i--) {
-            if (W[i] >= W[best]) best = i;
+        int[] right = new int[windows.length];
+        best = windows.length - 1;
+        for(int i = right.length - 1; i >= 0; i--){
+            if(windows[i] >= windows[best]) best = i;
             right[i] = best;
         }
-        
+
         int[] ans = new int[]{-1, -1, -1};
-        for (int j = k; j < W.length - k; j++) {
-            int i = left[j - k], l = right[j + k];
-            if (ans[0] == -1 || W[i] + W[j] + W[l] > W[ans[0]] + W[ans[1]] + W[ans[2]]) {
-                ans[0] = i;
+        for(int j = k; j < windows.length - k; j++){
+            int l = left[j - k], r = right[j + k];
+            if(ans[0] == -1 || windows[l] + windows[j] + windows[r] > windows[ans[0]] + windows[ans[1]] + windows[ans[2]]){
+                ans[0] = l;
                 ans[1] = j;
-                ans[2] = l;
+                ans[2] = r;
             }
         }
         return ans;
