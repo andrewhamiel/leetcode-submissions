7bcb62bc298d77036bc393a29ddb1171e3323806
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-30 18:24:38 -0600

    Time: 11 ms (17.34%) | Memory: 44.4 MB (33.08%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,41 +1,35 @@
 class Solution {
-    Map<Integer, GNode> adj = new HashMap<>();
+    class GNode {
+        List<Integer> outDegrees = new ArrayList<>();
+        int inDegrees = 0;
+    }
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         //1. Build adjacencies
-        for(int[] course : prerequisites){
-            GNode prevCourse = getGNode(course[1]), nextCourse = getGNode(course[0]);
-            prevCourse.outDegrees.add(course[0]);
-            nextCourse.inDegrees++;
+        Map<Integer, GNode> adj = new HashMap<>();
+        for(int[] prerequisite : prerequisites){
+            int nextCourse = prerequisite[0], prevCourse = prerequisite[1];
+            adj.putIfAbsent(nextCourse, new GNode());
+            adj.putIfAbsent(prevCourse, new GNode());
+            adj.get(nextCourse).inDegrees++;
+            adj.get(prevCourse).outDegrees.add(nextCourse);
         }
-        //2. Find courses with no dependencies
-        Queue<Integer> noDeps = new LinkedList<>();
-        for(Map.Entry<Integer, GNode> entry : adj.entrySet()){
-            Integer course = entry.getKey();
-            GNode node = entry.getValue();
-            if(node.inDegrees == 0) noDeps.add(course);
-        }
-        //3. Topological Sort to remove dependencies
-        int dependenciesRemoved = 0;
-        while(!noDeps.isEmpty()){
-            Integer course = noDeps.poll();
-            for(Integer nextCourse : adj.get(course).outDegrees){
-                GNode node = adj.get(nextCourse);
-                node.inDegrees--;
-                dependenciesRemoved++;
-                if(node.inDegrees == 0) noDeps.add(nextCourse);
+
+        //2. Find courses without dependencies
+        Queue<Integer> q = new LinkedList<>();
+        for(int course : adj.keySet()) if(adj.get(course).inDegrees == 0) q.add(course);
+
+        //3. Topological sort
+        int coursesRemoved = 0;
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            for(int nextCourse : adj.get(curr).outDegrees){
+                adj.get(nextCourse).inDegrees--;
+                coursesRemoved++;
+                if(adj.get(nextCourse).inDegrees == 0) q.add(nextCourse);
             }
         }
-        return dependenciesRemoved == prerequisites.length;
-    }
-
-    private GNode getGNode(int course){
-        adj.putIfAbsent(course, new GNode());
-        return adj.get(course);
-    }
-
-    class GNode {
-        int inDegrees = 0;
-        List<Integer> outDegrees = new ArrayList<>();
+        //4. Validate all prerequisites removed
+        return coursesRemoved == prerequisites.length;
     }
 }
