76bf5ea6cff61aeb1dfa06f8785a476817ff6770
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-12-01 09:51:43 -0600

    Time: 1 ms (98.10%) | Memory: 41.6 MB (5.75%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.java
+++ 408-valid-word-abbreviation/valid-word-abbreviation.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean validWordAbbreviation(String word, String abbr) {
         int wordPtr = 0, abbrPtr = 0, currNum = 0;
-        while(wordPtr < word.length () && abbrPtr < abbr.length()){
+        while(wordPtr < word.length() && abbrPtr < abbr.length()){
             if(Character.isDigit(abbr.charAt(abbrPtr))){
                 if(currNum == 0 && abbr.charAt(abbrPtr) == '0') return false;
                 currNum*= 10;
@@ -10,9 +10,9 @@ class Solution {
                 wordPtr+= currNum;
                 currNum = 0;
             }else if(word.charAt(wordPtr++) != abbr.charAt(abbrPtr++)) return false;
-        }
 
-        if(currNum != 0) wordPtr+= currNum;
+        }
+        if(currNum > 0) wordPtr+= currNum;
         return wordPtr == word.length() && abbrPtr == abbr.length();
     }
 }
