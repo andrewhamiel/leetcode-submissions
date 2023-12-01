71b52f6d72cfca7e8a53119de96070672bfa1a12
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-11-30 21:16:06 -0600

    Time: 10 ms (13.27%) | Memory: 45.1 MB (20.94%) - LeetSync

--- 490-the-maze/the-maze.java
+++ 490-the-maze/the-maze.java
@@ -2,14 +2,14 @@ class Solution {
     private int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
 
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(start);
-        boolean[][] visited = new boolean[maze.length][maze[0].length];
-        visited[start[0]][start[1]] = true;
+        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+        q.add(new Pair<>(start[0], start[1]));
+        Set<Pair<Integer, Integer>> visited = new HashSet<>();
+        visited.add(new Pair<>(start[0], start[1]));
 
         while(!q.isEmpty()){
-            int[] cell = q.poll();
-            int row = cell[0], col = cell[1];
+            Pair<Integer, Integer> p = q.poll();
+            int row = p.getKey(), col = p.getValue();
             if(row == destination[0] && col == destination[1]) return true;
 
             for(int[] dir : dirs){
@@ -21,9 +21,10 @@ class Solution {
                 }
                 newRow-= dir[0];
                 newCol-= dir[1];
-                if(!visited[newRow][newCol]){
-                    visited[newRow][newCol] = true;
-                    q.add(new int[]{newRow, newCol});
+
+                if(!visited.contains(new Pair<>(newRow, newCol))) {
+                    visited.add(new Pair<>(newRow, newCol));
+                    q.add(new Pair<>(newRow, newCol));
                 }
             }
         }
