22fe2053c6ffb2dc939bccfb4b50427cffc0cf67
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2023-12-16 07:23:14 -0600

    Time: 1 ms (99.86%) | Memory: 43.3 MB (61.95%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] sFreq = new int[26], tFreq = new int[26];
+        for(char c : s.toCharArray()) sFreq[c - 'a']++;
+        for(char c : t.toCharArray()) tFreq[c - 'a']++;
+        for(int i = 0; i < 26; i++) if(sFreq[i] != tFreq[i]) return false;
+        return true;
+    }
+}
