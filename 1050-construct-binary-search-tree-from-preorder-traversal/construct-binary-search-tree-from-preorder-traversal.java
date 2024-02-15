ccd700967df29d6dbce8ea04a5ad7e450536e435
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-02-14 18:04:16 -0600

    Time: 1 ms (21.02%) | Memory: 42 MB (14.27%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -1,27 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-  public TreeNode bstFromPreorder(int[] preorder) {
-    int n = preorder.length;
-    if (n == 0) return null;
+    public TreeNode bstFromPreorder(int[] preorder) {
+        if(preorder.length == 0) return null;
 
-    TreeNode root = new TreeNode(preorder[0]);
-    Deque<TreeNode> stack = new ArrayDeque<TreeNode>();
-    stack.addFirst(root);
+        TreeNode root = new TreeNode(preorder[0]);
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.addFirst(root);
 
-    for (int i = 1; i < n; i++) {
-      //Take the last element of the deque as a parent
-      // and create a child from the next preorder element
-      TreeNode node = stack.peekFirst();
-      TreeNode child = new TreeNode(preorder[i]);
-      //Adjust the parent 
-      while (!stack.isEmpty() && stack.peekFirst().val < child.val)
-        node = stack.pop();
+        for(int i = 1; i < preorder.length; i++){
+            TreeNode node = stack.peekFirst();
+            TreeNode child = new TreeNode(preorder[i]);
+            while(!stack.isEmpty() && stack.peekFirst().val < child.val) node = stack.removeFirst();
 
-      // follow BST logic to create a parent-child link
-      if (node.val < child.val) node.right = child;
-      else node.left = child;
-      // add the child into deque
-      stack.addFirst(child);
+            if(node.val < child.val) node.right = child;
+            else node.left = child;
+
+            stack.addFirst(child);
+        }
+        return root;
     }
-    return root;
-  }
 }
