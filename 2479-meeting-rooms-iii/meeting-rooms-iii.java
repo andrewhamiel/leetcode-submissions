11d7a5c34d8035689bd089146e19a0b2e586bc03
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-02-18 08:08:50 -0600

    Time: 54 ms (94.73%) | Memory: 101.2 MB (17.10%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.java
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.java
@@ -3,11 +3,11 @@ class Solution {
         long[] minTimeAvailable = new long[n];
         int[] meetingCount = new int[n];
         Arrays.sort(meetings, (a, b) -> a[0] - b[0]);
-        
+
         for(int[] meeting : meetings){
             int start = meeting[0], end = meeting[1];
-            long minRoomAvailableTime = Long.MAX_VALUE;
-            int minMeetingRoom = 0;
+            long minAvailableRoomTime = Long.MAX_VALUE;
+            int minAvailableRoom = 0;
             boolean isRoomFound = false;
 
             for(int i = 0; i < n; i++){
@@ -18,25 +18,25 @@ class Solution {
                     break;
                 }
 
-                if(minRoomAvailableTime > minTimeAvailable[i]){
-                    minRoomAvailableTime = minTimeAvailable[i];
-                    minMeetingRoom = i;
+                if(minAvailableRoomTime > minTimeAvailable[i]){
+                    minAvailableRoomTime = minTimeAvailable[i];
+                    minAvailableRoom = i;
                 }
             }
 
             if(!isRoomFound){
-                minTimeAvailable[minMeetingRoom]+= end - start;
-                meetingCount[minMeetingRoom]++;
+                minTimeAvailable[minAvailableRoom]+= end - start;
+                meetingCount[minAvailableRoom]++;
             }
         }
 
-        int maxMeetingCount = 0, maxMeetingRoom = 0;
+        int maxRoomCount = 0, maxRoom = 0;
         for(int i = 0; i < n; i++){
-            if(meetingCount[i] > maxMeetingCount){
-                maxMeetingCount = meetingCount[i];
-                maxMeetingRoom = i;
+            if(meetingCount[i] > maxRoomCount){
+                maxRoomCount = meetingCount[i];
+                maxRoom = i;
             }
         }
-        return maxMeetingRoom;
+        return maxRoom;
     }
 }
