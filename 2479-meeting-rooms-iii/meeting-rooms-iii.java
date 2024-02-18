02da3c6cd82c1f5cb922d373b627735efd421259
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-02-18 07:59:16 -0600

    Time: 52 ms (97.51%) | Memory: 100.7 MB (24.75%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.java
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.java
@@ -2,42 +2,41 @@ class Solution {
     public int mostBooked(int n, int[][] meetings) {
         long[] roomAvailabilityTime = new long[n];
         int[] meetingCount = new int[n];
-        Arrays.sort(meetings, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(meetings, (a, b) -> a[0] - b[0]);
 
-        for (int[] meeting : meetings) {
+        for(int[] meeting : meetings){
             int start = meeting[0], end = meeting[1];
-            long minRoomAvailabilityTime = Long.MAX_VALUE;
-            int minAvailableTimeRoom = 0;
-            boolean foundUnusedRoom = false;
+            long minAvailabilityTime = Long.MAX_VALUE;
+            int minAvailableRoom = 0;
+            boolean isRoomFound = false;
 
-            for (int i = 0; i < n; i++) {
-                if (roomAvailabilityTime[i] <= start) {
-                    foundUnusedRoom = true;
-                    meetingCount[i]++;
+            for(int i = 0; i < n; i++){
+                if(roomAvailabilityTime[i] <= start){
                     roomAvailabilityTime[i] = end;
+                    meetingCount[i]++;
+                    isRoomFound = true;
                     break;
                 }
 
-                if (minRoomAvailabilityTime > roomAvailabilityTime[i]) {
-                    minRoomAvailabilityTime = roomAvailabilityTime[i];
-                    minAvailableTimeRoom = i;
+                if(minAvailabilityTime > roomAvailabilityTime[i]){
+                    minAvailabilityTime = roomAvailabilityTime[i];
+                    minAvailableRoom = i;
                 }
             }
 
-            if (!foundUnusedRoom) {
-                roomAvailabilityTime[minAvailableTimeRoom] += end - start;
-                meetingCount[minAvailableTimeRoom]++;
+            if(!isRoomFound){
+                roomAvailabilityTime[minAvailableRoom]+= end - start;
+                meetingCount[minAvailableRoom]++;
             }
         }
 
         int maxMeetingCount = 0, maxMeetingCountRoom = 0;
-        for (int i = 0; i < n; i++) {
-            if (meetingCount[i] > maxMeetingCount) {
+        for(int i = 0; i < n; i++){
+            if(meetingCount[i] > maxMeetingCount){
                 maxMeetingCount = meetingCount[i];
                 maxMeetingCountRoom = i;
             }
         }
-
         return maxMeetingCountRoom;
     }
 }
