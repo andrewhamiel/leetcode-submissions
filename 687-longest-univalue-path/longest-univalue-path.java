9e3c67304f9e366d77efb96998059204aaddaaed
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-02-23 16:00:32 -0600

    Time: 2 ms (94.48%) | Memory: 47.2 MB (13.28%) - LeetSync

--- 687-longest-univalue-path/longest-univalue-path.java
+++ 687-longest-univalue-path/longest-univalue-path.java
@@ -1,22 +1,33 @@
-// Returns the length of the longest path (number of nodes) under the root
-// that have the value same as the root. The path could either be
-// on the left or right child of the root. The length includes the root as well.
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    int ans = 0;
+    private int ans = 0;
 
     public int longestUnivaluePath(TreeNode root) {
         solve(root, -1);
-        return ans;
+        return ans;    
     }
 
-    int solve(TreeNode root, int parent) {
-        if (root == null)  return 0; 
+    private int solve(TreeNode root, int parent){
+        if(root == null) return 0;
 
         int left = solve(root.left, root.val);
         int right = solve(root.right, root.val);
 
         ans = Math.max(ans, left + right);
-
         return root.val == parent ? Math.max(left, right) + 1 : 0;
     }
 }
