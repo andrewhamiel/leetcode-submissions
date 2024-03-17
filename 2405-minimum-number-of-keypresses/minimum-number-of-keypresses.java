c2007f32de540c7986bb39e4841970f2bcfa5265
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-03-17 14:30:36 -0500

    Time: 9 ms (49.91%) | Memory: 45.8 MB (5.26%) - LeetSync

--- 2405-minimum-number-of-keypresses/minimum-number-of-keypresses.java
+++ 2405-minimum-number-of-keypresses/minimum-number-of-keypresses.java
@@ -3,18 +3,15 @@ class Solution {
         int result = 0;
         Integer[] count = new Integer[26];
         Arrays.fill(count, 0);
-        //O(n) loop
-        for(int i=0; i<s.length(); i++) 
-            count[s.charAt(i)-'a']++;
-        
-        Arrays.sort(count, (a,b)->b-a);
-        
-        //O(1) loop
-        for(int i=0; i<26; i++) {
-            //Add num keystrokes depending on order in keypad
-            if(i<9) result += count[i];
-            else if(i<18) result += 2*count[i];
-            else result += 3*count[i];
+
+        for(char c : s.toCharArray()) count[c - 'a']++;
+
+        Arrays.sort(count, (a, b) -> b - a);
+
+        for(int i = 0; i < 26; i++){
+            if(i < 9) result+= count[i];
+            else if(i < 18) result+= 2 * count[i];
+            else result+= 3 * count[i];
         }
         return result;
     }
