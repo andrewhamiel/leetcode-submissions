0e073d30424b9fd7ed640838012cc764fc51cfad
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-03-20 16:04:53 -0500

    Time: 39 ms (28.66%) | Memory: 62.2 MB (21.03%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,27 +1,25 @@
 class Solution {
-    private Deque<Integer> deq = new ArrayDeque();
-    
     public int[] maxSlidingWindow(int[] nums, int k) {
-        //first sliding window
+        Deque<Integer> deq = new ArrayDeque<>();
         int localMax = 0;
         for(int i = 0; i < k; i++){
-            cleanDeque(i, k, nums);
+            cleanDeque(i, k, nums, deq);
             deq.addLast(i);
             if(nums[i] > nums[localMax]) localMax = i;
         }
-        
-        int[] answer = new int[nums.length - k + 1];
-        answer[0] = nums[localMax];
+
+        int[] result = new int[nums.length - k + 1];
+        result[0] = nums[localMax];
         for(int i = k; i < nums.length; i++){
-            cleanDeque(i, k, nums);
-            deq.add(i);
-            answer[i - k + 1] = nums[deq.peek()];
+            cleanDeque(i, k, nums, deq);
+            deq.addLast(i);
+            result[i - k + 1] = nums[deq.peekFirst()];
         }
-        return answer;
+        return result;
     }
-    
-    private void cleanDeque(int i, int k, int[] nums){
-        if(!deq.isEmpty() && deq.peek() == i - k) deq.pop();
+
+    private void cleanDeque(int i, int k, int[] nums, Deque<Integer> deq){
+        if(!deq.isEmpty() && deq.peekFirst() == i - k) deq.removeFirst();
         while(!deq.isEmpty() && nums[i] > nums[deq.peekLast()]) deq.removeLast();
     }
 }
