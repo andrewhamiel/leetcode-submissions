e94d3744c07e2beb16a1abbd4590752e6fb8b259
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-03-27 11:05:52 -0500

    Time: 12 ms (85.67%) | Memory: 44.6 MB (91.58%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -6,12 +6,19 @@ class Solution {
         int left = 1, right = maxPile;
         while(left < right){
             int mid = left + (right - left)/2;
-            int hoursSpent = 0;
-            for(int pile : piles)
-                hoursSpent+= Math.ceil((double)pile/mid);
-            if(hoursSpent <= h) right = mid;
+            if(isFeasible(piles, h, mid)) right = mid;
             else left = mid + 1;
         }
         return right;
+    }
+
+    private boolean isFeasible(int[] piles, int hours, int capacity){
+        int h = 0;
+        for(int pile : piles){
+            h+= (pile/capacity);
+            int rem = pile % capacity;
+            if(rem > 0) h++;
+        }
+        return h <= hours;
     }
 }
