8a49c352d3096b95d6ac6c1c9e89daa597c90b89
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-03-27 20:12:04 -0500

    Time: 19 ms (43.29%) | Memory: 44.1 MB (79.12%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -4,7 +4,6 @@ class Solution {
         int[] dp = new int[max];
         Arrays.fill(dp, max);
         dp[0] = 0;
-
         for(int i = 1; i < dp.length; i++){
             for(int coin : coins){
                 if(coin <= i) dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
