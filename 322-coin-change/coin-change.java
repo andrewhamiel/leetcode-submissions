ce81e3ebe6640bfc1bcdc4f502158dd4104db3dd
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-03-27 20:08:17 -0500

    Time: 14 ms (88.08%) | Memory: 44.5 MB (49.39%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,7 +1,7 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int max = amount + 1;
-        int[] dp = new int[max];
+        int[] dp = new int[amount + 1];
         Arrays.fill(dp, max);
         dp[0] = 0;
 
@@ -10,6 +10,6 @@ class Solution {
                 if(coin <= i) dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
             }
         }
-        return dp[amount] > amount ? -1 : dp[amount];
+        return dp[amount] < max ? dp[amount] : -1;
     }
 }
