a3f243852849c52a16e00635963c0ccfe11871b8
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-03-29 13:26:31 -0500

    Time: 62 ms (25.03%) | Memory: 49.4 MB (20.71%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.java
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.java
@@ -1,29 +1,27 @@
 class Solution {
     public int[] smallestRange(List<List<Integer>> nums) {
         int minLeft = 0, minRight = Integer.MAX_VALUE, maxSeen = Integer.MIN_VALUE;
-        int[] nextCol = new int[nums.size()];
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a,b) -> nums.get(a).get(nextCol[a]) - nums.get(b).get(nextCol[b]));
+        int[] nextCols = new int[nums.size()];
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a,b) -> nums.get(a).get(nextCols[a]) - nums.get(b).get(nextCols[b]));
 
         for(int row = 0; row < nums.size(); row++){
             minHeap.add(row);
-            maxSeen = Math.max(maxSeen, nums.get(row).get(nextCol[row]));
+            maxSeen = Math.max(maxSeen, nums.get(row).get(nextCols[row]));
         }
 
         for(int row = 0; row < nums.size(); row++){
             for(int col = 0; col < nums.get(row).size(); col++){
                 int rowWithLowestNext = minHeap.poll();
-                int lowestNext = nums.get(rowWithLowestNext).get(nextCol[rowWithLowestNext]);
-                if(minRight - minLeft > maxSeen - lowestNext) {
+                int lowestNext = nums.get(rowWithLowestNext).get(nextCols[rowWithLowestNext]);
+                if(minRight - minLeft > maxSeen - lowestNext){
                     minLeft = lowestNext;
                     minRight = maxSeen;
                 }
 
-                nextCol[rowWithLowestNext]++;
-                if(nextCol[rowWithLowestNext] == nums.get(rowWithLowestNext).size()){
-                    return new int[]{minLeft, minRight};
-                }
+                nextCols[rowWithLowestNext]++;
+                if(nextCols[rowWithLowestNext] == nums.get(rowWithLowestNext).size()) return new int[]{minLeft, minRight};
 
-                int nextValAtRow = nums.get(rowWithLowestNext).get(nextCol[rowWithLowestNext]);
+                int nextValAtRow = nums.get(rowWithLowestNext).get(nextCols[rowWithLowestNext]);
                 minHeap.add(rowWithLowestNext);
                 maxSeen = Math.max(maxSeen, nextValAtRow);
             }
