02cefabe6eabf3f1c14fc5a972772b0607bb27fd
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-03-29 12:57:30 -0500

    Time: 65 ms (15.93%) | Memory: 49.4 MB (18.77%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.java
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.java
@@ -1,31 +1,33 @@
 class Solution {
     public int[] smallestRange(List<List<Integer>> nums) {
-        int minx = 0, miny = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
-        int[] next = new int[nums.size()];
-        boolean flag = true;
-        
-        PriorityQueue <Integer> min_queue = new PriorityQueue <Integer> ((a, b) -> nums.get(a).get(next[a]) - nums.get(b).get(next[b]));
-        for (int i = 0; i < nums.size(); i++) {
-            min_queue.offer(i);
-            max = Math.max(max, nums.get(i).get(0));
+        int minLeft = 0, minRight = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        int[] next = new int[nums.size()]; //Column we should be viewing
+        boolean reachedEndOfList = false;
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> nums.get(a).get(next[a]) - nums.get(b).get(next[b]));
+        for(int i = 0; i < nums.size(); i++){
+            minHeap.add(i);
+            max = Math.max(max, nums.get(i).get(next[i]));
         }
-        
-        for (int i = 0; i < nums.size() && flag; i++) {
-            for (int j = 0; j < nums.get(i).size() && flag; j++) {
-                int min_i = min_queue.poll();
-                if (miny - minx > max - nums.get(min_i).get(next[min_i])) {
-                    minx = nums.get(min_i).get(next[min_i]);
-                    miny = max;
+
+        for(int row = 0; row < nums.size() && !reachedEndOfList; row++){
+            for(int col = 0; col < nums.get(row).size() && !reachedEndOfList; col++){
+                int rowWithLowestNext = minHeap.poll();
+                int lowestNext = nums.get(rowWithLowestNext).get(next[rowWithLowestNext]);
+                if(minRight - minLeft > max - lowestNext){
+                    minLeft = lowestNext;
+                    minRight = max;
                 }
-                next[min_i]++;
-                if (next[min_i] == nums.get(min_i).size()) {
-                    flag = false;
+                next[rowWithLowestNext]++;
+                if(next[rowWithLowestNext] == nums.get(rowWithLowestNext).size()){
+                    reachedEndOfList = true;
                     break;
                 }
-                min_queue.offer(min_i);
-                max = Math.max(max, nums.get(min_i).get(next[min_i]));
+                minHeap.add(rowWithLowestNext);
+                lowestNext = nums.get(rowWithLowestNext).get(next[rowWithLowestNext]); //reset
+                max = Math.max(max, lowestNext);
             }
         }
-        return new int[] { minx, miny};
+        return new int[]{minLeft, minRight};
     }
 }
