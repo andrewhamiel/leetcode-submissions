e2e064373af1cd50dc94eec7b1b3acdb5ca34c3d
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-03-29 13:10:13 -0500

    Time: 64 ms (18.77%) | Memory: 49.4 MB (18.77%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.java
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.java
@@ -1,31 +1,33 @@
 class Solution {
     public int[] smallestRange(List<List<Integer>> nums) {
-        int minLeft = 0, minRight = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
-        int[] next = new int[nums.size()]; //Column we should be viewing
+        int minLeft = 0, minRight = Integer.MAX_VALUE, maxSeen = Integer.MIN_VALUE;
+        int[] next = new int[nums.size()]; //store next columns
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a,b) -> nums.get(a).get(next[a]) - nums.get(b).get(next[b]));
         boolean reachedEndOfList = false;
 
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> nums.get(a).get(next[a]) - nums.get(b).get(next[b]));
         for(int i = 0; i < nums.size(); i++){
             minHeap.add(i);
-            max = Math.max(max, nums.get(i).get(next[i]));
+            maxSeen = Math.max(maxSeen, nums.get(i).get(next[i]));
         }
 
         for(int row = 0; row < nums.size() && !reachedEndOfList; row++){
             for(int col = 0; col < nums.get(row).size() && !reachedEndOfList; col++){
                 int rowWithLowestNext = minHeap.poll();
                 int lowestNext = nums.get(rowWithLowestNext).get(next[rowWithLowestNext]);
-                if(minRight - minLeft > max - lowestNext){
+                if(minRight - minLeft > maxSeen - lowestNext) {
                     minLeft = lowestNext;
-                    minRight = max;
+                    minRight = maxSeen;
                 }
+
                 next[rowWithLowestNext]++;
                 if(next[rowWithLowestNext] == nums.get(rowWithLowestNext).size()){
                     reachedEndOfList = true;
                     break;
                 }
+
+                int nextValAfterIncrement = nums.get(rowWithLowestNext).get(next[rowWithLowestNext]);
                 minHeap.add(rowWithLowestNext);
-                lowestNext = nums.get(rowWithLowestNext).get(next[rowWithLowestNext]); //reset
-                max = Math.max(max, lowestNext);
+                maxSeen = Math.max(maxSeen, nextValAfterIncrement);
             }
         }
         return new int[]{minLeft, minRight};
