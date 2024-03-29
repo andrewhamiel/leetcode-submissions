2da0d3f354fdf931ce48d08cc763ad346a564766
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-03-29 13:10:43 -0500

    Time: 61 ms (29.01%) | Memory: 49.3 MB (20.71%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.java
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.java
@@ -3,15 +3,14 @@ class Solution {
         int minLeft = 0, minRight = Integer.MAX_VALUE, maxSeen = Integer.MIN_VALUE;
         int[] next = new int[nums.size()]; //store next columns
         PriorityQueue<Integer> minHeap = new PriorityQueue<>((a,b) -> nums.get(a).get(next[a]) - nums.get(b).get(next[b]));
-        boolean reachedEndOfList = false;
 
         for(int i = 0; i < nums.size(); i++){
             minHeap.add(i);
             maxSeen = Math.max(maxSeen, nums.get(i).get(next[i]));
         }
 
-        for(int row = 0; row < nums.size() && !reachedEndOfList; row++){
-            for(int col = 0; col < nums.get(row).size() && !reachedEndOfList; col++){
+        for(int row = 0; row < nums.size(); row++){
+            for(int col = 0; col < nums.get(row).size(); col++){
                 int rowWithLowestNext = minHeap.poll();
                 int lowestNext = nums.get(rowWithLowestNext).get(next[rowWithLowestNext]);
                 if(minRight - minLeft > maxSeen - lowestNext) {
@@ -21,8 +20,7 @@ class Solution {
 
                 next[rowWithLowestNext]++;
                 if(next[rowWithLowestNext] == nums.get(rowWithLowestNext).size()){
-                    reachedEndOfList = true;
-                    break;
+                    return new int[]{minLeft, minRight};
                 }
 
                 int nextValAfterIncrement = nums.get(rowWithLowestNext).get(next[rowWithLowestNext]);
