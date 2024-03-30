b323855e81252c475a761b03d12e9d6b9256c57d
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-03-30 14:57:45 -0500

    Time: 1 ms (100.00%) | Memory: 46 MB (37.61%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -13,8 +13,9 @@ class Solution {
             int maxLeftB = partitionB == 0 ? Integer.MIN_VALUE : nums2[partitionB - 1];
             int minRightB = partitionB == nums2.length ? Integer.MAX_VALUE : nums2[partitionB];
 
+            //Partition size correct
             if(maxLeftA <= minRightB && maxLeftB <= minRightA){
-                if((nums1.length + nums2.length) % 2 == 0) return (double) (Math.max(maxLeftA, maxLeftB) + Math.min(minRightA, minRightB))/2;
+                if((nums1.length + nums2.length) % 2 == 0) return (double)(Math.max(maxLeftA, maxLeftB) + Math.min(minRightA, minRightB))/2;
                 else return Math.max(maxLeftA, maxLeftB);
             }else if(maxLeftA > minRightB) right = midAndPartitionA - 1; //left partition too large
             else left = midAndPartitionA + 1; //right partition too large
