7ba5a2ff9b9605b5f4d1c6002b8841c39fd8e8d6
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-04-02 21:10:18 -0500

    Time: 15 ms (92.45%) | Memory: 53 MB (65.28%) - LeetSync

--- 1061-number-of-valid-subarrays/number-of-valid-subarrays.java
+++ 1061-number-of-valid-subarrays/number-of-valid-subarrays.java
@@ -1,18 +1,18 @@
 class Solution {
     public int validSubarrays(int[] nums) {
-        int answer = 0;
+        int result = 0;
         Deque<Integer> stack = new ArrayDeque<>();
 
         for(int i = 0; i < nums.length; i++){
             while(!stack.isEmpty() && nums[i] < nums[stack.peekFirst()]){
-                answer+= (i - stack.removeFirst());
+                result+= (i - stack.removeFirst());
             }
             stack.addFirst(i);
         }
 
         while(!stack.isEmpty()){
-            answer+= (nums.length - stack.removeFirst());
+            result+= (nums.length - stack.removeFirst());
         }
-        return answer;
+        return result;
     }
 }
