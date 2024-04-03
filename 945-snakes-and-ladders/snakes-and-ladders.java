49443673e4ffce1389e78a31141e792e78a2a35b
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-04-03 16:00:51 -0500

    Time: 17 ms (12.52%) | Memory: 44.6 MB (63.69%) - LeetSync

---
+++ 945-snakes-and-ladders/snakes-and-ladders.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int snakesAndLadders(int[][] board) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = board.length;
+        for(int i = 2; i <= n * n; i++){
+            int[] pos = getPos(i, n);
+            int x = pos[0], y = pos[1];
+            if(board[x][y] != -1) map.put(i, board[x][y]);
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        Set<Integer> seen = new HashSet<>();
+        q.add(1);
+
+        int moves = 0;
+        while(!q.isEmpty()){
+            int size = q.size();
+            while(size-- > 0){
+                int pos = q.poll();
+                if(pos >= n * n) return moves;
+                if(seen.contains(pos)) continue;
+                for(int i = pos + 1; i <= pos + 6; i++){
+                    int nextPos = map.containsKey(i) ? map.get(i) : i;
+                    
+                    if(!seen.contains(nextPos)) q.add(nextPos);
+                    seen.add(pos);
+                }
+                
+            }
+            moves++;
+        }
+        return -1;
+    }
+
+    private int[] getPos(int ind, int n){
+        ind--; //0-indexed
+        int row = n - 1 - (ind/n); 
+        int col = ind % n;
+        boolean isEven = (ind/n) % 2 == 0;
+        return isEven ? new int[]{row, col} : new int[]{row, n - 1 - col};
+    }
+}
