b4b7b45315cc17b89086cd2494cd3f0d80bc50ea
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-04-05 21:27:27 -0500

    Time: 16 ms (33.03%) | Memory: 43 MB (65.21%) - LeetSync

---
+++ 16-3sum-closest/3sum-closest.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length - 2; i++){
+            int left = i + 1, right = nums.length - 1;
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(Math.abs(target - sum)  < Math.abs(min)) min = target - sum;
+                if(sum > target) right--;
+                else left++;
+            }
+        }
+        return target - min;
+    }
+}
