e5e86b94a7cfca9187f8f1ab4df4ae73068f8211
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-04-06 00:00:05 -0500

    Time: 1 ms (79.87%) | Memory: 41.2 MB (84.66%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\."), v2 = version2.split("\\.");
+        for(int i = 0; i < Math.max(v1.length, v2.length); i++){
+            int num1 = i < v1.length ? Integer.parseInt(v1[i]) : 0, num2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
+            if(num1 != num2) return num1 > num2 ? 1 : -1;
+        }
+        return 0;
+    }
+}
