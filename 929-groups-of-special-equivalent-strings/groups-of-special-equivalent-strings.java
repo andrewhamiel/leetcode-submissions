2db7e8bdf49ba2273a8c51f72df57862e0bc8202
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-08-16 15:54:45 -0500

    Time: 16 ms (47.22%) | Memory: 44.4 MB (56.02%) - LeetSync

--- 929-groups-of-special-equivalent-strings/groups-of-special-equivalent-strings.java
+++ 929-groups-of-special-equivalent-strings/groups-of-special-equivalent-strings.java
@@ -1,14 +1,13 @@
 class Solution {
     public int numSpecialEquivGroups(String[] words) {
         Set<String> result = new HashSet<>();
-
-        for(String word : words){
+        for(String word : words) {
             int[] odd = new int[26], even = new int[26];
-            for(int i = 0; i < word.length(); i++){
+            for(int i = 0; i < word.length(); i++) {
                 if(i % 2 == 0) even[word.charAt(i) - 'a']++;
                 else odd[word.charAt(i) - 'a']++;
             }
-            result.add(Arrays.toString(odd) + Arrays.toString(even));
+            result.add(Arrays.toString(even) + Arrays.toString(odd));
         }
         return result.size();
     }
