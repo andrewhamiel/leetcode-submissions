0870276bb50d5e8b40e10cd28b5b90d5885bfe99
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-08-23 15:41:40 -0500

    Time: 25 ms (67.03%) | Memory: 56.3 MB (92.75%) - LeetSync

--- 2374-steps-to-make-array-non-decreasing/steps-to-make-array-non-decreasing.java
+++ 2374-steps-to-make-array-non-decreasing/steps-to-make-array-non-decreasing.java
@@ -1,20 +1,23 @@
 class Solution {
     public int totalSteps(int[] nums) {
-        Stack stack = new Stack<Integer>();
-        stack.push(0);
-        int steps=0;
-        int[] step_arr = new int[nums.length]; 
-        for(int i=1;i<nums.length;i++){
-            int max_steps = 0;
-            while(!stack.empty() && nums[i]>=nums[(Integer)stack.peek()]){
-                max_steps=Math.max(max_steps,step_arr[(Integer)stack.peek()]);
-                stack.pop();
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.addFirst(0);
+
+        int steps = 0;
+        int[] stepArr = new int[nums.length];
+        for(int i = 1; i < nums.length; i++) {
+            int maxSteps = 0;
+            while(!stack.isEmpty() && nums[i] >= nums[stack.peekFirst()]){
+                maxSteps = Math.max(maxSteps, stepArr[stack.peekFirst()]);
+                stack.removeFirst();
             }
-            if(!stack.empty() && nums[i]<nums[(Integer)stack.peek()]){
-                    step_arr[i]=1+max_steps;
+
+            if(!stack.isEmpty() && nums[i] < nums[stack.peekFirst()]){
+                stepArr[i] = maxSteps + 1;
             }
-            steps = Math.max(steps,step_arr[i]);
-            stack.push(i);
+
+            steps = Math.max(steps, stepArr[i]);
+            stack.addFirst(i);
         }
         return steps;
     }
