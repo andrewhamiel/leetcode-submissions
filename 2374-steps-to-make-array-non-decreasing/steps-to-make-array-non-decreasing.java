434fec4d9d2ee276a89a0ba73b57bdeb0eb4e634
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-08-23 15:51:26 -0500

    Time: 25 ms (67.03%) | Memory: 56.3 MB (92.75%) - LeetSync

--- 2374-steps-to-make-array-non-decreasing/steps-to-make-array-non-decreasing.java
+++ 2374-steps-to-make-array-non-decreasing/steps-to-make-array-non-decreasing.java
@@ -3,22 +3,22 @@ class Solution {
         Deque<Integer> stack = new ArrayDeque<>();
         stack.addFirst(0);
 
-        int steps = 0;
-        int[] stepArr = new int[nums.length];
+        int result = 0;
+        int[] stepsArr = new int[nums.length];
         for(int i = 1; i < nums.length; i++){
             int maxSteps = 0;
             while(!stack.isEmpty() && nums[i] >= nums[stack.peekFirst()]){
-                maxSteps = Math.max(maxSteps, stepArr[stack.peekFirst()]);
+                maxSteps = Math.max(maxSteps, stepsArr[stack.peekFirst()]);
                 stack.removeFirst();
             }
 
             if(!stack.isEmpty() && nums[i] < nums[stack.peekFirst()]){
-                stepArr[i] = maxSteps + 1;
+                stepsArr[i] = maxSteps + 1;
             }
 
-            steps = Math.max(steps, stepArr[i]);
+            result = Math.max(result, stepsArr[i]);
             stack.addFirst(i);
         }
-        return steps;
+        return result;
     }
 }
