f85e554e6679f145be656ee6d1f716c1b7bdcea9
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-09-06 09:38:26 -0500

    Time: 1 ms (58.42%) | Memory: 41.3 MB (39.60%) - LeetSync

--- 1103-moving-stones-until-consecutive/moving-stones-until-consecutive.java
+++ 1103-moving-stones-until-consecutive/moving-stones-until-consecutive.java
@@ -1,21 +1,20 @@
 class Solution {
     public int[] numMovesStones(int a, int b, int c) {
-       int arr[]=new int[]{a, b, c};
-       Arrays.sort(arr);
+        int[] arr = new int[]{a, b, c};
+        Arrays.sort(arr);
 
-       int max=(arr[1]-arr[0]-1)+(arr[2]-arr[1]-1), min=0;
-       if(arr[2]-arr[1]==1){
-          min=Math.min(arr[1]-arr[0]-1,1);
-       } else{
-           if(arr[1]-arr[0]==1){
-               min=1;
-           }else{
-               if(arr[2]-arr[1]==2 || arr[1]-arr[0]==2)  min=1;
-               else min=2;   
-           }
-       }
+        int max = (arr[1] - arr[0] - 1) + (arr[2] - arr[1] - 1), min = 0;
 
-       int result[]=new int[]{min, max};
-       return result;
+        if(arr[2] - arr[1] == 1) min = Math.min(arr[1] - arr[0] - 1, 1);
+        else{
+            if(arr[1] - arr[0] == 1) min = 1;
+            else{
+                if(arr[2] - arr[1] == 2 || arr[1] - arr[0] == 2) min = 1;
+                else min = 2;
+            }
+        }
+
+        int[] result = new int[]{min, max};
+        return result;
     }
 }
