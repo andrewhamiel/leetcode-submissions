dd452f5900d1cdfe7f30511e377b24225b215535
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-09-18 13:27:01 -0500

    Time: 1 ms (19.88%) | Memory: 44.4 MB (38.34%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.java
+++ 777-toeplitz-matrix/toeplitz-matrix.java
@@ -1,16 +1,9 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        //check starting at each row
-        for(int row = 0; row < matrix.length; row++) if(!isToeplitz(row, 0, matrix)) return false;
-        //check starting in first row at each column after zero
-        for(int col = 1; col < matrix[0].length; col++) if(!isToeplitz(0, col, matrix)) return false;
-        return true;
-    }
-
-    private boolean isToeplitz(int row, int col, int[][] matrix){
-        int originalValue = matrix[row][col];
-        while(row < matrix.length && col < matrix[0].length){
-            if(matrix[row++][col++] != originalValue) return false;
+        for(int row = 1; row < matrix.length; row++) {
+            for(int col = 1; col < matrix[0].length; col++){
+                if(matrix[row - 1][col - 1] != matrix[row][col]) return false;
+            }
         }
         return true;
     }
