7b83f367f5c8f374faf221bb3438ef92e89b2738
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-09-18 12:11:18 -0500

    Time: 0 ms (100.00%) | Memory: 41.5 MB (52.18%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        while(root != null){
+            if(root.right != null){
+                TreeNode successor = root.right;
+                while(successor.left != null && successor.left != root){
+                    successor = successor.left;
+                }
+
+                if(successor.left == null){
+                    //unexplored 
+                    result.add(root.val);
+                    successor.left = root;
+                    root = root.right;
+                }else{
+                    successor.left = null;
+                    root = root.left;
+                }
+            }else{
+                result.add(root.val);
+                root = root.left;
+            }
+        }
+
+        Collections.reverse(result);
+        return result;
+    }
+}
