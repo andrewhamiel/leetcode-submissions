5b902752aeeb4bb4d0bab78e339f16c8b9665f45
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-09-23 13:48:00 -0500

    Time: 1 ms (100.00%) | Memory: 46.2 MB (13.46%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -4,21 +4,20 @@ class Solution {
 
         int left = 0, right = nums1.length;
         while(left <= right){
-            int midAndPartitionA = left + (right - left)/2;
-            int partitionB = (nums1.length + nums2.length + 1)/2 - midAndPartitionA;
+            int partitionA = left + (right - left)/2;
+            int partitionB = (nums1.length + nums2.length + 1)/2 - partitionA;
 
-            int maxLeftA = midAndPartitionA == 0 ? Integer.MIN_VALUE : nums1[midAndPartitionA - 1];
-            int minRightA = midAndPartitionA == nums1.length ? Integer.MAX_VALUE : nums1[midAndPartitionA];
+            int maxLeftA = partitionA == 0 ? Integer.MIN_VALUE : nums1[partitionA - 1];
+            int minRightA = partitionA == nums1.length ? Integer.MAX_VALUE : nums1[partitionA];
 
             int maxLeftB = partitionB == 0 ? Integer.MIN_VALUE : nums2[partitionB - 1];
             int minRightB = partitionB == nums2.length ? Integer.MAX_VALUE : nums2[partitionB];
 
-            //Partition size correct
-            if(maxLeftA <= minRightB && maxLeftB <= minRightA){
+            if(maxLeftA <= minRightB && maxLeftB <= minRightA) {
                 if((nums1.length + nums2.length) % 2 == 0) return (double)(Math.max(maxLeftA, maxLeftB) + Math.min(minRightA, minRightB))/2;
                 else return Math.max(maxLeftA, maxLeftB);
-            }else if(maxLeftA > minRightB) right = midAndPartitionA - 1; //left partition too large
-            else left = midAndPartitionA + 1; //right partition too large
+            }else if(maxLeftA > minRightB) right = partitionA - 1; //left partition too large
+            else left = partitionA + 1; //right partition too large
         }
         return -1.0;
     }
