1a9243e71129222598a0c7f7aa80064c61149810
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-10-16 13:02:25 -0500

    Time: 3 ms (56.49%) | Memory: 48.1 MB (33.35%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -5,10 +5,10 @@ class Solution {
         for(int right = 0; right < nums.length; right++) {
             currSum+= nums[right];
 
-            while(left < right && (nums[left] == 0 || currSum > goal)){
+            while(left < right && (nums[left] == 0 || currSum > goal)) {
                 if(nums[left] == 0) prefixZeroes++;
                 else prefixZeroes = 0;
-
+                
                 currSum-= nums[left++];
             }
 
