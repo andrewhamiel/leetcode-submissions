c42a6083234dcc34ee375e2893f08d09cdacc63c
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-10-18 18:47:31 -0500

    Time: 80 ms (27.38%) | Memory: 54.2 MB (73.58%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.java
+++ 493-reverse-pairs/reverse-pairs.java
@@ -9,12 +9,12 @@ class Solution {
         int mid = low + (high - low)/2;
         int pairs = mergeSort(low, mid, nums) + mergeSort(mid + 1, high, nums) + countPairs(low, high, nums);
 
-        merge(low, mid, high, nums);
+        merge(low, high, nums);
         return pairs;
     }
 
     private int countPairs(int low, int high, int[] nums) {
-        int count = 0, mid = low + (high - low)/2, j = mid + 1;
+        int mid = low + (high - low)/2, count = 0, j = mid + 1;
         for(int i = low; i <= mid; i++) {
             while(j <= high && nums[i] > 2 *(long) nums[j]) j++;
             count+= j - mid - 1;
@@ -22,14 +22,15 @@ class Solution {
         return count;
     }
 
-    private void merge(int low, int mid, int high, int[] nums) {
+    private void merge(int low, int high, int[] nums) {
         List<Integer> tmp = new ArrayList<>();
-        int left = low, right = mid + 1;
+
+        int mid = low + (high - low)/2, left = low, right = mid + 1;
         while(left <= mid && right <= high) {
             if(nums[left] <= nums[right]) tmp.add(nums[left++]);
             else tmp.add(nums[right++]);
         }
-
+        
         while(left <= mid) tmp.add(nums[left++]);
         while(right <= high) tmp.add(nums[right++]);
 
