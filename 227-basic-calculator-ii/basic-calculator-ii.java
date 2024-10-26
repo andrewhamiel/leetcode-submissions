65b572530b2d01a1e68eb34b274e45a4bf790763
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-10-25 19:00:12 -0500

    Time: 16 ms (77.25%) | Memory: 45.1 MB (76.94%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -1,28 +1,27 @@
 class Solution {
     public int calculate(String s) {
-        char operation = '+';
-        int ans = 0, prev = 0, curr = 0;
-        for(int i = 0; i < s.length(); i++){
+        int curr = 0, prev = 0, result = 0;
+        char op = '+';
+        for(int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if(Character.isDigit(c)){
                 curr*= 10;
-                curr+= (int)(c - '0');
+                curr+= (c - '0');
+
             }
             if(i == s.length() - 1 || (!Character.isDigit(c) && !Character.isWhitespace(c))){
-                if(operation == '+' || operation == '-'){
-                    if(operation == '-') curr*= -1;
-                    ans+= prev;
+                if(op == '+' || op == '-'){
+                    result+= prev;
+                    if(op == '-') curr*= -1;
                     prev = curr;
-                }else if(operation == '*') prev*= curr;
-                else if(operation == '/') prev/= curr;
+                }else if(op == '*') prev*= curr;
+                else if(op == '/') prev/= curr;
 
+                op = c;
                 curr = 0;
-                operation = c;
             }
         }
-
-        ans+= prev;
-
-        return ans;
+        result+= prev;
+        return result;
     }
 }
