ec61e6689959f54c1419e8f4d2b5f955fcbbdb6f
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-10-25 19:02:21 -0500

    Time: 16 ms (77.25%) | Memory: 44.4 MB (97.03%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -2,13 +2,14 @@ class Solution {
     public int calculate(String s) {
         int curr = 0, prev = 0, result = 0;
         char op = '+';
+        
         for(int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if(Character.isDigit(c)){
                 curr*= 10;
                 curr+= (c - '0');
-
             }
+
             if(i == s.length() - 1 || (!Character.isDigit(c) && !Character.isWhitespace(c))){
                 if(op == '+' || op == '-'){
                     result+= prev;
