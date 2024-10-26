a4893003322f19a7151f0c5cbcda190d88b8f26a
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-10-25 19:27:23 -0500

    Time: 7 ms (95.75%) | Memory: 57.8 MB (55.71%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,16 +1,16 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        return quickselect(0, nums.length - 1, nums, nums.length - k);
+        return quickselect(0, nums.length - 1, nums, k - 1);
     }
 
-    private int quickselect(int left, int right, int[] nums, int k){
+    private int quickselect(int left, int right, int[] nums, int k) {
         if(left >= right) return nums[left];
 
         int pivotIndex = new Random().nextInt(right - left) + left;
         pivotIndex = partition(left, right, nums, pivotIndex);
 
         if(pivotIndex == k) return nums[k];
-        else if(pivotIndex < k){
+        else if(pivotIndex < k) {
             pivotIndex++;
             while(pivotIndex < k && nums[pivotIndex] == nums[pivotIndex - 1]) pivotIndex++;
             return quickselect(pivotIndex, right, nums, k);
@@ -21,19 +21,18 @@ class Solution {
         }
     }
 
-    private int partition(int left, int right, int[] nums, int pivotIndex){
+    private int partition(int left, int right, int[] nums, int pivotIndex) {
         int pivotValue = nums[pivotIndex];
         swap(pivotIndex, right, nums);
         int swapIndex = left;
-
-        for(int i = left; i <= right; i++){
-            if(nums[i] < pivotValue) swap(i, swapIndex++, nums);
+        for(int i = left; i <= right; i++) {
+            if(nums[i] > pivotValue) swap(swapIndex++, i, nums);
         }
         swap(swapIndex, right, nums);
         return swapIndex;
     }
 
-    private void swap(int i, int j, int[] nums){
+    private void swap(int i, int j, int[] nums) {
         int tmp = nums[i];
         nums[i] = nums[j];
         nums[j] = tmp;
