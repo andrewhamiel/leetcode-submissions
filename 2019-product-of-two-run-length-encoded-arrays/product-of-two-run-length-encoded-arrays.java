a900d67fc4e533b62d899ea30ec6ca2bb241549c
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-10-30 18:49:53 -0500

    Time: 33 ms (27.50%) | Memory: 105.4 MB (14.88%) - LeetSync

--- 2019-product-of-two-run-length-encoded-arrays/product-of-two-run-length-encoded-arrays.java
+++ 2019-product-of-two-run-length-encoded-arrays/product-of-two-run-length-encoded-arrays.java
@@ -3,17 +3,15 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
 
         int e1 = 0, e2 = 0;
-        while(e1 < encoded1.length && e2 < encoded2.length){
+        while(e1 < encoded1.length && e2 < encoded2.length) {
             int[] first = encoded1[e1], second = encoded2[e2];
 
             int product = first[0] * second[0];
             int commonFreq = Math.min(first[1], second[1]);
-            if(!result.isEmpty() && result.get(result.size() -1).get(0) == product){
-                List<Integer> prev = result.get(result.size() - 1);
-                int prevFreq = prev.get(1);
-                prev.set(1, prevFreq + commonFreq);
-                result.set(result.size() - 1, prev);
-            }else{
+            if(!result.isEmpty() && result.get(result.size() - 1).get(0) == product) {
+                int prevFreq = result.get(result.size() - 1).get(1);
+                result.get(result.size() - 1).set(1, prevFreq + commonFreq);
+            }else {
                 List<Integer> curr = new ArrayList<>();
                 curr.add(product);
                 curr.add(commonFreq);
