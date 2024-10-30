6715ca8ced5b676dfb4a77651a2f5306791e7e6a
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-10-30 16:22:58 -0500

    Time: 0 ms (100.00%) | Memory: 45.1 MB (13.72%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
@@ -24,24 +24,36 @@
  * }
  */
 class Solution {
+    ListNode head;
     public TreeNode sortedListToBST(ListNode head) {
         if(head == null) return null;
-        List<Integer> list = new ArrayList<>();
+        this.head = head;
+        //1. Find size
+        int size = 0;
         ListNode curr = head;
         while(curr != null) {
-            list.add(curr.val);
             curr = curr.next;
+            size++;
         }
-
-        return divideAndConquer(0, list.size() - 1, list);
+        //2. Divide/Conquer inorder traversal
+        return divideAndConquer(0, size - 1);
     }
 
-    private TreeNode divideAndConquer(int left, int right, List<Integer> list) {
+    private TreeNode divideAndConquer(int left, int right) {
         if(left > right) return null;
+
         int mid = left + (right - left)/2;
-        TreeNode node = new TreeNode(list.get(mid));
-        node.left = divideAndConquer(left, mid - 1, list);
-        node.right = divideAndConquer(mid + 1, right, list);
-        return node;
+        
+        //Left 
+        TreeNode leftNode = divideAndConquer(left, mid - 1);
+        //Root 
+        TreeNode root = new TreeNode(head.val);
+        //Right
+        head = head.next;
+        TreeNode rightNode = divideAndConquer(mid + 1, right);
+
+        root.left = leftNode;
+        root.right = rightNode;
+        return root;
     }
 }
