afb895bbbe8e2a6ea1ec2e8eaa9e862ae99301cf
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-10-31 11:09:53 -0500

    Time: 1 ms (92.60%) | Memory: 41.7 MB (48.30%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -1,18 +1,18 @@
 class Solution {
-    TrieNode root;
-    String s;
+    private String s;
+    private TrieNode root;
 
     public List<String> wordBreak(String s, List<String> wordDict) {
-        //1. Build TrieNode
+        //1. Build Trie 
         root = new TrieNode();
-        for(String word : wordDict){
+        for(String word : wordDict) {
             TrieNode curr = root;
-            for(char c : word.toCharArray()){
+            for(char c : word.toCharArray()) {
                 curr.children.putIfAbsent(c, new TrieNode());
                 curr = curr.children.get(c);
             }
             curr.isWord = true;
-        }    
+        }
 
         this.s = s;
         List<String> result = new ArrayList<>();
@@ -20,30 +20,30 @@ class Solution {
         return result;
     }
 
-    private void dp(int ind, StringBuilder sb, List<String> result){
-        if(ind == s.length() && !sb.isEmpty()){
+    private void dp(int ind, StringBuilder sb, List<String> result) {
+        if(ind == s.length() && !sb.isEmpty()) {
             result.add(sb.toString());
             return;
         }
 
         TrieNode curr = root;
-        for(int i = ind; i < s.length(); i++){
+        for(int i = ind; i < s.length(); i++) {
             char c = s.charAt(i);
-            if(!curr.children.containsKey(c)) break;
+            if(!curr.children.containsKey(c)) return;
 
-            curr = curr.children.get(c);
             sb.append(c);
-            if(curr.isWord){
-                //2 options: use or don't use
-                //1. Use
+            curr = curr.children.get(c);
+            if(curr.isWord) {
+                //2 options: use or don't use 
+                //1. Use 
                 StringBuilder using = new StringBuilder(sb);
                 if(i != s.length() - 1) using.append(" ");
                 dp(i + 1, using, result);
-                //2. Don't use: Just continue
+                //2. Don't use. Do nothing, just continue 
             }
         }
     }
-    
+
     class TrieNode {
         Map<Character, TrieNode> children = new HashMap<>();
         boolean isWord = false;
