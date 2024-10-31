76d083746b86fb98853bf41f63ca76a726acf412
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-10-31 11:59:34 -0500

    Time: 71 ms (9.46%) | Memory: 55.9 MB (6.00%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -2,11 +2,11 @@ class Solution {
     public int[][] kClosest(int[][] points, int k) {
         quickselect(0, points.length - 1, points, k - 1);
         int[][] result = new int[k][2];
-        for(int i = 0; i < result.length; i++) result[i] = points[i];
+        for(int i = 0; i < k; i++) result[i] = points[i];
         return result;
     }
 
-    private void quickselect(int left, int right, int[][] points, int k){
+    private void quickselect(int left, int right, int[][] points, int k) {
         if(left >= right) return;
 
         int pivotIndex = new Random().nextInt(right - left) + left;
@@ -17,25 +17,24 @@ class Solution {
         else quickselect(left, pivotIndex - 1, points, k);
     }
 
-    private int partition(int left, int right, int[][] points, int pivotIndex){
-        int pivotDistance = getDistance(points, pivotIndex);
-        swap(pivotIndex, right, points);
+    private int partition(int left, int right, int[][] points, int pivotInd) {
+        int pivotDistance = getDistance(pivotInd, points);
+        swap(pivotInd, right, points);
         int swapIndex = left;
-
-        for(int i = left; i <= right; i++){
-            if(getDistance(points, i) < pivotDistance) swap(i, swapIndex++, points);
+        for(int i = left; i <= right; i++) {
+            if(getDistance(i, points) < pivotDistance) swap(swapIndex++, i, points);
         }
         swap(swapIndex, right, points);
         return swapIndex;
     }
 
-    private void swap(int i, int j, int[][] points){
+    private void swap(int i, int j, int[][] points) {
         int[] tmp = points[i];
         points[i] = points[j];
         points[j] = tmp;
     }
 
-    private int getDistance(int[][] points, int p){
-        return (points[p][0] * points[p][0]) + (points[p][1] * points[p][1]);
+    private int getDistance(int i, int[][] points) {
+        return (points[i][0] * points[i][0]) + (points[i][1] * points[i][1]);
     }
 }
