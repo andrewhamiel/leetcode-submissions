edfa7f5a568e091e78b56170199e16a101cb578b
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-01 13:30:55 -0500

    Time: 4 ms (20.77%) | Memory: 42.9 MB (21.75%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.java
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.java
@@ -14,35 +14,36 @@
  * }
  */
 class Solution {
-    Map<Integer, List<Pair<Integer, Integer>>> map = new HashMap<>();
-    int minCol = 0, maxCol = 0;
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+        if(root == null) return new ArrayList<>();
+        Map<Integer, List<Pair<Integer, Integer>>> map = new HashMap<>();
+        Queue<Pair<TreeNode, int[]>> q = new LinkedList<>();
+        q.add(new Pair<>(root, new int[]{0, 0}));
 
-    public List<List<Integer>> verticalOrder(TreeNode root) {    
-        dfs(root, 0, 0);
+        int minCol = 0, maxCol = 0;
+        while(!q.isEmpty()) {
+            Pair<TreeNode, int[]> p = q.poll();
+            TreeNode curr = p.getKey();
+            int[] cell = p.getValue();
+            int row = cell[0], col = cell[1];
+            minCol = Math.min(minCol, col);
+            maxCol = Math.max(maxCol, col);
+            map.putIfAbsent(col, new ArrayList<>());
+            map.get(col).add(new Pair<>(row, curr.val));
+            if(curr.left != null) q.add(new Pair<>(curr.left, new int[]{row + 1, col - 1}));
+            if(curr.right != null) q.add(new Pair<>(curr.right, new int[]{row + 1, col + 1}));
+        }
 
         List<List<Integer>> result = new ArrayList<>();
         for(int i = minCol; i <= maxCol; i++) {
             if(map.containsKey(i)){
-            List<Pair<Integer, Integer>> list = map.get(i);
-            Collections.sort(list, (a, b) -> a.getKey() - b.getKey());
-            List<Integer> toAdd = new ArrayList<>();
-            for(Pair<Integer, Integer> p : list) toAdd.add(p.getValue());
-            result.add(toAdd);
+                List<Pair<Integer, Integer>> pairList = map.get(i);
+                Collections.sort(pairList, (a, b) -> a.getKey() - b.getKey());
+                List<Integer> list = new ArrayList<>();
+                for(Pair<Integer, Integer> p : pairList) list.add(p.getValue());
+                result.add(list);
             }
-            
         }
         return result;
-    }
-
-    private void dfs(TreeNode root, int row, int col) {
-        if(root == null) return;
-        minCol = Math.min(minCol, col);
-        maxCol = Math.max(maxCol, col);
-
-        map.putIfAbsent(col, new ArrayList<>());
-        map.get(col).add(new Pair<>(row, root.val));
-
-        dfs(root.left, row + 1, col - 1);
-        dfs(root.right, row + 1, col + 1);
     }
 }
