5329d1fd8fcd19f3f88151696b9db854e84540c0
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-02 10:18:27 -0500

    Time: 12 ms (9.70%) | Memory: 47.2 MB (5.63%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,62 +1,23 @@
 class Solution {
-    class Node {
-        int low, high;
-        Node left, right;
-
-        public Node(int[] interval) {
-            low = interval[0];
-            high = interval[1];
-        }
-    }
-
     public int[][] merge(int[][] intervals) {
-        Node root = new Node(intervals[0]);
-        //1. Add 
-        for(int i = 1; i < intervals.length; i++) add(root, intervals[i]);
-        //2. Merge 
-        List<int[]> merged = merge(root);
-        //3. Build result
-        int[][] result = new int[merged.size()][2];
-        for(int i = 0; i < result.length; i++) result[i] = merged.get(i);
-        return result;
-    }
-
-    private List<int[]> merge(Node root) {
-        if(root == null) return new ArrayList<>();
-
-        List<int[]> result = new ArrayList<>();
-        List<int[]> left = merge(root.left), right = merge(root.right);
-
-        boolean isInserted = false;
-        for(int[] interval : left) {
-            if(interval[1] >= root.low) {
-                interval[0] = Math.min(interval[0], root.low);
-                interval[1] = Math.max(interval[1], root.high);
-                root = new Node(interval);
-                isInserted = true;
-            }
-            result.add(interval);
-        } 
-
-        if(!isInserted) result.add(new int[]{root.low, root.high});
-
-        for(int[] interval : right) {
-            if(interval[0] <= root.high) {
-                interval[0] = Math.min(interval[0], root.low);
-                interval[1] = Math.max(interval[1], root.high);
-                root.high = interval[1];
-                result.remove(result.size() - 1);
-            }
-            result.add(interval);
+        if (intervals.length == 0) return new int[0][0];
+        List<int[]> list = new ArrayList<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for(int[] itv : intervals){
+            map.put(itv[0], map.getOrDefault(itv[0], 0)+1);
+            map.put(itv[1], map.getOrDefault(itv[1], 0)-1);
         }
-        return result;
-    }
-
-    private Node add(Node root, int[] interval) {
-        if(root == null) return new Node(interval);
-
-        if(interval[0] < root.low) root.left = add(root.left, interval);
-        else root.right = add(root.right, interval);
-        return root;
+        int count = 0, start = Integer.MAX_VALUE, end = Integer.MIN_VALUE;
+        for (int k : map.keySet()){
+            count+=map.get(k);
+            start = Math.min(start, k);
+            end = Math.max(end, k);
+            if (count==0){
+                list.add(new int[]{start, end});
+                start = Integer.MAX_VALUE;
+                end = Integer.MIN_VALUE;
+            }
+        }
+        return list.toArray(new int[list.size()][2]);
     }
 }
