cf08c975c0db1c285e0113ff0d02dcc5d57f0294
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-02 09:21:34 -0500

    Time: 9 ms (50.92%) | Memory: 46.9 MB (11.27%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,33 +1,41 @@
 class Solution {
+	class Node {
+		int low, high;
+		Node left, right;
+
+		public Node(int[] interval){
+			low = interval[0];
+			high = interval[1];
+		}
+	}
+
     public int[][] merge(int[][] intervals) {
-        IntervalNode root = new IntervalNode(intervals[0][0], intervals[0][1]);
+        Node root = new Node(intervals[0]);
 
-				//1. Add
-				for(int[] interval : intervals) add(root, interval);
-
-				//2. Merge
-				List<int[]> list = merge(root);
-
-				//3. Build result
-				int[][] result = new int[list.size()][2];
-				for(int i = 0; i < result.length; i++) result[i] = list.get(i);
-				return result;
+            //1. Add intervals
+            for(int i = 1; i < intervals.length; i++) add(root, intervals[i]);
+            //2. Merge
+            List<int[]> merged = mergeNode(root);
+            //3. Build result
+            int[][] result = new int[merged.size()][2];
+            for(int i = 0; i < result.length; i++) result[i] = merged.get(i);
+            return result;
     }
 
-		private List<int[]> merge(IntervalNode root){
+		private List<int[]> mergeNode(Node root){
 			if(root == null) return new ArrayList<>();
 			List<int[]> result = new ArrayList<>();
 
-			List<int[]> left = merge(root.left);
-			List<int[]> right = merge(root.right);
+			List<int[]> left = mergeNode(root.left);
+			List<int[]> right = mergeNode(root.right);
 
 			boolean isInserted = false;
 			for(int[] interval : left){
 				if(interval[1] >= root.low){
-					isInserted = true;
 					interval[0] = Math.min(interval[0], root.low);
 					interval[1] = Math.max(interval[1], root.high);
-					root = new IntervalNode(interval[0], interval[1]);
+					root = new Node(interval);
+					isInserted = true;
 				}
 				result.add(interval);
 			}
@@ -45,21 +53,11 @@ class Solution {
 			return result;
 		}
 
-		private IntervalNode add(IntervalNode root, int[] interval){
-			if(root == null) return new IntervalNode(interval[0], interval[1]);
+		private Node add(Node root, int[] interval){
+			if(root == null) return new Node(interval);
 
 			if(interval[0] < root.low) root.left = add(root.left, interval);
 			else root.right = add(root.right, interval);
 			return root;
-		}
-
-		class IntervalNode {
-			int low, high;
-			IntervalNode left, right;
-
-			public IntervalNode(int low, int high){
-				this.low = low;
-				this.high = high;
-			}
 		}
 }
