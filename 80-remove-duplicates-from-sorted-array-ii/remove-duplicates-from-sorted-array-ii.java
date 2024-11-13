6f90cb818704b1f62ed1240ea8d08e9c23604e9a
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-13 12:39:44 -0600

    Time: 0 ms (100.00%) | Memory: 44.4 MB (51.03%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -3,12 +3,13 @@ class Solution {
         int unique = 0, nonUnique = 0;
         while(nonUnique < nums.length) {
             if(nonUnique < nums.length - 1 && nums[nonUnique] == nums[nonUnique + 1]) {
+                //first element
                 swap(unique++, nonUnique++, nums);
+                //find last element
                 while(nonUnique < nums.length - 1 && nums[nonUnique] == nums[nonUnique + 1]) nonUnique++;
             }
             swap(unique++, nonUnique++, nums);
         }
-
         return unique;
     }
 
