f86e09d2c4b306fac764e5db298a6db59583e477
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-13 16:56:02 -0600

    Time: 1 ms (92.62%) | Memory: 45 MB (80.92%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.java
+++ 419-battleships-in-a-board/battleships-in-a-board.java
@@ -3,9 +3,9 @@ class Solution {
         int battleships = 0;
         for(int row = 0; row < board.length; row++) {
             for(int col = 0; col < board[0].length; col++) {
-                //check for nothing above, nothing to the left 
-                if((board[row][col] == 'X') && (col == 0 || board[row][col - 1] == '.')
-                    && (row == 0 || board[row - 1][col] == '.')) battleships++;
+                if((board[row][col] == 'X') 
+                    && (row == 0 || board[row - 1][col] == '.')
+                    && (col == 0 || board[row][col - 1] == '.')) battleships++;
             }
         }
         return battleships;
