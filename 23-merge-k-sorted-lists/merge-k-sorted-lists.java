7c285e7721be566e72441831bfd9399caf489988
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-13 12:51:44 -0600

    Time: 1 ms (99.87%) | Memory: 44.5 MB (30.66%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -14,34 +14,34 @@ class Solution {
         return mergeK(0, lists.length - 1, lists);
     }
 
-    private ListNode mergeK(int left, int right, ListNode[] lists){
+    private ListNode mergeK(int left, int right, ListNode[] lists) {
         if(left >= right) return lists[left];
-
         int mid = left + (right - left)/2;
         ListNode leftNode = mergeK(left, mid, lists), rightNode = mergeK(mid + 1, right, lists);
         return merge(leftNode, rightNode);
     }
 
-    private ListNode merge(ListNode a, ListNode b){
-        ListNode result = new ListNode(), curr = result;
-        while(a != null || b != null){
-            if(a != null && b != null){
-                if(a.val <= b.val){
+    private ListNode merge(ListNode a, ListNode b) {
+        ListNode curr = new ListNode(), head = curr;
+        while(a != null || b != null) {
+            if(a != null && b != null) {
+                if(a.val <= b.val) {
                     curr.next = a;
                     a = a.next;
-                }else{
+                }
+                else {
                     curr.next = b;
                     b = b.next;
                 }
-            }else if(a != null){
+            }else if(a != null) {
                 curr.next = a;
                 a = a.next;
-            }else{
+            }else {
                 curr.next = b;
                 b = b.next;
             }
             curr = curr.next;
         }
-        return result.next;
+        return head.next;
     }
 }
