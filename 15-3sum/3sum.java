c04992966aa35a7f334dfd23af9a392f8c0c6524
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-13 13:27:24 -0600

    Time: 25 ms (95.65%) | Memory: 51.1 MB (85.62%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -2,21 +2,21 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        for(int i = 0; i < nums.length - 2 && nums[i] <= 0; i++) {
-            if(i == 0 || nums[i] != nums[i - 1]) twoSumSorted(i + 1, -nums[i], nums, result); 
+        for(int i = 0; i < nums.length - 2; i++) {
+            if(nums[i] <= 0 && (i == 0 || nums[i] != nums[i - 1])) twoSumSorted(-nums[i], i + 1, nums, result);
         }
         return result;
     }
 
-    private void twoSumSorted(int i, int target, int[] nums, List<List<Integer>> result) {
-        int j = nums.length - 1;
-        while(i < j) {
-            int sum = nums[i] + nums[j];
+    private void twoSumSorted(int target, int ind, int[] nums, List<List<Integer>> result) {
+        int left = ind, right = nums.length - 1;
+        while(left < right) {
+            int sum = nums[left] + nums[right];
             if(sum == target) {
-                result.add(List.of(-target, nums[i++], nums[j]));
-                while(i < j && nums[i] == nums[i - 1]) i++;
-            }else if(sum < target) i++;
-            else j--;
+                result.add(Arrays.asList(-target, nums[left++], nums[right]));
+                while(left < right && nums[left] == nums[left - 1]) left++;
+            }else if(sum < target) left++;
+            else right--;
         }
     }
 }
