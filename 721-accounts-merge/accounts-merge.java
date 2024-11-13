fc8378751454dc86bb88d0ca7f98e8b0ec8b30ee
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-13 10:43:40 -0600

    Time: 27 ms (94.40%) | Memory: 47.3 MB (88.87%) - LeetSync

--- 721-accounts-merge/accounts-merge.java
+++ 721-accounts-merge/accounts-merge.java
@@ -2,17 +2,17 @@ class Solution {
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
         UnionFind uf = new UnionFind(accounts.size());
         Map<String, Integer> groups = new HashMap<>();
-        for(int i = 0; i < accounts.size(); i++){
+        for(int i = 0; i < accounts.size(); i++) {
             List<String> account = accounts.get(i);
-            for(int j = 1; j < account.size(); j++){
+            for(int j = 1; j < account.size(); j++) {
                 String email = account.get(j);
                 if(!groups.containsKey(email)) groups.put(email, i);
-                else uf.union(i, groups.get(email));
+                else uf.union(groups.get(email), i);
             }
         }
 
         Map<Integer, List<String>> components = new HashMap<>();
-        for(String email : groups.keySet()){
+        for(String email : groups.keySet()) {
             int oldGroup = groups.get(email);
             int rankedGroup = uf.find(oldGroup);
             components.putIfAbsent(rankedGroup, new ArrayList<>());
@@ -20,47 +20,47 @@ class Solution {
         }
 
         List<List<String>> result = new ArrayList<>();
-        for(int group : components.keySet()){
+        for(Integer group : components.keySet()) {
             List<String> emails = components.get(group);
             Collections.sort(emails);
-            String account = accounts.get(group).get(0);
-            List<String> toAdd = new ArrayList<>();
-            toAdd.add(account);
-            toAdd.addAll(emails);
-            result.add(toAdd);
+            String accountName = accounts.get(group).get(0);
+            List<String> list = new ArrayList<>();
+            list.add(accountName);
+            list.addAll(emails);
+            result.add(list);
         }
         return result;
     }
 
     class UnionFind {
-        int[] rank;
         int[] group;
+        int[] rank;
 
-        public UnionFind(int n){
-            rank = new int[n];
+        public UnionFind(int n) {
             group = new int[n];
-            
-            for(int i = 0; i < rank.length; i++){
+            rank = new int[n];
+            for(int i = 0; i < n; i++) {
                 group[i] = i;
                 rank[i] = 1;
             }
         }
 
-        public int find(int x){
+        public int find(int x) {
             if(group[x] != x) group[x] = find(group[x]);
             return group[x];
         }
 
-        public void union(int a, int b){
-            int group1 = find(a), group2 = find(b);
-            if(group1 == group2) return;
+        public void union(int a, int b) {
+            int groupA = find(a), groupB = find(b);
 
-            if(rank[group1] >= rank[group2]){
-                rank[group1]+= rank[group2];
-                group[group2] = group[group1];
+            if(groupA == groupB) return;
+
+            if(rank[groupA] <= rank[groupB]) {
+                group[groupA] = group[groupB];
+                rank[groupB]+= rank[groupA];
             }else {
-                rank[group2]+= rank[group1];
-                group[group1] = group[group2];
+                group[groupB] = group[groupA];
+                rank[groupA]+= rank[groupB];
             }
         }
     }
