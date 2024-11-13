188478a4d436949e9cb5834306ab28f22db55213
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-13 15:17:18 -0600

    Time: 13 ms (7.67%) | Memory: 46.8 MB (18.30%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        List<int[]> list = new ArrayList<>();
         TreeMap<Integer, Integer> count = new TreeMap<>();
         for(int[] interval : intervals) {
             count.put(interval[0], count.getOrDefault(interval[0], 0) + 1);
             count.put(interval[1], count.getOrDefault(interval[1], 0) - 1);
         }
 
+        List<int[]> list = new ArrayList<>();
         int currCount = 0, start = Integer.MAX_VALUE, end = Integer.MIN_VALUE;
-        for(int k : count.keySet()) {
-            currCount+= count.get(k);
-            start = Math.min(start, k);
-            end = Math.max(end, k);
-            
+        for(int key : count.keySet()) {
+            currCount+= count.get(key);
+            start = Math.min(start, key);
+            end = Math.max(end, key);
+
             if(currCount == 0) {
                 list.add(new int[]{start, end});
                 start = Integer.MAX_VALUE;
