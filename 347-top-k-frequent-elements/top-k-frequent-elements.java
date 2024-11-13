bac8ed6f1d1bef5ae27870f9a94d3fe47f9323c5
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-13 15:10:48 -0600

    Time: 12 ms (83.00%) | Memory: 45.4 MB (92.49%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,41 +1,42 @@
 class Solution {
-    Map<Integer, Integer> count = new HashMap<>();
+    Map<Integer, Integer> map = new HashMap<>();
 
     public int[] topKFrequent(int[] nums, int k) {
-        for(int num : nums) count.put(num, count.getOrDefault(num, 0) + 1);
+        for(int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
 
-        int[] freqs = new int[count.size()];
+        int[] freqs = new int[map.size()];
         int ind = 0;
-        for(int num : count.keySet()) freqs[ind++] = num;
+        for(int num : map.keySet()) freqs[ind++] = num;
 
         quickselect(0, freqs.length - 1, freqs, k - 1);
 
         int[] result = new int[k];
-        for(int i = 0; i < k; i++) result[i] = freqs[i];
+        for(int i = 0; i < result.length; i++) result[i] = freqs[i];
         return result;
     }
 
-    private void quickselect(int left, int right, int[] nums, int k){
+    private void quickselect(int left, int right, int[] nums, int k) {
         if(left >= right) return;
 
-        int pivotIndex = new Random().nextInt(right - left) + left;
-        pivotIndex = partition(left, right, nums, pivotIndex);
+        int pivotInd = new Random().nextInt(right - left) + left;
+        pivotInd = partition(left, right, nums, pivotInd);
 
-        if(pivotIndex == k) return;
-        else if(pivotIndex < k) quickselect(pivotIndex + 1, right, nums, k);
-        else quickselect(left, pivotIndex - 1, nums, k); 
+        if(pivotInd == k) return;
+        else if(pivotInd < k) quickselect(pivotInd + 1, right, nums, k);
+        else quickselect(left, pivotInd - 1, nums, k);
     }
 
-    private int partition(int left, int right, int[] nums, int pivotIndex) {
-        int pivotValue = count.get(nums[pivotIndex]);
-        swap(pivotIndex, right, nums);
-        int swapIndex = left;
-        
+    private int partition(int left, int right, int[] nums, int pivotInd) {
+        int pivotFreq = map.get(nums[pivotInd]);
+        swap(pivotInd, right, nums);
+        int swapInd = left;
         for(int i = left; i <= right; i++) {
-            if(count.get(nums[i]) > pivotValue) swap(swapIndex++, i, nums);
+            if(map.get(nums[i]) > pivotFreq) swap(swapInd++, i, nums);
         }
-        swap(swapIndex, right, nums);
-        return swapIndex;
+        swap(swapInd, right, nums);
+        return swapInd;
     }
 
     private void swap(int i, int j, int[] nums) {
