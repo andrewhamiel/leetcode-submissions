05931daaf3323ef14fafe39579001130dc49ea85
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-13 07:56:09 -0600

    Time: 1 ms (100.00%) | Memory: 42.1 MB (97.03%) - LeetSync

--- 65-valid-number/valid-number.java
+++ 65-valid-number/valid-number.java
@@ -1,18 +1,20 @@
 class Solution {
     public boolean isNumber(String s) {
         boolean isDigit = false, isExp = false, isSign = false, isDot = false;
-        for(int i = 0; i < s.length(); i++){
+        for(int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if(Character.isDigit(c)) isDigit = true;
-            else if(c == 'e' || c == 'E'){
-                if(i == 0 || !isDigit || isExp) return false;
-                isExp = true;
-                isDigit = false;
-            }else if(c == '+' || c == '-'){
+            if(Character.isDigit(c)) {
+                isDigit = true;
+                isSign = false;
+            }else if(c == '+' || c == '-') {
                 if(i > 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') return false;
                 isSign = true;
                 isDigit = false;
-            }else if(c == '.'){
+            }else if(c == 'e' || c == 'E') {
+                if(isExp || !isDigit) return false;
+                isExp = true;
+                isDigit = false;
+            }else if(c == '.') {
                 if(isExp || isDot) return false;
                 isDot = true;
             }else return false;
