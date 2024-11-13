458a019c66bf80c0d368ea9698cc997d18fb81b8
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-13 17:02:50 -0600

    Time: 0 ms (100.00%) | Memory: 44.6 MB (41.77%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode tortoise = findCycle(head);
+        if(tortoise == null) return null;
+
+        ListNode hare = head;
+        while(tortoise != head) {
+            tortoise = tortoise.next;
+            hare = hare.next;
+            if(tortoise == hare) break;
+        }
+        return tortoise;
+    }
+
+    private ListNode findCycle(ListNode head) {
+        ListNode tortoise = head, hare = head;
+        while(hare != null && hare.next != null) {
+            tortoise = tortoise.next;
+            hare = hare.next.next;
+            if(tortoise == hare) return tortoise;
+        }
+        return null;
+    }
+}
