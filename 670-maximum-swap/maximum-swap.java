ac07a8f7c9437c0059ed3dfb2518d5096eaf2d1e
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-13 13:56:01 -0600

    Time: 0 ms (100.00%) | Memory: 40.7 MB (19.06%) - LeetSync

--- 670-maximum-swap/maximum-swap.java
+++ 670-maximum-swap/maximum-swap.java
@@ -1,23 +1,23 @@
 class Solution {
     public int maximumSwap(int num) {
-        char[] nums = String.valueOf(num).toCharArray();
         int[] rightmost = new int[10];
-        for(int i = 0; i < nums.length; i++) rightmost[nums[i] - '0'] = i;
+        char[] arr = String.valueOf(num).toCharArray();
+        for(int i = 0; i < arr.length; i++) rightmost[arr[i] - '0'] = i;
 
-        for(int i = 0; i < nums.length; i++){
-            for(char c = '9'; c > nums[i]; c--){
-                if(rightmost[c - '0'] > i){
-                    swap(i, rightmost[c - '0'], nums);
-                    return Integer.parseInt(new String(nums));
+        for(int i = 0; i < arr.length; i++) {
+            for(char c = '9'; c > arr[i]; c--) {
+                if(rightmost[c - '0'] > i) {
+                    swap(i, rightmost[c - '0'], arr);
+                    return Integer.parseInt(new String(arr));
                 }
             }
         }
-        return Integer.parseInt(new String(nums));
+        return Integer.parseInt(new String(arr));
     }
 
-    private void swap(int i, int j, char[] nums){
-        char tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
+    private void swap(int i, int j, char[] arr) {
+        char tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 }
