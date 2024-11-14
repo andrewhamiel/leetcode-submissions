7e69078446a67f7c3f029561aed9e89c9ae6e6fd
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-13 19:08:54 -0600

    Time: 67 ms (6.33%) | Memory: 45.8 MB (18.95%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -2,21 +2,24 @@ class Solution {
     private int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
 
     public int shortestPathBinaryMatrix(int[][] grid) {
-        if(grid[0][0] != 0 || grid[grid.length - 1][grid[0].length - 1] != 0) return -1;
+        if(grid[0][0] == 1) return -1;
+
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{0, 0, 1});
+        Set<Pair<Integer, Integer>> visited = new HashSet<>();
 
-        while(!q.isEmpty()){
-            int[] cell = q.poll();
-            int row = cell[0], col = cell[1], steps = cell[2];
-            if(row == grid.length - 1 && col == grid[0].length - 1) return steps;
-            for(int[] dir : dirs){
+        while(!q.isEmpty()) {
+            int[] curr = q.poll();
+            int row = curr[0], col = curr[1], length = curr[2];
+            if(row == grid.length - 1 && col == grid[0].length - 1) return length;
+            
+            for(int[] dir : dirs) {
                 int newRow = row + dir[0], newCol = col + dir[1];
-                if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length
-                    && grid[newRow][newCol] == 0){
-                        grid[newRow][newCol] = 1;
-                        q.add(new int[]{newRow, newCol, steps + 1});
-                }
+                if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length 
+                    && grid[newRow][newCol] == 0 && !visited.contains(new Pair<>(newRow, newCol))) {
+                        visited.add(new Pair<>(newRow, newCol));
+                        q.add(new int[]{newRow, newCol, length + 1});
+                    }
             }
         }
         return -1;
