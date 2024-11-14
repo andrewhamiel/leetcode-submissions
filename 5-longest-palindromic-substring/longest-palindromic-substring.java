437a6b6ed3764900c69dc55b966a6c9b55f6313b
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-13 18:37:46 -0600

    Time: 15 ms (87.25%) | Memory: 42.2 MB (79.82%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,13 +1,13 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int longest = 0, leftInd = 0, rightInd = 0;
-        for(int i = 0; i < s.length() - 1; i++) {
+        int longestSubstring = 0, leftInd = 0, rightInd = 0;
+        for(int i = 0; i < s.length(); i++) {
             int oddLength = middleOut(i, i, s);
             int evenLength = middleOut(i, i + 1, s);
-            if(oddLength > longest || evenLength > longest) {
-                longest = Math.max(oddLength, evenLength);
-                leftInd = i - (longest - 1)/2;
-                rightInd = i + longest/2;
+            if(oddLength > longestSubstring || evenLength > longestSubstring) {
+                longestSubstring = Math.max(oddLength, evenLength);
+                leftInd = i - (longestSubstring - 1)/2;
+                rightInd = i + longestSubstring/2;
             }
         }
         return s.substring(leftInd, rightInd + 1);
@@ -15,7 +15,7 @@ class Solution {
 
     private int middleOut(int left, int right, String s) {
         int longest = 0;
-        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             longest = right - left + 1;
             left--;
             right++;
