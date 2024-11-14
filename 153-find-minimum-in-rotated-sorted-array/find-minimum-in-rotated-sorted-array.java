ffae2dbbf3991f107d5015a3058f33236857e3d7
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-13 18:50:28 -0600

    Time: 0 ms (100.00%) | Memory: 41.8 MB (74.84%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,11 +1,12 @@
 class Solution {
     public int findMin(int[] nums) {
-        if(nums.length == 0 || nums[0] < nums[nums.length - 1]) return nums[0];
+        if(nums[0] < nums[nums.length - 1]) return nums[0];
+
         int left = 0, right = nums.length;
-        while(left < right){
+        while(left < right) {
             int mid = left + (right - left)/2;
-            if(mid < nums.length - 1 && nums[mid] > nums[mid + 1]) return nums[mid + 1];
-            else if(nums[0] < nums[mid]) left = mid + 1;
+            if(mid == 0 || nums[mid - 1] > nums[mid]) return nums[mid];
+            else if(nums[mid] > nums[0]) left = mid + 1;
             else right = mid;
         }
         return nums[left];
