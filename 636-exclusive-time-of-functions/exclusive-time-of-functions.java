0516aab0864205611500e1052c236e2fc88bf456
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-13 19:31:58 -0600

    Time: 10 ms (97.72%) | Memory: 45 MB (90.93%) - LeetSync

--- 636-exclusive-time-of-functions/exclusive-time-of-functions.java
+++ 636-exclusive-time-of-functions/exclusive-time-of-functions.java
@@ -2,6 +2,7 @@ class Solution {
     public int[] exclusiveTime(int n, List<String> logs) {
         Deque<Integer> stack = new ArrayDeque<>();
         int[] result = new int[n];
+        
         int prevTime = 0;
         for(String log : logs) {
             String[] commaDelimited = log.split(":");
@@ -10,8 +11,8 @@ class Solution {
                 if(!stack.isEmpty()) result[stack.peekFirst()]+= currTime - prevTime;
                 stack.addFirst(Integer.parseInt(commaDelimited[0]));
                 prevTime = currTime;
-            }else {
-                currTime++; //ending function takes one unit of time
+            }else{
+                currTime++; //Ending function takes one unit of time
                 result[stack.removeFirst()]+= currTime - prevTime;
                 prevTime = currTime;
             }
