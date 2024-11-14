4af89addfed3838c9add0978f2d0945f8e7cac65
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-13 18:28:09 -0600

    Time: 29 ms (19.37%) | Memory: 58.1 MB (42.33%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/continuous-subarray-sum.java
@@ -3,7 +3,7 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, 0);
         int sum = 0;
-        for(int i = 0; i < nums.length; i++){
+        for(int i = 0; i < nums.length; i++) {
             sum+= nums[i];
             if(!map.containsKey(sum % k)) map.put(sum % k, i + 1);
             else if(map.get(sum % k) < i) return true;
