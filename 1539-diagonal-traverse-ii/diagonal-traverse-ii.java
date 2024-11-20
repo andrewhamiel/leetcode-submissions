bcdbedd6f987cca3f078966c7787187e2dfcd83e
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-20 16:22:45 -0600

    Time: 21 ms (93.95%) | Memory: 64.8 MB (75.11%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.java
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[] findDiagonalOrder(List<List<Integer>> nums) {
         List<Integer> list = new ArrayList<>();
-        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
-        q.add(new Pair<>(0, 0));
 
-        while(!q.isEmpty()){
-            Pair<Integer, Integer> p = q.poll();
-            int row = p.getKey(), col = p.getValue();
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0});
+
+        while(!q.isEmpty()) {
+            int[] cell = q.poll();
+            int row = cell[0], col = cell[1];
 
             list.add(nums.get(row).get(col));
 
-            if(col == 0 && row + 1 < nums.size()) q.add(new Pair<>(row + 1, col));
-            if(row < nums.size() && col + 1 < nums.get(row).size()) q.add(new Pair<>(row, col + 1));
+            if(col == 0 && row + 1 < nums.size()) q.add(new int[]{row + 1, col});
+            if(col + 1 < nums.get(row).size()) q.add(new int[]{row, col + 1});
         }
 
         int[] result = new int[list.size()];
