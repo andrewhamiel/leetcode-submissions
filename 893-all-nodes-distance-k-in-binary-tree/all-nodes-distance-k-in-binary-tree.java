1df9958b9daacc88a687b5d781c32a44ab27c43f
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-20 17:12:03 -0600

    Time: 14 ms (45.19%) | Memory: 44.5 MB (7.15%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -8,40 +8,55 @@
  * }
  */
 class Solution {
-    Map<Integer, List<Integer>> graph = new HashMap<>();
-
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        buildGraph(root, null);
+        Map<TreeNode, Set<TreeNode>> adj = new HashMap<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
 
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{target.val, 0});
-        Set<Integer> visited = new HashSet<>();
-        visited.add(target.val);
+        while(!q.isEmpty()) {
+            TreeNode curr = q.poll();
+            adj.putIfAbsent(curr, new HashSet<>());
+
+            if(curr.left != null) {               
+                adj.putIfAbsent(curr.left, new HashSet<>());
+                adj.get(curr).add(curr.left);
+                adj.get(curr.left).add(curr);
+                q.add(curr.left);
+            }
+            if(curr.right != null) {
+                adj.putIfAbsent(curr.right, new HashSet<>());
+                adj.get(curr).add(curr.right);
+                adj.get(curr.right).add(curr);
+                q.add(curr.right);
+            }
+        }
+
+        if(!adj.containsKey(target)) return new ArrayList<>();
+
         List<Integer> result = new ArrayList<>();
+        Set<TreeNode> visited = new HashSet<>();
+        visited.add(target);
+        q.add(target);
 
-        while(!q.isEmpty()){
-            int[] entry = q.poll();
-            int curr = entry[0], distance = entry[1];
-            visited.add(curr);
-            if(distance == k){
-                result.add(curr);
-                continue;
+        int dist = 0;
+        while(!q.isEmpty()) {
+            int size = q.size();
+            while(size-- > 0) {
+                TreeNode curr = q.poll();
+                if(dist == k) {
+                    result.add(curr.val);
+                    continue;
+                }
+
+                for(TreeNode neighbor : adj.get(curr)) {
+                    if(!visited.contains(neighbor)) {
+                        visited.add(neighbor);
+                        q.add(neighbor);
+                    }
+                }
             }
-            for(int neighbor : graph.getOrDefault(curr, new ArrayList<>())){
-                if(!visited.contains(neighbor)) q.add(new int[]{neighbor, distance + 1});
-            }
+            dist++;
         }
         return result;
-    }
-
-    private void buildGraph(TreeNode root, TreeNode parent){
-        if(root != null && parent != null){
-            graph.putIfAbsent(root.val, new ArrayList<>());
-            graph.putIfAbsent(parent.val, new ArrayList<>());
-            graph.get(root.val).add(parent.val);
-            graph.get(parent.val).add(root.val);
-        }
-        if(root.left != null) buildGraph(root.left, root);
-        if(root.right != null) buildGraph(root.right, root);
     }
 }
