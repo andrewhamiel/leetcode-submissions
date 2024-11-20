71b300d8ae110a3a88ef0be3fd23291f09b52f2b
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-20 17:51:26 -0600

    Time: 1 ms (100.00%) | Memory: 45 MB (46.09%) - LeetSync

--- 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.java
@@ -1,36 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-
     public ListNode insertGreatestCommonDivisors(ListNode head) {
-        // If the list contains only one node, return the head as no insertion is needed
-        if (head.next == null) return head;
+        if(head.next == null) return head;
 
-        // Initialize pointers to traverse the list
-        ListNode node1 = head;
-        ListNode node2 = head.next;
+        ListNode curr = head;
 
-        // Traverse the linked list
-        while (node2 != null) {
-            int gcdValue = calculateGCD(node1.val, node2.val);
-            ListNode gcdNode = new ListNode(gcdValue);
+        while(curr.next != null) {
+            ListNode nextNode = curr.next;
+            int gcdVal = calculateGcd(curr.val, nextNode.val);
+            ListNode gcd = new ListNode(gcdVal);
+            gcd.next = nextNode;
+            curr.next = gcd;
 
-            // Insert the GCD node between node1 and node2
-            node1.next = gcdNode;
-            gcdNode.next = node2;
-
-            // Move to the next pair of nodes
-            node1 = node2;
-            node2 = node2.next;
+            curr = nextNode;
         }
-
         return head;
     }
 
-    // Helper method to calculate the greatest common divisor using the Euclidean algorithm
-    private int calculateGCD(int a, int b) {
-        while (b != 0) {
-            int temp = b;
+    private int calculateGcd(int a, int b) {
+        while(b != 0) {
+            int tmp = b;
             b = a % b;
-            a = temp;
+            a = tmp;
         }
         return a;
     }
