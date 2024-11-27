2b8ab1d7a03eb1ac7e26d168fe6b765af4c9fe23
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-27 14:25:42 -0600

    Time: 137 ms (41.09%) | Memory: 45.4 MB (37.49%) - LeetSync

--- 691-stickers-to-spell-word/stickers-to-spell-word.java
+++ 691-stickers-to-spell-word/stickers-to-spell-word.java
@@ -1,41 +1,42 @@
 class Solution {
-    private Map<String, Integer> memo = new HashMap<>();
-    private List<int[]> freqs = new ArrayList<>();
+    List<int[]> freqs = new ArrayList<>();
+    Map<String, Integer> memo = new HashMap<>();
 
     public int minStickers(String[] stickers, String target) {
         for(String sticker : stickers) {
             int[] freq = new int[26];
-            for(char c : sticker.toCharArray()) freq[c - 'a']++;
+            for(char c : sticker.toCharArray()) {
+                freq[c - 'a']++;
+            }
             freqs.add(freq);
-        }
+        }    
 
         int minStickers = backtrack(target, null);
         return minStickers == Integer.MAX_VALUE ? -1 : minStickers;
     }
 
-    private int backtrack(String target, int[] currFreq) {
-        if(memo.containsKey(target)) return memo.get(target.toString());
+    private int backtrack(String target, int[] currSticker) {
+        if(memo.containsKey(target)) return memo.get(target);
 
-        StringBuilder remain = new StringBuilder();
+        StringBuilder remaining = new StringBuilder();
         int minStickers = 0;
-        if(currFreq != null) {
+        if(currSticker != null) {
             for(char c : target.toCharArray()) {
-                if(currFreq[c - 'a'] > 0){
-                    currFreq[c - 'a']--;
+                if(currSticker[c - 'a'] > 0) {
+                    currSticker[c - 'a']--;
                     minStickers = 1;
-                } 
-                else remain.append(c);
+                }else remaining.append(c);
             }
-        }else remain = new StringBuilder(target);
+        }else remaining = new StringBuilder(target);
 
-        if(!remain.isEmpty()) {
+        if(!remaining.isEmpty()) {
             int minAdditional = Integer.MAX_VALUE;
             for(int[] freq : freqs) {
-                if(freq[remain.charAt(0) - 'a'] > 0) {
-                    minAdditional = Math.min(minAdditional, backtrack(remain.toString(), freq.clone()));                    
+                if(freq[remaining.charAt(0) - 'a'] > 0) {
+                    minAdditional = Math.min(minAdditional, backtrack(remaining.toString(), freq.clone()));
                 }
             }
-            memo.put(remain.toString(), minAdditional);
+            memo.put(remaining.toString(), minAdditional);
             minStickers = minAdditional == Integer.MAX_VALUE ? Integer.MAX_VALUE : minStickers + minAdditional;
         }
         return minStickers;
