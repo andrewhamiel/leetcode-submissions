37e264d66a77454b946cddff25c416406ff242e0
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-27 08:17:30 -0600

    Time: 4 ms (84.36%) | Memory: 42.4 MB (27.85%) - LeetSync

--- 269-alien-dictionary/alien-dictionary.java
+++ 269-alien-dictionary/alien-dictionary.java
@@ -1,22 +1,22 @@
 class Solution {
     public String alienOrder(String[] words) {
-        //1. Build adjacencies
+        //1. Build adjacencies 
         Map<Character, List<Character>> adj = new HashMap<>();
         Map<Character, Integer> deps = new HashMap<>();
-        for(String word : words){
-            for(char c : word.toCharArray()){
+        for(String word : words) {
+            for(char c : word.toCharArray()) {
                 adj.putIfAbsent(c, new ArrayList<>());
                 deps.putIfAbsent(c, 0);
             }
         }
 
-        //2. Connect components
-        for(int i = 1; i < words.length; i++){
+        //2. Connect components 
+        for(int i = 1; i < words.length; i++) {
             String word1 = words[i - 1], word2 = words[i];
             if(word1.length() > word2.length() && word1.startsWith(word2)) return ""; //prevent substrings coming before
-            for(int wordInd = 0; wordInd < Math.min(word1.length(), word2.length()); wordInd++){
+            for(int wordInd = 0; wordInd < Math.min(word1.length(), word2.length()); wordInd++) {
                 char c1 = word1.charAt(wordInd), c2 = word2.charAt(wordInd);
-                if(c1 != c2){
+                if(c1 != c2) {
                     adj.get(c1).add(c2);
                     deps.put(c2, deps.get(c2) + 1);
                     break;
@@ -24,20 +24,21 @@ class Solution {
             }
         }
 
-        //3. Find vertices with no edges, topological sort
+        //3. Find vertices with no deps, topological sort
         Queue<Character> q = new LinkedList<>();
         for(char c : deps.keySet()) if(deps.get(c) == 0) q.add(c);
 
         StringBuilder result = new StringBuilder();
-        while(!q.isEmpty()){
+        while(!q.isEmpty()) {
             char c = q.poll();
             result.append(c);
-            for(char nextChar : adj.get(c)){
+            for(char nextChar : adj.get(c)) {
                 deps.put(nextChar, deps.get(nextChar) - 1);
                 if(deps.get(nextChar) == 0) q.add(nextChar);
             }
         }
-        //4. validate all characters added to dictionary
+
+        //4. Validate all characters added to dictionary
         return result.length() == deps.size() ? result.toString() : "";
     }
 }
