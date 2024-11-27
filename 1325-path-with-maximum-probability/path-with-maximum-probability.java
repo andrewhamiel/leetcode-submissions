3ea998af24d3f732f6ecf59f75632ba5c8a975f0
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-27 14:57:31 -0600

    Time: 44 ms (44.68%) | Memory: 59.6 MB (5.69%) - LeetSync

---
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.java
@@ -0,0 +1,33 @@
+class Solution {
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
+        Map<Integer, List<Pair<Integer, Double>>> graph = new HashMap<>();
+        for(int i = 0; i < edges.length; i++) {
+            int u = edges[i][0], v = edges[i][1];
+            double prob = succProb[i];
+            graph.computeIfAbsent(u, k -> new ArrayList<>()).add(new Pair<>(v, prob));
+            graph.computeIfAbsent(v, k -> new ArrayList<>()).add(new Pair<>(u, prob));
+        }
+
+        double[] maxProb = new double[n];
+        maxProb[start_node] = 1.0;
+
+        PriorityQueue<Pair<Integer, Double>> maxHeap = new PriorityQueue<>((a, b) -> -Double.compare(a.getValue(), b.getValue()));
+        maxHeap.add(new Pair<>(start_node, maxProb[start_node]));
+        while(!maxHeap.isEmpty()){
+            Pair<Integer, Double> p = maxHeap.poll();
+            int node = p.getKey();
+            double currProb = p.getValue();
+            if(node == end_node) return currProb;
+
+            for(Pair<Integer, Double> nextP : graph.getOrDefault(node, new ArrayList<>())){
+                int nextNode = nextP.getKey();
+                double nextProb = nextP.getValue();
+                if(currProb * nextProb > maxProb[nextNode]){
+                    maxProb[nextNode] = currProb * nextProb;
+                    maxHeap.add(new Pair<>(nextNode, maxProb[nextNode]));
+                }
+            }
+        }
+        return 0.0;
+    }
+}
