6ef794f79261e2875ef2625363c8ddf9e92b50a9
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-27 14:43:18 -0600

    Time: 4 ms (58.58%) | Memory: 45.6 MB (34.17%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.java
+++ 1028-interval-list-intersections/interval-list-intersections.java
@@ -1,21 +1,18 @@
 class Solution {
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
-        Queue<int[]> q = new LinkedList<>();
-
         int firstInd = 0, secondInd = 0;
+        List<int[]> list = new ArrayList<>();
+        
         while(firstInd < firstList.length && secondInd < secondList.length) {
             int[] first = firstList[firstInd], second = secondList[secondInd];
-
-            int xOverlap = Math.max(first[0], second[0]);
-            int yOverlap = Math.min(first[1], second[1]);
-            if(xOverlap <= yOverlap) q.add(new int[]{xOverlap, yOverlap});
-
+            int leftOverlap = Math.max(first[0], second[0]);
+            int rightOverlap = Math.min(first[1], second[1]);
+            if(leftOverlap <= rightOverlap) list.add(new int[]{leftOverlap, rightOverlap});
             if(first[1] <= second[1]) firstInd++;
             else secondInd++;
         }
-
-        int[][] result = new int[q.size()][2];
-        for(int i = 0; i < result.length; i++) result[i] = q.poll();
+        int[][] result = new int[list.size()][2];
+        for(int i = 0; i < result.length; i++) result[i] = list.get(i);
         return result;
     }
 }
