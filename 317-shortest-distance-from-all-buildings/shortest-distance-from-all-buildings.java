4490402b7b6b5f79c61c65c4539a49b69053689d
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-28 07:20:42 -0600

    Time: 97 ms (90.16%) | Memory: 44.7 MB (65.37%) - LeetSync

--- 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.java
+++ 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.java
@@ -3,20 +3,21 @@ class Solution {
 
     public int shortestDistance(int[][] grid) {
         int minDistance = Integer.MAX_VALUE, emptyLandValue = 0;
-        int[][] total = new int[grid.length][grid[0].length];
+        int[][] totalSteps = new int[grid.length][grid[0].length];
+
         for(int row = 0; row < grid.length; row++) {
             for(int col = 0; col < grid[0].length; col++) {
-                if(grid[row][col] == 1) minDistance = bfs(row, col, grid, total, emptyLandValue--);
+                if(grid[row][col] == 1) minDistance = bfs(row, col, grid, totalSteps, emptyLandValue--);
             }
         }    
         return minDistance == Integer.MAX_VALUE ? -1 : minDistance;
     }
 
     private int bfs(int i, int j, int[][] grid, int[][] total, int emptyLandValue) {
-        int steps = 1, minDistance = Integer.MAX_VALUE;
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{i, j});
 
+        int minDistance = Integer.MAX_VALUE, steps = 1;
         while(!q.isEmpty()) {
             int size = q.size();
             while(size-- > 0) {
@@ -25,7 +26,7 @@ class Solution {
 
                 for(int[] dir : dirs) {
                     int newRow = row + dir[0], newCol = col + dir[1];
-                    if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length
+                    if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length 
                         && grid[newRow][newCol] == emptyLandValue) {
                             grid[newRow][newCol]--;
                             total[newRow][newCol]+= steps;
