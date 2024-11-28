dbebdd041f142dc4c5cb5e2e021d429057b25888
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-28 07:10:11 -0600

    Time: 99 ms (87.84%) | Memory: 45.4 MB (8.17%) - LeetSync

--- 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.java
+++ 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.java
@@ -5,34 +5,35 @@ class Solution {
         int[][] total = new int[grid.length][grid[0].length];
         int minDistance = Integer.MAX_VALUE, emptyLandValue = 0;
 
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[0].length; j++){
-                if(grid[i][j] == 1) minDistance = bfs(i, j, grid, total, emptyLandValue--);
+        for(int row = 0; row < grid.length; row++) {
+            for(int col = 0; col < grid[0].length; col++) {
+                if(grid[row][col] == 1) minDistance = bfs(row, col, grid, total, emptyLandValue--);
             }
-        }
+        }  
+
         return minDistance == Integer.MAX_VALUE ? -1 : minDistance;
     }
 
-    private int bfs(int i, int j, int[][] grid, int[][] total, int emptyLandValue){
+    private int bfs(int i, int j, int[][] grid, int[][] total, int emptyLandValue) {
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{i, j});
         int minDistance = Integer.MAX_VALUE, steps = 0;
 
-        while(!q.isEmpty()){
+        while(!q.isEmpty()) {
             steps++;
             int size = q.size();
-            while(size-- > 0){
+            while(size-- > 0) {
                 int[] cell = q.poll();
                 int row = cell[0], col = cell[1];
-                for(int[] dir : dirs){
+                for(int[] dir : dirs) {
                     int newRow = row + dir[0], newCol = col + dir[1];
-                    if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length
-                        && grid[newRow][newCol] == emptyLandValue){
+                    if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length 
+                        && grid[newRow][newCol] == emptyLandValue) {
                             grid[newRow][newCol]--;
                             total[newRow][newCol]+= steps;
                             q.add(new int[]{newRow, newCol});
                             minDistance = Math.min(minDistance, total[newRow][newCol]);
-                    }
+                        }
                 }
             }
         }
