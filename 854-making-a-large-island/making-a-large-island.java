7bf4d6f4fbc9122402379fafbd19d7e43c8caaea
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-28 08:37:28 -0600

    Time: 80 ms (78.74%) | Memory: 89.7 MB (5.05%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.java
+++ 854-making-a-large-island/making-a-large-island.java
@@ -3,15 +3,15 @@ class Solution {
 
     public int largestIsland(int[][] grid) {
         Map<Integer, Integer> islandSizes = new HashMap<>();
-        int islandId = 2, maxSize = 0;
+        int maxArea = 0, islandId = 2;
         Queue<int[]> q = new LinkedList<>();
 
         for(int row = 0; row < grid.length; row++) {
             for(int col = 0; col < grid[0].length; col++) {
                 if(grid[row][col] == 1) {
-                    int size = dfs(row, col, grid, islandId);
-                    islandSizes.put(islandId++, size);
-                    maxSize = Math.max(maxSize, size);
+                    int area = dfs(row, col, grid, islandId);
+                    islandSizes.put(islandId++, area);
+                    maxArea = Math.max(maxArea, area);
                 }else if(grid[row][col] == 0) q.add(new int[]{row, col});
             }
         }
@@ -19,28 +19,29 @@ class Solution {
         while(!q.isEmpty()) {
             int[] cell = q.poll();
             int row = cell[0], col = cell[1];
+            Set<Integer> visitedIslands = new HashSet<>();
             int area = 1;
-            Set<Integer> visited = new HashSet<>();
             for(int[] dir : dirs) {
                 int newRow = row + dir[0], newCol = col + dir[1];
-                if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length && !visited.contains(grid[newRow][newCol])){
-                    visited.add(grid[newRow][newCol]);
-                    area+= islandSizes.getOrDefault(grid[newRow][newCol], 0);
-                }
+                if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length
+                    && grid[newRow][newCol] != 0 && !visitedIslands.contains(grid[newRow][newCol])){
+                        visitedIslands.add(grid[newRow][newCol]);
+                        area+= islandSizes.getOrDefault(grid[newRow][newCol], 0);
+                    }
             }
-            maxSize = Math.max(maxSize, area);
+            maxArea = Math.max(maxArea, area);
         }
-        return maxSize;
+        return maxArea;
     }
 
-    private int dfs(int row, int col, int[][] grid, int islandId) {
-        if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] != 1) return 0;
-        grid[row][col] = islandId;
+    private int dfs(int i, int j, int[][] grid, int islandId) {
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 1) return 0;
 
-        int left = dfs(row, col - 1, grid, islandId);
-        int right = dfs(row, col + 1, grid, islandId);
-        int up = dfs(row - 1, col, grid, islandId);
-        int down = dfs(row + 1, col, grid, islandId);
-        return 1 + left + right + up + down;
+        grid[i][j] = islandId;
+        int left = dfs(i, j - 1, grid, islandId);
+        int right = dfs(i, j + 1, grid, islandId);
+        int up = dfs(i - 1, j, grid, islandId);
+        int down = dfs(i + 1, j, grid, islandId);
+        return 1 + left + right + up + down; 
     }
 }
