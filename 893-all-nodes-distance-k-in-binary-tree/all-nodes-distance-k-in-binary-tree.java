1b613ddbaee880b648d5db6dc05208f755b58440
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-29 12:05:08 -0600

    Time: 14 ms (44.40%) | Memory: 43.3 MB (10.49%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -8,58 +8,38 @@
  * }
  */
 class Solution {
+    Map<TreeNode, List<TreeNode>> map = new HashMap<>();
+
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        Map<TreeNode, Set<TreeNode>> adj = buildGraph(root);
-
-        Queue<TreeNode> q = new LinkedList<>();
-        Set<TreeNode> visited = new HashSet<>();
-        q.add(target);
-        visited.add(target);
-
         List<Integer> result = new ArrayList<>();
-        
-        int dist = 0;
-        while(!q.isEmpty()) {
+        findNeighbors(root, null);
+        Set<TreeNode> visited = new HashSet<>();
+        visited.add(target);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(target);
+        while(!q.isEmpty() && k >= 0) {
             int size = q.size();
             while(size-- > 0) {
                 TreeNode curr = q.poll();
-                if(dist == k) result.add(curr.val);
-
-                for(TreeNode neighbor : adj.getOrDefault(curr, new HashSet<>())) {
+                if(k == 0) result.add(curr.val);
+                for(TreeNode neighbor : map.getOrDefault(curr, new ArrayList<>())) {
                     if(!visited.contains(neighbor)) {
                         visited.add(neighbor);
                         q.add(neighbor);
                     }
                 }
             }
-            dist++;
+            k--;
         }
         return result;
     }
 
-    private Map<TreeNode, Set<TreeNode>> buildGraph(TreeNode root) {
-        Map<TreeNode, Set<TreeNode>> adj = new HashMap<>();
-        
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-
-        while(!q.isEmpty()) {
-            TreeNode curr = q.poll();
-            adj.putIfAbsent(curr, new HashSet<>());
-
-            if(curr.left != null) {
-                adj.putIfAbsent(curr.left, new HashSet<>());
-                adj.get(curr).add(curr.left);
-                adj.get(curr.left).add(curr);
-                q.add(curr.left);
-            }
-            if(curr.right != null) {
-                adj.putIfAbsent(curr.right, new HashSet<>());
-                adj.get(curr).add(curr.right);
-                adj.get(curr.right).add(curr);
-                q.add(curr.right);
-            }
+    private void findNeighbors(TreeNode child, TreeNode parent) {
+        if(parent != null) {
+            map.computeIfAbsent(parent, k -> new ArrayList<>()).add(child);
+            map.computeIfAbsent(child, k -> new ArrayList<>()).add(parent);
         }
-        return adj;
+        if(child.left != null) findNeighbors(child.left, child);
+        if(child.right != null) findNeighbors(child.right, child);
     }
 }
