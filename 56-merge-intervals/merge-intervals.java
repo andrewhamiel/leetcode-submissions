1f4415bceef22273e1d985d9317a562fa0e06817
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-30 08:10:41 -0600

    Time: 13 ms (7.67%) | Memory: 46.9 MB (11.92%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -7,19 +7,19 @@ class Solution {
         }
 
         List<int[]> list = new ArrayList<>();
-        int currCount = 0, start = Integer.MAX_VALUE, end = Integer.MIN_VALUE;
+        int currCount = 0, left = Integer.MAX_VALUE, right = Integer.MIN_VALUE;
         for(int key : count.keySet()) {
             currCount+= count.get(key);
-            start = Math.min(start, key);
-            end = Math.max(end, key);
-
+            left = Math.min(left, key);
+            right = Math.max(right, key);
+            
             if(currCount == 0) {
-                list.add(new int[]{start, end});
-                start = Integer.MAX_VALUE;
-                end = Integer.MIN_VALUE;
+                list.add(new int[]{left, right});
+                left = Integer.MAX_VALUE;
+                right = Integer.MIN_VALUE;
             }
         }
-
+        
         int[][] result = new int[list.size()][2];
         for(int i = 0; i < result.length; i++) result[i] = list.get(i);
         return result;
