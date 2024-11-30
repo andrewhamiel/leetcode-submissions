f4f7ec97f1b450d05d8e9ac7caed5ca0820c5c36
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-11-30 08:57:25 -0600

    Time: 1 ms (92.80%) | Memory: 46.1 MB (6.31%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.java
+++ 419-battleships-in-a-board/battleships-in-a-board.java
@@ -1,13 +1,12 @@
 class Solution {
     public int countBattleships(char[][] board) {
-        int battleships = 0;
+        int result = 0;
         for(int row = 0; row < board.length; row++) {
             for(int col = 0; col < board[0].length; col++) {
-                if((board[row][col] == 'X') 
-                    && (row == 0 || board[row - 1][col] == '.')
-                    && (col == 0 || board[row][col - 1] == '.')) battleships++;
+                if(board[row][col] == 'X' && ((col + 1 >= board[0].length || board[row][col + 1] == '.')
+                && (row + 1 >= board.length || board[row + 1][col] == '.'))) result++;
             }
         }
-        return battleships;
+        return result;
     }
 }
