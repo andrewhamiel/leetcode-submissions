c3abd21591b81f6711f635d6f41f53bb5caa2ffe
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-04 17:33:46 -0600

    Time: 3 ms (87.76%) | Memory: 46.5 MB (82.57%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -3,7 +3,7 @@ class Solution {
         int left = 0, count = 0;
         for(int right = 0; right < nums.length; right++) {
             if(nums[right] == 0) k--;
-            while(k < 0 && nums[left++] == 0) k++;
+            if(k < 0 && nums[left++] == 0) k++;
             if(k >= 0) count = Math.max(count, right - left + 1);
         }
         return count;
