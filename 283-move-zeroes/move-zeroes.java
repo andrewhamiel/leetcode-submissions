fd84d75ab2ff134e8e7ed9aa635b06b86211ed80
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-04 14:05:30 -0600

    Time: 2 ms (84.49%) | Memory: 46.2 MB (14.18%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,13 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int zero = 0, nonZero = 0;
-        while(nonZero < nums.length){
+        while(nonZero < nums.length) {
             if(nums[nonZero] == 0) nonZero++;
             else swap(zero++, nonZero++, nums);
         }
     }
 
-    private void swap(int i, int j, int[] nums){
+    private void swap(int i, int j, int[] nums) {
         int tmp = nums[i];
         nums[i] = nums[j];
         nums[j] = tmp;
