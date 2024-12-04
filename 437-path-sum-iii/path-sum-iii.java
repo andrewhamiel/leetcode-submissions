d2c5c3ceb95c0b906a314e9081ddef50599a0f89
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-04 14:54:39 -0600

    Time: 3 ms (96.11%) | Memory: 44.4 MB (29.02%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int count = 0, k = 0;
+    private Map<Long, Integer> map = new HashMap<>();
+
+    public int pathSum(TreeNode root, int targetSum) {
+        k = targetSum;
+        preorder(root, 0);
+        return count;    
+    }
+
+    private void preorder(TreeNode root, long currSum) {
+        if(root == null) return;
+
+        currSum+= root.val;
+        if(currSum == k) count++;
+        count+= map.getOrDefault(currSum - k, 0);
+
+        map.put(currSum, map.getOrDefault(currSum, 0) + 1);
+
+        preorder(root.left, currSum);
+        preorder(root.right, currSum);
+
+        map.put(currSum, map.getOrDefault(currSum, 0) - 1);
+    }
+}
