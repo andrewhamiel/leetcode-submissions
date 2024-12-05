680a4ae98a028585206b6b07553d6794f3b34f6d
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-04 18:25:15 -0600

    Time: 11 ms (91.65%) | Memory: 45.5 MB (35.34%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.java
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.java
@@ -8,12 +8,12 @@ class Solution {
         characterMap['o' - 'a'] = 8;
         characterMap['u' - 'a'] = 16;
 
-        //2. First seen
+        //2. First seen indeces
         int[] firstSeen = new int[32];
         Arrays.fill(firstSeen, -1);
 
         //3. PrefixXOR
-        int longestSubstring = 0, prefixXOR = 0;
+        int prefixXOR = 0, longestSubstring = 0;
         for(int i = 0; i < s.length(); i++) {
             prefixXOR^= characterMap[s.charAt(i) - 'a'];
             if(firstSeen[prefixXOR] == -1 && prefixXOR != 0) firstSeen[prefixXOR] = i;
