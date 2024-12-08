cafde55ac9dc0176ba5f4fd14cddfa337b46afd9
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-08 11:28:18 -0600

    Time: 98 ms (89.52%) | Memory: 45.4 MB (11.32%) - LeetSync

--- 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.java
+++ 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.java
@@ -2,18 +2,19 @@ class Solution {
     private int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
 
     public int shortestDistance(int[][] grid) {
-        int minDistance = Integer.MAX_VALUE, emptyLandValue = 0;
         int[][] totalSteps = new int[grid.length][grid[0].length];
+        int minDistance = Integer.MAX_VALUE, emptyLandValue = 0;
 
         for(int row = 0; row < grid.length; row++) {
             for(int col = 0; col < grid[0].length; col++) {
                 if(grid[row][col] == 1) minDistance = bfs(row, col, grid, totalSteps, emptyLandValue--);
             }
-        }    
+        }
+
         return minDistance == Integer.MAX_VALUE ? -1 : minDistance;
     }
 
-    private int bfs(int i, int j, int[][] grid, int[][] total, int emptyLandValue) {
+    private int bfs(int i, int j, int[][] grid, int[][] totalSteps, int emptyLandValue) {
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{i, j});
 
@@ -26,12 +27,12 @@ class Solution {
 
                 for(int[] dir : dirs) {
                     int newRow = row + dir[0], newCol = col + dir[1];
-                    if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length 
+                    if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length
                         && grid[newRow][newCol] == emptyLandValue) {
+                            totalSteps[newRow][newCol]+= steps;
                             grid[newRow][newCol]--;
-                            total[newRow][newCol]+= steps;
                             q.add(new int[]{newRow, newCol});
-                            minDistance = Math.min(minDistance, total[newRow][newCol]);
+                            minDistance = Math.min(minDistance, totalSteps[newRow][newCol]);
                         }
                 }
             }
