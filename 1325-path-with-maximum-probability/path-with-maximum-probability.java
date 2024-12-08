15a2ce2ac33f050586f19572ff1a3cb46156affe
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-08 11:07:35 -0600

    Time: 49 ms (34.37%) | Memory: 59.5 MB (7.01%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.java
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.java
@@ -1,28 +1,30 @@
 class Solution {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
-        Map<Integer, List<Pair<Integer, Double>>> graph = new HashMap<>();
+        Map<Integer, List<Pair<Integer, Double>>> adj = new HashMap<>();
         for(int i = 0; i < edges.length; i++) {
             int u = edges[i][0], v = edges[i][1];
-            double prob = succProb[i];
-            graph.computeIfAbsent(u, k -> new ArrayList<>()).add(new Pair<>(v, prob));
-            graph.computeIfAbsent(v, k -> new ArrayList<>()).add(new Pair<>(u, prob));
+            double currProb = succProb[i];
+            adj.computeIfAbsent(u, k -> new ArrayList<>()).add(new Pair<>(v, currProb));
+            adj.computeIfAbsent(v, k -> new ArrayList<>()).add(new Pair<>(u, currProb));
         }
+
+        if(!adj.containsKey(end_node)) return 0.0;
 
         double[] maxProb = new double[n];
         maxProb[start_node] = 1.0;
-
-        PriorityQueue<Pair<Integer, Double>> maxHeap = new PriorityQueue<>((a, b) -> -Double.compare(a.getValue(), b.getValue()));
+        PriorityQueue<Pair<Integer, Double>> maxHeap = new PriorityQueue<>((a, b) -> Double.compare(b.getValue(), a.getValue()));
         maxHeap.add(new Pair<>(start_node, maxProb[start_node]));
+
         while(!maxHeap.isEmpty()) {
             Pair<Integer, Double> p = maxHeap.poll();
             int currNode = p.getKey();
             double currProb = p.getValue();
             if(currNode == end_node) return currProb;
 
-            for(Pair<Integer, Double> nextP : graph.getOrDefault(currNode, new ArrayList<>())) {
-                int nextNode = nextP.getKey();
-                double nextProb = nextP.getValue();
-                if(currProb * nextProb > maxProb[nextNode]) {
+            for(Pair<Integer, Double> nextPair : adj.getOrDefault(currNode, new ArrayList<>())) {
+                int nextNode = nextPair.getKey();
+                double nextProb = nextPair.getValue();
+                if(nextProb * currProb > maxProb[nextNode]) {
                     maxProb[nextNode] = currProb * nextProb;
                     maxHeap.add(new Pair<>(nextNode, maxProb[nextNode]));
                 }
