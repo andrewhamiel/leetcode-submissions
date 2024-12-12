76a4b96d70ea945a777c5559f2725638dacd4e1f
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-11 22:22:15 -0600

    Time: 3 ms (81.82%) | Memory: 60.9 MB (41.96%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.java
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.java
@@ -1,17 +1,18 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        int maxVal = 0, maxLength = 0, currLength = 0;
+        int maxVal = 0, currLength = 0, result = 0;
         for(int num : nums) {
             if(num > maxVal) {
                 maxVal = num;
-                maxLength = 0;
                 currLength = 0;
+                result = 0;
             }
+
             if(num == maxVal) currLength++;
             else currLength = 0;
-            
-            maxLength = Math.max(maxLength, currLength);
+
+            result = Math.max(result, currLength);
         }
-        return maxLength;
+        return result;
     }
 }
