9ec5bdb7e69f0847cff3856a9c9b8fd76903796a
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-13 15:01:33 -0600

    Time: 1 ms (86.06%) | Memory: 41.4 MB (9.74%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -6,6 +6,6 @@ class Solution {
             result+= x % 10;
             x/= 10;
         }
-        return result < Integer.MIN_VALUE || result > Integer.MAX_VALUE ? 0 : (int)result;
+        return result < Integer.MIN_VALUE || result > Integer.MAX_VALUE ? 0 : (int) result;
     }
 }
