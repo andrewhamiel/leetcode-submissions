b4b3bb2cfb423fa692f2b335a640cb65735669bc
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-13 15:05:00 -0600

    Time: 24 ms (97.94%) | Memory: 51.4 MB (57.95%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -4,18 +4,18 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         for(int i = 0; i < nums.length - 2 && nums[i] <= 0; i++) {
-            if(i == 0 || nums[i] != nums[i - 1]) twoSumSorted(i + 1, nums, -nums[i]);
-        }
+            if(i == 0 || nums[i] != nums[i - 1]) twoSumSorted(i + 1, -nums[i], nums);
+        }    
         return result;
     }
 
-    private void twoSumSorted(int left, int[] nums, int target) {
+    private void twoSumSorted(int left, int target, int[] nums) {
         int right = nums.length - 1;
         while(left < right) {
             int sum = nums[left] + nums[right];
             if(sum == target) {
                 result.add(Arrays.asList(-target, nums[left++], nums[right]));
-                while(left < right && nums[left] == nums[left - 1]) left++; //Avoid duplicates 
+                while(left < right && nums[left] == nums[left - 1]) left++;
             }else if(sum < target) left++;
             else right--;
         }
