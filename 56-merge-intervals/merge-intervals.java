b33579d88840601652fd5f81e359550ed73f087b
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-13 16:18:42 -0600

    Time: 12 ms (9.89%) | Memory: 47 MB (6.66%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,25 +1,24 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        TreeMap<Integer, Integer> count = new TreeMap<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
         for(int[] interval : intervals) {
-            count.put(interval[0], count.getOrDefault(interval[0], 0) + 1);
-            count.put(interval[1], count.getOrDefault(interval[1], 0) - 1);
+            map.put(interval[0], map.getOrDefault(interval[0], 0) - 1);
+            map.put(interval[1], map.getOrDefault(interval[1], 0) + 1);
         }
 
         List<int[]> list = new ArrayList<>();
-        int currCount = 0, left = Integer.MAX_VALUE, right = Integer.MIN_VALUE;
-        for(int key : count.keySet()) {
-            currCount+= count.get(key);
-            left = Math.min(left, key);
-            right = Math.max(right, key);
-            
+        int currCount = 0, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for(int key : map.keySet()) {
+            currCount+= map.get(key);
+            min = Math.min(min, key);
+            max = Math.max(max, key);
             if(currCount == 0) {
-                list.add(new int[]{left, right});
-                left = Integer.MAX_VALUE;
-                right = Integer.MIN_VALUE;
+                list.add(new int[]{min, max});
+                min = Integer.MAX_VALUE;
+                max = Integer.MIN_VALUE;
             }
         }
-        
+
         int[][] result = new int[list.size()][2];
         for(int i = 0; i < result.length; i++) result[i] = list.get(i);
         return result;
