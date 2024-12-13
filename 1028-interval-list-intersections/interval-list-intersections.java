ef37e8fb0c26b5e588ce7efd4c4d86070370daf0
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-13 14:46:14 -0600

    Time: 4 ms (58.21%) | Memory: 45.7 MB (25.92%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.java
+++ 1028-interval-list-intersections/interval-list-intersections.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
         List<int[]> list = new ArrayList<>();
+
         int firstInd = 0, secondInd = 0;
         while(firstInd < firstList.length && secondInd < secondList.length) {
             int[] first = firstList[firstInd], second = secondList[secondInd];
@@ -8,6 +9,7 @@ class Solution {
             int xOverlap = Math.max(first[0], second[0]);
             int yOverlap = Math.min(first[1], second[1]);
             if(xOverlap <= yOverlap) list.add(new int[]{xOverlap, yOverlap});
+
             if(first[1] <= second[1]) firstInd++;
             else secondInd++;
         }
