cb1a3651a71106a83b98f3e584439a364e884fa3
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-13 17:14:31 -0600

    Time: 93 ms (93.74%) | Memory: 45.2 MB (17.27%) - LeetSync

--- 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.java
+++ 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.java
@@ -1,9 +1,9 @@
 class Solution {
-    int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+    private int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
 
     public int shortestDistance(int[][] grid) {
-        int minDistance = Integer.MAX_VALUE, emptyLandValue = 0;
         int[][] totalSteps = new int[grid.length][grid[0].length];
+        int minDistance = Integer.MAX_VALUE, emptyLandValue = 0;
         for(int row = 0; row < grid.length; row++) {
             for(int col = 0; col < grid[0].length; col++) {
                 if(grid[row][col] == 1) minDistance = bfs(row, col, grid, totalSteps, emptyLandValue--);
@@ -20,18 +20,18 @@ class Solution {
         while(!q.isEmpty()) {
             int size = q.size();
             while(size-- > 0) {
-                int[] cell = q.poll();
-                int row = cell[0], col = cell[1];
+                int[] curr = q.poll();
+                int row = curr[0], col = curr[1];
 
                 for(int[] dir : dirs) {
                     int newRow = row + dir[0], newCol = col + dir[1];
-                    if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length
+                    if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length 
                         && grid[newRow][newCol] == emptyLandValue) {
-                            totalSteps[newRow][newCol]+= steps;
                             grid[newRow][newCol]--;
+                            totalSteps[newRow][newCol]+= steps;
                             q.add(new int[]{newRow, newCol});
                             minDistance = Math.min(minDistance, totalSteps[newRow][newCol]);
-                        }
+                    }
                 }
             }
             steps++;
