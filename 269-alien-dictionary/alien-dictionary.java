448ddaa8049640a2e9f5165c3ef36ca2af1b21d4
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-13 16:43:17 -0600

    Time: 4 ms (84.29%) | Memory: 42 MB (64.11%) - LeetSync

--- 269-alien-dictionary/alien-dictionary.java
+++ 269-alien-dictionary/alien-dictionary.java
@@ -1,16 +1,17 @@
 class Solution {
     public String alienOrder(String[] words) {
-        //1. Build adjacencies
+        //1. Build graph
         Map<Character, GNode> adj = new HashMap<>();
         for(String word : words) {
             for(char c : word.toCharArray()) {
                 adj.putIfAbsent(c, new GNode());
             }
         }
+
         //2. Connect components
         for(int i = 1; i < words.length; i++) {
             String word1 = words[i - 1], word2 = words[i];
-            if(word1.length() > word2.length() && word1.startsWith(word2)) return ""; //Avoid substrings
+            if(word1.length() > word2.length() && word1.startsWith(word2)) return ""; //prevent substrings
             for(int wordInd = 0; wordInd < Math.min(word1.length(), word2.length()); wordInd++) {
                 char c1 = word1.charAt(wordInd), c2 = word2.charAt(wordInd);
                 if(c1 != c2) {
@@ -21,7 +22,7 @@ class Solution {
             }
         }
 
-        //3. Topological sort 
+        //3. Topological Sort
         Queue<Character> q = new LinkedList<>();
         for(char key : adj.keySet()) if(adj.get(key).dependencies == 0) q.add(key);
 
@@ -35,8 +36,7 @@ class Solution {
                 if(adj.get(nextChar).dependencies == 0) q.add(nextChar);
             }
         }
-        
-        //4. Make sure all characters accounted for
+        //4. Make sure all characters included in result
         return result.length() == adj.size() ? result.toString() : "";
     }
 
