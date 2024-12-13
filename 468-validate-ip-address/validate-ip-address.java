6b47df35ed7c355b0fdd3e08aee4aced183bd2c3
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-13 14:16:06 -0600

    Time: 6 ms (13.52%) | Memory: 42.8 MB (5.59%) - LeetSync

--- 468-validate-ip-address/validate-ip-address.java
+++ 468-validate-ip-address/validate-ip-address.java
@@ -1,17 +1,14 @@
-import java.util.regex.Pattern;
 class Solution {
-  public String validIPAddress(String queryIP) {
-      //case 1: ([0-9]
-      //case 2: |[1-9][0-9]
-      //case 3: |1[0-9][0-9]
-      //case 4: |2[0-4][0-9]
-      //case 5: |25[0-5]
-        String regexIpv4 = "(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])";
-        
-        String regexIpv6 = "((([0-9a-fA-F]){1,4})\\:){7}(([0-9a-fA-F]){1,4})";
-        
-        if(queryIP.matches(regexIpv4)) return "IPv4";
-        else if(queryIP.matches(regexIpv6)) return "IPv6";
+    public String validIPAddress(String queryIP) {
+        //case 1: [[0-9]
+        //case 2: |[1-9][0-9]
+        //case 3: |1[0-9][0-9]
+        //case 4: |2[0-4][0-9]
+        //case 5: |25[0-5]
+        String ipV4 = "(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])";
+        String ipV6 = "((([0-9a-fA-F]){1,4})\\:){7}(([0-9a-fA-F]){1,4})";
+        if(queryIP.matches(ipV4)) return "IPv4";
+        else if(queryIP.matches(ipV6)) return "IPv6";
         else return "Neither";
     }
 }
