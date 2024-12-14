9c2fff15152eb3d055fdcff962cf9794a4a79130
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-13 18:38:54 -0600

    Time: 18 ms (57.85%) | Memory: 45.8 MB (11.76%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,28 +1,30 @@
 class Solution {
     public String minWindow(String s, String t) {
-        Map<Character, Integer> tFreq = new HashMap<>();
-        for(char c : t.toCharArray()) tFreq.put(c, tFreq.getOrDefault(c, 0) + 1);
+        Map<Character, Integer> tFreqs = new HashMap<>();
+        for(char c : t.toCharArray()) tFreqs.put(c, tFreqs.getOrDefault(c, 0) + 1);
 
-        int minWindow = Integer.MAX_VALUE, required = tFreq.size(), counted = 0;
-        int left = 0, right = 0, leftInd = 0, rightInd = 0;;
         Map<Character, Integer> seen = new HashMap<>();
-        while(right < s.length()){
+        int required = tFreqs.size(), counted = 0;
+        int leftInd = 0, rightInd = 0, minWindow = Integer.MAX_VALUE;
+        int left = 0, right = 0;
+        while(right < s.length()) {
             char c = s.charAt(right);
-            if(tFreq.containsKey(c)){
+            if(tFreqs.containsKey(c)) {
                 seen.put(c, seen.getOrDefault(c, 0) + 1);
-                if(seen.get(c).intValue() == tFreq.get(c).intValue()) counted++;
+                if(seen.get(c).intValue() == tFreqs.get(c).intValue()) counted++;
             }
 
-            while(left <= right && counted == required){
-                if(right - left + 1 < minWindow){
+            while(left <= right && counted == required) {
+                c = s.charAt(left);
+                if(right - left + 1 < minWindow) {
                     minWindow = right - left + 1;
                     leftInd = left;
                     rightInd = right;
                 }
-                c = s.charAt(left);
-                if(seen.containsKey(c)){
-                    seen.put(c, seen.get(c) - 1);
-                    if(seen.get(c) < tFreq.get(c)) counted--;
+
+                if(tFreqs.containsKey(c)) {
+                    seen.put(c, seen.getOrDefault(c, 0) - 1);
+                    if(seen.get(c).intValue() < tFreqs.get(c).intValue()) counted--;
                 }
                 left++;
             }
