e3a577df945d94c311210a1519bb522209c16f5d
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-18 11:40:35 -0600

    Time: 5 ms (66.07%) | Memory: 44.6 MB (90.82%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -5,15 +5,15 @@ class Solution {
 
         int[][] dp = new int[word1.length() + 1][word2.length() + 1];
 
-        for(int row = 1; row < dp.length; row++) dp[row][0] = row;
-        for(int col = 1; col < dp[0].length; col++) dp[0][col] = col;
+        for(int word1Ind = 1; word1Ind < dp.length; word1Ind++) dp[word1Ind][0] = word1Ind;
+        
+        for(int word2Ind = 1; word2Ind < dp[0].length; word2Ind++) dp[0][word2Ind] = word2Ind;
 
         for(int word1Ind = 1; word1Ind < dp.length; word1Ind++) {
             for(int word2Ind = 1; word2Ind < dp[0].length; word2Ind++) {
-                char c1 = word1.charAt(word1Ind - 1), c2 = word2.charAt(word2Ind - 1);
-                if(c1 == c2) dp[word1Ind][word2Ind] = dp[word1Ind - 1][word2Ind - 1];
+                if(word1.charAt(word1Ind - 1) == word2.charAt(word2Ind - 1)) dp[word1Ind][word2Ind] = dp[word1Ind - 1][word2Ind - 1];
                 else {
-                    //insert, replace, delete 
+                    //insert, replace, delete
                     int insert = dp[word1Ind][word2Ind - 1];
                     int replace = dp[word1Ind - 1][word2Ind - 1];
                     int delete = dp[word1Ind - 1][word2Ind];
