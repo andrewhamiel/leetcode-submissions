51da71b3d4560a545edf9679591fc5ae8e9c2928
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-18 12:03:44 -0600

    Time: 14 ms (96.57%) | Memory: 58 MB (13.35%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -6,8 +6,8 @@ class Solution {
         int totalSum = 0;
         for(int num : nums) {
             totalSum+= num;
-        }    
-
+        }
+        
         if(totalSum % 2 != 0) return false;
         int subsetSum = totalSum / 2;
         this.nums = nums;
@@ -16,13 +16,13 @@ class Solution {
         return dp(nums.length - 1, subsetSum);
     }
 
-    private boolean dp(int ind, int sum) {
-        if(sum == 0) return true;
-        if(ind < 0 || sum < 0) return false;
+    private boolean dp(int ind, int currSum) {
+        if(currSum == 0) return true;
+        if(ind < 0 || currSum < 0) return false;
 
-        if(memo[ind][sum] != null) return memo[ind][sum];
+        if(memo[ind][currSum] != null) return memo[ind][currSum];
 
-        //2 options: use or do not use 
-        return memo[ind][sum] = dp(ind - 1, sum - nums[ind]) || dp(ind - 1, sum);
+        //2 options: use or do not use
+        return memo[ind][currSum] = dp(ind - 1, currSum - nums[ind]) || dp(ind - 1, currSum);
     }
 }
