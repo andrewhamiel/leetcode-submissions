1d749e18daf94f2e64299be3579ea225b68b464a
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-18 12:57:15 -0600

    Time: 1 ms (87.35%) | Memory: 42 MB (60.99%) - LeetSync

--- 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.java
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.java
@@ -15,19 +15,24 @@
  */
 class Solution {
     public boolean isCompleteTree(TreeNode root) {
-        if(root == null) return true;
-
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        boolean nullNodeFound = false;
+        
+        boolean isNull = false;
+        while(!q.isEmpty()) {
+            int size = q.size();
+            while(size-- > 0) {
+                TreeNode curr = q.poll();
 
-        while(!q.isEmpty()){
-            TreeNode curr = q.poll();
-            if(curr == null) nullNodeFound = true;
-            else{
-                if(nullNodeFound) return false;
-                q.add(curr.left);
-                q.add(curr.right);
+                if(isNull && (curr.left != null || curr.right != null)) return false;
+
+                if(curr.left == null || curr.right == null) {
+                    if(curr.left == null && curr.right != null) return false;
+                    else isNull = true;
+                }
+
+                if(curr.left != null) q.add(curr.left);
+                if(curr.right != null) q.add(curr.right);
             }
         }
         return true;
