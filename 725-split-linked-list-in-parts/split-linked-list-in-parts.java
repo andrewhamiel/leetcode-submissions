c7d022a297757e0e9fb8162406e5aa40e2c38077
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-18 10:55:50 -0600

    Time: 0 ms (100.00%) | Memory: 43.1 MB (95.68%) - LeetSync

--- 725-split-linked-list-in-parts/split-linked-list-in-parts.java
+++ 725-split-linked-list-in-parts/split-linked-list-in-parts.java
@@ -10,27 +10,27 @@
  */
 class Solution {
     public ListNode[] splitListToParts(ListNode head, int k) {
-        //1. Find size of linked list 
-        int size = 0;
+        //1. Find size of list 
         ListNode curr = head;
+        int size = 0;
         while(curr != null) {
             curr = curr.next;
             size++;
         }
 
-        //2. Split into parts 
+        //2. Split into parts
         ListNode[] result = new ListNode[k];
         int parts = size / k, rem = size % k;
         curr = head;
         for(int i = 0; i < k; i++) {
             ListNode ptr = curr;
-            //Advance to last node in part
+            //iterate up until last element in part
             int carry = i < rem ? 1 : 0;
             for(int j = 0; j < parts + carry - 1; j++) {
                 if(curr != null) curr = curr.next;
             }
 
-            //If not null, delink last element in part
+            //delink last node in part if not at end of list
             if(curr != null) {
                 ListNode prev = curr;
                 curr = curr.next;
