d76d63e27dfe0b4cdf485d9999c1a7aadb7f28fa
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-18 10:51:59 -0600

    Time: 4 ms (21.66%) | Memory: 43 MB (31.64%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.java
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.java
@@ -16,11 +16,9 @@
 class Solution {
     public List<List<Integer>> verticalOrder(TreeNode root) {
         if(root == null) return new ArrayList<>();
-
-        Map<Integer, List<Pair<Integer, Integer>>> map = new HashMap<>();
+        Map<Integer, List<Pair<Integer, Integer>>> adj = new HashMap<>();
         Queue<Pair<TreeNode, Integer>> q = new LinkedList<>();
         q.add(new Pair<>(root, 0));
-
         int row = 0, minCol = 0, maxCol = 0;
         while(!q.isEmpty()) {
             int size = q.size();
@@ -30,8 +28,8 @@ class Solution {
                 int col = p.getValue();
                 minCol = Math.min(minCol, col);
                 maxCol = Math.max(maxCol, col);
-
-                map.computeIfAbsent(col, k -> new ArrayList<>()).add(new Pair<>(curr.val, row));
+                
+                adj.computeIfAbsent(col, k -> new ArrayList<>()).add(new Pair<>(row, curr.val));
 
                 if(curr.left != null) q.add(new Pair<>(curr.left, col - 1));
                 if(curr.right != null) q.add(new Pair<>(curr.right, col + 1));
@@ -40,15 +38,14 @@ class Solution {
         }
 
         List<List<Integer>> result = new ArrayList<>();
-        for(int i = minCol; i <= maxCol; i++) {
-            if(map.containsKey(i)) {
-                List<Pair<Integer, Integer>> pairs = map.get(i);
-                Collections.sort(pairs, (a, b) ->  a.getValue() - b.getValue());
-
+        for(int col = minCol; col <= maxCol; col++) {
+            if(adj.containsKey(col)){
+                List<Pair<Integer, Integer>> pairs = adj.get(col);
+                Collections.sort(pairs, (a, b) -> a.getKey() - b.getKey());
                 List<Integer> list = new ArrayList<>();
-                for(Pair<Integer, Integer> p : pairs) list.add(p.getKey());
+                for(Pair<Integer, Integer> pair : pairs) list.add(pair.getValue());
                 result.add(list);
-            }
+            }          
         }
         return result;
     }
