f6b04ff6ce7e635df543fbe10245b7030d9cea8d
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-18 11:27:36 -0600

    Time: 6 ms (53.02%) | Memory: 43.1 MB (60.50%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -3,15 +3,15 @@ class Solution {
         //1. Convert nums to strings
         String[] strs = new String[nums.length];
         for(int i = 0; i < nums.length; i++) strs[i] = String.valueOf(nums[i]);
-
         //2. Sort in descending order
-        Arrays.sort(strs, (a, b) -> (b+a).compareTo(a+b));
+        Arrays.sort(strs, (a, b) -> (b + a).compareTo(a + b));
+        //3. Concatenate into single string
+        StringBuilder sb = new StringBuilder();
+        for(String str : strs) {
+            sb.append(str);
+        }
 
-        //3. Concatenate into single String
-        StringBuilder result = new StringBuilder();
-        for(String str : strs) result.append(str);
-
-        //4. Handle case where leading digit is '0'
-        return result.charAt(0) == '0' ? "0" : result.toString();
+        //4. Catch case of largest digit being '0'
+        return sb.charAt(0) == '0' ? "0" : sb.toString();
     }
 }
