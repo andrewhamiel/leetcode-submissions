6202796a789dc85e240ce3772f4c1f1f4206a5fa
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-19 10:31:06 -0600

    Time: 6 ms (32.15%) | Memory: 42.6 MB (29.17%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -4,39 +4,33 @@ class Solution {
 
         StringBuilder firstNumber = new StringBuilder(num1), secondNumber = new StringBuilder(num2);
 
-        //1. Reverse both numbers
+        //Reverse both numbers
         firstNumber.reverse();
         secondNumber.reverse();
 
-        //2. Store multiplication result of each digit of secondNumber with firstNumber
+        //Store 0 in each digit for result
         StringBuilder result = new StringBuilder();
-        for(int i = 0; i < firstNumber.length() + secondNumber.length(); i++) {
-            result.append(0);
-        }
+        for(int i = 0; i < firstNumber.length() + secondNumber.length(); i++) result.append('0');
 
         for(int place2 = 0; place2 < secondNumber.length(); place2++) {
             int digit2 = secondNumber.charAt(place2) - '0';
 
-            //for each digit in second num multiply by all digits in firstNum
             for(int place1 = 0; place1 < firstNumber.length(); place1++) {
                 int digit1 = firstNumber.charAt(place1) - '0';
 
-                //number of zeroes depends on place1 and place2
-                int currentPos = place1 + place2;
-
-                int carry = result.charAt(currentPos) - '0';
+                int currPos = place1 + place2;
+                int carry = result.charAt(currPos) - '0';
                 int product = digit1 * digit2 + carry;
 
-                //Set ones place of multiplication result
-                result.setCharAt(currentPos, (char) (product % 10 + '0'));
-
-                //Carry tens place of multiplication result
-                carry = (result.charAt(currentPos + 1) - '0') + product/10;
-                result.setCharAt(currentPos + 1, (char) (carry + '0'));
+                //Set ones digit
+                result.setCharAt(currPos, (char) (product % 10 + '0'));
+                //Set tens digit
+                carry = (result.charAt(currPos + 1) - '0') + product/10;
+                result.setCharAt(currPos + 1, (char)(carry + '0'));
             }
         }
 
-        //Pop excess 0 from end of result
+        //Pop extra 0 at the end
         if(result.charAt(result.length() - 1) == '0') result.deleteCharAt(result.length() - 1);
 
         result.reverse();
