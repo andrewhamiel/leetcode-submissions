ecfd1ce516d305006628349866d258c360723904
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-23 09:51:21 -0600

    Time: 75 ms (10.87%) | Memory: 44.5 MB (33.35%) - LeetSync

--- 787-sliding-puzzle/sliding-puzzle.java
+++ 787-sliding-puzzle/sliding-puzzle.java
@@ -1,21 +1,23 @@
 class Solution {
-    private int[][] dirs = new int[][]{{1, 3}, {0, 2, 4}, {1, 5}, {0, 4}, {3, 5, 1}, {4, 2}};
+    private int[][] dirs = new int[][]{{1, 3}, {0, 2, 4}, {1, 5}, {0, 4}, {1, 3, 5}, {2, 4}};
     private Map<String, Integer> visited = new HashMap<>();
 
     public int slidingPuzzle(int[][] board) {
-        StringBuilder startState = new StringBuilder();
-        for(int row = 0; row < 2; row++) {
-            for(int col = 0; col < 3; col++) {
-                startState.append(board[row][col]);
+        StringBuilder currState = new StringBuilder();
+        int zeroInd = 0;
+        for(int row = 0; row < board.length; row++) {
+            for(int col = 0; col < board[0].length; col++) {
+                currState.append(board[row][col]);
+                if(board[row][col] == 0) zeroInd = 3 * row + col;
             }
-        }
+        }    
 
-        dfs(startState.toString(), startState.indexOf("0"), 0);
+        dfs(currState.toString(), board, zeroInd, 0);
 
         return visited.getOrDefault("123450", -1);
     }
 
-    private void dfs(String currState, int zeroInd, int moves) {
+    private void dfs(String currState, int[][] board, int zeroInd, int moves) {
         if(visited.containsKey(currState) && visited.get(currState) <= moves) return;
 
         visited.put(currState, moves);
@@ -23,7 +25,7 @@ class Solution {
         for(int nextInd : dirs[zeroInd]) {
             char[] arr = currState.toCharArray();
             swap(zeroInd, nextInd, arr);
-            dfs(new String(arr), nextInd, moves + 1);
+            dfs(new String(arr), board, nextInd, moves + 1);
         }
     }
 
