bcc33e689f5735556cd754ca0f744d5e3847e909
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-23 11:45:37 -0600

    Time: 66 ms (80.99%) | Memory: 45.8 MB (16.46%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.java
+++ 282-expression-add-operators/expression-add-operators.java
@@ -6,7 +6,6 @@ class Solution {
     public List<String> addOperators(String num, int target) {
         this.num = num;
         this.target = target;
-
         helper(0, 0, 0, 0, new ArrayList<>());
         return result;    
     }
@@ -22,11 +21,11 @@ class Solution {
             return;
         }
 
-        //2. Fix value, shift base 10
+        //2. Fix curr, shift base 10
         curr*= 10;
         curr+= num.charAt(ind) - '0';
 
-        //3. No op if curr != 0 to avoid leading zeroes 
+        //3. No op if curr != 0 to avoid leading zeroes
         if(curr != 0) helper(ind + 1, value, prev, curr, expr);
 
         //4. Addition
@@ -36,7 +35,7 @@ class Solution {
         expr.remove(expr.size() - 1);
         expr.remove(expr.size() - 1);
 
-        //Only if elements already added
+        //Only if other operations already added
         if(expr.size() > 0) {
             //5. Subtraction
             expr.add("-");
@@ -44,7 +43,6 @@ class Solution {
             helper(ind + 1, value - curr, -curr, 0, expr);
             expr.remove(expr.size() - 1);
             expr.remove(expr.size() - 1);
-
             //6. Multiplication
             expr.add("*");
             expr.add(String.valueOf(curr));
