3de3a097fe5ea6ee95202b735f9342c86661d00a
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-23 11:58:36 -0600

    Time: 119 ms (15.82%) | Memory: 45.5 MB (49.98%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.java
+++ 282-expression-add-operators/expression-add-operators.java
@@ -6,11 +6,11 @@ class Solution {
     public List<String> addOperators(String num, int target) {
         this.num = num;
         this.target = target;
-        helper(0, 0, 0, 0, new ArrayList<>());
+        backtrack(0, 0, 0, 0, new ArrayList<>());
         return result;    
     }
 
-    private void helper(int ind, long value, long prev, long curr, List<String> expr) {
+    private void backtrack(int ind, long value, long prev, long curr, List<String> expr) {
         //1. Base case/exit condition
         if(ind == num.length()) {
             if(value == target && curr == 0) {
@@ -21,34 +21,29 @@ class Solution {
             return;
         }
 
-        //2. Fix curr, shift base 10
+        //2. Fix curr num, shift base 10 
         curr*= 10;
         curr+= num.charAt(ind) - '0';
 
         //3. No op if curr != 0 to avoid leading zeroes
-        if(curr != 0) helper(ind + 1, value, prev, curr, expr);
+        if(curr != 0) backtrack(ind + 1, value, prev, curr, expr);
 
         //4. Addition
-        expr.add("+");
-        expr.add(String.valueOf(curr));
-        helper(ind + 1, value + curr, curr, 0, expr);
-        expr.remove(expr.size() - 1);
-        expr.remove(expr.size() - 1);
+        expr.addAll(Arrays.asList("+", String.valueOf(curr)));
+        backtrack(ind + 1, value + curr, curr, 0, expr);
+        expr.subList(expr.size() - 2, expr.size()).clear();
 
-        //Only if other operations already added
+        //Only perform ops if expr not empty
         if(expr.size() > 0) {
             //5. Subtraction
-            expr.add("-");
-            expr.add(String.valueOf(curr));
-            helper(ind + 1, value - curr, -curr, 0, expr);
-            expr.remove(expr.size() - 1);
-            expr.remove(expr.size() - 1);
-            //6. Multiplication
-            expr.add("*");
-            expr.add(String.valueOf(curr));
-            helper(ind + 1, value - prev + (prev * curr), prev * curr, 0, expr);
-            expr.remove(expr.size() - 1);
-            expr.remove(expr.size() - 1);
+            expr.addAll(Arrays.asList("-", String.valueOf(curr)));
+            backtrack(ind + 1, value - curr, -curr, 0, expr);
+            expr.subList(expr.size() - 2, expr.size()).clear();
+
+            //6. Multiplication 
+            expr.addAll(Arrays.asList("*", String.valueOf(curr)));
+            backtrack(ind + 1, value - prev + (prev * curr), prev * curr, 0, expr);
+            expr.subList(expr.size() - 2, expr.size()).clear();
         }
     }
 }
