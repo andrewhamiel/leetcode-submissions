73a40b153dcedd00b06302a32ac8423410e919d7
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-23 10:25:17 -0600

    Time: 13 ms (77.60%) | Memory: 45.5 MB (85.58%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
@@ -7,34 +7,27 @@ public class Solution {
 
         while (startIndex < startLength || targetIndex < startLength) {
             // Skip underscores in start
-            while (
-                startIndex < startLength && start.charAt(startIndex) == '_'
-            ) {
+            while (startIndex < startLength && start.charAt(startIndex) == '_') {
                 startIndex++;
             }
             // Skip underscores in target
-            while (
-                targetIndex < startLength && target.charAt(targetIndex) == '_'
-            ) {
+            while (targetIndex < startLength && target.charAt(targetIndex) == '_') {
                 targetIndex++;
             }
+
             // If one string is exhausted, both should be exhausted
             if (startIndex == startLength || targetIndex == startLength) {
                 return startIndex == startLength && targetIndex == startLength;
             }
-
             // Check if the pieces match and follow movement rules
-            if (
-                start.charAt(startIndex) != target.charAt(targetIndex) ||
-                (start.charAt(startIndex) == 'L' && startIndex < targetIndex) ||
-                (start.charAt(startIndex) == 'R' && startIndex > targetIndex)
-            ) return false;
+            if (start.charAt(startIndex) != target.charAt(targetIndex) 
+            || (start.charAt(startIndex) == 'L' && startIndex < targetIndex) 
+            || (start.charAt(startIndex) == 'R' && startIndex > targetIndex)) return false;
 
             startIndex++;
             targetIndex++;
         }
 
-        // If all conditions are satisfied, return true
         return true;
     }
 }
