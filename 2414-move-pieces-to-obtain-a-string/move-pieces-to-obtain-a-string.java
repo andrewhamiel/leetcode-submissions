0899d8e7ab8a98b9a70d58f153d48dabd805083d
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-23 10:47:19 -0600

    Time: 15 ms (46.53%) | Memory: 45.5 MB (85.58%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.java
@@ -2,11 +2,11 @@ class Solution {
     public boolean canChange(String start, String target) {
         int startInd = 0, targetInd = 0;
         while(startInd < start.length() || targetInd < target.length()) {
-            //Advance past underscores
+            //Advance underscores
             while(startInd < start.length() && start.charAt(startInd) == '_') startInd++;
             while(targetInd < target.length() && target.charAt(targetInd) == '_') targetInd++;
 
-            //Exit conditions
+            //Exit conditions 
             if(startInd == start.length() || targetInd == target.length()) return startInd == start.length() && targetInd == target.length();
             if(start.charAt(startInd) != target.charAt(targetInd) 
                 || (start.charAt(startInd) == 'L' && startInd < targetInd)
