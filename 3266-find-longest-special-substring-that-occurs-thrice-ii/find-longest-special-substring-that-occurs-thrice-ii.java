7359ffe6755ed79dba0dd327dd1cdb86755b5c17
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-23 15:21:50 -0600

    Time: 20 ms (95.52%) | Memory: 48.4 MB (71.07%) - LeetSync

--- 3266-find-longest-special-substring-that-occurs-thrice-ii/find-longest-special-substring-that-occurs-thrice-ii.java
+++ 3266-find-longest-special-substring-that-occurs-thrice-ii/find-longest-special-substring-that-occurs-thrice-ii.java
@@ -3,17 +3,15 @@ class Solution {
         int substrLength = 0, result = -1;
         char prev = '\0';
         int[][] substrLengths = new int[26][3];
-
         for(int[] arr : substrLengths) Arrays.fill(arr, -1);
 
         for(char c : s.toCharArray()) {
             if(c == prev) substrLength++;
             else {
-                substrLength = 1;
                 prev = c;
+                substrLength = 1;
             }
 
-            //Replace min freq with curr length if it is greater
             int ind = c - 'a';
             int minLength = Math.min(substrLengths[ind][0], Math.min(substrLengths[ind][1], substrLengths[ind][2]));
 
@@ -24,9 +22,11 @@ class Solution {
             }
         }
 
-        //Find character with max value of its minimum frequence
+        //Find longest special substring for each character
         for(int i = 0; i < substrLengths.length; i++) {
-            result = Math.max(result, Math.min(substrLengths[i][0], Math.min(substrLengths[i][1], substrLengths[i][2])));
+            int minLength = Integer.MAX_VALUE;
+            for(int j = 0; j < substrLengths[i].length; j++) minLength = Math.min(minLength, substrLengths[i][j]);
+            result = Math.max(result, minLength);
         }
         return result;
     }
