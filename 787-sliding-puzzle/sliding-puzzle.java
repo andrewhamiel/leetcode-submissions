290e6da48b5d13eafc22df814d31e00cbaac2f64
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-23 09:43:41 -0600

    Time: 94 ms (10.32%) | Memory: 44.8 MB (9.07%) - LeetSync

--- 787-sliding-puzzle/sliding-puzzle.java
+++ 787-sliding-puzzle/sliding-puzzle.java
@@ -1,47 +1,35 @@
 class Solution {
-    // Direction map for zero's possible moves in a flattened 1D array (2x3 board)
-    private final int[][] directions = {
-        { 1, 3 },
-        { 0, 2, 4 },
-        { 1, 5 },
-        { 0, 4 },
-        { 3, 5, 1 },
-        { 4, 2 },
-    };
+    private int[][] dirs = new int[][]{{1, 3}, {0, 2, 4}, {1, 5}, {0, 4}, {3, 5, 1}, {4, 2}};
     private Map<String, Integer> visited = new HashMap<>();
 
     public int slidingPuzzle(int[][] board) {
-        // Convert the 2D board into a string representation to use as state
         StringBuilder startState = new StringBuilder();
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 3; j++) {
-                startState.append(board[i][j]);
+        for(int row = 0; row < 2; row++) {
+            for(int col = 0; col < 3; col++) {
+                startState.append(board[row][col]);
             }
-        }       
+        }
 
-        dfs(startState.toString(), visited, startState.indexOf("0"), 0);
+        dfs(startState.toString(), startState.indexOf("0"), 0);
 
         return visited.getOrDefault("123450", -1);
     }
 
-    private void dfs(String state, Map<String, Integer> visited, int zeroPos, int moves) {
-        // Skip if this state has been visited with fewer or equal moves
-        if (visited.containsKey(state) && visited.get(state) <= moves) return;
-        
-        visited.put(state, moves);
+    private void dfs(String currState, int zeroInd, int moves) {
+        if(visited.containsKey(currState) && visited.get(currState) <= moves) return;
 
-        // Try moving zero to each possible adjacent position
-        for (int nextPos : directions[zeroPos]) {
-            String newState = swap(state, zeroPos, nextPos); // Swap to generate new state
-            dfs(newState, visited, nextPos, moves + 1); // Recursive DFS with updated state and move count
+        visited.put(currState, moves);
+
+        for(int nextInd : dirs[zeroInd]) {
+            char[] arr = currState.toCharArray();
+            swap(zeroInd, nextInd, arr);
+            dfs(new String(arr), nextInd, moves + 1);
         }
     }
 
-    // Helper method to swap characters at indices i and j in the string
-    private String swap(String str, int i, int j) {
-        StringBuilder sb = new StringBuilder(str);
-        sb.setCharAt(i, str.charAt(j));
-        sb.setCharAt(j, str.charAt(i));
-        return sb.toString();
+    private void swap(int i, int j, char[] arr) {
+        char tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 }
