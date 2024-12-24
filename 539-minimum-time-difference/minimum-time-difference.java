1b7768bf45437ae602bc4b43e2f409875b2c6346
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-24 10:05:45 -0600

    Time: 13 ms (27.15%) | Memory: 46.6 MB (13.97%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.java
+++ 539-minimum-time-difference/minimum-time-difference.java
@@ -1,24 +1,26 @@
 class Solution {
     public int findMinDifference(List<String> timePoints) {
-        List<Integer> mins = new ArrayList<>();
+        List<Integer> minutes = new ArrayList<>();
         for(String time : timePoints) {
-            String[] split = time.split(":");
-            mins.add(calculateMins(Integer.parseInt(split[0]), Integer.parseInt(split[1])));
+            int mins = calculateMins(time);
+            minutes.add(mins);
         }
 
-        Collections.sort(mins);
-        int minTime = Integer.MAX_VALUE;
-        for(int i = 1; i < mins.size(); i++) {
-            minTime = Math.min(minTime, mins.get(i) - mins.get(i - 1));
+        Collections.sort(minutes);
+
+        int minDifference = Integer.MAX_VALUE;
+        for(int i = 1; i < minutes.size(); i++) {
+            minDifference = Math.min(minDifference, minutes.get(i) - minutes.get(i - 1));
         }
-        
-        //calculate distance from first/last 
-        int minsFromMidnight = calculateMins(24, 0) - mins.get(mins.size() - 1);
-        minTime = Math.min(minTime, minsFromMidnight + mins.get(0));
-        return minTime;
+        //Compare first and last minutes
+        int minutesFromMidnight = calculateMins("24:00") - minutes.get(minutes.size() - 1);
+        minDifference = Math.min(minDifference, minutesFromMidnight + minutes.get(0));
+
+        return minDifference;
     }
 
-    private int calculateMins(int hours, int mins) {
-        return 60 * hours + mins;
+    private int calculateMins(String time) {
+        String[] split = time.split(":");
+        return Integer.parseInt(split[0]) * 60 + Integer.parseInt(split[1]);
     }
 }
