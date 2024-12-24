5d29d27bab8c759d9887c2c8f30a57996a4b729f
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-24 11:53:05 -0600

    Time: 39 ms (27.18%) | Memory: 59.5 MB (46.02%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -21,7 +21,7 @@ class Solution {
     private void cleanDeque(int i, int k, int[] nums, Deque<Integer> deq) {
         //If window size is too large
         if(!deq.isEmpty() && deq.peekFirst() == i - k) deq.removeFirst();
-        //Remove nums from deq < nums[i]
-        while(!deq.isEmpty() && nums[i] > nums[deq.peekLast()]) deq.removeLast();
+        //Remove all elements in deq < nums[i]
+        while(!deq.isEmpty() && nums[deq.peekLast()] <= nums[i]) deq.removeLast();
     }
 }
