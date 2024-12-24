c609139bb9cc2fb0077dec091c50add02e31429e
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-24 09:45:54 -0600

    Time: 46 ms (40.74%) | Memory: 58.8 MB (16.87%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.java
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.java
@@ -1,6 +1,7 @@
 class Solution {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
         Map<Integer, List<Pair<Integer, Double>>> adj = new HashMap<>();
+        //1. Build Graph
         for(int i = 0; i < edges.length; i++) {
             int u = edges[i][0], v = edges[i][1];
             double currProb = succProb[i];
@@ -8,26 +9,24 @@ class Solution {
             adj.computeIfAbsent(v, k -> new ArrayList<>()).add(new Pair<>(u, currProb));
         }
 
-        if(!adj.containsKey(end_node)) return 0.0;
+        double[] result = new double[n];
+        result[start_node] = 1.0;
 
-        double[] maxProb = new double[n];
-        maxProb[start_node] = 1.0;
         PriorityQueue<Pair<Integer, Double>> maxHeap = new PriorityQueue<>((a, b) -> Double.compare(b.getValue(), a.getValue()));
-        maxHeap.add(new Pair<>(start_node, maxProb[start_node]));
+        maxHeap.add(new Pair<>(start_node, result[start_node]));
 
         while(!maxHeap.isEmpty()) {
-            Pair<Integer, Double> p = maxHeap.poll();
-            int currNode = p.getKey();
-            double currProb = p.getValue();
+            Pair<Integer, Double> currPair = maxHeap.poll();
+            int currNode = currPair.getKey();
+            double currProb = currPair.getValue();
             if(currNode == end_node) return currProb;
 
             for(Pair<Integer, Double> nextPair : adj.getOrDefault(currNode, new ArrayList<>())) {
                 int nextNode = nextPair.getKey();
                 double nextProb = nextPair.getValue();
-                
-                if(currProb * nextProb > maxProb[nextNode]) {
-                    maxProb[nextNode] = currProb * nextProb;
-                    maxHeap.add(new Pair<>(nextNode, maxProb[nextNode]));
+                if(currProb * nextProb > result[nextNode]) {
+                    result[nextNode] = currProb * nextProb;
+                    maxHeap.add(new Pair<>(nextNode, result[nextNode]));
                 }
             }
         }
