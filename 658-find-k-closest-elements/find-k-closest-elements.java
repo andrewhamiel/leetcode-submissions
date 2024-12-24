8e3bf23ff0437415455951839060f2f19b6b70b0
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-24 10:27:57 -0600

    Time: 4 ms (96.01%) | Memory: 46.2 MB (28.52%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -6,7 +6,7 @@ class Solution {
             if(x - arr[mid] > arr[mid + k] - x) left = mid + 1;
             else right = mid;
         }
-
+        
         List<Integer> result = new ArrayList<>();
         for(int i = left; i < left + k; i++) result.add(arr[i]);
         return result;
