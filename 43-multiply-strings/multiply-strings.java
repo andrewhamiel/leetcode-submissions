7fdd6b4ed0f49bd17103908a8d19fb48fa960133
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-25 07:34:21 -0600

    Time: 5 ms (33.01%) | Memory: 42.6 MB (29.52%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -8,29 +8,27 @@ class Solution {
         firstNum.reverse();
         secondNum.reverse();
 
-        //Initialize result with all 0 digits to start
+        //Initialize result to all digits as '0'
         StringBuilder result = new StringBuilder();
-        for(int i = 0; i < firstNum.length() + secondNum.length(); i++) result.append("0");
+        for(int i = 0; i < firstNum.length() + secondNum.length(); i++) result.append('0');
 
         for(int place2 = 0; place2 < secondNum.length(); place2++) {
             int secondDigit = secondNum.charAt(place2) - '0';
-            
             for(int place1 = 0; place1 < firstNum.length(); place1++) {
                 int firstDigit = firstNum.charAt(place1) - '0';
 
                 int currPos = place1 + place2;
                 int carry = result.charAt(currPos) - '0';
                 int product = firstDigit * secondDigit + carry;
-                
                 //Set ones digit
-                result.setCharAt(currPos, (char)((product % 10) + '0'));
+                result.setCharAt(currPos, (char) ((product % 10) + '0'));
                 //Set tens digit
-                carry = (result.charAt(currPos + 1) - '0') + product/10;
-                result.setCharAt(currPos + 1, (char) (carry + '0'));
+                carry = result.charAt(currPos + 1) - '0';
+                result.setCharAt(currPos + 1, (char) ((product/10) + carry + '0'));
             }
         }
 
-        //Remove 0 if last digit
+        //Remove potential last zero digit
         if(result.charAt(result.length() - 1) == '0') result.deleteCharAt(result.length() - 1);
 
         result.reverse();
