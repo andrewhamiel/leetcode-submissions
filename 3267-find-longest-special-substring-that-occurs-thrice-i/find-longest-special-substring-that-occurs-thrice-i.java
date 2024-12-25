55982afb03adb0f4d7cb4ef13fa74caf554dc2ee
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-25 07:08:48 -0600

    Time: 2 ms (98.43%) | Memory: 43.4 MB (66.92%) - LeetSync

--- 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.java
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.java
@@ -3,24 +3,25 @@ class Solution {
         int[][] substrLengths = new int[26][3];
         for(int[] row : substrLengths) Arrays.fill(row, -1);
 
-        int currLength = 0, longest = -1;
+        int currLength = 0;
         char prev = '\0';
-
         for(char c : s.toCharArray()) {
             if(c == prev) currLength++;
             else {
-                prev = c;
                 currLength = 1;
+                prev = c;
             }
 
-            int minLength = findMin((int)(c - 'a'), substrLengths);
+            int ind = (int) (c - 'a');
+            int minLength = findMin(ind, substrLengths);
             if(currLength > minLength) {
-                if(substrLengths[c - 'a'][0] == minLength) substrLengths[c - 'a'][0] = currLength;
-                else if(substrLengths[c - 'a'][1] == minLength) substrLengths[c - 'a'][1] = currLength;
-                else substrLengths[c - 'a'][2] = currLength;
+                if(substrLengths[ind][0] == minLength) substrLengths[ind][0] = currLength;
+                else if(substrLengths[ind][1] == minLength) substrLengths[ind][1] = currLength;
+                else substrLengths[ind][2] = currLength;
             }
         }
 
+        int longest = -1;
         for(int ind = 0; ind < substrLengths.length; ind++) {
             int minLength = findMin(ind, substrLengths);
             longest = Math.max(longest, minLength);
