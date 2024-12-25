6da3963b42d26d058dc4a141053f3252a7c168f0
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-25 07:20:33 -0600

    Time: 2 ms (44.38%) | Memory: 42.1 MB (18.84%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -8,10 +8,10 @@ class Solution {
 
         for(int day : days) isTravelNeeded.add(day);
 
-        return solve(costs, dp, 1);    
+        return solve(1, dp, costs);
     }
 
-    private int solve(int[] costs, int[] dp, int day) {
+    private int solve(int day, int[] dp, int[] costs) {
         //1. Base case
         if(day >= dp.length) return 0;
 
@@ -19,12 +19,12 @@ class Solution {
         if(dp[day] != -1) return dp[day];
 
         //3. If travel not needed
-        if(!isTravelNeeded.contains(day)) return solve(costs, dp, day + 1);
+        if(!isTravelNeeded.contains(day)) return solve(day + 1, dp, costs);
 
         //4. Min cost
-        int oneDay = costs[0] + solve(costs, dp, day + 1);
-        int sevenDay = costs[1] + solve(costs, dp, day + 7);
-        int thirtyDay = costs[2] + solve(costs, dp, day + 30);
+        int oneDay = costs[0] + solve(day + 1, dp, costs);
+        int sevenDay = costs[1] + solve(day + 7, dp, costs);
+        int thirtyDay = costs[2] + solve(day + 30, dp, costs);
 
         return dp[day] = Math.min(oneDay, Math.min(sevenDay, thirtyDay));
     }
