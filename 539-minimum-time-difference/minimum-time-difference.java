e95acfbac298d287f3a21d4c8cac710befe6caa3
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-25 17:18:49 -0600

    Time: 3 ms (83.91%) | Memory: 45.4 MB (59.17%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.java
+++ 539-minimum-time-difference/minimum-time-difference.java
@@ -1,32 +1,31 @@
 class Solution {
     public int findMinDifference(List<String> timePoints) {
         boolean[] minutes = new boolean[24 * 60];
-        int minMins = Integer.MAX_VALUE;
+        int firstTime = Integer.MAX_VALUE;
         for(String time : timePoints) {
             int mins = calculateMins(time);
             if(minutes[mins]) return 0;
             minutes[mins] = true;
-            minMins = Math.min(minMins, mins);
+            firstTime = Math.min(firstTime, mins);
         }
 
-        int left = minMins, right = left + 1, maxMins = left, minDifference = Integer.MAX_VALUE;
-        while(right < minutes.length) {
-            if(minutes[right]) {
-                minDifference = Math.min(minDifference, right - left);
-                maxMins = right;
-                left = right;
+        int minDifference = Integer.MAX_VALUE, maxTime = firstTime, prevTime = firstTime;
+        for(int currTime = firstTime + 1; currTime < minutes.length; currTime++) {
+            if(minutes[currTime]) {
+                minDifference = Math.min(minDifference, currTime - prevTime);
+                prevTime = currTime;
+                maxTime = currTime;
             }
-            right++;
         }
 
-        //Calculate between first and last element 
-        int minsFromMidnight = calculateMins("24:00") - maxMins;
-        minDifference = Math.min(minDifference, minsFromMidnight + minMins);
+        //Calculate difference between last and first time
+        int minsFromMidnight = calculateMins("24:00") - maxTime;
+        minDifference = Math.min(minDifference, minsFromMidnight + firstTime);
         return minDifference;
     }
 
     private int calculateMins(String time) {
         String[] split = time.split(":");
-        return 60 * Integer.parseInt(split[0]) + Integer.parseInt(split[1]);
+        return Integer.parseInt(split[0]) * 60 + Integer.parseInt(split[1]);
     }
 }
