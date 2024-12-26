1ac2bee9b9bb7188952781a0cf3ae7c5f3a02ace
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-26 08:11:34 -0600

    Time: 2 ms (85.12%) | Memory: 42 MB (24.41%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.java
+++ 1095-two-city-scheduling/two-city-scheduling.java
@@ -1,9 +1,19 @@
 class Solution {
     public int twoCitySchedCost(int[][] costs) {
+        //1. Sort based on priceA - priceB. 
+        //[10, 20] = 10 - 20 = -10. Cheaper to send to city A
+        //[400, 50] = 400 - 50 = 350. Cheaper to send to city B
         Arrays.sort(costs, (a, b) -> (a[0] - a[1]) - (b[0] - b[1]));
 
-        int total = 0, n = costs.length / 2;
-        for(int i = 0; i < n; i++) total+= costs[i][0] + costs[i + n][1];
-        return total;
+        //2. Send first half to city A
+        int totalCost = 0, n = costs.length / 2;
+        for(int i = 0; i < n; i++) {
+            totalCost+= costs[i][0];
+        }
+        //3. Send second half to city B
+        for(int i = n; i < costs.length; i++) {
+            totalCost+= costs[i][1];
+        }
+        return totalCost;
     }
 }
