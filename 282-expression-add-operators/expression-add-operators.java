42781aacf154144e87802277a8b9ae69750af464
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-26 08:41:12 -0600

    Time: 127 ms (10.71%) | Memory: 45.3 MB (63.31%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.java
+++ 282-expression-add-operators/expression-add-operators.java
@@ -15,7 +15,6 @@ class Solution {
         if(ind == num.length()) {
             if(value == target && curr == 0) {
                 StringBuilder sb = new StringBuilder();
-                //Do not include first operator
                 for(int i = 1; i < expr.size(); i++) sb.append(expr.get(i));
                 result.add(sb.toString());
             }
@@ -25,22 +24,18 @@ class Solution {
         //2. Fix curr, shift base 10
         curr*= 10;
         curr+= num.charAt(ind) - '0';
-
         //3. If curr != 0, no op
         if(curr != 0) backtrack(ind + 1, value, prev, curr, expr);
-
         //4. Addition
         expr.addAll(Arrays.asList("+", String.valueOf(curr)));
         backtrack(ind + 1, value + curr, curr, 0, expr);
         expr.subList(expr.size() - 2, expr.size()).clear();
-
-        //If numbers present in expression
+        //Only if expression not empty
         if(!expr.isEmpty()) {
             //5. Subtraction
             expr.addAll(Arrays.asList("-", String.valueOf(curr)));
             backtrack(ind + 1, value - curr, -curr, 0, expr);
             expr.subList(expr.size() - 2, expr.size()).clear();
-
             //6. Multiplication
             expr.addAll(Arrays.asList("*", String.valueOf(curr)));
             backtrack(ind + 1, value - prev + (prev * curr), prev * curr, 0, expr);
