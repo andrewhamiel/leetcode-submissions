9b2e2663e4cccea7e74b6be017426273c0ccd36a
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-27 06:45:54 -0600

    Time: 0 ms (100.00%) | Memory: 42.1 MB (75.11%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,22 +1,23 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        //2 Steps
+        //2 steps
+
         //1. Transpose
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = i; j < matrix[0].length; j++){
-                int tmp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = tmp;
+        for(int row = 0; row < matrix.length; row++) {
+            for(int col = row; col < matrix[0].length; col++) {
+                int tmp = matrix[row][col];
+                matrix[row][col] = matrix[col][row];
+                matrix[col][row] = tmp;
             }
         }
 
         //2. Reflect
-        for(int i = 0; i < matrix.length; i++){
-            int left = 0, right = matrix[i].length - 1;
-            while(left < right){
-                int tmp = matrix[i][left];
-                matrix[i][left++] = matrix[i][right];
-                matrix[i][right--] = tmp;
+        for(int row = 0; row < matrix.length; row++) {
+            int left = 0, right = matrix[0].length - 1;
+            while(left < right) {
+                int tmp = matrix[row][left];
+                matrix[row][left++] = matrix[row][right];
+                matrix[row][right--] = tmp;
             }
         }
     }
