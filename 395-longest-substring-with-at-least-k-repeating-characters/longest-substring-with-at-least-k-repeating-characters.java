e84e24e54c57ff7b363f5c6055ef52744be3c9e5
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-27 07:46:02 -0600

    Time: 5 ms (63.72%) | Memory: 41.4 MB (75.44%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
@@ -2,36 +2,34 @@ class Solution {
     public int longestSubstring(String s, int k) {
         char[] arr = s.toCharArray();
         int[] freqs = new int[26];
-        int maxUnique = getUniqueLetters(s), result = 0;
+        int maxUnique = getUniqueChars(arr), result = 0;
 
         for(int currUnique = 1; currUnique <= maxUnique; currUnique++) {
-            //reset freqs 
             Arrays.fill(freqs, 0);
-            int left = 0, right = 0, ind = 0, unique = 0, countAtLeastK = 0;
+            int left = 0, right = 0, unique = 0, containsAtLeastK = 0;
             while(right < arr.length) {
-                //Expand sliding window
-                if(unique <= currUnique) {
+                //Expanding sliding window
+                if(right < arr.length && unique <= currUnique) {
                     char c = arr[right++];
                     if(freqs[c - 'a'] == 0) unique++;
                     freqs[c - 'a']++;
-                    if(freqs[c - 'a'] == k) countAtLeastK++; 
+                    if(freqs[c - 'a'] == k) containsAtLeastK++;
                 }else {
-                    //Shrink sliding window
                     char c = arr[left++];
-                    if(freqs[c - 'a'] == k) countAtLeastK--;
+                    if(freqs[c - 'a'] == k) containsAtLeastK--;
                     freqs[c - 'a']--;
                     if(freqs[c - 'a'] == 0) unique--;
                 }
-                if(unique == currUnique && unique == countAtLeastK) result = Math.max(result, right - left);
-            }
+                if(unique == currUnique && containsAtLeastK == currUnique) result = Math.max(result, right - left);
+            }            
         }
         return result;
     }
 
-    private int getUniqueLetters(String s) {
+    private int getUniqueChars(char[] arr) {
         int unique = 0;
         boolean[] seen = new boolean[26];
-        for(char c : s.toCharArray()) {
+        for(char c : arr) {
             if(!seen[c - 'a']) unique++;
             seen[c - 'a'] = true;
         }
