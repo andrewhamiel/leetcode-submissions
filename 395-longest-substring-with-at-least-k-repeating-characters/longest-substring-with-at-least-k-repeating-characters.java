f1c6f6a6601636137677761f4a5b31f899de51f6
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-27 07:23:47 -0600

    Time: 5 ms (63.72%) | Memory: 41.3 MB (90.48%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
@@ -1,28 +1,28 @@
 public class Solution {
     public int longestSubstring(String s, int k) {
-        char[] str = s.toCharArray();
-        int[] countMap = new int[26];
+        char[] arr = s.toCharArray();
+        int[] freqs = new int[26];
         int maxUnique = getMaxUniqueLetters(s);
         int result = 0;
         for (int currUnique = 1; currUnique <= maxUnique; currUnique++) {
             // reset countMap
-            Arrays.fill(countMap, 0);
+            Arrays.fill(freqs, 0);
             int windowStart = 0, windowEnd = 0, idx = 0, unique = 0, countAtLeastK = 0;
-            while (windowEnd < str.length) {
+            while (windowEnd < arr.length) {
                 // expand the sliding window
                 if (unique <= currUnique) {
-                    idx = str[windowEnd] - 'a';
-                    if (countMap[idx] == 0) unique++;
-                    countMap[idx]++;
-                    if (countMap[idx] == k) countAtLeastK++;
+                    idx = arr[windowEnd] - 'a';
+                    if (freqs[idx] == 0) unique++;
+                    freqs[idx]++;
+                    if (freqs[idx] == k) countAtLeastK++;
                     windowEnd++;
                 }
                 // shrink the sliding window
                 else {
-                    idx = str[windowStart] - 'a';
-                    if (countMap[idx] == k) countAtLeastK--;
-                    countMap[idx]--;
-                    if (countMap[idx] == 0) unique--;
+                    idx = arr[windowStart] - 'a';
+                    if (freqs[idx] == k) countAtLeastK--;
+                    freqs[idx]--;
+                    if (freqs[idx] == 0) unique--;
                     windowStart++;
                 }
                 if (unique == currUnique && unique == countAtLeastK)
@@ -35,13 +35,11 @@ public class Solution {
 
     // get the maximum number of unique letters in the string s
     int getMaxUniqueLetters(String s) {
-        boolean map[] = new boolean[26];
+        boolean seen[] = new boolean[26];
         int maxUnique = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (!map[s.charAt(i) - 'a']) {
-                maxUnique++;
-                map[s.charAt(i) - 'a'] = true;
-            }
+            if (!seen[s.charAt(i) - 'a'])  maxUnique++;
+            seen[s.charAt(i) - 'a'] = true;
         }
         return maxUnique;
     }
