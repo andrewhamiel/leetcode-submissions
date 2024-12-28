67bc875f96e81ab691a521eb71c0087e9406a733
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-28 07:44:26 -0600

    Time: 22 ms (26.71%) | Memory: 45.4 MB (77.50%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,72 +1,50 @@
-class Solution {   
-    private int[][] dirs = new int[][]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
-    
+class Solution {
+    private int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
+
     /* A* Approach */
-    public int shortestPathBinaryMatrix(int[][] grid) {    
-        if (grid[0][0] != 0 || grid[grid.length - 1][grid[0].length - 1] != 0) return -1;
-        
-        // Set up the A* search. Min Heap
-        PriorityQueue<Candidate> minHeap = new PriorityQueue<>((a,b) -> a.totalEstimate - b.totalEstimate);
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        if(grid[0][0] == 1 || grid[grid.length - 1][grid[0].length - 1] == 1) return -1;
+
+        PriorityQueue<Candidate> minHeap = new PriorityQueue<>((a, b) -> a.totalEstimate - b.totalEstimate);
         minHeap.add(new Candidate(0, 0, 1, estimate(0, 0, grid)));
-        
         boolean[][] visited = new boolean[grid.length][grid[0].length];
-        
-        while (!minHeap.isEmpty()) {    
+
+        while(!minHeap.isEmpty()) {
             Candidate best = minHeap.poll();
-            
-            // Exit condition
-            if (best.row == grid.length - 1 && best.col == grid[0].length - 1) return best.distanceSoFar;
-            
-            if (visited[best.row][best.col]) continue;
-            
-            visited[best.row][best.col] = true;
-            
-            for (int[] neighbor : getNeighbors(best.row, best.col, grid)) {
-                int neighborRow = neighbor[0], neighborCol = neighbor[1];
-                
-                // This check isn't necessary for correctness, but it greatly
-                // improves performance.
-                if (visited[neighborRow][neighborCol]) continue;
-                
-                // Otherwise, we need to create a Candidate object for the option
-                // of going to neighbor through the current cell.
-                int newDistance = best.distanceSoFar + 1;
-                int totalEstimate = newDistance + estimate(neighborRow, neighborCol, grid);
-                Candidate candidate = new Candidate(neighborRow, neighborCol, newDistance, totalEstimate);
-                minHeap.add(candidate);
-            }   
-        }
-        return -1;  
-    }
-    
-    private List<int[]> getNeighbors(int row, int col, int[][] grid) {
-        List<int[]> neighbors = new ArrayList<>();
-        for (int[] dir : dirs) {
-            int newRow = row + dir[0], newCol = col + dir[1];
-            if(newRow >=  0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length
-                && grid[newRow][newCol] == 0) {
-                    neighbors.add(new int[]{newRow, newCol});
+            int row = best.row, col = best.col;
+            if(row == grid.length - 1 && col == grid[0].length - 1) return best.currDistance;
+            if(visited[row][col]) continue;
+            visited[row][col] = true;
+
+            for(int[] dir : dirs) {
+                int newRow = row + dir[0], newCol = col + dir[1];
+                if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length
+                    && grid[newRow][newCol] == 0 && !visited[newRow][newCol]) {
+                        int nextDistance = best.currDistance + 1;
+                        int totalEstimate = nextDistance + estimate(newRow, newCol, grid);
+                        Candidate nextCandidate = new Candidate(newRow, newCol, nextDistance, totalEstimate);
+                        minHeap.add(nextCandidate);
+                }
             }
         }
-        return neighbors; 
+        return -1;
     }
-    
+
     private int estimate(int row, int col, int[][] grid) {
-        int remainingRows = grid.length - row - 1;
-        int remainingCols = grid[0].length - col - 1;
+        int remainingRows = grid.length - row - 1, remainingCols = grid[0].length - col - 1;
         return Math.max(remainingRows, remainingCols);
     }
 
-    class Candidate {      
-        public int row;
-        public int col;
-        public int distanceSoFar;
-        public int totalEstimate;
-        
-        public Candidate(int row, int col, int distanceSoFar, int totalEstimate) {
+    class Candidate {
+        int row = 0;
+        int col = 0;
+        int currDistance = 0;
+        int totalEstimate = 0;
+
+        public Candidate(int row, int col, int currDistance, int totalEstimate) {
             this.row = row;
             this.col = col;
-            this.distanceSoFar = distanceSoFar;
+            this.currDistance = currDistance;
             this.totalEstimate = totalEstimate;
         }
     }
