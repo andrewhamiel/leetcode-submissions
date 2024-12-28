e0e1894020f1149e095fd9c182a6790af3dd49ea
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-28 07:15:35 -0600

    Time: 64 ms (6.39%) | Memory: 45.6 MB (57.69%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,31 +1,33 @@
 class Solution {
     private int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
-    
+
     public int shortestPathBinaryMatrix(int[][] grid) {
         if(grid[0][0] == 1 || grid[grid.length - 1][grid[0].length - 1] == 1) return -1;
 
-        Set<Pair<Integer, Integer>> visited = new HashSet<>();
-        visited.add(new Pair<>(0, 0));
         Queue<Pair<Integer, Integer>> q = new LinkedList<>();
         q.add(new Pair<>(0, 0));
-        int steps = 1;
+        Set<Pair<Integer, Integer>> visited = new HashSet<>();
+        visited.add(new Pair<>(0, 0));
+
+        int length = 1;
+
         while(!q.isEmpty()) {
             int size = q.size();
             while(size-- > 0) {
                 Pair<Integer, Integer> p = q.poll();
                 int row = p.getKey(), col = p.getValue();
-                if(row == grid.length - 1 && col == grid[0].length - 1) return steps;
+                if(row == grid.length - 1 && col == grid[0].length - 1) return length;
 
                 for(int[] dir : dirs) {
                     int newRow = row + dir[0], newCol = col + dir[1];
-                    if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length 
+                    if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length
                         && grid[newRow][newCol] == 0 && !visited.contains(new Pair<>(newRow, newCol))) {
                             visited.add(new Pair<>(newRow, newCol));
                             q.add(new Pair<>(newRow, newCol));
                         }
                 }
             }
-            steps++;
+            length++;
         }
         return -1;
     }
