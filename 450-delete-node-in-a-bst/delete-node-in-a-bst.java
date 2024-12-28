3715dd5f2adc9dae3258774f8c13e25fb8cb931d
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-28 06:37:09 -0600

    Time: 0 ms (100.00%) | Memory: 46.5 MB (9.34%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -19,12 +19,13 @@ class Solution {
 
         if(root.val < key) root.right = deleteNode(root.right, key);
         else if(root.val > key) root.left = deleteNode(root.left, key);
-        else{
+        else {
+            //1. Root, 2. Predecessor, 3. Inorder Successor
             if(root.left == null && root.right == null) root = null;
-            else if(root.left != null){
+            else if(root.left != null) {
                 root.val = predecessor(root);
                 root.left = deleteNode(root.left, root.val);
-            }else{
+            }else {
                 root.val = inorderSuccessor(root);
                 root.right = deleteNode(root.right, root.val);
             }
@@ -32,15 +33,15 @@ class Solution {
         return root;
     }
 
-    private int predecessor(TreeNode root){
-        TreeNode rightmost = root.left;
-        while(rightmost.right != null) rightmost = rightmost.right;
-        return rightmost.val;
+    private int predecessor(TreeNode root) {
+        TreeNode curr = root.left;
+        while(curr.right != null) curr = curr.right;
+        return curr.val;
     }
 
-    private int inorderSuccessor(TreeNode root){
-        TreeNode leftmost = root.right;
-        while(leftmost.left != null) leftmost = leftmost.left;
-        return leftmost.val;
+    private int inorderSuccessor(TreeNode root) {
+        TreeNode curr = root.right;
+        while(curr.left != null) curr = curr.left;
+        return curr.val;
     }
 }
