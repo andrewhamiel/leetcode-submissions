2bb12becb3c45bcc3d2354c9e9e8dff200ce03a2
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-29 07:10:16 -0600

    Time: 2 ms (100.00%) | Memory: 59.5 MB (52.06%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
@@ -1,11 +1,11 @@
 class Solution {
     public long findScore(int[] nums) {
         long result = 0;
-        //Simulate window size 3 by incrementing by 2
+        //Simulate window size 3 by 2
         for(int i = 0; i < nums.length; i+= 2) {
             //Find local minima
             int left = i;
-            while(i + 1 < nums.length && nums[i + 1] < nums[i]) i++;
+            while(i < nums.length - 1 && nums[i + 1] < nums[i]) i++;
 
             //Simulate marking neighbors by decrementing by 2
             for(int right = i; right >= left; right-= 2) result+= nums[right];
