09ab54f7f47498af577b5ed4b7d1fc5186785057
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-30 09:31:31 -0600

    Time: 5 ms (64.62%) | Memory: 41.6 MB (67.94%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
@@ -1,27 +1,26 @@
 class Solution {
     public int longestSubstring(String s, int k) {
         char[] arr = s.toCharArray();
-        int[] freqs = new int[26];
         int maxUnique = getUniqueChars(arr), result = 0;
         
         for(int currUnique = 1; currUnique <= maxUnique; currUnique++) {
-            Arrays.fill(freqs, 0); //Reset freqs
-            int left = 0, right = 0, unique = 0, containsAtLeastK = 0;
+            int[] freqs = new int[26];
+            int left = 0, right = 0, uniqueSeen = 0, containsAtLeastK = 0;
             while(right < arr.length) {
                 //Expand sliding window
-                if(unique <= currUnique) {
+                if(uniqueSeen <= currUnique) {
                     char c = arr[right++];
-                    if(freqs[c - 'a'] == 0) unique++;
+                    if(freqs[c - 'a'] == 0) uniqueSeen++;
                     freqs[c - 'a']++;
                     if(freqs[c - 'a'] == k) containsAtLeastK++;
                 }else {
                     char c = arr[left++];
                     if(freqs[c - 'a'] == k) containsAtLeastK--;
                     freqs[c - 'a']--;
-                    if(freqs[c - 'a'] == 0) unique--;
+                    if(freqs[c - 'a'] == 0) uniqueSeen--;
                 }
-                
-                if(unique == currUnique && containsAtLeastK == currUnique) result = Math.max(result, right - left);
+                //Right currently at next character to be seen
+                if(uniqueSeen == currUnique && containsAtLeastK == currUnique) result = Math.max(result, right - left);
             }
         }
         return result;
