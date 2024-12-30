ca80a48cf3b1af2ec1b759ebf9bffb4941927547
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-30 08:13:25 -0600

    Time: 49 ms (11.92%) | Memory: 55.9 MB (6.83%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -2,12 +2,17 @@ class Solution {
     private static final int MOD = 1_000_000_007;
 
     public int numSubseq(int[] nums, int target) {
-        Arrays.sort(nums);
+        //1. Sort
+        Arrays.sort(nums);    
+
+        //2. 2P and Binary Exp
         int left = 0, right = nums.length - 1, result = 0;
         while(left <= right) {
             if(nums[left] + nums[right] > target) right--;
             else {
-                result =  (result + (int) binaryExp(2, right - left)) % MOD;
+                //All subsequences in range [left, right] have 2 choices: include or not include
+                //Total subsequences is 2^(right - left)
+                result = (result + (int) binaryExp(2, right - left)) % MOD;
                 left++;
             }
         }
