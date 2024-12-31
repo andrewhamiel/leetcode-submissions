c283373301dd3ad15e0fd10ce3506fc89315abe6
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-31 10:54:37 -0600

    Time: 0 ms (100.00%) | Memory: 41.5 MB (11.17%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,7 +1,7 @@
 class Solution {
     public int rob(int[] nums) {
         if(nums.length == 1) return nums[0];
-        
+
         int t0 = nums[0], t1 = Math.max(nums[0], nums[1]);
         for(int i = 2; i < nums.length; i++) {
             int tmp = Math.max(t0 + nums[i], t1);
