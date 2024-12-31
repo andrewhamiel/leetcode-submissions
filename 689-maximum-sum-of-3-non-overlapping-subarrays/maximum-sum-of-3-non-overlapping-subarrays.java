ebcae5901aabb323be243d1af01505b0f7c91dfa
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-31 08:22:18 -0600

    Time: 6 ms (40.68%) | Memory: 46.4 MB (88.67%) - LeetSync

--- 689-maximum-sum-of-3-non-overlapping-subarrays/maximum-sum-of-3-non-overlapping-subarrays.java
+++ 689-maximum-sum-of-3-non-overlapping-subarrays/maximum-sum-of-3-non-overlapping-subarrays.java
@@ -1,38 +1,65 @@
 class Solution {
+
     public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
-        int[] windows = new int[nums.length - k + 1];
-        int sum = 0;
-        for(int i = 0; i < nums.length; i++){
-            sum+= nums[i];
-            if(i >= k) sum-= nums[i - k];
-            if(i >= k - 1) windows[i - k + 1] = sum;
+        
+        // Variables to track the best indices for one, two, and three subarray configurations
+        int bestSingleStart = 0;
+        int[] bestDoubleStart = { 0, k };
+        int[] bestTripleStart = { 0, k, k * 2 };
+
+        // Compute the initial sums for the first three subarrays
+        int singleCurrSum = 0, doubleCurrSum = 0, tripleCurrSum = 0;
+        for (int i = 0; i < k; i++) {
+            int secondInd = i + k;
+            int thirdInd = i + 2 * k;
+            singleCurrSum += nums[i];           
+            doubleCurrSum+= nums[secondInd];
+            tripleCurrSum+= nums[thirdInd];
         }
 
-        int[] left = new int[windows.length];
-        int best = 0;
-        for(int i = 0; i < left.length; i++){
-            if(windows[i] > windows[best]) best = i;
-            left[i] = best;
-        }
+        // Track the best sums found so far
+        int bestSingleSum = singleCurrSum;
+        int bestDoubleSum = singleCurrSum + doubleCurrSum;
+        int bestTripleSum = singleCurrSum + doubleCurrSum + tripleCurrSum;
+        
 
-        int[] right = new int[windows.length];
-        best = right.length - 1;
-        for(int i = right.length - 1; i >= 0; i--){
-            if(windows[i] >= windows[best]) best = i;
-            right[i] = best;
-        }
+        int singleLeft = 1;
+        int doubleStartInd = k + 1;
+        int tripleStartInd = k * 2 + 1;
+        // Slide the windows across the array
+        while (tripleStartInd <= nums.length - k) {
+            // Update the sums using the sliding window technique
+            singleCurrSum = singleCurrSum - nums[singleLeft - 1] + nums[singleLeft + k - 1];
+            doubleCurrSum = doubleCurrSum - nums[doubleStartInd - 1] + nums[doubleStartInd + k - 1];
+            tripleCurrSum = tripleCurrSum - nums[tripleStartInd - 1] + nums[tripleStartInd + k - 1];
 
-        int[] ans = new int[]{-1 ,-1, -1};
-        int maxWindow = Integer.MIN_VALUE;
-        for(int j = k; j < windows.length - k; j++){
-            int l = left[j - k], r = right[j + k];
-            if(maxWindow == Integer.MIN_VALUE || windows[l] + windows[j] + windows[r] > maxWindow){
-                ans[0] = l;
-                ans[1] = j;
-                ans[2] = r;
-                maxWindow = windows[l] + windows[j] + windows[r];
+            // Update the best single subarray start index if a better sum is found
+            if (singleCurrSum > bestSingleSum) {
+                bestSingleStart = singleLeft;
+                bestSingleSum = singleCurrSum;
             }
+
+            // Update the best double subarray start indices if a better sum is found
+            if (doubleCurrSum + bestSingleSum > bestDoubleSum) {
+                bestDoubleStart[0] = bestSingleStart;
+                bestDoubleStart[1] = doubleStartInd;
+                bestDoubleSum = doubleCurrSum + bestSingleSum;
+            }
+
+            // Update the best triple subarray start indices if a better sum is found
+            if (tripleCurrSum + bestDoubleSum > bestTripleSum) {
+                bestTripleStart[0] = bestDoubleStart[0];
+                bestTripleStart[1] = bestDoubleStart[1];
+                bestTripleStart[2] = tripleStartInd;
+                bestTripleSum = tripleCurrSum + bestDoubleSum;
+            }
+
+            // Move the sliding windows forward
+            singleLeft++;
+            doubleStartInd++;
+            tripleStartInd++;
         }
-        return ans;
+
+        return bestTripleStart;
     }
 }
