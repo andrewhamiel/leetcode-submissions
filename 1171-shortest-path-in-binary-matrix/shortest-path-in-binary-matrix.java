12e9435e129c8de6a64c974034514db4ae310f9b
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2024-12-31 10:30:33 -0600

    Time: 13 ms (80.58%) | Memory: 45.6 MB (47.91%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,51 +1,33 @@
 class Solution {
     private int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
 
-    /* A* Approach */
     public int shortestPathBinaryMatrix(int[][] grid) {
         if(grid[0][0] == 1 || grid[grid.length - 1][grid[0].length - 1] == 1) return -1;
 
-        PriorityQueue<Candidate> minHeap = new PriorityQueue<>((a, b) -> a.totalEstimate - b.totalEstimate);
-        minHeap.add(new Candidate(0, 0, 1, estimate(0, 0, grid)));
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0});
+        grid[0][0] = 1;
+        
+        int moves = 1;
 
-        while(!minHeap.isEmpty()) {
-            Candidate best = minHeap.poll();
-            int row = best.row, col = best.col;
-            if(row == grid.length - 1 && col == grid[0].length - 1) return best.currDistance;
-            if(visited[row][col]) continue;
-            visited[row][col] = true;
+        while(!q.isEmpty()) {
+            int size = q.size();
+            while(size-- > 0) {
+                int[] cell = q.poll();
+                int row = cell[0], col = cell[1];
+                if(row == grid.length - 1 && col == grid[0].length - 1) return moves;
 
-            for(int[] dir : dirs) {
-                int newRow = row + dir[0], newCol = col + dir[1];
-                if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length
-                    && grid[newRow][newCol] == 0 && !visited[newRow][newCol]) {
-                        int nextDistance = best.currDistance + 1;
-                        int totalEstimate = nextDistance + estimate(newRow, newCol, grid);
-                        Candidate nextCandidate = new Candidate(newRow, newCol, nextDistance, totalEstimate);
-                        minHeap.add(nextCandidate);
+                for(int[] dir : dirs) {
+                    int newRow = row + dir[0], newCol = col + dir[1];
+                    if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length
+                        && grid[newRow][newCol] == 0) {
+                            grid[newRow][newCol] = 1;
+                            q.add(new int[]{newRow, newCol});
+                    }
                 }
             }
+            moves++;
         }
         return -1;
-    }
-
-    private int estimate(int row, int col, int[][] grid) {
-        int remainingRows = grid.length - row - 1, remainingCols = grid[0].length - col - 1;
-        return Math.max(remainingRows, remainingCols);
-    }
-
-    class Candidate {
-        int row = 0;
-        int col = 0;
-        int currDistance = 0;
-        int totalEstimate = 0;
-
-        public Candidate(int row, int col, int currDistance, int totalEstimate) {
-            this.row = row;
-            this.col = col;
-            this.currDistance = currDistance;
-            this.totalEstimate = totalEstimate;
-        }
     }
 }
