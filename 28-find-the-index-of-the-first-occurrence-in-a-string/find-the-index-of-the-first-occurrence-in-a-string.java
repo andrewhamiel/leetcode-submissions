a5850b5fe0024b05c887ea68d5ce96fc79af2569
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-02 17:07:51 -0600

    Time: 1 ms (23.71%) | Memory: 41.3 MB (88.90%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,20 +1,45 @@
 class Solution {
+    int RADIX = 26;
+    int MOD = 1000000033;    
+
     public int strStr(String haystack, String needle) {
-        int haystackPtr = 0;
-        while(haystackPtr < haystack.length()) {
-            if(haystack.charAt(haystackPtr) == needle.charAt(0)) {
-                boolean isFound = true;
-                for(int i = 0; i < needle.length(); i++) {
-                    if(haystackPtr + i >= haystack.length()) return -1;
-                    if(haystack.charAt(haystackPtr + i) != needle.charAt(i)) {
-                        isFound = false;
-                        break;
-                    }
-                }
-                if(isFound) return haystackPtr;
+        int m = needle.length(), n = haystack.length();
+        if (n < m) return -1;
+
+        long MAX_WEIGHT = 1;
+        for (int i = 0; i < m; i++) MAX_WEIGHT = (MAX_WEIGHT * RADIX) % MOD;
+
+        // Compute hash of needle
+        long hashNeedle = hashValue(needle, RADIX, MOD, m), hashHay = 0;
+
+        // Check for each m-substring of haystack, starting at index windowStart
+        for (int windowStart = 0; windowStart <= n - m; windowStart++) {
+            // Compute hash of the First Substring
+            if (windowStart == 0) hashHay = hashValue(haystack, RADIX, MOD, m);
+            else {
+                // Update Hash using Previous Hash Value in O(1)
+                hashHay = (((hashHay * RADIX) % MOD) - (((int) (haystack.charAt(windowStart - 1) - 'a') * MAX_WEIGHT) % MOD) +
+                    (int) (haystack.charAt(windowStart + m - 1) - 'a') + MOD) 
+                    % MOD;
             }
-            haystackPtr++;
+            // If the hash matches, Check Character by Character.
+            // Because of Mod, spurious hits can be there.
+            if (hashNeedle == hashHay) {
+                for (int i = 0; i < m; i++) {
+                    if (needle.charAt(i) != haystack.charAt(i + windowStart)) break;
+                    if (i == m - 1) return windowStart;
+                }
+            }
         }
         return -1;
+    }
+
+    private int hashValue(String string, int RADIX, int MOD, int m) {
+        long ans = 0, factor = 1;
+        for (int i = m - 1; i >= 0; i--) {
+            ans = (ans + (string.charAt(i) - 'a') * factor) % MOD;
+            factor = (factor * RADIX) % MOD;
+        }
+        return (int) ans;
     }
 }
