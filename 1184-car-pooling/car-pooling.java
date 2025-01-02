3c8650afd358ba95554b36066f3c154c80f0d836
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-02 08:43:52 -0600

    Time: 2 ms (66.16%) | Memory: 44.8 MB (14.47%) - LeetSync

--- 1184-car-pooling/car-pooling.java
+++ 1184-car-pooling/car-pooling.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int[] timestamps = new int[1001];
         for(int[] trip : trips) {
             int passengers = trip[0], from = trip[1], to = trip[2];
-            map.put(from, map.getOrDefault(from, 0) + passengers);
-            map.put(to, map.getOrDefault(to, 0) - passengers);
+            timestamps[from]+= passengers;
+            timestamps[to]-= passengers;
         }
 
         int currPassengers = 0;
-        for(int pos : map.keySet()) {
-            currPassengers+= map.get(pos);
+        for(int i = 0; i < timestamps.length; i++) {
+            currPassengers+= timestamps[i];
             if(currPassengers > capacity) return false;
         }
         return true;
