46eebf1cfeddb15786a2107d8b9825e243109372
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-03 11:28:12 -0600

    Time: 2 ms (100.00%) | Memory: 59.3 MB (59.07%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i = 0; i < nums.length; i+= 2) {
             //Expand sliding window
             int left = i;
-            while(i < nums.length - 1 && nums[i + 1] < nums[i]) i++;
+            while(i < nums.length - 1 && nums[i] > nums[i + 1]) i++;
 
             //Simulate marking neighbors by decrementing by 2
             for(int right = i; right >= left; right-= 2) result+= nums[right];
