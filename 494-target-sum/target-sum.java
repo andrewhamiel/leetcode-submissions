07813b86bd8090cc364473deb36968532f132acc
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-03 07:32:49 -0600

    Time: 8 ms (77.77%) | Memory: 45.1 MB (16.66%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,18 +1,18 @@
 class Solution {
     private int totalSum = 0;
-    private int[][] memo;
-    private int[] nums;
     private int target = 0;
+    private int[] nums;
+    private int[][] memo;
 
     public int findTargetSumWays(int[] nums, int target) {
         for(int num : nums) totalSum+= num;
 
-        //Instead of [-totalSum, totalSum], we shift range by adding totalSum to lower and upper bounds
-        memo = new int[nums.length][2 * totalSum + 1];
-        for(int[] row : memo) Arrays.fill(row, Integer.MIN_VALUE);
-
-        this.nums = nums;
         this.target = target;
+        this.nums = nums;    
+
+        //Instead of using [-totalSum, totalSum], we shift to fit array by adding totalSum to all results making range [0, 2 * totalSum]
+        memo = new int[nums.length][totalSum * 2 + 1];
+        for(int[] row : memo) Arrays.fill(row, Integer.MIN_VALUE);
 
         return calculate(0, 0);
     }
@@ -24,7 +24,7 @@ class Solution {
         //2. Memoization
         if(memo[ind][sum + totalSum] != Integer.MIN_VALUE) return memo[ind][sum + totalSum];
 
-        //3. 2 choices: add or subtract
+        //3. 2 Choices: Add or subtract
         return memo[ind][sum + totalSum] = calculate(ind + 1, sum + nums[ind]) + calculate(ind + 1, sum - nums[ind]);
     }
 }
