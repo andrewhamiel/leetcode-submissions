b66b73f47c4c68d901286c47a8fd91bb4bc21860
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-03 12:59:06 -0600

    Time: 11 ms (31.54%) | Memory: 58.1 MB (24.09%) - LeetSync

--- 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.java
+++ 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.java
@@ -1,21 +1,24 @@
 class SparseVector {
-    private Map<Integer, Integer> map = new HashMap<>();
-    
+    List<int[]> pairs = new ArrayList<>();
+
     SparseVector(int[] nums) {
-        for(int i = 0; i < nums.length; i++) if(nums[i] != 0) map.put(i, nums[i]);
-    }
-    
-	// Return the dotProduct of two sparse vectors
-    public int dotProduct(SparseVector vec) {
-        int dotProduct = 0;
-        for(int key : vec.map.keySet()) {
-            if(map.containsKey(key)) dotProduct+= (vec.map.get(key) * map.get(key));
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) pairs.add(new int[]{i, nums[i]});
         }
-        return dotProduct;
+    }
+
+    // Return the dotProduct of two sparse vectors
+    public int dotProduct(SparseVector vec) {
+        int result = 0, p = 0, q = 0;
+        while (p < pairs.size() && q < vec.pairs.size()) {
+            if (pairs.get(p)[0] == vec.pairs.get(q)[0]) {
+                result += pairs.get(p)[1] * vec.pairs.get(q)[1];
+                p++;
+                q++;
+            }
+            else if (pairs.get(p)[0] > vec.pairs.get(q)[0]) q++;
+            else p++;
+        }
+        return result;
     }
 }
-
-// Your SparseVector object will be instantiated and called as such:
-// SparseVector v1 = new SparseVector(nums1);
-// SparseVector v2 = new SparseVector(nums2);
-// int ans = v1.dotProduct(v2);
