f08d14fc4fa742a64f9350e35c741478ba636f95
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-03 08:51:52 -0600

    Time: 1 ms (23.71%) | Memory: 41.4 MB (68.04%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -4,9 +4,9 @@ class Solution {
 
         //1. Preprocessing
         int[] longestBorder = new int[needle.length()];
-        int longestPrev = 0, ind = 1; //longestBorder[0] always 0, string cannot be prefix/suffix of itself
+        int longestPrev = 0, ind = 1; //longestBorder[0] always 0 because string cannot be prefix/suffix of itself
         while(ind < needle.length()) {
-            //Increase longest prev
+            //Increment longest prev
             if(needle.charAt(ind) == needle.charAt(longestPrev)) {
                 longestPrev++;
                 longestBorder[ind++] = longestPrev;
@@ -16,14 +16,14 @@ class Solution {
             }
         }
 
-        //2. Searching
+        //2. Searching 
         int haystackInd = 0, needleInd = 0;
         while(haystackInd < haystack.length()) {
             if(haystack.charAt(haystackInd) == needle.charAt(needleInd)) {
                 haystackInd++;
                 needleInd++;
                 //All characters matched
-                if(needleInd == needle.length()) return haystackInd - needle.length(); 
+                if(needleInd == needle.length()) return haystackInd - needle.length();
             }else {
                 if(needleInd == 0) haystackInd++; //No characters matched
                 else needleInd = longestBorder[needleInd - 1];
