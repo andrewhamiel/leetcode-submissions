9aa0dd6c2fd2fb45929f7db7a42adede5edfa850
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-03 11:17:14 -0600

    Time: 0 ms (100.00%) | Memory: 42.7 MB (17.01%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,7 +1,7 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int left = 0, right = nums.length - 1; //Comparing mid + 1
-        //[left, right) variation
+        int left = 0, right = nums.length - 1; //Comparing mid + 1 in search space
+        //[left, right) variant 
         while(left < right) {
             int mid = left + (right - left)/2;
             if(nums[mid] < nums[mid + 1]) left = mid + 1;
