30d7033788583ad290773c70c1dc50e70fe4ef5c
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-02 18:05:22 -0600

    Time: 3 ms (80.56%) | Memory: 43.5 MB (59.28%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,15 +1,32 @@
 class Solution {
+    class TrieNode {
+        Map<Character, TrieNode> children = new HashMap<>();
+        boolean isWord = false;
+    }
+    
     public boolean wordBreak(String s, List<String> wordDict) {
+        //1. Build TrieNode
+        TrieNode root = new TrieNode();
+        for(String word : wordDict){
+            TrieNode curr = root;
+            for(char c : word.toCharArray()){
+                curr.children.putIfAbsent(c, new TrieNode());
+                curr = curr.children.get(c);
+            }
+            curr.isWord = true;
+        }
+
+        //2. Bottom-up DP
         boolean[] dp = new boolean[s.length()];
         for(int i = 0; i < s.length(); i++){
-            for(String word : wordDict){
-                if(i - word.length() + 1 < 0) continue;
+            if(i == 0 || dp[i - 1]){
+                TrieNode curr = root;
+                for(int j = i; j < s.length(); j++){
+                    char c=  s.charAt(j);
+                    if(!curr.children.containsKey(c)) break;
 
-                if(i == word.length() - 1 || dp[i - word.length()]){
-                    if(s.substring(i - word.length() + 1, i + 1).equals(word)){
-                        dp[i] = true;
-                        break;
-                    }
+                    curr = curr.children.get(c);
+                    if(curr.isWord) dp[j] = true;
                 }
             }
         }
