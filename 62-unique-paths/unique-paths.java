5dad1838150c4029f572658b89ad928080e09d8c
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-03 10:40:33 -0600

    Time: 0 ms (100.00%) | Memory: 40.6 MB (39.13%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,17 +1,10 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-
-        //Fill first row
-        Arrays.fill(dp[0], 1);
-        //Fill first col
-        for(int row = 0; row < dp.length; row++) dp[row][0] = 1;
-
-        for(int row = 1; row < dp.length; row++) {
-            for(int col = 1; col < dp[0].length; col++) {
-                dp[row][col] = dp[row - 1][col] + dp[row][col - 1];
-            }
+        long answer = 1;
+        for (int i = n; i < (m + n - 1); i++) {
+            answer *= i;
+            answer /= (i - n + 1);
         }
-        return dp[m - 1][n - 1];
+        return (int) answer;
     }
 }
