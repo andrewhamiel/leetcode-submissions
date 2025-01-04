48954ee9d7ed89bebbe6d6cda37832efa6a55e07
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-04 08:42:58 -0600

    Time: 1 ms (99.82%) | Memory: 53 MB (59.17%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,16 +1,11 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int majorityElement = 0;
+        int count = 0, candidate = Integer.MAX_VALUE;
 
-        for(int i = 0; i < 32; i++) {
-            int mask = 1 << i;
-
-            int bitCount = 0;
-            for(int num : nums) {
-                if((num & mask) != 0) bitCount++;
-            }
-            if(bitCount > nums.length / 2) majorityElement |= mask;
+        for(int num : nums) {
+            if(count == 0) candidate = num;
+            count+= num == candidate ? 1 : -1;
         }
-        return majorityElement;
+        return candidate;
     }
 }
