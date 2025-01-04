bf06d9fd3fd4c19afd8c33bd6cae8761bc3da204
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-04 16:27:48 -0600

    Time: 8 ms (75.91%) | Memory: 43.6 MB (79.26%) - LeetSync

--- 224-basic-calculator/basic-calculator.java
+++ 224-basic-calculator/basic-calculator.java
@@ -1,30 +1,29 @@
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> stack = new Stack<Integer>();
-        int operand = 0, result = 0, sign = 1;  
-
-        for (int i = 0; i < s.length(); i++) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int operand = 0, result = 0, sign = 1;
+        for(int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (Character.isDigit(c)) {
-                operand = 10 * operand + (int) (c - '0');
-
-            } else if (c == '+') {
-                result += sign * operand;
+            if(Character.isDigit(c)) {
+                operand*= 10;
+                operand+= (c - '0');
+            }else if(c == '+') {
+                result+= sign * operand;
                 sign = 1;
                 operand = 0;
-            } else if (c == '-') {
-                result += sign * operand;
+            }else if(c == '-') {
+                result+= sign * operand;
                 sign = -1;
                 operand = 0;
-            } else if (c == '(') {
-                stack.push(result);
-                stack.push(sign);
+            }else if(c == '(') {
+                stack.addFirst(result);
+                stack.addFirst(sign);
                 sign = 1;
                 result = 0;
-            } else if (c == ')') {
-                result += sign * operand;
-                result *= stack.pop();
-                result += stack.pop();
+            }else if(c == ')') {
+                result+= sign * operand;
+                result*= stack.removeFirst();
+                result+= stack.removeFirst();
                 operand = 0;
             }
         }
