79b7bb61b981b0da5f668a8ee1a8bb64d9fef6a6
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-04 09:23:04 -0600

    Time: 2 ms (91.17%) | Memory: 41.4 MB (56.13%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,33 +1,30 @@
 class Solution {
     public String countAndSay(int n) {
+        //1. Base Cases
         if(n == 0) return "";
-        return dp(n);
-    }
-
-    private String dp(int n) {
-        //1. Base case 
         if(n == 1) return "1";
 
         //2. Recursion
+        String s = countAndSay(n - 1);
         StringBuilder result = new StringBuilder();
-        String s = dp(n - 1);
         int count = 0;
         char prev = s.charAt(0);
 
-        //3. Count Variables
+        //3. Count
         for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if(c == prev) count++;
+            char curr = s.charAt(i);
+            if(curr == prev) count++;
             else {
-                //5. Append
+                //4. Append 
                 result.append(count);
                 result.append(prev);
-                //6. Reset variables
+                //5. Reset variables 
                 count = 1;
-                prev = c;
+                prev = curr;
             }
         }
-        //7. Set final num
+
+        //6. Append last count
         result.append(count);
         result.append(prev);
 
