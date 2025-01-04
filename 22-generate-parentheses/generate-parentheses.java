3fdc58436b77268cadc30f53255a540d90e06b38
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-04 16:48:53 -0600

    Time: 1 ms (82.61%) | Memory: 42.9 MB (84.67%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -3,7 +3,7 @@ class Solution {
 
     public List<String> generateParenthesis(int n) {
         if(n == 0) return new ArrayList<>();
-        backtrack(new StringBuilder(),n, n);
+        backtrack(new StringBuilder(), n, n);
         return result;    
     }
 
@@ -16,19 +16,17 @@ class Solution {
 
         int currLength = sb.length();
 
-        //2. Add left paren if remaining
+        //2. Add left paren if any remaining
         if(leftCount > 0) {
             sb.append('(');
             backtrack(sb, leftCount - 1, rightCount);
             sb.deleteCharAt(currLength);
         }
-        //3. Add right paren if valid
+        //3. Add right paren if valid to do so
         if(leftCount < rightCount) {
             sb.append(')');
             backtrack(sb, leftCount, rightCount - 1);
             sb.deleteCharAt(currLength);
         }
     }
-
-
 }
