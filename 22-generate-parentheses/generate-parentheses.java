90a2a989f43e91d237902893581b5b20099ce1ed
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-04 16:43:05 -0600

    Time: 8 ms (6.86%) | Memory: 45 MB (5.05%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,32 +1,15 @@
 class Solution {
-    private List<String> result = new ArrayList<>();
-
     public List<String> generateParenthesis(int n) {
-        if(n == 0) return new ArrayList<>();
-        backtrack(new StringBuilder(), 0, n, n);
+        if (n == 0) return new ArrayList(Arrays.asList(""));
+
+        List<String> result = new ArrayList();
+        for (int leftCount = 0; leftCount < n; leftCount++) {
+            for (String leftString : generateParenthesis(leftCount)) {
+                for (String rightString : generateParenthesis(n - 1 - leftCount)) {
+                    result.add("(" + leftString + ")" + rightString);
+                }
+            } 
+        } 
         return result;
-    }
-
-    private void backtrack(StringBuilder sb, int ind, int leftCount, int rightCount) {
-        //1. Exit condition
-        if(leftCount == 0 && rightCount == 0) {
-            result.add(sb.toString());
-            return;
-        }
-
-        int currLength = sb.length();
-
-        //2. If left parens left to add
-        if(leftCount > 0) {
-            sb.append('(');
-            backtrack(sb, ind + 1, leftCount - 1, rightCount);
-            sb.deleteCharAt(currLength);
-        }
-        //3. If valid to add right parens
-        if(leftCount < rightCount) {
-            sb.append(')');
-            backtrack(sb, ind + 1, leftCount, rightCount - 1);
-            sb.deleteCharAt(currLength);
-        }
     }
 }
