2df6196aa523f6ed42eb2d263f844e3f2ea09f11
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-04 08:02:59 -0600

    Time: 4 ms (96.95%) | Memory: 45.1 MB (21.09%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -6,15 +6,15 @@ class Solution {
 
     private List<List<Integer>> kSum(int left, int[] nums, long target, int k) {
         List<List<Integer>> result = new ArrayList<>();
-        //1. Exit Condition
+        //1. Exit condition
         if(left == nums.length) return result;
-        //2. Out of bouds sum. K nums left, if nums[left] > averageVal || nums[nums.length - 1] < averageVal then not possible
+        //2. Out of bounds sum. K nums left, if nums[left] > averageVal || nums[nums.length - 1] < averageVal then sum not possible
         long averageVal = target / k;
         if(nums[left] > averageVal || nums[nums.length - 1] < averageVal) return result;
         //3. Base case
         if(k == 2) return twoSumSorted(left, nums, target);
         //4. KSum
-        for(int i = left; i < nums.length - 1; i++) {
+        for(int i = left; i < nums.length; i++) {
             if(i == left || nums[i] != nums[i - 1]) {
                 for(List<Integer> subList : kSum(i + 1, nums, target - nums[i], k - 1)) {
                     List<Integer> list = new ArrayList<>(Arrays.asList(nums[i]));
@@ -32,7 +32,7 @@ class Solution {
         while(left < right) {
             int sum = nums[left] + nums[right];
             if(sum == target) {
-                result.add(Arrays.asList(nums[left++], nums[right--]));
+                result.add(new ArrayList<>(Arrays.asList(nums[left++], nums[right--])));
                 while(left < nums.length && nums[left] == nums[left - 1]) left++;
             }else if(sum < target) left++;
             else right--;
