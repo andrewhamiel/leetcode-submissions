0c3850a8b1f36b85a4c69ae4b5ecb5d0e1057b8d
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-04 16:30:25 -0600

    Time: 6 ms (90.93%) | Memory: 44.9 MB (31.79%) - LeetSync

--- 224-basic-calculator/basic-calculator.java
+++ 224-basic-calculator/basic-calculator.java
@@ -2,24 +2,23 @@ class Solution {
     public int calculate(String s) {
         Deque<Integer> stack = new ArrayDeque<>();
         int operand = 0, result = 0, sign = 1;
-        for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+        for(char c : s.toCharArray()) {
             if(Character.isDigit(c)) {
                 operand*= 10;
                 operand+= (c - '0');
             }else if(c == '+') {
-                result+= sign * operand;
+                result+= operand * sign;
                 sign = 1;
                 operand = 0;
             }else if(c == '-') {
-                result+= sign * operand;
+                result+= operand * sign;
                 sign = -1;
                 operand = 0;
             }else if(c == '(') {
                 stack.addFirst(result);
                 stack.addFirst(sign);
-                sign = 1;
                 result = 0;
+                sign = 1;
             }else if(c == ')') {
                 result+= sign * operand;
                 result*= stack.removeFirst();
@@ -27,6 +26,6 @@ class Solution {
                 operand = 0;
             }
         }
-        return result + (sign * operand);
+        return result + (operand * sign);
     }
 }
