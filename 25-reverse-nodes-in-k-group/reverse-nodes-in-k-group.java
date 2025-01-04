caeeab575913411a799f2bd48cdfa57357d74768
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-04 10:09:18 -0600

    Time: 0 ms (100.00%) | Memory: 44.9 MB (5.33%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -10,31 +10,39 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode curr = head, newHead = null, kTail = null;
-        while(curr != null){
+        ListNode curr = head, newHead = null, prevTail = null;
+        while(curr != null) {
+            //1. Advance K nodes
             int count = 0;
-            while(curr != null && count < k){
+            while(curr != null && count < k) {
                 curr = curr.next;
                 count++;
             }
-            if(count == k){
-                ListNode reverseHead = reverse(head, k);
+            //2. Final group remains as is if size < k
+            if(count == k) {
+                //3. Head becomes delinked -> Is now last reversed element in K-Group
+                ListNode firstInGroup = head, reverseHead = reverseK(head, k); 
+                //4. Set new head after reversing first group
                 if(newHead == null) newHead = reverseHead;
-                if(kTail != null) kTail.next = reverseHead;
-                kTail = head;
+                //5. Connect previous component to next component
+                if(prevTail != null) prevTail.next = reverseHead;
+                //6. Set tail as last element in reversed group
+                prevTail = head;
+                //7. Reset head to first element in next group
                 head = curr;
             }
         }
-        if(kTail != null) kTail.next = head;
+        //8. Connect final group
+        if(prevTail != null) prevTail.next = head;
         return newHead;
     }
 
-    private ListNode reverse(ListNode head, int k){
+    private ListNode reverseK(ListNode head, int k) {
         ListNode curr = head, prev = null;
-        while(k-- > 0){
+        while(curr != null && k-- > 0) {
             ListNode nextNode = curr.next;
             curr.next = prev;
-            prev = curr;
+            prev = curr; 
             curr = nextNode;
         }
         return prev;
