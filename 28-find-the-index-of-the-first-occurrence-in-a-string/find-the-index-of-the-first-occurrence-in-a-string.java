88dbbd1c778b17e4d8dbe1c4efb72682131016ae
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-04 11:09:01 -0600

    Time: 1 ms (23.66%) | Memory: 41.6 MB (40.42%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,21 +1,21 @@
 class Solution {
     /*
-    * KMP. LPS aka Longest Border tells us longest prefix that is also a suffix. We can reuse prefixes to compute
+    * KMP. LPS aka longest border finds longest prefix that is also suffix. Allows us to reuse prefixes
     */
     public int strStr(String haystack, String needle) {
         if(needle.length() > haystack.length()) return -1;
 
         //1. Preprocessing
         int[] longestBorder = new int[needle.length()];
-        int prevLongest = 0, ind = 1; //longestBorder[0] is always 0. Cannot be prefix/suffix of self
+        int prevLongest = 0, ind = 1; //longestBorder[0] always 0 because str cannot be prefix/suffix of self
         while(ind < needle.length()) {
-            //Increment prev longest 
+            //Increment prevLongest
             if(needle.charAt(ind) == needle.charAt(prevLongest)) {
                 prevLongest++;
                 longestBorder[ind++] = prevLongest;
             }else {
                 if(prevLongest == 0) longestBorder[ind++] = 0; //No characters match
-                else prevLongest = longestBorder[prevLongest - 1]; //Look for smaller prefix for match
+                else prevLongest = longestBorder[prevLongest - 1]; //Look for smaller calculated prefix match
             }
         }
 
@@ -29,7 +29,7 @@ class Solution {
                 if(needleInd == needle.length()) return haystackInd - needle.length();
             }else {
                 if(needleInd == 0) haystackInd++; //No characters match
-                else needleInd = longestBorder[needleInd - 1];
+                else needleInd = longestBorder[needleInd - 1]; //Look for smaller prefix match
             }
         }
         return -1;
