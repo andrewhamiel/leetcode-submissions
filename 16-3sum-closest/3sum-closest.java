9277f5d5faf171ea011ae85571426efe8a600d70
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-04 07:15:45 -0600

    Time: 13 ms (90.80%) | Memory: 43.6 MB (18.95%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -3,7 +3,7 @@ class Solution {
         Arrays.sort(nums);
         int minDiff = Integer.MAX_VALUE;
         for(int i = 0; i < nums.length - 2; i++) {
-            if(i == 0 || nums[i] != nums[i - 1])  {
+            if(i == 0 || nums[i] != nums[i - 1]) {
                 int closest = twoSumSorted(i + 1, nums, nums[i], target);
                 if(Math.abs(target - closest) < Math.abs(minDiff)) minDiff = target - closest;
             }
@@ -14,7 +14,7 @@ class Solution {
     private int twoSumSorted(int left, int[] nums, int currSum, int target) {
         int right = nums.length - 1, minDiff = Integer.MAX_VALUE;
         while(left < right) {
-            int sum = nums[left] + nums[right] + currSum;
+            int sum = currSum + nums[left] + nums[right];
             if(Math.abs(target - sum) < Math.abs(minDiff)) minDiff = target - sum;
             if(sum <= target) left++;
             else right--;
