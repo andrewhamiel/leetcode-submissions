e16d803cae33963ca93b03e69c5c2d850b7bd019
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-05 12:11:12 -0600

    Time: 1 ms (100.00%) | Memory: 47 MB (68.76%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,6 +1,8 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
+        //Key intuition: array is sorted. Largest powers would be farthest left and farthest right because -num * -num = num^2
         int[] result = new int[nums.length];
+        //2P 
         int left = 0, right = nums.length - 1;
         for(int k = nums.length - 1; k >= 0; k--) {
             int powLeft = nums[left] * nums[left];
