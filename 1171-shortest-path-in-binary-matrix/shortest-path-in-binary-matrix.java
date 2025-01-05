154361e3d80627b33aa06a3621de6be28b1b8aaf
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-05 12:18:56 -0600

    Time: 17 ms (49.65%) | Memory: 45.9 MB (21.05%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -2,51 +2,39 @@ class Solution {
     private int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
 
     public int shortestPathBinaryMatrix(int[][] grid) {
+        //Key intuition: Shortest Path -> BFS good option
+        //1. Base case
         if(grid[0][0] == 1 || grid[grid.length - 1][grid[0].length - 1] == 1) return -1;
 
-        PriorityQueue<Candidate> minHeap = new PriorityQueue<>((a, b) -> a.totalEstimate - b.totalEstimate);
-        minHeap.add(new Candidate(0, 0, 1, getEstimate(0, 0, grid)));
+        //2. Create our Queue and initial state
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0});
         boolean[][] visited = new boolean[grid.length][grid[0].length];
+        visited[0][0] = true;
 
-        while(!minHeap.isEmpty()) {
-            Candidate best = minHeap.poll();
-            int row = best.row, col = best.col;
-            //Exit condition
-            if(row == grid.length - 1 && col == grid[0].length - 1) return best.currDistance;
-            //Memoization
-            if(visited[row][col]) continue;
-            visited[row][col] = true;
+        int moves = 1;
 
-            for(int[] dir : dirs) {
-                int newRow = row + dir[0], newCol = col + dir[1];
-                if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length 
-                    && grid[newRow][newCol] == 0 && !visited[newRow][newCol]) {
-                        int nextDistance = best.currDistance + 1;
-                        int totalEstimate = nextDistance + getEstimate(newRow, newCol, grid);
-                        Candidate nextCandidate = new Candidate(newRow, newCol, nextDistance, totalEstimate);
-                        minHeap.add(nextCandidate);
+        //3. BFS
+        while(!q.isEmpty()) {
+            int size = q.size();
+            while(size-- > 0) {
+                int[] cell = q.poll();
+                int row = cell[0], col = cell[1];
+                //Exit condition
+                if(row == grid.length - 1 && col == grid[0].length - 1) return moves;
+
+                for(int[] dir : dirs) {
+                    int newRow = row + dir[0], newCol = col + dir[1];
+                    //Want to see if this is valid, unvisited next node to traverse
+                    if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length
+                        && !visited[newRow][newCol] && grid[newRow][newCol] == 0) {
+                            visited[newRow][newCol] = true; //Only visit once
+                            q.add(new int[]{newRow, newCol});
+                        }
                 }
             }
+            moves++;
         }
         return -1;
-    }
-
-    private int getEstimate(int row, int col, int[][] grid) {
-        int remainingRows = grid.length - row - 1, remainingCols = grid[0].length - col - 1;
-        return Math.max(remainingRows, remainingCols);
-    }
-
-    class Candidate {
-        int row = 0;
-        int col = 0;
-        int currDistance = 0;
-        int totalEstimate = 0;
-
-        public Candidate(int row, int col, int currDistance, int totalEstimate) {
-            this.row = row;
-            this.col = col;
-            this.currDistance = currDistance;
-            this.totalEstimate = totalEstimate;
-        }
     }
 }
