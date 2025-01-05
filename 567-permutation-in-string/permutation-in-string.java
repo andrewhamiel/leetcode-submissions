72502c009327f200ce6739c82007b280855b5330
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-05 11:24:28 -0600

    Time: 9 ms (48.58%) | Memory: 43 MB (44.93%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,7 +1,11 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        //Key intuition: permutation is same length, but has potentially different order of characters
+
+        //Base case: Not possible if s1 larger than s2
         if(s1.length() > s2.length()) return false;
 
+        //First K
         int[] s1Freqs = new int[26], s2Freqs = new int[26];
         for(int i = 0; i < s1.length(); i++) {
             s1Freqs[s1.charAt(i) - 'a']++;
@@ -10,10 +14,13 @@ class Solution {
 
         //Sliding window
         for(int i = 0; i < s2.length() - s1.length(); i++) {
+            //Evaluate current window for a match
             if(matches(s1Freqs, s2Freqs)) return true;
 
-            s2Freqs[s2.charAt(i) - 'a']--; //Decrement sliding window
-            s2Freqs[s2.charAt(s1.length() + i) - 'a']++; //Expand sliding window
+            //Shrink the sliding window
+            s2Freqs[s2.charAt(i) - 'a']--;
+            //Expand the sliding window
+            s2Freqs[s2.charAt(s1.length() + i) - 'a']++;
         }
         return matches(s1Freqs, s2Freqs);
     }
