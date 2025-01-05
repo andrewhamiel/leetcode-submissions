5b197897bd23fe4ce29ed14e9093b1f3c3ba63c5
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-05 11:02:34 -0600

    Time: 1 ms (64.50%) | Memory: 46.4 MB (51.58%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,18 +1,14 @@
 class Solution {
     public int trap(int[] height) {
-        int[] leftHeights = new int[height.length], rightHeights = new int[height.length];
-        //Left hand: monotonically non-decreasing
-        leftHeights[0] = height[0];
-        for(int i = 1; i < leftHeights.length; i++) leftHeights[i] = Math.max(leftHeights[i - 1], height[i]);
-        //Right hand: monotonically non-increasing
-        rightHeights[rightHeights.length - 1] = height[height.length - 1];
-        for(int i = rightHeights.length - 2; i >= 0; i--) rightHeights[i] = Math.max(rightHeights[i + 1], height[i]);
-
-        //Compare for volume
-        int vol = 0;
-        for(int i = 0; i < height.length; i++) {
-            int minPrefixHeight = Math.min(leftHeights[i], rightHeights[i]);
-            if(height[i] < minPrefixHeight) vol+= minPrefixHeight - height[i];
+        int left = 0, right = height.length - 1, leftHeight = height[0], rightHeight = height[height.length - 1], vol = 0;
+        while(left <= right) {
+            if(height[left] < height[right]) {
+                leftHeight = Math.max(leftHeight, height[left]);
+                vol+= leftHeight - height[left++];
+            }else {
+                rightHeight = Math.max(rightHeight, height[right]);
+                vol+= rightHeight - height[right--];
+            }
         }
         return vol;
     }
