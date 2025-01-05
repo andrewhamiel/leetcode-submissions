082c296b659b263ca28b0b513a8ec34affd48d09
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-05 10:29:56 -0600

    Time: 0 ms (100.00%) | Memory: 42.4 MB (33.94%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -2,8 +2,9 @@ class Solution {
     public void rotate(int[][] matrix) {
         //2 Steps
 
-        //1. Transpose: Swap rows/cols to flip diagonal top left -> bottom right
-        for(int row = 0; row < matrix.length; row++) {
+        //1. Transpose: Swap rows and cols. Flips across main diagonal from top left -> bottom right
+        for(int row = 0; row < matrix.length; row++){
+            //Avoid redundant swaps
             for(int col = row; col < matrix[0].length; col++) {
                 int tmp = matrix[row][col];
                 matrix[row][col] = matrix[col][row];
@@ -11,9 +12,9 @@ class Solution {
             }
         }
 
-        //2. Reflect. Flip horizontally from left -> right
+        //2. Reflect: Flip horizontally from left -> right
         for(int row = 0; row < matrix.length; row++) {
-            int left = 0, right = matrix[row].length - 1;
+            int left =  0, right = matrix[row].length - 1;
             while(left < right) {
                 int tmp = matrix[row][left];
                 matrix[row][left++] = matrix[row][right];
