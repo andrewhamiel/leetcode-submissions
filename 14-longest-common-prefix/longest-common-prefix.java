d784a827aea88254b0eabf2a981089bd2fb6626e
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-05 15:29:07 -0600

    Time: 1 ms (63.55%) | Memory: 41.9 MB (30.16%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -3,7 +3,6 @@ class Solution {
         StringBuilder result = new StringBuilder();
         for(int i = 0; i < strs[0].length(); i++) {
             char c = strs[0].charAt(i);
-            boolean isBreakFound = false;
             for(String str : strs) {
                 if(i >= str.length() || str.charAt(i) != c) return result.toString();
             }
