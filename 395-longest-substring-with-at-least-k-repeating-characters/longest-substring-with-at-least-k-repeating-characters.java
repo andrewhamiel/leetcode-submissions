ea9e5c26f5587e453457ec6ceb25449249eea431
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-05 11:37:09 -0600

    Time: 5 ms (63.42%) | Memory: 41.8 MB (55.49%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
@@ -1,26 +1,34 @@
 class Solution {
     public int longestSubstring(String s, int k) {
+        //Intuition: Number of unique characters we are including in a substring
+        //[1, maxUniqueCharacters], see what the largest substring size is with that number of unique characters. Limits our iterations to 26N
         char[] arr = s.toCharArray();
         int maxUnique = getUniqueChars(arr), result = 0;
+        //Sliding window
         for(int currUnique = 1; currUnique <= maxUnique; currUnique++) {
             int[] freqs = new int[26];
             int left = 0, right = 0, uniqueSeen = 0, containsAtLeastK = 0;
             while(right < arr.length) {
-                //Advance sliding window
-                if(uniqueSeen <= currUnique) {          
-                    char c = arr[right++];         
+                //Expand sliding window
+                if(uniqueSeen <= currUnique) {
+                    char c = arr[right++];
                     if(freqs[c - 'a'] == 0) uniqueSeen++;
                     freqs[c - 'a']++;
                     if(freqs[c - 'a'] == k) containsAtLeastK++;
                 }else {
+                    //Shrink sliding window
                     char c = arr[left++];
                     if(freqs[c - 'a'] == k) containsAtLeastK--;
                     freqs[c - 'a']--;
                     if(freqs[c - 'a'] == 0) uniqueSeen--;
                 }
-                //Right at next index to be observed
+
+                //Compare to result
+                //Right now past last element we evaluated in window, so we do not need to add 1 for length
                 if(uniqueSeen == currUnique && containsAtLeastK == currUnique) result = Math.max(result, right - left);
             }
+
+            
         }
         return result;
     }
