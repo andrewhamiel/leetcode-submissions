0d7cfd87718673b2380c2af9c29e4adad731eb88
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-05 15:49:27 -0600

    Time: 1 ms (87.15%) | Memory: 41 MB (47.26%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,7 +1,8 @@
 class Solution {
     public int mySqrt(int x) {
         if(x < 2) return x;
-        int left = 0, right = x/2;
+
+        int left = 2, right = x/2;
         while(left <= right) {
             int mid = left + (right - left)/2;
             long pow = (long) mid * mid;
