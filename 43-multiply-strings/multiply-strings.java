9384389df243480a55844361f9b00a803c8d89b7
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-05 11:17:41 -0600

    Time: 6 ms (32.32%) | Memory: 42.4 MB (44.85%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,41 +1,45 @@
 class Solution {
     public String multiply(String num1, String num2) {
+        //Intuition: use elementary long multiplication
+        //1. Base case
         if(num1.equals("0") || num2.equals("0")) return "0";
 
-        //Calculation is right -> left
+        //123 * 456
+        //Start by multiplying last digits together first, work backwards
+        //So to make this easier to iterate through, we can first reverse the strings
         StringBuilder firstNum = new StringBuilder(num1), secondNum = new StringBuilder(num2);
-        //Reverse to start with multiplying ones
+
         firstNum.reverse();
         secondNum.reverse();
-        //Will need to worry about carries: 9 * 2 = 18, which is 8 carry 1
-        //Initialize all digits to 0 because no carries seen yet
-        //Why is length of result firstNum.length() + secondNum.length()?
+
+        //Length of result will be at most num1.length() + num2.length(). Last digit could potentially be 0, so range of valid lengths is [nums1.length() + nums2.length() - 1, nums1.length() + nums2.length()]
+        //Initialize result, and set all of its digits initially to '0' so we can keep track of any carries that are a result of elementary multiplication
         StringBuilder result = new StringBuilder();
         for(int i = 0; i < firstNum.length() + secondNum.length(); i++) result.append('0');
 
-        for(int place1 = 0; place1 < firstNum.length(); place1++) {
-            int firstDigit = firstNum.charAt(place1) - '0';
-            int carry = 0;
-            for(int place2 = 0; place2 < secondNum.length(); place2++) {
-                int secondDigit = secondNum.charAt(place2) - '0';
+        //Now we will traverse through each number and perform elementary multiplication
+        //Loop through second number first because this will make keeping track of the carry value much easier
+        for(int place2 = 0; place2 < secondNum.length(); place2++) {
+            int secondDigit = secondNum.charAt(place2) - '0';
+            for(int place1 = 0; place1 < firstNum.length(); place1++) {
+                int firstDigit = firstNum.charAt(place1) - '0';
 
+                //To find carry and location where to insert in result, need current position
                 int currPos = place1 + place2;
-                int currSumAtPos = result.charAt(currPos) - '0';
-
-                int product = firstDigit * secondDigit + currSumAtPos + carry;
+                int carry = result.charAt(currPos) - '0';
+                int product = firstDigit * secondDigit + carry;
+                //Set ones digit first because reverse order
                 result.setCharAt(currPos, (char) ((product % 10) + '0'));
-                carry = product / 10;
+                //Set tens digit
+                carry = result.charAt(currPos + 1) - '0'; //Tens carry
+                result.setCharAt(currPos + 1, (char) ((product / 10 + carry) + '0'));
             }
-            //Handle remaining carry
-            int carryPos = place1 + secondNum.length();
-            int existingCarry = result.charAt(carryPos) - '0';
-            result.setCharAt(carryPos, (char)((existingCarry + carry) + '0'));
         }
 
-        //If there is a leading zero because no carry existed, remove it. Why?
+        //Handle leading zero if present
         if(result.charAt(result.length() - 1) == '0') result.deleteCharAt(result.length() - 1);
 
-        //Reverse to restore to original order
+        //Restore correct orientation
         result.reverse();
         return result.toString();
     }
