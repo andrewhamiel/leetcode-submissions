8ec40b7a376e6c552eec1385d8e3d4927534188e
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-05 11:07:03 -0600

    Time: 2 ms (100.00%) | Memory: 59.3 MB (61.42%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
@@ -1,14 +1,13 @@
 class Solution {
     public long findScore(int[] nums) {
         long result = 0;
-        //SImulate window size 3 by incrementing by 2
+        //Simulate window size 3 by incrementing by 2
         for(int i = 0; i < nums.length; i+= 2) {
-            //Expand sliding window
+            //Expand the sliding window
             int left = i;
             while(i + 1 < nums.length && nums[i + 1] < nums[i]) i++;
-
-            //Simulate marking neighbors by decrementing by 2
-            for(int right = i; right >= left; right-= 2) result+= nums[right];
+            //Simulate marking neighbors by decrementing by two
+            for(int right = i; right >= left; right -= 2) result+= nums[right];
         }
         return result;
     }
