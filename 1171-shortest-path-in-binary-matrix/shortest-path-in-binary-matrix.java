9ebdba9b5e40c99e8f1024f535580b6185c5d0db
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-06 14:29:15 -0600

    Time: 22 ms (27.31%) | Memory: 45.6 MB (61.72%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -2,30 +2,29 @@ class Solution {
     private int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
 
     public int shortestPathBinaryMatrix(int[][] grid) {
-        //1. Base case
         if(grid[0][0] == 1 || grid[grid.length - 1][grid[0].length - 1] == 1) return -1;
 
         PriorityQueue<Candidate> minHeap = new PriorityQueue<>((a, b) -> a.totalEstimate - b.totalEstimate);
         minHeap.add(new Candidate(0, 0, 1, getEstimate(0, 0, grid)));
         boolean[][] visited = new boolean[grid.length][grid[0].length];
 
-        //A* traversal
         while(!minHeap.isEmpty()) {
             Candidate best = minHeap.poll();
             int row = best.row, col = best.col;
             //Exit condition
             if(row == grid.length - 1 && col == grid[0].length - 1) return best.currDistance;
-            //Memoization
+            //2. Memoization
             if(visited[row][col]) continue;
             visited[row][col] = true;
 
             for(int[] dir : dirs) {
                 int newRow = row + dir[0], newCol = col + dir[1];
                 if(newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length
-                    && !visited[newRow][newCol] && grid[newRow][newCol] == 0) {
+                    && grid[newRow][newCol] == 0 && !visited[newRow][newCol]) {
                         int nextDistance = best.currDistance + 1;
                         int totalEstimate = nextDistance + getEstimate(newRow, newCol, grid);
-                        minHeap.add(new Candidate(newRow, newCol, nextDistance, totalEstimate));
+                        Candidate nextCandidate = new Candidate(newRow, newCol, nextDistance, totalEstimate);
+                        minHeap.add(nextCandidate);
                 }
             }
         }
