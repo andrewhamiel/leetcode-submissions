63929cc615c85e23078f0af183ffdc30110010a8
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-06 12:55:28 -0600

    Time: 12 ms (97.98%) | Memory: 43.1 MB (78.54%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -1,24 +1,25 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int minDiff = Integer.MAX_VALUE;
+        int closest = 100_000;
         for(int i = 0; i < nums.length - 2; i++) {
             if(i == 0 || nums[i] != nums[i - 1]) {
-                int closest = twoSumSorted(i + 1, nums, nums[i], target);
-                if(Math.abs(target - closest) < Math.abs(minDiff)) minDiff = target - closest;
+                int sum = nums[i] + twoSumSorted(i + 1, nums, target - nums[i]);
+                if(Math.abs(target - sum) < Math.abs(target - closest)) closest = sum;
             }
         }
-        return target - minDiff;
+        return closest;
     }
 
-    private int twoSumSorted(int left, int[] nums, int currSum, int target) {
-        int right = nums.length - 1, minDiff = Integer.MAX_VALUE;
+    private int twoSumSorted(int left, int[] nums, int target) {
+        int right = nums.length - 1, closest = 100_000;
         while(left < right) {
-            int sum = currSum + nums[left] + nums[right];
-            if(Math.abs(target - sum) < Math.abs(minDiff)) minDiff = target - sum;
-            if(sum <= target) left++;
+            int sum = nums[left] + nums[right];
+            if(Math.abs(target - sum) < Math.abs(target - closest)) closest = sum;
+            if(sum == target) return sum;
+            else if(sum < target) left++;
             else right--;
         }
-        return target - minDiff;
+        return closest;
     }
 }
