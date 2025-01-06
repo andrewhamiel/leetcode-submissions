02f8e757fdbc228d0b48ddc0d87329f8c603ea25
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-06 15:23:15 -0600

    Time: 0 ms (100.00%) | Memory: 43.3 MB (17.46%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,12 +1,11 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        //Key intuition: monotonically non-increasing sequence, when reversed, becomes mononically non-decreasing
         int i = nums.length - 2;
         while(i >= 0 && nums[i] >= nums[i + 1]) i--;
 
         if(i >= 0) {
             int j = nums.length - 1;
-            while(j > 0 && nums[j] <= nums[i]) j--;
+            while(j > i && nums[j] <= nums[i]) j--;
             swap(i, j, nums);
         }
         reverse(i + 1, nums);
