d4012073cf853d354c24aabc68b2feb73177f725
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-06 14:56:16 -0600

    Time: 1 ms (100.00%) | Memory: 46.5 MB (9.21%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,13 +1,8 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        //Key intuition here is that the arrays are both sorted
-        //Want to do binary search, but need a way to do so without provisioning extra space to maintain runtime requirements. Can partition each array to do this and compare the partitions similar to a min and max heap
-        
-        //Base case: to avoid overflow error, nums1 must be <= nums2 length
         if(nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
 
-        int left = 0, right = nums1.length; //Will handle
-        //[left, right] variant
+        int left = 0, right = nums1.length;
         while(left <= right) {
             int partitionA = left + (right - left)/2;
             int partitionB = (nums1.length + nums2.length + 1)/2 - partitionA;
@@ -20,7 +15,7 @@ class Solution {
 
             if(maxLeftA <= minRightB && maxLeftB <= minRightA) {
                 if((nums1.length + nums2.length) % 2 == 0) return (double) (Math.max(maxLeftA, maxLeftB) + Math.min(minRightA, minRightB))/2;
-                else return (double) (Math.max(maxLeftA, maxLeftB));
+                else return (double) Math.max(maxLeftA, maxLeftB);
             }else if(maxLeftA > minRightB) right = partitionA - 1;
             else left = partitionA + 1;
         }
