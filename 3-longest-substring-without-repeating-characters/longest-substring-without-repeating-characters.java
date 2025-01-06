e04a750aa7ebadcd54908228c36a628a0548ade6
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-06 17:49:58 -0600

    Time: 6 ms (70.00%) | Memory: 44.9 MB (37.44%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -5,7 +5,7 @@ class Solution {
         while(right < s.length()) {
             if(seen.contains(s.charAt(right))) seen.remove(s.charAt(left++));
             else seen.add(s.charAt(right++));
-            longest = Math.max(longest, right - left); //Right at next index we are evaluating so do not need to add 1
+            longest = Math.max(longest, right - left);
         }
         return longest;
     }
