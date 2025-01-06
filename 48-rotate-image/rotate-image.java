e858390949a2f33124c3c43a6d540508fb013dee
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-06 13:57:01 -0600

    Time: 0 ms (100.00%) | Memory: 42.2 MB (66.96%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,10 +1,11 @@
 class Solution {
     public void rotate(int[][] matrix) {
+        //Key intuition: on 2D plane, 90 degree rotation is (x, y) -> (y, -x)
         //2 Steps
 
-        //1. Transpose: Swap rows and cols. Flips across main diagonal from top left -> bottom right
-        for(int row = 0; row < matrix.length; row++){
-            //Avoid redundant swaps
+        //1. Transpose: Flip across diagonal from top left -> bottom right
+        for(int row = 0; row < matrix.length; row++) {
+            //Avoid duplicate swaps
             for(int col = row; col < matrix[0].length; col++) {
                 int tmp = matrix[row][col];
                 matrix[row][col] = matrix[col][row];
@@ -14,7 +15,7 @@ class Solution {
 
         //2. Reflect: Flip horizontally from left -> right
         for(int row = 0; row < matrix.length; row++) {
-            int left =  0, right = matrix[row].length - 1;
+            int left = 0, right = matrix[row].length - 1;
             while(left < right) {
                 int tmp = matrix[row][left];
                 matrix[row][left++] = matrix[row][right];
