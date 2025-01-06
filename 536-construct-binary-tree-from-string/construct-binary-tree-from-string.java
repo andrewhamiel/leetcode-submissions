f349abdc3507a3a439946cf9b4cc380136b4cde7
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-06 14:41:03 -0600

    Time: 8 ms (58.05%) | Memory: 45.3 MB (35.29%) - LeetSync

--- 536-construct-binary-tree-from-string/construct-binary-tree-from-string.java
+++ 536-construct-binary-tree-from-string/construct-binary-tree-from-string.java
@@ -1,64 +1,27 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public TreeNode str2tree(String s) {
-        if(s.isEmpty()) return null;
-
-        TreeNode root = new TreeNode();
-        Deque<TreeNode> stack = new ArrayDeque<>();
-        stack.addFirst(root);
-
-        for(int i = 0; i < s.length(); i++) {
-            TreeNode curr = stack.removeFirst();
-            //Not started
-            if(Character.isDigit(s.charAt(i)) || s.charAt(i) == '-') {
-                Pair<Integer, Integer> p = getNumber(i, s);
-                i = p.getKey();
-                curr.val = p.getValue();
-
-                //First subtree will be left child if any data left
-                if(i < s.length() && s.charAt(i) == '(') {
-                    stack.addFirst(curr);
-                    curr.left = new TreeNode();
-                    stack.addFirst(curr.left);
-                }
-            }else if(s.charAt(i) == '(' && curr.left != null) {
-                //Left done
-                stack.addFirst(curr);
-                curr.right = new TreeNode();
-                stack.addFirst(curr.right);
-            }
-        }
-        return !stack.isEmpty() ? stack.removeFirst() : root;
-    }
-
-    private Pair<Integer, Integer> getNumber(int ind, String s) {
-        boolean isNegative = false;
-        if(s.charAt(ind) == '-') {
-            isNegative = true;
-            ind++;
-        }
-
-        int num = 0;
-        while(ind < s.length() && Character.isDigit(s.charAt(ind))) {
-            num*= 10;
-            num+= (s.charAt(ind++) - '0');
-        }
+        final Deque<TreeNode> stack = new ArrayDeque<>();
+        final StringBuilder sb = new StringBuilder();
         
-        if(isNegative) num*= -1;
-        return new Pair<>(ind, num);
+        for (char c: s.toCharArray()) {
+            //1. Parens
+            if (c == '(' || c == ')') {
+                //Add current value to stack
+                if (sb.length() > 0) { 
+                    stack.addFirst(new TreeNode(Integer.parseInt(sb.toString())));
+                    sb.delete(0, sb.length());
+                }
+                //If close paren, start popping
+                if (c == ')') { 
+                    TreeNode top = stack.removeFirst();
+                    if (stack.peekFirst().left == null) stack.peekFirst().left = top;
+                    else stack.peekFirst().right = top;
+                }
+            } else sb.append(c); //2. Digit or sign
+        }
+        //3. Place unadded value on stack if present ie -> s = "4"
+        if (sb.length() > 0) stack.addFirst(new TreeNode(Integer.parseInt(sb.toString())));
+        
+        return stack.isEmpty() ? null : stack.removeFirst();
     }
 }
