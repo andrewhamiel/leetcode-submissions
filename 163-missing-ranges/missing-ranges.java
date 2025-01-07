bb4b9d5f7774c71b90765895435721c6edc6f083
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-06 18:22:21 -0600

    Time: 0 ms (100.00%) | Memory: 41.9 MB (7.98%) - LeetSync

--- 163-missing-ranges/missing-ranges.java
+++ 163-missing-ranges/missing-ranges.java
@@ -2,19 +2,19 @@ class Solution {
     public List<List<Integer>> findMissingRanges(int[] nums, int lower, int upper) {
         List<List<Integer>> result = new ArrayList<>();
 
-        if(nums == null || nums.length == 0){
-            result.add(List.of(lower, upper));
+        if(nums == null || nums.length == 0) {
+            result.add(Arrays.asList(lower, upper));
             return result;
         }
 
-        if(lower < nums[0]) result.add(List.of(lower, nums[0] - 1));
+        if(lower < nums[0]) result.add(Arrays.asList(lower, nums[0] - 1));
 
-        for(int i = 1; i < nums.length; i++){
+        for(int i = 1; i < nums.length; i++) {
             if(nums[i] - nums[i - 1] <= 1) continue;
-            result.add(List.of(nums[i - 1] + 1, nums[i] - 1));
+            result.add(Arrays.asList(nums[i - 1] + 1, nums[i] - 1));
         }
 
-        if(upper > nums[nums.length - 1]) result.add(List.of(nums[nums.length - 1] + 1, upper));
+        if(upper > nums[nums.length - 1]) result.add(Arrays.asList(nums[nums.length - 1] + 1, upper));
         return result;
     }
 }
