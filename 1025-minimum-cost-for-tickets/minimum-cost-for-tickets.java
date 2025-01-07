582a5e97e890897939d26f70aba7e70cf19736c8
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-06 19:52:41 -0600

    Time: 2 ms (37.95%) | Memory: 42.3 MB (9.03%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -1,31 +1,34 @@
 class Solution {
-    private Set<Integer> isTravelNeeded = new HashSet<>();
+    Set<Integer> isTravelNeeded = new HashSet<>();
     private int[] costs;
+    private int[] dp;
 
     public int mincostTickets(int[] days, int[] costs) {
         int lastDay = days[days.length - 1];
-        int[] dp = new int[lastDay + 1];
+        dp = new int[lastDay + 1];
         Arrays.fill(dp, -1);
-        this.costs = costs;
 
         for(int day : days) isTravelNeeded.add(day);
-        return solve(1, dp);
+
+        this.costs = costs;
+
+        return solve(1);
     }
 
-    private int solve(int day, int[] dp) {
-        //1. Base case/exit condition
+    private int solve(int day) {
+        //1. Exit condition
         if(day >= dp.length) return 0;
 
         //2. Memoization
         if(dp[day] != -1) return dp[day];
 
-        //3. If travel not needed
-        if(!isTravelNeeded.contains(day)) return solve(day + 1, dp);
+        //3. If travel is not needed
+        if(!isTravelNeeded.contains(day)) return solve(day + 1);
 
         //4. Min cost
-        int oneDay = costs[0] + solve(day + 1, dp);
-        int sevenDay = costs[1] + solve(day + 7, dp);
-        int thirtyDay = costs[2] + solve(day + 30, dp);
+        int oneDay = costs[0] + solve(day + 1);
+        int sevenDay = costs[1] + solve(day + 7);
+        int thirtyDay = costs[2] + solve(day + 30);
 
         return dp[day] = Math.min(oneDay, Math.min(sevenDay, thirtyDay));
     }
