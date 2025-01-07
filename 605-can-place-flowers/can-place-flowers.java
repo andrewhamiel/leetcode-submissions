a90a42fd0c60dc9675038a830417e28db156ea74
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-06 19:10:47 -0600

    Time: 1 ms (96.77%) | Memory: 45.4 MB (89.07%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -2,13 +2,14 @@ class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
         int count = 0;
         for(int i = 0; i < flowerbed.length; i++) {
+            //if flower already planted
             if(flowerbed[i] == 1) continue;
 
             boolean isLeftFree = i == 0 || flowerbed[i - 1] == 0;
             boolean isRightFree = i == flowerbed.length - 1 || flowerbed[i + 1] == 0;
 
             if(isLeftFree && isRightFree) {
-                flowerbed[i] = 1;
+                flowerbed[i] = 1; //Mark to not count neighbors as well
                 count++;
             }
         }
