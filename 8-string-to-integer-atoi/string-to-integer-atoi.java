8a9f442d12d2b0aaffb343300ad7f2be2618d1bc
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-07 07:32:17 -0600

    Time: 2 ms (31.64%) | Memory: 42.6 MB (31.71%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -3,23 +3,21 @@ class Solution {
         //1. Whitespace
         s = s.trim();
         //2. Sign
-        int sign = 1, ind = 0;
+        int ind = 0, sign = 1;
         if(ind < s.length() && (s.charAt(ind) == '+' || s.charAt(ind) == '-')) {
             sign = s.charAt(ind++) == '+' ? 1 : -1;
         }
         //3. Conversion
         long result = 0;
-        while(ind < s.length() && Character.isDigit(s.charAt(ind)) && Math.abs(result) < Integer.MAX_VALUE) {
+        while(ind < s.length() && Character.isDigit(s.charAt(ind)) && result < Integer.MAX_VALUE) {
             result*= 10;
             result+= s.charAt(ind++) - '0';
         }
         result*= sign;
-
         //4. Rounding
-        if(result < Integer.MIN_VALUE || result > Integer.MAX_VALUE) {
+        if(result < Integer.MIN_VALUE || result > Integer.MAX_VALUE ) {
             return result < Integer.MIN_VALUE ? Integer.MIN_VALUE : Integer.MAX_VALUE;
         }
-               
         return (int) result;
     }
 }
