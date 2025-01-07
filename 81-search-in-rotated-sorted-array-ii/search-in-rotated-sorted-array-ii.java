77674d60ce2237a707b8c0c1aa40b93b42d65629
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-07 07:45:39 -0600

    Time: 0 ms (100.00%) | Memory: 43.6 MB (14.67%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -3,16 +3,29 @@ class Solution {
         int left = 0, right = nums.length - 1;
         while(left <= right) {
             int mid = left + (right - left)/2;
+            //1. Check target
             if(nums[mid] == target) return true;
-            
-            if(nums[mid] > nums[left]) {
+            //2. Make sure binary search helpful
+            if(!isBinarySearchHelpful(left, mid, nums)) {
+                left++;
+                continue;
+            }
+            //3. Mid in left Subarray or right subarray
+            if(nums[left] < nums[mid]) {
                 if(nums[mid] > target && nums[left] <= target) right = mid - 1;
                 else left = mid + 1;
-            }else if(nums[mid] < nums[left]) {
+            }else{
                 if(nums[mid] < target && nums[left] > target) left = mid + 1;
                 else right = mid - 1;
-            }else left++;
+            }
         }
         return false;
+    }
+
+    /*
+    * If arr[mid] == arr[left], arr[mid] could belone to either subarray and binary search cannot help us find relative position
+    */
+    private boolean isBinarySearchHelpful(int left, int mid, int[] nums) {
+        return nums[left] != nums[mid];
     }
 }
