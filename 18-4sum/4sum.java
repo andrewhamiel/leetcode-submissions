39443df74fc712f3ad91b507771e5c16273ab488
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-07 07:18:05 -0600

    Time: 5 ms (94.14%) | Memory: 44.7 MB (25.31%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -6,15 +6,19 @@ class Solution {
 
     private List<List<Integer>> kSum(int left, int[] nums, long target, int k) {
         List<List<Integer>> result = new ArrayList<>();
+
         //1. Exit condition
         if(left == nums.length) return result;
-        //2. Out of bounds sum. K nums left, if nums[left] > average val || nums[nums.length - 1] < average val then not possible
+
+        //2. Out of bounds sums. K nums left, if nums[left] > averageVal or nums[nums.length - 1] < averageVal then sum not possible
         long averageVal = target / k;
         if(nums[left] > averageVal || nums[nums.length - 1] < averageVal) return result;
+
         //3. Base case
         if(k == 2) return twoSumSorted(left, nums, target);
+
         //4. KSum
-        for(int i = left; i < nums.length; i++) {
+        for(int i = left; i < nums.length - k + 1; i++) {
             if(i == left || nums[i] != nums[i - 1]) {
                 for(List<Integer> subList : kSum(i + 1, nums, target - nums[i], k - 1)) {
                     List<Integer> list = new ArrayList<>(Arrays.asList(nums[i]));
