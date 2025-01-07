ea83f7faae884be9aa8d703c0616d4a8352adfae
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-06 18:25:41 -0600

    Time: 0 ms (100.00%) | Memory: 41.5 MB (54.58%) - LeetSync

--- 163-missing-ranges/missing-ranges.java
+++ 163-missing-ranges/missing-ranges.java
@@ -2,19 +2,24 @@ class Solution {
     public List<List<Integer>> findMissingRanges(int[] nums, int lower, int upper) {
         List<List<Integer>> result = new ArrayList<>();
 
+        //1. No input
         if(nums == null || nums.length == 0) {
             result.add(Arrays.asList(lower, upper));
             return result;
         }
 
+        //2. Lower bound check
         if(lower < nums[0]) result.add(Arrays.asList(lower, nums[0] - 1));
 
+        //3. Middle range of array
         for(int i = 1; i < nums.length; i++) {
             if(nums[i] - nums[i - 1] <= 1) continue;
-            result.add(Arrays.asList(nums[i - 1] + 1, nums[i] - 1));
+            else result.add(Arrays.asList(nums[i - 1] + 1, nums[i] - 1));
         }
 
-        if(upper > nums[nums.length - 1]) result.add(Arrays.asList(nums[nums.length - 1] + 1, upper));
+        //4. Upper bound check
+        if(nums[nums.length - 1] < upper) result.add(Arrays.asList(nums[nums.length - 1] + 1, upper));
+
         return result;
     }
 }
