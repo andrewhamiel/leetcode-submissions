0f0ba8551e6029ecc758192a523c692dc46e46a1
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-06 18:41:02 -0600

    Time: 1 ms (100.00%) | Memory: 45.1 MB (12.21%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,27 +1,26 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        // Use a binary number to record previous occurrence
         int[] rows = new int[9], cols = new int[9], boxes = new int[9];
-        for (int row = 0; row < board.length; row++) {
-            for (int col = 0; col < board[0].length; col++) {
-                // Check if the position is filled with number
-                if (board[row][col] == '.') continue;
-                
+        for(int row = 0; row < board.length; row++) {
+            for(int col = 0; col < board[0].length; col++) {
+                //1. Check position for number
+                if(board[row][col] == '.') continue;
+
                 int val = board[row][col] - '0';
                 int pos = 1 << (val - 1);
 
-                // Check the row
-                if ((rows[row] & pos) > 0) return false;                
-                rows[row] |= pos;
+                //2. Check row
+                if((rows[row] & pos) > 0) return false;
+                rows[row]|= pos;
 
-                // Check the column
-                if ((cols[col] & pos) > 0) return false;
-                cols[col] |= pos;
+                //3. Check col
+                if((cols[col] & pos) > 0) return false;
+                cols[col]|= pos;
 
-                // Check the box
-                int ind = (row / 3) * 3 + col / 3;
-                if ((boxes[ind] & pos) > 0) return false;
-                boxes[ind] |= pos;
+                //4. Check box
+                int ind = (row / 3) * 3 + (col / 3);
+                if((boxes[ind] & pos) > 0) return false;
+                boxes[ind]|= pos;
             }
         }
         return true;
