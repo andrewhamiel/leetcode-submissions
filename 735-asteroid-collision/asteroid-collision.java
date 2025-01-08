9c816cb6317d5684f002145b2ea935f055e7c4b9
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-08 11:09:13 -0600

    Time: 4 ms (95.03%) | Memory: 45.3 MB (59.49%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         Deque<Integer> stack = new ArrayDeque<>();
-        for(int asteroid : asteroids){
-            while(!stack.isEmpty() && stack.peekFirst() > 0 && asteroid < 0){
+        for(int asteroid : asteroids) {
+            while(!stack.isEmpty() && stack.peekFirst() > 0 && asteroid < 0) {
                 int sum = stack.peekFirst() + asteroid;
                 if(sum < 0) stack.removeFirst();
-                else if(sum == 0){
+                else if(sum == 0) {
                     asteroid = 0;
                     stack.removeFirst();
-                }else if(sum > 0){
+                }else if(sum > 0) {
                     asteroid = 0;
                     break;
                 }
@@ -17,7 +17,7 @@ class Solution {
         }
 
         int[] result = new int[stack.size()];
-        for(int i = 0; i < result.length; i++) result[i] = stack.removeLast();
+        for(int i = result.length - 1; i >= 0; i--) result[i] = stack.removeFirst();
         return result;
     }
 }
