a447a48d38f9bad3271ae318414af177b8cbb7f2
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-07 21:39:36 -0600

    Time: 7 ms (48.10%) | Memory: 61.7 MB (12.55%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.java
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.java
@@ -2,10 +2,10 @@ class Solution {
     public int[] findBuildings(int[] heights) {
         Deque<Integer> stack = new ArrayDeque<>();
         stack.addFirst(heights.length - 1);
-        for(int i = heights.length - 2; i >= 0; i--){
+        for(int i = heights.length - 2; i >= 0; i--) {
             if(heights[i] > heights[stack.peekFirst()]) stack.addFirst(i);
         }
-
+        
         int[] result = new int[stack.size()];
         for(int i = 0; i < result.length; i++) result[i] = stack.removeFirst();
         return result;
