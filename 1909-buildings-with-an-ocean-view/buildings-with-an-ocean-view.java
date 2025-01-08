366e3b6fbd1fce59f1ac9ba72fa1101664e95195
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-08 09:32:47 -0600

    Time: 7 ms (48.10%) | Memory: 61.2 MB (42.58%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.java
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.java
@@ -5,7 +5,6 @@ class Solution {
         for(int i = heights.length - 2; i >= 0; i--) {
             if(heights[i] > heights[stack.peekFirst()]) stack.addFirst(i);
         }
-        
         int[] result = new int[stack.size()];
         for(int i = 0; i < result.length; i++) result[i] = stack.removeFirst();
         return result;
