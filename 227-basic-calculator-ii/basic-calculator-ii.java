cb5a153749a50b2f8210d444e0949ccf1baa98c9
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-07 20:03:49 -0600

    Time: 17 ms (71.81%) | Memory: 44.8 MB (86.03%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -1,19 +1,17 @@
 class Solution {
     public int calculate(String s) {
-        int curr = 0, prev = 0, result = 0;
         char op = '+';
-        
+        int curr = 0, prev = 0, result = 0;
         for(int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if(Character.isDigit(c)){
+            if(Character.isDigit(c)) {
                 curr*= 10;
-                curr+= (c - '0');
+                curr+= c - '0';
             }
-
-            if(i == s.length() - 1 || (!Character.isDigit(c) && !Character.isWhitespace(c))){
-                if(op == '+' || op == '-'){
-                    result+= prev;
+            if(i == s.length() - 1 || (!Character.isDigit(c) && !Character.isWhitespace(c))) {
+                if(op == '+' || op == '-') {                   
                     if(op == '-') curr*= -1;
+                    result+= prev;
                     prev = curr;
                 }else if(op == '*') prev*= curr;
                 else if(op == '/') prev/= curr;
@@ -22,6 +20,8 @@ class Solution {
                 curr = 0;
             }
         }
+
+        //Add remaining prev
         result+= prev;
         return result;
     }
