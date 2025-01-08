a67edca7663ac120be0e473fbbaa7417c2b45330
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-07 21:12:38 -0600

    Time: 3 ms (98.58%) | Memory: 43.3 MB (63.52%) - LeetSync

--- 71-simplify-path/simplify-path.java
+++ 71-simplify-path/simplify-path.java
@@ -1,20 +1,17 @@
 class Solution {
     public String simplifyPath(String path) {
         Deque<String> stack = new ArrayDeque<>();
-        String[] dirs = path.split("/");
-
-        for(String dir : dirs){
-            if(dir.equals("/") || dir.equals(".") || dir.isEmpty()) continue;
-            else if(dir.equals("..")){
+        for(String dir : path.split("/")) {
+            if(dir.isEmpty() || dir.equals(".")) continue;
+            else if(dir.equals("..")) {
                 if(!stack.isEmpty()) stack.removeFirst();
             }else stack.addFirst(dir);
         }
 
         StringBuilder result = new StringBuilder();
-        while(!stack.isEmpty()){
-            result.append("/");
-            result.append(stack.removeLast());
+        while(!stack.isEmpty()) {
+            result.append("/").append(stack.removeLast());
         }
-        return result.isEmpty() ? "/" : result.toString(); 
+        return result.isEmpty() ? "/" : result.toString();
     }
 }
