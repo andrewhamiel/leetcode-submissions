86ecc827c2aa23d96009fd28668c0cc6c4264607
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-07 19:05:06 -0600

    Time: 15 ms (83.89%) | Memory: 45.8 MB (25.59%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,27 +1,27 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        //1. Remove all invalid ')'
-        int leftParens = 0, balance = 0;
+        //1. Remove invalid ')'
         StringBuilder sb = new StringBuilder();
+        int leftParens = 0, balance = 0;
         for(char c : s.toCharArray()) {
             if(c == '(') {
                 leftParens++;
                 balance++;
             }else if(c == ')') {
-                if(balance == 0) continue; //To not add paren
+                if(balance == 0) continue;
                 balance--;
             }
             sb.append(c);
         }
 
-        //2. Remove rightmost '('
+        //2. Remove extra rightmost '('
         StringBuilder result = new StringBuilder();
-        int openToKeep = leftParens - balance;
+        int leftRemaining = leftParens - balance;
         for(int i = 0; i < sb.length(); i++) {
             char c = sb.charAt(i);
             if(c == '(') {
-                if(openToKeep == 0) continue;
-                openToKeep--;
+                if(leftRemaining == 0) continue;
+                leftRemaining--;
             }
             result.append(c);
         }
