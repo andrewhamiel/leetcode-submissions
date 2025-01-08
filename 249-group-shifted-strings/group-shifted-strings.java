4a0373c317f5f1915747159bc0939f2e13cd2814
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-08 08:35:16 -0600

    Time: 2 ms (79.26%) | Memory: 43.4 MB (48.88%) - LeetSync

--- 249-group-shifted-strings/group-shifted-strings.java
+++ 249-group-shifted-strings/group-shifted-strings.java
@@ -1,31 +1,28 @@
 class Solution {
     public List<List<String>> groupStrings(String[] strings) {
-        Map<String, List<String>> collisions = new HashMap<>();
-        for(String str : strings){
-            String hash = getHash(str);
-            collisions.putIfAbsent(hash, new ArrayList<>());
-            collisions.get(hash).add(str);
-        }
-        
         List<List<String>> result = new ArrayList<>();
-        for(String hash : collisions.keySet()) result.add(collisions.get(hash));
+        Map<String, List<String>> map = new HashMap<>();
+        for(String str : strings) {
+            String hash = getHash(str);
+            map.computeIfAbsent(hash, k -> new ArrayList<>()).add(str);
+        }
+
+        for(String hash : map.keySet()) result.add(map.get(hash));
         return result;
     }
 
-    private String getHash(String str){
-        char[] arr = str.toCharArray();
-        int shift = arr[0] - 'a';
-        
+    private String getHash(String str) {
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < arr.length; i++){
-            sb.append(getShift(shift, arr[i]));
+        char first = str.charAt(0);
+        for(char c : str.toCharArray()) {
+            sb.append(getShiftedChar(c, first));
         }
         return sb.toString();
     }
 
-    private char getShift(int shift, char c){
-        int preMod = (int)(c - 'a') - shift + 26;
+    private char getShiftedChar(char c, char first) {
+        int preMod = c - first - 'a';
         int preShift = preMod % 26;
-        return (char)(preShift + 'a');
+        return (char) (preShift + 'a');
     }
 }
