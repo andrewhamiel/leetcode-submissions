ea3040f2fbda5b8cc668aba2e4b1b1d612cee78d
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-11 13:41:36 -0600

    Time: 1 ms (81.50%) | Memory: 42.1 MB (31.15%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -7,8 +7,8 @@ class Solution {
             int current = 0;
             if(s.charAt(i) != '0') current = oneBack;
 
-            int twoDigit = Integer.parseInt(s.substring(i - 1, i + 1));
-            if(twoDigit >= 10 && twoDigit <= 26) current+= twoBack;
+            int tensDigit = Integer.parseInt(s.substring(i - 1, i + 1));
+            if(tensDigit >= 10 && tensDigit <= 26) current+= twoBack;
 
             twoBack = oneBack;
             oneBack = current;
