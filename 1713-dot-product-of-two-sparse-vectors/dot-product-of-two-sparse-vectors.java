f916791d7c619620c1a7075b7fabb173cfe320c2
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-11 13:51:45 -0600

    Time: 10 ms (41.45%) | Memory: 58 MB (31.64%) - LeetSync

--- 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.java
+++ 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.java
@@ -1,22 +1,22 @@
 class SparseVector {
-    List<int[]> pairs = new ArrayList<>();
+    private List<int[]> list = new ArrayList<>();
     
     SparseVector(int[] nums) {
         for(int i = 0; i < nums.length; i++) {
-            if(nums[i] != 0) pairs.add(new int[]{i, nums[i]});
+            if(nums[i] != 0) list.add(new int[]{i, nums[i]});
         }
     }
     
 	// Return the dotProduct of two sparse vectors
     public int dotProduct(SparseVector vec) {
-        int ptrA = 0, ptrB = 0, dotProduct = 0;
-        while(ptrA < pairs.size() && ptrB < vec.pairs.size()) {
-            int[] pairA = pairs.get(ptrA), pairB = vec.pairs.get(ptrB);
-            if(pairA[0] == pairB[0]) {
-                dotProduct+= (pairA[1] * pairB[1]);
+        int dotProduct = 0, ptrA = 0, ptrB = 0;
+        while(ptrA < list.size() && ptrB < vec.list.size()) {
+            int[] a = list.get(ptrA), b = vec.list.get(ptrB);
+            if(a[0] == b[0]) {
+                dotProduct+= a[1] * b[1];
                 ptrA++;
                 ptrB++;
-            }else if(pairA[0] < pairB[0]) ptrA++;
+            }else if(a[0] < b[0]) ptrA++;
             else ptrB++;
         }
         return dotProduct;
