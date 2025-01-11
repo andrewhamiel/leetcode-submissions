73e70511a311ec5f0e37639cd1025e944d3d6c0a
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-11 09:27:08 -0600

    Time: 7 ms (81.83%) | Memory: 45.2 MB (14.49%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,32 +1,32 @@
 class Solution {
-    private int[] nums;
     private int target = 0;
+    private int[] nums;
+    private int[][] dp;
     private int totalSum = 0;
-    private int[][] memo;
 
     public int findTargetSumWays(int[] nums, int target) {
-        int totalSum = 0;
-        for(int num : nums) totalSum+= num;
-        this.totalSum = totalSum;
+        for(int num : nums) {
+            totalSum+= num;
+        }    
+
+        //Range of possible values is [-totalSum, totalSum]. To make fit in 2D array, make range [0, 2 * totalSum]
+        dp = new int[nums.length][2 * totalSum + 1];
+        for(int[] row : dp) Arrays.fill(row, -1);
 
         this.nums = nums;
         this.target = target;
 
-        //Make range [-totalSum, totalSum] fit into 2D array
-        memo = new int[nums.length][2 * totalSum + 1];
-        for(int[] row : memo) Arrays.fill(row, -1);
-
-        return calculate(0, 0);    
+        return calculate(0, 0);
     }
 
     private int calculate(int ind, int sum) {
-        //1. Base case
+        //1. Exit condition
         if(ind == nums.length) return sum == target ? 1 : 0;
 
         //2. Memoization
-        if(memo[ind][totalSum + sum] != -1) return memo[ind][totalSum + sum];
+        if(dp[ind][totalSum + sum] != -1) return dp[ind][totalSum + sum];
 
-        //3. For each num in array, Two choices: Add or Subtract
-        return  memo[ind][totalSum + sum] = calculate(ind + 1, sum + nums[ind]) + calculate(ind + 1, sum - nums[ind]);
+        //2 choices for each num. Add or subtract
+        return dp[ind][totalSum + sum] = calculate(ind + 1, sum + nums[ind]) + calculate(ind + 1, sum - nums[ind]);
     }
 }
