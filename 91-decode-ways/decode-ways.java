8c93d5507b0b214e34e57f4b5ce9ed569f90ee47
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-11 13:40:03 -0600

    Time: 1 ms (81.50%) | Memory: 42.4 MB (11.13%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,14 +1,14 @@
 class Solution {
     public int numDecodings(String s) {
-        if (s.charAt(0) == '0') return 0;     
+        if(s.charAt(0) == '0') return 0;
 
         int twoBack = 1, oneBack = 1;
-        for (int i = 1; i < s.length(); i++) {
+        for(int i = 1; i < s.length(); i++) {
             int current = 0;
-            if (s.charAt(i) != '0') current = oneBack;
-            
+            if(s.charAt(i) != '0') current = oneBack;
+
             int twoDigit = Integer.parseInt(s.substring(i - 1, i + 1));
-            if (twoDigit >= 10 && twoDigit <= 26) current += twoBack;
+            if(twoDigit >= 10 && twoDigit <= 26) current+= twoBack;
 
             twoBack = oneBack;
             oneBack = current;
