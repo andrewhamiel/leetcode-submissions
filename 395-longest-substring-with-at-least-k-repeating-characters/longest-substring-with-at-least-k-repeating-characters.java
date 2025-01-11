e9fa5feb0eb3a68f004724fa0a6f4f7f747f82ab
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-11 12:15:21 -0600

    Time: 7 ms (52.11%) | Memory: 41.7 MB (56.19%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
@@ -1,39 +1,36 @@
 class Solution {
     public int longestSubstring(String s, int k) {
-        //Key intuition: only 26 possible unique characters, find longest substring with each combination of unique characters
-        char[] arr = s.toCharArray();
-        int maxUnique = getUniqueChars(arr), longest = 0;
+        int maxUnique = getUniqueChars(s), result = 0;
+        //Sliding window
         for(int currUnique = 1; currUnique <= maxUnique; currUnique++) {
             int[] freqs = new int[26];
             int left = 0, right = 0, uniqueSeen = 0, containsAtLeastK = 0;
-            while(right < arr.length) {
+            while(right < s.length()) {
                 if(uniqueSeen <= currUnique) {
-                    //Expand sliding window
-                    char c = arr[right++];
+                    char c = s.charAt(right++);
                     if(freqs[c - 'a'] == 0) uniqueSeen++;
                     freqs[c - 'a']++;
                     if(freqs[c - 'a'] == k) containsAtLeastK++;
                 }else {
-                    //Shrink sliding window
-                    char c = arr[left++];
+                    char c = s.charAt(left++);
                     if(freqs[c - 'a'] == k) containsAtLeastK--;
                     freqs[c - 'a']--;
                     if(freqs[c - 'a'] == 0) uniqueSeen--;
                 }
 
-                //Right currently at index of next char to be viewed
-                if(uniqueSeen == currUnique && containsAtLeastK == currUnique) longest = Math.max(longest, right - left);
+                //Right at next index to be evaluated
+                if(uniqueSeen == currUnique && containsAtLeastK == currUnique) result = Math.max(result, right - left);
             }
         }
-        return longest;
+        return result;
     }
 
-    private int getUniqueChars(char[] arr) {
-        boolean[] seen = new boolean[26];
+    private int getUniqueChars(String s) {
         int unique = 0;
-        for(char c : arr) {
+        boolean[] seen = new boolean[26];
+        for(char c : s.toCharArray()) {
             if(!seen[c - 'a']) unique++;
-            seen[c - 'a'] = true;
+            seen[c - 'a'] = true; 
         }
         return unique;
     }
