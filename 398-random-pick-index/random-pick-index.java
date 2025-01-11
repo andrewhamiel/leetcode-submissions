4db6cb63c82de3403ddbf294166b3364fa77ab8b
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-11 14:25:06 -0600

    Time: 1578 ms (5.04%) | Memory: 55.3 MB (88.63%) - LeetSync

--- 398-random-pick-index/random-pick-index.java
+++ 398-random-pick-index/random-pick-index.java
@@ -1,26 +1,25 @@
 class Solution {
-
     private int[] nums;
-    private Random rand;
-    
+    private Random rand = new Random();
+
     public Solution(int[] nums) {
         this.nums = nums;
-        this.rand = new Random();
     }
     
     public int pick(int target) {
         int count = 0, ind = 0;
-        for (int i = 0; i < nums.length; i++) {
-            // if nums[i] is equal to target, i is a potential candidate
-            // which needs to be chosen uniformly at random
-            if (nums[i] == target) {
-                // increment the count of total candidates
-                // available to be chosen uniformly at random
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == target) {
                 count++;
-                // we pick the current number with probability 1 / count (reservoir sampling)
-                if (rand.nextInt(count) == 0) ind = i;
+                if(rand.nextInt(count) == 0) ind = i;
             }
         }
         return ind;
     }
 }
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
