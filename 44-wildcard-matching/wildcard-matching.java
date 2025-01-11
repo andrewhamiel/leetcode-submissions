b7c095b6690eb2db76c5dd4657bd66daa8aac37e
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-10 18:11:01 -0600

    Time: 2 ms (95.48%) | Memory: 43.9 MB (85.55%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,30 +1,30 @@
 class Solution {
     public boolean isMatch(String s, String p) {
         int sInd = 0, pInd = 0;
-        int starInd = -1, starSBeginInd = -1;
-
+        int starPInd = -1, starSInd = -1;
+        
         while(sInd < s.length()) {
-            //1. char p == '?' or characters equal
-            //2. char p == '*'
-            //3. chars not equal and '*' seen previously
-            //4. chars not equal and no '*' seen
+            //1. char p is '?' or characters match
+            //2. char p is '*'
+            //3. chars do not match and previous '*' seen
+            //4. chars do not match and no '*' seen
             if(pInd < p.length() && (p.charAt(pInd) == '?' || s.charAt(sInd) == p.charAt(pInd))) {
                 sInd++;
                 pInd++;
             }else if(pInd < p.length() && p.charAt(pInd) == '*') {
                 //Advance 0 characters to start
-                starInd = pInd;
-                starSBeginInd = sInd;
+                starPInd = pInd;
+                starSInd = sInd;
                 pInd++;
-            }else if(starInd >= 0) {
-                //Backtrack to last seen '*' and advance one character
-                pInd = starInd + 1;
-                starSBeginInd++; //Advance one character
-                sInd = starSBeginInd;
+            }else if(starPInd >= 0) {
+                //Backtrack to prev '*' seen and advance s one char
+                pInd = starPInd + 1;
+                starSInd++; //Advance s one char
+                sInd = starSInd;
             }else return false;
         }
 
-        //All characters remaining should be '*'
+        //All remaining chars should be '*'
         for(int i = pInd; i < p.length(); i++) {
             if(p.charAt(i) != '*') return false;
         }
