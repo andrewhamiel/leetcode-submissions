584142ba4229ccc0f8df042a15a867e7e5368025
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-15 15:45:24 -0600

    Time: 1 ms (24.49%) | Memory: 41.5 MB (74.32%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,20 +1,17 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        //Key intuition: longest border, KMP
         int[] longestBorder = new int[needle.length()];
         //1. Preprocess
-        int longestPrev = 0, ind = 1; //longestBorder[0] always 0, cannot be prefix of self
+        int longestPrev = 0, ind = 1; //LongestBorder[0] = 0 because cannot be suffix of itself
         while(ind < needle.length()) {
-            //Expand longest prev
-            if(needle.charAt(longestPrev) == needle.charAt(ind)) {
+            if(needle.charAt(ind) == needle.charAt(longestPrev)) {
                 longestPrev++;
                 longestBorder[ind++] = longestPrev;
             }else {
                 if(longestPrev == 0) longestBorder[ind++] = 0; //No characters match
-                else longestPrev = longestBorder[longestPrev - 1]; //Rollback to previous prefix
+                else longestPrev = longestBorder[longestPrev - 1];
             }
         }
-
         //2. Search
         int haystackInd = 0, needleInd = 0;
         while(haystackInd < haystack.length()) {
@@ -24,8 +21,8 @@ class Solution {
                 //All characters match
                 if(needleInd == needle.length()) return haystackInd - needle.length();
             }else {
-                if(needleInd == 0) haystackInd++; //No chars match
-                else needleInd = longestBorder[needleInd - 1]; //Find prev prefix as starting point
+                if(needleInd == 0) haystackInd++; //No characters match
+                else needleInd = longestBorder[needleInd - 1];
             }
         }
         return -1;
