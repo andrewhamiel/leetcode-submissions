6acb962f292a43af6a047fabe81d31b731a58a82
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-17 17:59:14 -0600

    Time: 0 ms (100.00%) | Memory: 41.7 MB (24.78%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -4,19 +4,19 @@ class Solution {
         int rows = matrix.length, cols = matrix[0].length;
         int left = 0, right = matrix[0].length - 1, up = 0, down = matrix.length - 1;
         while(result.size() < rows * cols) {
-            //Left -> right
+            //1. Left -> Right
             for(int col = left; col <= right; col++) result.add(matrix[up][col]);
-            //Up -> Down
+            //2. Up -> Down
             for(int row = up + 1; row <= down; row++) result.add(matrix[row][right]);
-            //Make sure rows are not equal, then right -> left
+            //3. Make sure rows different, then Right -> Left
             if(up != down) {
                 for(int col = right - 1; col >= left; col--) result.add(matrix[down][col]);
             }
-            //Make sure cols are not equal, then down -> up
+            //4. Make sure cols different, then Down -> Up
             if(left != right) {
                 for(int row = down - 1; row > up; row--) result.add(matrix[row][left]);
             }
-            //Increment
+
             left++;
             right--;
             up++;
