ba61afccfc84c1a13cf311152ef76c64e6012561
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-17 17:48:11 -0600

    Time: 8 ms (59.17%) | Memory: 42.7 MB (76.04%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        //Key permutation: permutation is same length, different ordering of characters
         if(s1.length() > s2.length()) return false;
 
         int[] s1Freqs = new int[26], s2Freqs = new int[26];
@@ -9,15 +8,13 @@ class Solution {
             s2Freqs[s2.charAt(i) - 'a']++;
         }
 
-        //Sliding window
         for(int i = 0; i < s2.length() - s1.length(); i++) {
-            //Check for match
             if(matches(s1Freqs, s2Freqs)) return true;
-            //Shrink sliding window
+            //Advance sliding window
             s2Freqs[s2.charAt(i) - 'a']--;
-            //Expand sliding window
             s2Freqs[s2.charAt(s1.length() + i) - 'a']++;
         }
+        
         return matches(s1Freqs, s2Freqs);
     }
 
