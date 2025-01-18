1493057ca11204e3652439079cb23e353a2e1e1a
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-18 08:26:23 -0600

    Time: 1 ms (100.00%) | Memory: 43.7 MB (22.13%) - LeetSync

--- 249-group-shifted-strings/group-shifted-strings.java
+++ 249-group-shifted-strings/group-shifted-strings.java
@@ -1,26 +1,26 @@
 class Solution {
     public List<List<String>> groupStrings(String[] strings) {
-        List<List<String>> result = new ArrayList<>();
         Map<String, List<String>> map = new HashMap<>();
         for(String str : strings) {
             String hash = getHash(str);
             map.computeIfAbsent(hash, k -> new ArrayList<>()).add(str);
         }
+        List<List<String>> result = new ArrayList<>();
         result.addAll(map.values());
         return result;
     }
 
     private String getHash(String str) {
-        StringBuilder sb = new StringBuilder();
-        char first = str.charAt(0);
+        StringBuilder result = new StringBuilder();
+        char firstChar = str.charAt(0);
         for(char c : str.toCharArray()) {
-            sb.append(getShiftedChar(c, first));
+            result.append(getShiftedChar(c, firstChar));
         }
-        return sb.toString();
+        return result.toString();
     }
 
-    private char getShiftedChar(char c, char first) {
-        int preMod = c - first - 'a';
+    private char getShiftedChar(char c, char firstChar) {
+        int preMod = c - firstChar - 'a' + 26;
         int preShift = preMod % 26;
         return (char) (preShift + 'a');
     }
