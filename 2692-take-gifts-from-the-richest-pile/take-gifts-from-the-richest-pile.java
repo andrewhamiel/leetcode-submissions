ea95dcebfa72644226cb3f015364f23fd02cd89f
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-18 10:10:46 -0600

    Time: 5 ms (91.96%) | Memory: 42.9 MB (18.40%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.java
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.java
@@ -1,32 +1,19 @@
 class Solution {
     public long pickGifts(int[] gifts, int k) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Long> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
         long totalGifts = 0;
         for(int gift : gifts) {
+            maxHeap.add((long) gift);
             totalGifts+= gift;
-            maxHeap.add(gift);
         }
 
         long removedGifts = 0;
-        for(int i = k; i > 0; i--) {
-            int giftToBeRemoved = maxHeap.poll();
-            int reducedPile = sqrt(giftToBeRemoved);
-            removedGifts+= giftToBeRemoved - reducedPile;
-            maxHeap.add(reducedPile);
+        for(int i = 0; i < k; i++) {
+            long maxPile = maxHeap.poll();
+            int reducedPile = (int) Math.floor(Math.sqrt(maxPile));
+            removedGifts+= maxPile - reducedPile;
+            maxHeap.add((long)reducedPile);
         }
         return totalGifts - removedGifts;
-    }
-
-    private int sqrt(int num) {
-        if(num < 2) return 1;
-        int left = 2, right = num / 2;
-        while(left <= right) {
-            int mid = left + (right - left)/2;
-            long pow = (long) mid * mid;
-            if(pow == num) return mid;
-            else if(pow < num) left = mid + 1;
-            else right = mid - 1;
-        }
-        return right;
     }
 }
