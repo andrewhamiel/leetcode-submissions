6fe42fb29f32b1136ce046e2dc20249d16303412
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-17 19:19:33 -0600

    Time: 4 ms (65.51%) | Memory: 53.8 MB (16.23%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.java
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.java
@@ -1,11 +1,10 @@
 class Solution {
     public int maxScoreSightseeingPair(int[] values) {
-        //Key intuition: left -> values[i] + i, right -> values[j] - j
-        int maxLeftScore = values[0], result = 0;
+        int bestLeftScore = values[0], result = 0;
         for(int i = 1; i < values.length; i++) {
             int currRightScore = values[i] - i;
-            result = Math.max(result, maxLeftScore + currRightScore);
-            maxLeftScore = Math.max(maxLeftScore, values[i] + i);
+            result = Math.max(result, bestLeftScore + currRightScore);
+            bestLeftScore = Math.max(bestLeftScore, values[i] + i);
         }
         return result;
     }
