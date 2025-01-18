4d79b3bd6c7a0151edbae10fabcf34c23cdbf4da
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-17 19:03:08 -0600

    Time: 2 ms (95.32%) | Memory: 43.8 MB (91.55%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -2,12 +2,12 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int sInd = 0, pInd = 0;
         int starPInd = -1, starSInd = -1;
-        
+
         while(sInd < s.length()) {
             //1. char p is '?' or characters match
             //2. char p is '*'
-            //3. chars do not match and previous '*' seen
-            //4. chars do not match and no '*' seen
+            //3. characters do not match and previous '*' seen
+            //4. characters do not match and no '*' seen
             if(pInd < p.length() && (p.charAt(pInd) == '?' || s.charAt(sInd) == p.charAt(pInd))) {
                 sInd++;
                 pInd++;
@@ -19,7 +19,7 @@ class Solution {
             }else if(starPInd >= 0) {
                 //Backtrack to prev '*' seen and advance s one char
                 pInd = starPInd + 1;
-                starSInd++; //Advance s one char
+                starSInd++;
                 sInd = starSInd;
             }else return false;
         }
@@ -28,6 +28,7 @@ class Solution {
         for(int i = pInd; i < p.length(); i++) {
             if(p.charAt(i) != '*') return false;
         }
+        
         return true;
     }
 }
