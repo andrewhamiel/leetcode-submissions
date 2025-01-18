09186e2c92015bc9e3f44c179b85503fe8ad5f3f
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-17 18:31:00 -0600

    Time: 8 ms (84.86%) | Memory: 45.7 MB (12.10%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -1,29 +1,29 @@
 class Solution {
     private int[][] memo;
-    private int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    
+
     public int longestIncreasingPath(int[][] matrix) {
         memo = new int[matrix.length][matrix[0].length];
-        int max = 0;
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[0].length; j++){
-                max = Math.max(max, dfs(i, j, matrix));
+
+        int longest = 0;
+        for(int row = 0; row < matrix.length; row++) {
+            for(int col = 0; col < matrix[0].length; col++) {
+                longest = Math.max(longest, dfs(row, col, matrix, -1));
             }
         }
-        return max;
+        return longest;
     }
-    
-    private int dfs(int i, int j, int[][] matrix){
-        if(i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length) return 0;
+
+    private int dfs(int i, int j, int[][] matrix, int prevVal) {
+        if(i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length || matrix[i][j] <= prevVal) return 0;
+
         if(memo[i][j] != 0) return memo[i][j];
 
-        int max = 0;
-        for(int[] dir : dirs){
-            int row = i + dir[0], col = j + dir[1];
-            if(row >= 0 && row < matrix.length && col >= 0 && col < matrix[0].length 
-                && matrix[row][col] > matrix[i][j]) max = Math.max(max, dfs(row, col, matrix));
-        }
-        memo[i][j] = 1 + max;
-        return memo[i][j];
+        int currVal = matrix[i][j];
+        int left = dfs(i, j - 1, matrix, currVal);
+        int right = dfs(i, j + 1, matrix, currVal);
+        int up = dfs(i - 1, j, matrix, currVal);
+        int down = dfs(i + 1, j, matrix, currVal);
+
+        return memo[i][j] = 1 + Math.max(left, Math.max(right, Math.max(up, down)));
     }
 }
