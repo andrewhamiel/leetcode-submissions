7df2219865bb880d9459b59cc1ef21ca09e8e86e
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-18 12:18:25 -0600

    Time: 0 ms (100.00%) | Memory: 40.2 MB (91.25%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,6 +1,6 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        //Catalan numbers: n!/(n - k)!
+        //Math: Catalan numbers
         long result = 1;
         for(int i = n; i < n + m - 1; i++) {
             result*= i;
