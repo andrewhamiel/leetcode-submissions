ab98483ad5679a086418ebd9ac0143b6f26ffffa
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-18 07:55:34 -0600

    Time: 1 ms (100.00%) | Memory: 44.2 MB (92.35%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -6,7 +6,7 @@ class Solution {
                 //1. Check value at pos
                 if(board[row][col] == '.') continue;
                 int val = board[row][col] - '0';
-                int pos = 1 << (val - 1); //0-indexed bitmask that only sets val - 1th bit
+                int pos = 1 << (val - 1); //0-indexed bitmask. Only sets (val - 1)th bit as 1
                 //2. Check row. num & num == 1, so if present return false
                 if((rows[row] & pos) > 0) return false;
                 rows[row]|= pos;
@@ -14,7 +14,7 @@ class Solution {
                 if((cols[col] & pos) > 0) return false;
                 cols[col]|= pos;
                 //4. Check box
-                int ind = (row/3) * 3 + (col/3);
+                int ind = (row/3) * 3 + col/3;
                 if((boxes[ind] & pos) > 0) return false;
                 boxes[ind]|= pos;
             }
