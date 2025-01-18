1e5bfe76cf13ad252164449fb431a811b7db1dbf
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-18 07:47:39 -0600

    Time: 1 ms (100.00%) | Memory: 44.1 MB (92.35%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        int[] rows = new int[board.length], cols = new int[board.length], boxes = new int[board.length];
+        int[] rows = new int[9], cols = new int[9], boxes = new int[9];
         for(int row = 0; row < board.length; row++) {
             for(int col = 0; col < board[0].length; col++) {
                 //1. Check value at pos
                 if(board[row][col] == '.') continue;
                 int val = board[row][col] - '0';
-                int pos = 1 << (val - 1); //0-indexed bitmask. Only sets bit corresponding to number val
-                //2. Check row. num & num == 1, so if already present in row return false
+                int pos = 1 << (val - 1); //0-indexed bitmask. Only sets bit at position of val
+                //2. Check row. num & num == 1, so if already present in row return false;
                 if((rows[row] & pos) > 0) return false;
                 rows[row]|= pos;
                 //3. Check col
