7811cdc5f1c729f5988a39d4b00ba3e12d03c783
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-19 17:38:34 -0600

    Time: 1 ms (100.00%) | Memory: 44.3 MB (87.40%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,17 +1,22 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
         int maxArea = 0;
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[0].length; j++){
-                if(grid[i][j] == 1) maxArea = Math.max(maxArea, dfs(i, j, grid));
+        for(int row = 0; row < grid.length; row++) {
+            for(int col = 0; col < grid[0].length; col++) {
+                if(grid[row][col] == 1) maxArea = Math.max(maxArea, dfs(row, col, grid));
             }
         }
         return maxArea;
     }
 
-    private int dfs(int i, int j, int[][] grid){
-        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 1) return 0;
-        grid[i][j] = 0;
-        return 1 + dfs(i, j - 1, grid) + dfs(i, j + 1, grid) + dfs(i - 1, j, grid) + dfs(i + 1, j, grid);
+    private int dfs(int row, int col, int[][] grid) {
+        if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] != 1) return 0;
+        grid[row][col] = 0;
+        int left = dfs(row, col - 1, grid);
+        int right = dfs(row, col + 1, grid);
+        int up = dfs(row - 1, col, grid);
+        int down = dfs(row + 1, col, grid);
+
+        return 1 + left + right + up + down;
     }
 }
