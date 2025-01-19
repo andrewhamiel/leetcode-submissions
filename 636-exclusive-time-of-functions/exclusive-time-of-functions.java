8d70678782b856c7283d000a8dcdfaac05ee7acf
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-19 17:33:20 -0600

    Time: 11 ms (86.70%) | Memory: 45.8 MB (7.15%) - LeetSync

--- 636-exclusive-time-of-functions/exclusive-time-of-functions.java
+++ 636-exclusive-time-of-functions/exclusive-time-of-functions.java
@@ -2,20 +2,18 @@ class Solution {
     public int[] exclusiveTime(int n, List<String> logs) {
         Deque<Integer> stack = new ArrayDeque<>();
         int[] result = new int[n];
-
         int prevTime = 0;
         for(String log : logs) {
-            String[] commaDelimited = log.split(":");
-            int currTime = Integer.parseInt(commaDelimited[2]);
-            if("start".equals(commaDelimited[1])) {
+            String[] split = log.split(":");
+            int currTime = Integer.parseInt(split[2]), functionId = Integer.parseInt(split[0]);
+            if("start".equals(split[1])) {
                 if(!stack.isEmpty()) result[stack.peekFirst()]+= currTime - prevTime;
-                stack.addFirst(Integer.parseInt(commaDelimited[0]));
-                prevTime = currTime;
+                stack.addFirst(functionId);
             }else {
-                currTime++; //Ending function takes 1 unit of time 
+                currTime++; //Ending function takes 1 unit of time
                 result[stack.removeFirst()]+= currTime - prevTime;
-                prevTime = currTime;
             }
+            prevTime = currTime;
         }
         return result;
     }
