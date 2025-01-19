6e1a2ddb89ec34a830cd14c8236e850f63dde2ce
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-19 16:18:57 -0600

    Time: 0 ms (100.00%) | Memory: 42.1 MB (43.80%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,16 +1,12 @@
 class Solution {
-    /*
-  Dutch National Flag problem solution.
-  */
     public void sortColors(int[] nums) {
         int red = 0, white = 0, blue = nums.length - 1;
-
-        while (white <= blue) {
+        while(white <= blue) {
             //1. Red
             //2. White
             //3. Blue
-            if (nums[white] == 0) swap(red++, white++, nums);
-            else if (nums[white] == 1) white++;
+            if(nums[white] == 0) swap(red++, white++, nums);
+            else if(nums[white] == 1) white++;
             else swap(white, blue--, nums);
         }
     }
