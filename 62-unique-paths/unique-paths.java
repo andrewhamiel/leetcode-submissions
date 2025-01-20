103266d009966ca5c66b34571d1bde7c9557c6b3
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-20 14:54:54 -0600

    Time: 0 ms (100.00%) | Memory: 40.8 MB (24.92%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,11 +1,16 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        //Math: Catalan numbers
-        long result = 1;
-        for(int i = n; i < n + m - 1; i++) {
-            result*= i;
-            result/= (i - n + 1);
+        int[][] dp = new int[m][n];
+        //First row
+        for(int col = 0; col  < dp[0].length; col++) dp[0][col] = 1;
+        //First col
+        for(int[] row : dp) row[0] = 1;
+
+        for(int row = 1; row < dp.length; row++) {
+            for(int col = 1; col < dp[0].length; col++) {
+                dp[row][col] = dp[row - 1][col] + dp[row][col - 1];
+            }
         }
-        return (int) result;
+        return dp[m - 1][n - 1];
     }
 }
