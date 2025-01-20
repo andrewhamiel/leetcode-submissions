7db5e380b378d4f11557137708b1da4652b63806
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-20 15:54:44 -0600

    Time: 1 ms (100.00%) | Memory: 44 MB (98.47%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -3,17 +3,20 @@ class Solution {
         int[] rows = new int[9], cols = new int[9], boxes = new int[9];
         for(int row = 0; row < board.length; row++) {
             for(int col = 0; col < board[0].length; col++) {
-                //1. Check value at pos
+                //1. Check val
                 if(board[row][col] == '.') continue;
                 int val = board[row][col] - '0';
-                int pos = 1 << (val - 1); //0-indexed bitmask. Only sets (val - 1)th bit as 1
-                //2. Check row. num & num == 1, so if present return false
+                int pos = 1 << (val - 1);
+
+                //2. Check rows. num & num is 1, so if num present then return false
                 if((rows[row] & pos) > 0) return false;
                 rows[row]|= pos;
-                //3. Check col
+
+                //3. Check cols
                 if((cols[col] & pos) > 0) return false;
                 cols[col]|= pos;
-                //4. Check box
+
+                //4. Check boxes
                 int ind = (row/3) * 3 + col/3;
                 if((boxes[ind] & pos) > 0) return false;
                 boxes[ind]|= pos;
