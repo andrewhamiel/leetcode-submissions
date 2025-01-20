ad5a4e08e1c202b20fccfafe9a22030d7fbb14e6
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-19 18:25:15 -0600

    Time: 0 ms (100.00%) | Memory: 42.1 MB (85.78%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,12 +1,15 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int p1 = m - 1, p2 = n - 1;
-        for(int k = nums1.length - 1; k >= 0; k--) {
-            if(p1 >= 0 && p2 >= 0) {
-                if(nums1[p1] >= nums2[p2]) nums1[k] = nums1[p1--];
-                else nums1[k] = nums2[p2--];
-            }else if(p1 >= 0) nums1[k] = nums1[p1--];
-            else nums1[k] = nums2[p2--];
+        int ptrA = m - 1, ptrB = n - 1;
+        for(int i = nums1.length - 1; i >= 0; i--) {
+            if(ptrA >= 0 && ptrB >= 0) {
+                if(nums1[ptrA] >= nums2[ptrB]) nums1[i] = nums1[ptrA--];
+                else nums1[i] = nums2[ptrB--];
+            }else if(ptrA >= 0) {
+                nums1[i] = nums1[ptrA--];
+            }else if(ptrB >= 0) {
+                nums1[i] = nums2[ptrB--];
+            }
         }
     }
 }
