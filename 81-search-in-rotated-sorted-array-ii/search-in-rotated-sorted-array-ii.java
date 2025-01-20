cba9d1f6678661e23ac700b8b956ff2951407aaf
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-20 16:55:04 -0600

    Time: 0 ms (100.00%) | Memory: 43.6 MB (26.76%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,19 +1,21 @@
 class Solution {
     public boolean search(int[] nums, int target) {
+        if(nums.length == 1) return nums[0] == target;
+
         int left = 0, right = nums.length - 1;
         //[left, right] variant
         while(left <= right) {
             int mid = left + (right - left)/2;
             //1. Check mid
             if(nums[mid] == target) return true;
-            //2. See if BS will be helpful. If nums[left] == nums[right], cannot determine next partition
+            //2. See if BS helpful. If nums[left] == nums[mid], cannot tell which partition to go to next
             if(nums[left] == nums[mid]) {
                 left++;
                 continue;
             }
             //3. Find next partition
             if(nums[left] < nums[mid]) {
-                if(nums[left] <= target && nums[mid] > target) right = mid - 1;
+                if(nums[mid] > target && nums[left] <= target) right = mid - 1;
                 else left = mid + 1;
             }else {
                 if(nums[left] > target && nums[mid] < target) left = mid + 1;
