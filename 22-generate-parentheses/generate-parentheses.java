27b5411dbbda41a86382fdcc25aaf5541ca63d3b
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-20 17:00:25 -0600

    Time: 1 ms (82.03%) | Memory: 42.9 MB (87.61%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,12 +1,11 @@
 class Solution {
-    private Set<String> result = new HashSet<>();
-
     public List<String> generateParenthesis(int n) {
-        backtrack(new StringBuilder(), n, n);
+        Set<String> result = new HashSet<>();
+        backtrack(new StringBuilder(), n, n, result);
         return new ArrayList<>(result);    
     }
 
-    private void backtrack(StringBuilder sb, int leftCount, int rightCount) {
+    private void backtrack(StringBuilder sb, int leftCount, int rightCount, Set<String> result) {
         //1. Exit condition
         if(leftCount == 0 && rightCount == 0) {
             result.add(sb.toString());
@@ -15,17 +14,16 @@ class Solution {
 
         int currLength = sb.length();
 
-        //2. Add left if available
+        //2. Add left if any remaining
         if(leftCount > 0) {
             sb.append('(');
-            backtrack(sb, leftCount - 1, rightCount);
+            backtrack(sb, leftCount - 1, rightCount, result);
             sb.deleteCharAt(currLength);
-        }       
-
+        }
         //3. Add right if valid
         if(leftCount < rightCount) {
             sb.append(')');
-            backtrack(sb, leftCount, rightCount - 1);
+            backtrack(sb, leftCount, rightCount - 1, result);
             sb.deleteCharAt(currLength);
         }
     }
