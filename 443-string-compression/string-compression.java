0fc57646480761eb02ed04d5cec50f80e8f5ab47
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-20 16:47:44 -0600

    Time: 1 ms (99.53%) | Memory: 44.1 MB (65.12%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,26 +1,28 @@
 class Solution {
     public int compress(char[] chars) {
-        int compressedInd = 0, currLength = 1;
+        int ind = 0;
         char prev = chars[0];
-        for(int i = 1; i < chars.length; i++) {
-            char c = chars[i];
+        int currLength = 0;
+        for(char c : chars) {
             if(c == prev) currLength++;
             else {
-                //Replace in array
-                chars[compressedInd++] = prev;
+                chars[ind++] = prev;
                 if(currLength > 1) {
-                    for(char digit : String.valueOf(currLength).toCharArray()) chars[compressedInd++] = digit;
-                }               
-                //Reset values
+                    String digit = String.valueOf(currLength);
+                    for(int i = 0; i < digit.length(); i++) chars[ind++] = digit.charAt(i); 
+                }                               
                 prev = c;
                 currLength = 1;
             }
         }
-        //Last letter
-        chars[compressedInd++] = prev;
+
+        //Last char
+        chars[ind++] = prev;
         if(currLength > 1) {
-            for(char digit : String.valueOf(currLength).toCharArray()) chars[compressedInd++] = digit;
-        }
-        return compressedInd;
+            String digit = String.valueOf(currLength);
+            for(int i = 0; i < digit.length(); i++) chars[ind++] = digit.charAt(i); 
+        }      
+
+        return ind;
     }
 }
