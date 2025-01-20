1b79674fb5842fca343954ae006b77a532166488
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-20 16:38:44 -0600

    Time: 2 ms (95.34%) | Memory: 44 MB (86.00%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -2,33 +2,30 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int sInd = 0, pInd = 0;
         int starSInd = -1, starPInd = -1;
-
         while(sInd < s.length()) {
-            //1. p is '?' or chars match
+            //1. p is '?' or characters match
             //2. p is '*'
-            //3. chars do not match and prev '*' seen
-            //4. chars do not match and no '*' seen
+            //3. chars do not match but prev '*' seen
+            //4. chars do not match and no prev '*'
             if(pInd < p.length() && (p.charAt(pInd) == '?' || s.charAt(sInd) == p.charAt(pInd))) {
                 sInd++;
                 pInd++;
             }else if(pInd < p.length() && p.charAt(pInd) == '*') {
-                //Store indeces of both when last star seen
+                //Start by advancing 0
                 starSInd = sInd;
-                starPInd = pInd;
                 pInd++;
+                starPInd = pInd;
             }else if(starPInd >= 0) {
-                //Backtrack to first char after last seen '*' and advance s by one character
-                pInd = starPInd + 1;
-                starSInd++;
+                pInd = starPInd;
+                starSInd++; //Increment by 1
                 sInd = starSInd;
             }else return false;
         }
-
-        //All remaining chars in p should be '*'
-        for(int i = pInd; i < p.length(); i++) {
-            if(p.charAt(i) != '*') return false;
-        }
         
+        //Remaining chars should all be '*'
+        for(int i = pInd; i < p.length(); i++) if(p.charAt(i) != '*') {
+            return false;
+        }
         return true;
     }
 }
