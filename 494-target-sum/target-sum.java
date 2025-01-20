77fe37284c7f5a4c23c722ad5e89f8cee859e7ef
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-20 16:25:54 -0600

    Time: 7 ms (81.83%) | Memory: 44.9 MB (23.47%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,18 +1,18 @@
 class Solution {
+    private int[] nums;
+    private int target = 0;
     private int[][] memo;
     private int totalSum = 0;
-    private int target = 0;
-    private int[] nums;
 
     public int findTargetSumWays(int[] nums, int target) {
         for(int num : nums) {
             totalSum+= num;
-        }    
+        }
 
         this.nums = nums;
         this.target = target;
 
-        //Range of values is [-totalSum, totalSum]. To make compatible with array, make range [0, 2 * totalSum]
+        //Make range [0, 2 * totalSum] to fit in 2D matrix
         memo = new int[nums.length][2 * totalSum + 1];
         for(int[] row : memo) Arrays.fill(row, -1);
 
@@ -20,13 +20,13 @@ class Solution {
     }
 
     private int calculate(int ind, int sum) {
-        //1. Base case
+        //1. Exit condition
         if(ind == nums.length) return sum == target ? 1 : 0;
 
         //2. Memoization
         if(memo[ind][totalSum + sum] != -1) return memo[ind][totalSum + sum];
 
-        //3. For each num, two choices: Add or Subtract
+        //3. Two choices for each num: add or subtract
         return memo[ind][totalSum + sum] = calculate(ind + 1, sum + nums[ind]) + calculate(ind + 1, sum - nums[ind]);
     }
 }
