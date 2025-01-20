65466b764658f2a79aa21439833950f066f3e8ed
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-19 18:14:20 -0600

    Time: 4 ms (23.37%) | Memory: 42.8 MB (79.17%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.java
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.java
@@ -16,20 +16,22 @@
 class Solution {
     public List<List<Integer>> verticalOrder(TreeNode root) {
         if(root == null) return new ArrayList<>();
-        Map<Integer, List<Pair<Integer, Integer>>> adj = new HashMap<>();
+        Map<Integer, List<Pair<Integer, Integer>>> columnMap = new HashMap<>();
         Queue<Pair<TreeNode, Integer>> q = new LinkedList<>();
         q.add(new Pair<>(root, 0));
+
         int row = 0, minCol = 0, maxCol = 0;
+
         while(!q.isEmpty()) {
             int size = q.size();
             while(size-- > 0) {
                 Pair<TreeNode, Integer> p = q.poll();
                 TreeNode curr = p.getKey();
                 int col = p.getValue();
+
+                columnMap.computeIfAbsent(col, k -> new ArrayList<>()).add(new Pair<>(curr.val, row));
                 minCol = Math.min(minCol, col);
                 maxCol = Math.max(maxCol, col);
-                
-                adj.computeIfAbsent(col, k -> new ArrayList<>()).add(new Pair<>(row, curr.val));
 
                 if(curr.left != null) q.add(new Pair<>(curr.left, col - 1));
                 if(curr.right != null) q.add(new Pair<>(curr.right, col + 1));
@@ -39,14 +41,15 @@ class Solution {
 
         List<List<Integer>> result = new ArrayList<>();
         for(int col = minCol; col <= maxCol; col++) {
-            if(adj.containsKey(col)){
-                List<Pair<Integer, Integer>> pairs = adj.get(col);
-                Collections.sort(pairs, (a, b) -> a.getKey() - b.getKey());
-                List<Integer> list = new ArrayList<>();
-                for(Pair<Integer, Integer> pair : pairs) list.add(pair.getValue());
-                result.add(list);
-            }          
+            List<Pair<Integer, Integer>> pairs = columnMap.get(col);
+            Collections.sort(pairs, (a, b) -> a.getValue() - b.getValue());
+            List<Integer> list = new ArrayList<>();
+            for(Pair<Integer, Integer> p : pairs) {
+                list.add(p.getKey());
+            }
+            result.add(list);
         }
         return result;
+        
     }
 }
