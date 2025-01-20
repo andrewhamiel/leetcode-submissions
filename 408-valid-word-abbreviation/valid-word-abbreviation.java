1cd33f37d3508f25ffcc295380cdea9a70cb9357
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-19 18:09:20 -0600

    Time: 1 ms (97.07%) | Memory: 41.9 MB (59.12%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.java
+++ 408-valid-word-abbreviation/valid-word-abbreviation.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean validWordAbbreviation(String word, String abbr) {
-        int wordInd = 0, abbrInd = 0, currDigit = 0;
+        int wordInd = 0, abbrInd = 0, currNum = 0;
         while(abbrInd < abbr.length()) {
-            char c = abbr.charAt(abbrInd++);
+            char c = abbr.charAt(abbrInd);
             if(Character.isDigit(c)) {
-                if(currDigit == 0 && c == '0') return false; //Leading zeroes
-                currDigit*= 10;
-                currDigit+= c - '0';
+                if(c == '0' && currNum == 0) return false;
+                currNum*= 10;
+                currNum+= abbr.charAt(abbrInd++) - '0';
             }else {
-                wordInd+= currDigit;
-                currDigit = 0;
-                if(wordInd >= word.length() || word.charAt(wordInd++) != c) return false;
+                wordInd+= currNum;
+                currNum = 0;
+                if(wordInd >= word.length() || word.charAt(wordInd++) != abbr.charAt(abbrInd++)) return false;
             }
         }
-        wordInd+= currDigit;
+
+        wordInd+= currNum;
         return wordInd == word.length();
     }
 }
