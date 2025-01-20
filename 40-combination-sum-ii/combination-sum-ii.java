1eaabdfaab15f853d901dde7c70f340bc8b7ca92
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-20 17:14:18 -0600

    Time: 5 ms (20.02%) | Memory: 43.3 MB (51.08%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,28 +1,28 @@
 class Solution {
     private List<List<Integer>> result = new ArrayList<>();
     private int[] candidates;
+    private int target = 0;
 
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        //1. Sort to maintain relative order
-        Arrays.sort(candidates);
-        //2. Backtrack
-        this.candidates = candidates;
-        backtrack(0, target, new ArrayList<>());
+        Arrays.sort(candidates); //Combinations -> order does not matter
 
-        return result; 
+        this.candidates = candidates;
+        this.target = target;
+
+        backtrack(0, 0, new ArrayList<>());    
+        return result;
     }
 
     private void backtrack(int ind, int sum, List<Integer> list) {
-        //1. Base cases
-        if(sum == 0) result.add(new ArrayList<>(list));
-        if(sum < 0 || ind == candidates.length) return;
+        //Base cases
+        if(sum == target) result.add(new ArrayList<>(list));
+        if(ind == candidates.length || sum > target) return;
 
-        //2. Backtrack
         for(int i = ind; i < candidates.length; i++) {
             if(i == ind || candidates[i] != candidates[i - 1]) {
                 list.add(candidates[i]);
-                backtrack(i + 1, sum - candidates[i], list);
-                list.remove(list.size() - 1);
+                backtrack(i + 1, sum + candidates[i], list);
+                list.remove(list.size() -1);
             }
         }
     }
