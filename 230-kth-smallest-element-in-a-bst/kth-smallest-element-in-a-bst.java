16f16ace2a169a33f2400d53d256709d5f95624a
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-20 14:34:51 -0600

    Time: 0 ms (100.00%) | Memory: 44.6 MB (34.08%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -15,27 +15,19 @@
  */
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
-        //Inorder Morris Traversal
-        while(root != null) {
-            if(root.left != null) {
-                TreeNode predecessor = root.left;
-                while(predecessor.right != null && predecessor.right != root) predecessor = predecessor.right;
-
-                if(predecessor.right == null) {
-                    //Unexplored
-                    predecessor.right = root;
-                    root = root.left;
-                }else {
-                    k--;
-                    if(k == 0) return root.val;
-                    predecessor.right = null;
-                    root = root.right;
-                }
-            }else {
-                k--;
-                if(k == 0) return root.val;
-                root = root.right;
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        while(root != null || !stack.isEmpty()) {
+            //Left
+            while(root != null) {
+                stack.addFirst(root);
+                root = root.left;
             }
+            //Root
+            root = stack.removeFirst();
+            k--;
+            if(k == 0) return root.val;
+            //Right
+            root = root.right;
         }
         return -1;
     }
