0f2586e27bdff04dd5656ffebaeb7b8edd9b1e6a
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-20 17:09:02 -0600

    Time: 8 ms (59.77%) | Memory: 45.3 MB (45.40%) - LeetSync

--- 536-construct-binary-tree-from-string/construct-binary-tree-from-string.java
+++ 536-construct-binary-tree-from-string/construct-binary-tree-from-string.java
@@ -15,20 +15,20 @@
  */
 class Solution {
     public TreeNode str2tree(String s) {
-        //Key Intuition: String ordered in preorder fashion
         Deque<TreeNode> stack = new ArrayDeque<>();
         StringBuilder currNum = new StringBuilder();
         for(char c : s.toCharArray()) {
-            //1. Parens or Digit/Sign
+            //1. Left or Right Paren
+            //2. Digit or Sign
             if(c == '(' || c == ')') {
-                //2. Push to stack if currNum not empty
+                //If value is present, add to stack
                 if(!currNum.isEmpty()) {
                     int val = Integer.parseInt(currNum.toString());
                     stack.addFirst(new TreeNode(val));
                     currNum = new StringBuilder();
                 }
 
-                //3. Right paren. Pop from stack and connect child to parent
+                //If right paren, pop from stack and connect to parent
                 if(c == ')') {
                     TreeNode child = stack.removeFirst();
                     TreeNode parent = stack.peekFirst();
@@ -38,12 +38,11 @@ class Solution {
             }else currNum.append(c);
         }
 
-        //4. Add remaining num if present. ie "4"
+        //3. Add remaining num to stack if present. ie "4"
         if(!currNum.isEmpty()) {
             int val = Integer.parseInt(currNum.toString());
             stack.addFirst(new TreeNode(val));
         }
-
         return stack.isEmpty() ? null : stack.removeFirst();
     }
 }
