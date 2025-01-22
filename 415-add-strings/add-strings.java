3e2b5a7c85ab567a880233d23946b2365354231e
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-21 20:25:29 -0600

    Time: 1 ms (100.00%) | Memory: 42.9 MB (39.74%) - LeetSync

--- 415-add-strings/add-strings.java
+++ 415-add-strings/add-strings.java
@@ -1,28 +1,31 @@
 class Solution {
     public String addStrings(String num1, String num2) {
-        int ptr1 = num1.length() - 1, ptr2 = num2.length() - 1;
-        int carry = 0;
         StringBuilder result = new StringBuilder();
-        while(ptr1 >= 0 || ptr2 >= 0){
-            int sum = 0;
-            if(ptr1 >= 0 && ptr2 >= 0){
-                int c1 = (int)(num1.charAt(ptr1--) - '0'), c2 = (int)(num2.charAt(ptr2--) - '0');
-                sum = c1 + c2 + carry;
-                result.append(sum % 10);
-                carry = sum/10;
-            }else if(ptr1 >= 0){
-                int c1 = (int)(num1.charAt(ptr1--) - '0');
-                sum = c1 + carry;
-                result.append(sum % 10);
-                carry = sum/10;
-            }else if(ptr2 >= 0){
-                int c2 = (int)(num2.charAt(ptr2--) - '0');
-                sum = c2 + carry;
-                result.append(sum % 10);
-                carry = sum/10;
+
+        int ptrA = num1.length() - 1, ptrB = num2.length() - 1;
+        int carry = 0;
+        while(ptrA >= 0 || ptrB >= 0) {
+            if(ptrA >= 0 && ptrB >= 0) {
+                int digit1 = num1.charAt(ptrA--) - '0';
+                int digit2 = num2.charAt(ptrB--) - '0';
+                int sum = digit1 + digit2 + carry;
+                result.append((char) ((sum % 10) + '0'));
+                carry = sum / 10;
+            }else if(ptrA >= 0) {
+                int digit1 = num1.charAt(ptrA--) - '0';
+                int sum = digit1 + carry;
+                result.append((char) ((sum % 10) + '0'));
+                carry = sum / 10;
+            }else if(ptrB >= 0) {
+                int digit2 = num2.charAt(ptrB--) - '0';
+                int sum = digit2 + carry;
+                result.append((char) ((sum % 10) + '0'));
+                carry = sum / 10;
             }
         }
-        if(carry != 0) result.append(carry);
-        return result.reverse().toString();
+
+        if(carry != 0) result.append((char)(carry + '0'));
+        result.reverse();
+        return result.toString();
     }
 }
