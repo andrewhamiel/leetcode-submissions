4b3ed1ee836af396cfa31aa88ad832998a9c9ac8
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-21 19:40:14 -0600

    Time: 0 ms (100.00%) | Memory: 43 MB (34.89%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -3,7 +3,6 @@ class Solution {
         if(missingNums(arr.length - 1, arr) < k) return arr[arr.length - 1] + k - missingNums(arr.length - 1, arr);
 
         int left = 0, right = arr.length - 1;
-        //[left, right] invariant 
         while(left <= right) {
             int mid = left + (right - left)/2;
             if(missingNums(mid, arr) < k) left = mid + 1;
@@ -17,6 +16,6 @@ class Solution {
     }
 
     private int missingNums(int ind, int[] nums) {
-        return nums[ind] - ind - 1; //first number is 1
+        return nums[ind] - ind - 1; //1 is first num
     }
 }
