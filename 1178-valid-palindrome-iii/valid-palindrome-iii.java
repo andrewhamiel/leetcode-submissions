29c210604207a707085c9dd16ab154cf82ad2a70
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-21 21:02:14 -0600

    Time: 15 ms (24.11%) | Memory: 45.3 MB (87.30%) - LeetSync

--- 1178-valid-palindrome-iii/valid-palindrome-iii.java
+++ 1178-valid-palindrome-iii/valid-palindrome-iii.java
@@ -1,20 +1,16 @@
 class Solution {
     public boolean isValidPalindrome(String s, int k) {
-        return longestPalindromicSubstring(s) + k >= s.length();
+        return longestCommonSubsequence(s, new StringBuilder(s).reverse().toString()) + k >= s.length();
     }
 
-    private int longestPalindromicSubstring(String s){
-        return longestCommonSubsequence(s, new StringBuilder(s).reverse().toString());
-    }
-
-    private int longestCommonSubsequence(String word1, String word2){
-        int[][] memo = new int[word1.length() + 1][word2.length() + 1];
-        for(int i = word1.length() - 1; i >= 0; i--){
-            for(int j = word2.length() - 1; j >= 0; j--){
-                if(word1.charAt(i) == word2.charAt(j)) memo[i][j] = 1 + memo[i + 1][j + 1];
-                else memo[i][j] = Math.max(memo[i + 1][j], memo[i][j + 1]);
+    private int longestCommonSubsequence(String s1, String s2) {
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        for(int i = s1.length() - 1; i >= 0; i--) {
+            for(int j = s2.length() - 1; j >= 0; j--) {
+                if(s1.charAt(i) == s2.charAt(j)) dp[i][j] = 1 + dp[i + 1][j + 1];
+                else dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
             }
         }
-        return memo[0][0];
+        return dp[0][0];
     }
 }
