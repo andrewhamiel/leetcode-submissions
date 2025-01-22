bd16b83582c2e892422a2e0a4f575e4b731a79ac
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-21 21:09:39 -0600

    Time: 29 ms (79.40%) | Memory: 47.6 MB (73.91%) - LeetSync

--- 721-accounts-merge/accounts-merge.java
+++ 721-accounts-merge/accounts-merge.java
@@ -1,34 +1,30 @@
 class Solution {
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
         UnionFind uf = new UnionFind(accounts.size());
-        //1. Find groups
-        Map<String, Integer> emails = new HashMap<>();
+        Map<String, Integer> emailGroups = new HashMap<>();
         for(int i = 0; i < accounts.size(); i++) {
             List<String> account = accounts.get(i);
             for(int j = 1; j < account.size(); j++) {
                 String email = account.get(j);
-                if(emails.containsKey(email)) uf.union(emails.get(email), i);
-                else emails.put(email, i);
+                if(!emailGroups.containsKey(email)) emailGroups.put(email, i);
+                else uf.union(emailGroups.get(email), i);
             }
         }
 
-        //2. Group components
         Map<Integer, List<String>> components = new HashMap<>();
-        for(String email : emails.keySet()) {
-            int unrankedGroup = emails.get(email);
+        for(String email : emailGroups.keySet()) {
+            int unrankedGroup = emailGroups.get(email);
             int rankedGroup = uf.find(unrankedGroup);
             components.computeIfAbsent(rankedGroup, k -> new ArrayList<>()).add(email);
         }
 
-        //3. Build result
         List<List<String>> result = new ArrayList<>();
-        for(int rankedGroup : components.keySet()) {
-            List<String> emailList = components.get(rankedGroup);
-            String accountName = accounts.get(rankedGroup).get(0);
-            Collections.sort(emailList);
+        for(Integer rankedGroup : components.keySet()) {
+            List<String> emails = components.get(rankedGroup);
+            Collections.sort(emails);
             List<String> list = new ArrayList<>();
-            list.add(accountName);
-            list.addAll(emailList);
+            list.add(accounts.get(rankedGroup).get(0));
+            list.addAll(emails);
             result.add(list);
         }
         return result;
@@ -41,7 +37,6 @@ class Solution {
         public UnionFind(int n) {
             group = new int[n];
             rank = new int[n];
-
             for(int i = 0; i < n; i++) {
                 group[i] = i;
                 rank[i] = 1;
