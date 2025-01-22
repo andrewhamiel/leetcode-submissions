ce3b543d6f7bab70267843874d48ef14d0c701c0
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-21 20:33:15 -0600

    Time: 0 ms (100.00%) | Memory: 46.2 MB (28.95%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,22 +1,20 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if(nums == null || nums.length == 0) return new int[]{-1, -1};
-        int firstIndex = binarySearch(nums, target, true);
-        if(firstIndex == -1) return new int[]{-1, -1};
-        
-        int secondIndex = binarySearch(nums, target, false);
-        return new int[]{firstIndex, secondIndex};
+        int first = binarySearch(nums, target, true);
+        if(first == -1) return new int[]{-1, -1};
+        int second = binarySearch(nums, target, false);
+        return new int[]{first, second};
     }
 
-    private int binarySearch(int[] nums, int target, boolean isLeft){
+    private int binarySearch(int[] nums, int target, boolean isLeft) {
         int left = 0, right = nums.length - 1;
-        while(left <= right){
+        while(left <= right) {
             int mid = left + (right - left)/2;
-            if(isLeft){
+            if(isLeft) {
                 if(nums[mid] == target && (mid == 0 || nums[mid - 1] != target)) return mid;
                 else if(nums[mid] >= target) right = mid - 1;
                 else left = mid + 1;
-            }else{
+            }else {
                 if(nums[mid] == target && (mid == nums.length - 1 || nums[mid + 1] != target)) return mid;
                 else if(nums[mid] <= target) left = mid + 1;
                 else right = mid - 1;
