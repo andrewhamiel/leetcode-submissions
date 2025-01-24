505a762f7373a5096728ec34d2a11ef5a3ee9a84
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-24 11:18:37 -0600

    Time: 1 ms (99.81%) | Memory: 53.5 MB (21.21%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,9 +1,11 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int candidate = 0, count = 0;
+        //Boyer moore
+        int candidate = nums[0], count = 0;
         for(int num : nums) {
             if(count == 0) candidate = num;
-            count+= num == candidate ? 1 : -1;
+
+            count+= candidate == num ? 1 : -1;           
         }
         return candidate;
     }
