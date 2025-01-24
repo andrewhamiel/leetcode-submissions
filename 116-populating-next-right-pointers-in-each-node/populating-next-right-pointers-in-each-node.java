24491f1ff17d969f21ee8056bde48af3e1316c74
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-24 10:45:00 -0600

    Time: 0 ms (100.00%) | Memory: 44 MB (98.05%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -1,50 +1,23 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
-
 class Solution {
     public Node connect(Node root) {
-        //1. Base case: if root  is null
-        if(root == null) return null;
-        //2. Level-order traversal
-        Queue<Node> q = new LinkedList<>();
-        q.add(root);
+        if (root == null) return root;
 
-        
+        Node leftmost = root;
+        while (leftmost.left != null) {
+            Node head = leftmost;
+            while (head != null) {
+                // CONNECTION 1
+                head.left.next = head.right;
 
-        //BFS
-        while(!q.isEmpty()) {
-            //Iterate level by level
-            Node prev = null;
-            int size = q.size();
-            while(size-- > 0) {
-                Node curr = q.poll();
-                if(prev != null) prev.next = curr;
+                // CONNECTION 2
+                if (head.next != null) head.right.next = head.next.left;
 
-                if(curr.left != null) q.add(curr.left);
-                if(curr.right != null) q.add(curr.right);
-//q: [], size = 1, prev = 7, curr = 7
-                prev = curr;
+                // Progress along the list (nodes on the current level)
+                head = head.next;
             }
+
+            // Move onto the next level
+            leftmost = leftmost.left;
         }
         return root;
     }
