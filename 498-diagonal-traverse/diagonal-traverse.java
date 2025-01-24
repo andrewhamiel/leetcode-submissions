5985b645bbec82f4b05e3b66e02904ab0719edb3
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-24 11:13:00 -0600

    Time: 5 ms (41.57%) | Memory: 47.2 MB (69.70%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.java
+++ 498-diagonal-traverse/diagonal-traverse.java
@@ -7,21 +7,23 @@ class Solution {
             result[k] = mat[row][col];
             int newRow = row, newCol = col;
             if(direction == 1) {
+                //up and to the right
                 newRow--;
                 newCol++;
             }else {
+                //Down and to the left
                 newRow++;
                 newCol--;
             }
 
-            //Correct at bounds 
             if(newRow < 0 || newRow >= rows || newCol < 0 || newCol >= cols) {
+                //Out of bounds
                 if(direction == 1) {
                     row+= (col == cols - 1 ? 1 : 0);
-                    col+= (col < cols - 1 ? 1 : 0);
+                    col+= (col < cols - 1 ? 1 : 0);                  
                 }else {
                     col+= (row == rows - 1 ? 1 : 0);
-                    row+= (row < rows - 1 ? 1 : 0);
+                    row+= (row < rows - 1 ? 1 : 0);                    
                 }
                 direction*= -1;
             }else {
