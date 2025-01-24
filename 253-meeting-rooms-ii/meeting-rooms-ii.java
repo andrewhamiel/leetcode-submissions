87179d669a0abc9c8ddf5063be926bc73b9af71a
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-24 12:19:36 -0600

    Time: 19 ms (5.43%) | Memory: 45.8 MB (86.79%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/meeting-rooms-ii.java
@@ -1,20 +1,29 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        PriorityQueue<Integer> startTimes = new PriorityQueue<>(), endTimes = new PriorityQueue<>();
+        TreeMap<Integer, Integer> startTimes = new TreeMap<>(), endTimes = new TreeMap<>();
         for(int[] interval : intervals) {
-            startTimes.add(interval[0]);
-            endTimes.add(interval[1]);
+            startTimes.put(interval[0], startTimes.getOrDefault(interval[0], 0) + 1);
+            endTimes.put(interval[1], endTimes.getOrDefault(interval[1], 0) - 1);
         }
 
-        int usedRooms = 0;
-        while(!startTimes.isEmpty()) {
-            int startTime = startTimes.peek(), endTime = endTimes.peek();
-            if(startTime >= endTime) {
-                endTimes.poll();
+        Integer usedRooms = 0, startKey = startTimes.firstKey(), endKey = endTimes.firstKey();
+        int startCount = startTimes.get(startKey), endCount = endTimes.get(endKey);
+        while(startKey != null) {
+            if(startKey >= endKey) {
                 usedRooms--;
+                
+                endCount++;
+                if(endCount == 0) {
+                    endKey = endTimes.higherKey(endKey);
+                    if(endKey != null) endCount = endTimes.get(endKey);
+                }
             }
             usedRooms++;
-            startTimes.poll();
+            startCount--;
+            if(startCount == 0) {
+                startKey = startTimes.higherKey(startKey);
+                if(startKey != null) startCount = startTimes.get(startKey);
+            }
         }
         return usedRooms;
     }
