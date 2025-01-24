a20c7d71c294d30d1b5b2470047adb83d345b202
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-24 12:57:45 -0600

    Time: 6 ms (73.53%) | Memory: 41.5 MB (72.26%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,20 +1,20 @@
 class Solution {
     public int countSubstrings(String s) {
-        int count = 0;
+        int result = 0;
         for(int i = 0; i < s.length(); i++) {
-            int oddLength = countPalindromes(i, i, s);
-            int evenLength = countPalindromes(i, i + 1, s);
-            count+= oddLength + evenLength;
+            int oddSubstrings = middleOut(i, i, s);
+            int evenSubstrings = middleOut(i, i + 1, s);
+            result+= oddSubstrings + evenSubstrings;
         }
-        return count;
+        return result;
     }
 
-    private int countPalindromes(int left, int right, String s) {
+    private int middleOut(int left, int right, String s) {
         int count = 0;
         while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            count++;
             left--;
             right++;
+            count++;
         }
         return count;
     }
