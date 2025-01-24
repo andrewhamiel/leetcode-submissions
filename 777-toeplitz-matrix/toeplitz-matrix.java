127620117626a190842ade5f39dca845399c007d
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-24 11:13:56 -0600

    Time: 0 ms (100.00%) | Memory: 44.1 MB (90.10%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.java
+++ 777-toeplitz-matrix/toeplitz-matrix.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
         for(int row = 1; row < matrix.length; row++) {
             for(int col = 1; col < matrix[0].length; col++) {
-                if(matrix[row][col] != matrix[row - 1][col - 1]) return false;
+                if(matrix[row - 1][col - 1] != matrix[row][col]) return false;
             }
         }
         return true;
