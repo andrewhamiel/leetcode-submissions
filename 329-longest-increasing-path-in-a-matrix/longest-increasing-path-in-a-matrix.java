c8ec34f08e07e6d358dbd9e0a39b90b3be3c697a
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-24 13:14:56 -0600

    Time: 8 ms (85.48%) | Memory: 45.2 MB (43.24%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -1,27 +1,26 @@
 class Solution {
     public int longestIncreasingPath(int[][] matrix) {
-        int[][] memo = new int[matrix.length][matrix[0].length];
+        int[][] visited = new int[matrix.length][matrix[0].length];
         int result = 0;
         for(int row = 0; row < matrix.length; row++) {
             for(int col = 0; col < matrix[0].length; col++) {
-                result = Math.max(result, dfs(row, col, matrix, memo, -1));
+                result = Math.max(result, dfs(row, col, matrix, visited, -1));
             }
         }
         return result;
     }
 
-    private int dfs(int row, int col, int[][] matrix, int[][] memo, int prevVal) {
-        if(row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length || matrix[row][col] <= prevVal) return 0;
+    private int dfs(int row, int col, int[][] matrix, int[][] visited, int prev) {
+        if(row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length || matrix[row][col] <= prev) return 0;
+        //Memoization
+        if(visited[row][col] != 0) return visited[row][col];
 
-        if(memo[row][col] != 0) return memo[row][col];
+        int curr = matrix[row][col];
+        int left = dfs(row, col - 1, matrix, visited, curr);
+        int right = dfs(row, col + 1, matrix, visited, curr);
+        int up = dfs(row - 1, col, matrix, visited, curr);
+        int down = dfs(row + 1, col, matrix, visited, curr);
 
-        int currVal = matrix[row][col];
-
-        int left = dfs(row, col - 1, matrix, memo, currVal);
-        int right = dfs(row, col + 1, matrix, memo, currVal);
-        int up = dfs(row -1, col, matrix, memo, currVal);
-        int down = dfs(row + 1, col, matrix, memo, currVal);
-
-        return memo[row][col] = 1 + Math.max(left, Math.max(right, Math.max(up, down)));
+        return visited[row][col] = 1 + Math.max(left, Math.max(right, Math.max(up, down)));
     }
 }
