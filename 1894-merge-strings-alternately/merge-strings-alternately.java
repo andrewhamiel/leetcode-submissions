ffeeef584639afc72f8a3a7cd66c1d2021a60f63
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-24 11:20:40 -0600

    Time: 1 ms (83.50%) | Memory: 41.8 MB (71.77%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,15 +1,15 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int word1Ind = 0, word2Ind = 0, ind = 0;
-        StringBuilder sb = new StringBuilder();
-        while(word1Ind < word1.length() && word2Ind < word2.length()){
-            if(ind % 2 == 0) sb.append(word1.charAt(word1Ind++));
-            else sb.append(word2.charAt(word2Ind++));
-            ind++;
+        StringBuilder result = new StringBuilder();
+        int word1Ind = 0, word2Ind = 0;
+        while(word1Ind < word1.length() && word2Ind < word2.length()) {
+            result.append(word1.charAt(word1Ind++));
+            result.append(word2.charAt(word2Ind++));
         }
 
-        while(word1Ind < word1.length()) sb.append(word1.charAt(word1Ind++));
-        while(word2Ind < word2.length()) sb.append(word2.charAt(word2Ind++));
-        return sb.toString();
+        while(word1Ind < word1.length()) result.append(word1.charAt(word1Ind++));
+        while(word2Ind < word2.length()) result.append(word2.charAt(word2Ind++));
+
+        return result.toString();
     }
 }
