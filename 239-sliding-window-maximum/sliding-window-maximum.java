10a9f8713141efda3d4171a508b7bb951c9f88e6
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-25 15:53:23 -0600

    Time: 39 ms (27.68%) | Memory: 58.8 MB (61.88%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,14 +1,13 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         Deque<Integer> deq = new ArrayDeque<>();
-        //First k
         int localMax = 0;
         for(int i = 0; i < k; i++) {
             cleanDeque(i, nums, deq, k);
             deq.addLast(i);
             if(nums[i] > nums[localMax]) localMax = i;
         }
-        
+
         int[] result = new int[nums.length - k + 1];
         result[0] = nums[localMax];
         for(int i = k; i < nums.length; i++) {
@@ -20,7 +19,9 @@ class Solution {
     }
 
     private void cleanDeque(int i, int[] nums, Deque<Integer> deq, int k) {
+        //If number out of window
         if(!deq.isEmpty() && deq.peekFirst() == i - k) deq.removeFirst();
+        //Remove nums in deq < nums[i]
         while(!deq.isEmpty() && nums[deq.peekLast()] < nums[i]) deq.removeLast();
     }
 }
