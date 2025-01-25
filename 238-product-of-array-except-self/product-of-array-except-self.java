c46ccd58af7a040030623fd7be96bf63355644ec
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-25 15:43:24 -0600

    Time: 2 ms (88.60%) | Memory: 58 MB (5.27%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,14 +1,23 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int totalProduct = 1, zeroCount = 0;
+        int zeroCount = 0, totalProduct = 1;
         for(int num : nums) {
             if(num == 0) zeroCount++;
             else totalProduct*= num;
         }
 
+        if(zeroCount > 1) {
+            Arrays.fill(nums, 0);
+            return nums;
+        }
+
         for(int i = 0; i < nums.length; i++) {
-            if(zeroCount > 0) nums[i] = nums[i] != 0 || zeroCount > 1 ? 0 : totalProduct;
-            else nums[i] = totalProduct / nums[i];
+            if(zeroCount > 0) {
+                if(nums[i] == 0) nums[i] = totalProduct;
+                else nums[i] = 0; 
+            }else {
+                nums[i] = totalProduct / nums[i];
+            }
         }
         return nums;
     }
