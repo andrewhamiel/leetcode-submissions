32eb6a160614f73ebacf47cc48d03f0aee2f25a7
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-25 15:50:59 -0600

    Time: 40 ms (25.84%) | Memory: 58.6 MB (71.95%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,27 +1,26 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         Deque<Integer> deq = new ArrayDeque<>();
+        //First k
         int localMax = 0;
         for(int i = 0; i < k; i++) {
-            cleanDeque(i, k, nums, deq);
+            cleanDeque(i, nums, deq, k);
             deq.addLast(i);
             if(nums[i] > nums[localMax]) localMax = i;
         }
-
+        
         int[] result = new int[nums.length - k + 1];
         result[0] = nums[localMax];
         for(int i = k; i < nums.length; i++) {
-            cleanDeque(i, k, nums, deq);
+            cleanDeque(i, nums, deq, k);
             deq.addLast(i);
             result[i - k + 1] = nums[deq.peekFirst()];
         }
         return result;
     }
 
-    private void cleanDeque(int i, int k, int[] nums, Deque<Integer> deq) {
-        //If window size is too large 
+    private void cleanDeque(int i, int[] nums, Deque<Integer> deq, int k) {
         if(!deq.isEmpty() && deq.peekFirst() == i - k) deq.removeFirst();
-        //Remove nums from deq < nums[i]
         while(!deq.isEmpty() && nums[deq.peekLast()] < nums[i]) deq.removeLast();
     }
 }
