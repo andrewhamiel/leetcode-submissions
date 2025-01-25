31c5548d30f78dc0a4d910a47cbae265e2d5bddd
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-25 16:11:20 -0600

    Time: 3 ms (34.04%) | Memory: 44.8 MB (83.70%) - LeetSync

--- 529-minesweeper/minesweeper.java
+++ 529-minesweeper/minesweeper.java
@@ -1,19 +1,17 @@
 class Solution {
-    private int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}, {-1, -1}, {1, -1}, {-1, 1}, {1, 1}};
+    private int[][] dirs = new int[][]{{0, -1}, {0, 1}, {-1, 0}, {1, 0}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
 
     public char[][] updateBoard(char[][] board, int[] click) {
         Queue<int[]> q = new LinkedList<>();
         q.add(click);
 
         while(!q.isEmpty()) {
-            int size = q.size();
             int[] cell = q.poll();
             int row = cell[0], col = cell[1];
-
             if(board[row][col] == 'M') board[row][col] = 'X';
             else {
                 int mines = getMines(row, col, board);
-                if(mines > 0) board[row][col] = (char) (mines + '0');
+                if(mines > 0) board[row][col] = (char)(mines + '0');
                 else {
                     board[row][col] = 'B';
                     for(int[] dir : dirs) {
@@ -22,20 +20,20 @@ class Solution {
                             && board[newRow][newCol] == 'E') {
                                 board[newRow][newCol] = 'B';
                                 q.add(new int[]{newRow, newCol});
-                            }
+                        }
                     }
                 }
             }
         }
-        return board;    
+        return board;
     }
 
     private int getMines(int row, int col, char[][] board) {
         int mines = 0;
         for(int[] dir : dirs) {
             int newRow = row + dir[0], newCol = col + dir[1];
-            if(newRow >= 0 && newRow < board.length && newCol >= 0 && newCol < board[0].length
-                && (board[newRow][newCol] == 'M' || board[newRow][newCol] == 'X')) mines++;
+            if(newRow >= 0 && newRow < board.length && newCol >= 0 && newCol < board[0].length 
+                && (board[newRow][newCol] == 'X' || board[newRow][newCol] == 'M')) mines++;
         }
         return mines;
     }
