fb501a3e3da26183ea7631fcab10835b9f5f7bee
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-26 07:40:54 -0600

    Time: 0 ms (100.00%) | Memory: 42.3 MB (8.08%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -10,22 +10,16 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        //1. Find size
+        ListNode prev = head, curr = head;
         int size = 0;
-        ListNode curr =  head;
-        while(curr != null) {
-            curr = curr.next;
+        while(curr != null) {                                
+            if(size > n) prev = prev.next;
+            curr = curr.next;  
             size++;
         }
-        //2. Remove nth node
-        curr = head;
-        //Advance to pointer before nth node
-        for(int currNode = 1; currNode < size - n; currNode++) {
-            curr = curr.next;
-        }
-        //3. Link to nth + 1 node if present
         if(size == n) head = head.next;
-        else curr.next = curr.next.next;
+        else prev.next = prev.next.next;
+
         return head;
     }
 }
