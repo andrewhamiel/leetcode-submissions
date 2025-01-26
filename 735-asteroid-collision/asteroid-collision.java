e6fe7649f14117d0b1cddff636f8445a4d59bcdb
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-26 09:08:46 -0600

    Time: 4 ms (94.88%) | Memory: 45.4 MB (70.33%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -4,23 +4,19 @@ class Solution {
         for(int asteroid : asteroids) {
             while(!stack.isEmpty() && stack.peekFirst() > 0 && asteroid < 0) {
                 int sum = stack.peekFirst() + asteroid;
-                if(sum == 0) {
+                if(sum > 0) asteroid = 0;
+                else if(sum < 0) stack.removeFirst();
+                else {
                     //Both explode
                     stack.removeFirst();
-                    asteroid = 0;
-                }else if(sum < 0) {
-                    //Asteroid on stack explodes
-                    stack.removeFirst();
-                }else {
-                    //Asteroid explodes
                     asteroid = 0;
                 }
             }
             if(asteroid != 0) stack.addFirst(asteroid);
         }
 
-    int[] result = new int[stack.size()];
-    for(int i = result.length - 1; i >= 0; i--) result[i] = stack.removeFirst();
-    return result;
-    }   
+        int[] result = new int[stack.size()];
+        for(int i = result.length - 1; i >= 0; i--) result[i] = stack.removeFirst();
+        return result;
+    }
 }
