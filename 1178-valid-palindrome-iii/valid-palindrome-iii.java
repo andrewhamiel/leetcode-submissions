678fa40947987a22c12f8ce5becc6f12bf3a7f3a
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-26 07:22:40 -0600

    Time: 13 ms (40.85%) | Memory: 45.2 MB (88.88%) - LeetSync

--- 1178-valid-palindrome-iii/valid-palindrome-iii.java
+++ 1178-valid-palindrome-iii/valid-palindrome-iii.java
@@ -3,14 +3,19 @@ class Solution {
         return longestCommonSubsequence(s, new StringBuilder(s).reverse().toString()) + k >= s.length();
     }
 
-    private int longestCommonSubsequence(String s1, String s2) {
-        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
-        for(int i = s1.length() - 1; i >= 0; i--) {
-            for(int j = s2.length() - 1; j >= 0; j--) {
-                if(s1.charAt(i) == s2.charAt(j)) dp[i][j] = 1 + dp[i + 1][j + 1];
-                else dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+    private int longestCommonSubsequence(String num1, String num2) {
+        if(num1.length() > num2.length()) return longestCommonSubsequence(num2, num1);
+
+        int[] prev = new int[num2.length() + 1];
+
+        for(int row = num1.length() - 1; row >= 0; row--) {
+            int[] curr = new int[num2.length() + 1];
+            for(int col = num2.length() - 1; col >= 0; col--) {
+                if(num1.charAt(row) == num2.charAt(col)) curr[col] = 1 + prev[col + 1];
+                else curr[col] = Math.max(prev[col], curr[col + 1]);
             }
+            prev = curr;
         }
-        return dp[0][0];
+        return prev[0];
     }
 }
