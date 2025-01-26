9897477ce5614908126963699ea356941a52bf8b
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-26 06:48:09 -0600

    Time: 0 ms (100.00%) | Memory: 41.7 MB (88.97%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.java
+++ 408-valid-word-abbreviation/valid-word-abbreviation.java
@@ -4,16 +4,15 @@ class Solution {
         while(abbrInd < abbr.length()) {
             char c = abbr.charAt(abbrInd);
             if(Character.isDigit(c)) {
-                if(c == '0' && currNum == 0) return false;
+                if(abbr.charAt(abbrInd++) == '0' && currNum == 0) return false;
                 currNum*= 10;
-                currNum+= abbr.charAt(abbrInd++) - '0';
+                currNum+= c - '0';
             }else {
                 wordInd+= currNum;
                 currNum = 0;
                 if(wordInd >= word.length() || word.charAt(wordInd++) != abbr.charAt(abbrInd++)) return false;
             }
         }
-
         wordInd+= currNum;
         return wordInd == word.length();
     }
