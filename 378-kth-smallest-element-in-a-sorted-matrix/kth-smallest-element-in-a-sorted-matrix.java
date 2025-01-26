09f09e9244d8fc5a1ab8c64b0af16c4147b26332
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-26 12:02:37 -0600

    Time: 4 ms (66.84%) | Memory: 49.9 MB (26.59%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -1,39 +1,36 @@
 class Solution {
-    private int[][] matrix;
-    private int rows, cols;
+    private int rows = 0;
+    private int cols = 0;
 
     public int kthSmallest(int[][] matrix, int k) {
-        this.matrix = matrix;
         rows = matrix.length;
         cols = matrix[0].length;
-        return quickselect(0, rows * cols - 1, rows * cols - k);
+        return quickselect(0, rows * cols  - 1, matrix, k - 1);
     }
 
-    private int quickselect(int left, int right, int k){
+    private int quickselect(int left, int right, int[][] matrix, int k) {
         if(left >= right) return matrix[left / cols][left % cols];
 
-        int pivotIndex = new Random().nextInt(right - left) + left;
-        pivotIndex = partition(left, right, pivotIndex);
-
-        if(pivotIndex == k) return matrix[k / cols][k % cols];
-        else if(pivotIndex < k) return quickselect(pivotIndex + 1, right, k);
-        else return quickselect(left, pivotIndex - 1, k);
+        int pivotInd = new Random().nextInt(right - left) + left;
+        pivotInd = partition(left, right, matrix, pivotInd);
+        
+        if(pivotInd == k) return matrix[k / cols][k % cols];
+        else if(pivotInd < k) return quickselect(pivotInd + 1, right, matrix, k);
+        else return quickselect(left, pivotInd - 1, matrix, k);
     }
 
-    private int partition(int left, int right, int pivotIndex){
-        int pivotValue = matrix[pivotIndex / cols][pivotIndex % cols];
-        swap(pivotIndex, right);
-        int swapIndex = left;
-
-        for(int i = left; i <= right; i++){
-            if(matrix[i / cols][i % cols] > pivotValue) swap(swapIndex++, i);
+    private int partition(int left, int right, int[][] matrix, int pivotInd) {
+        int pivotValue = matrix[pivotInd / cols][pivotInd % cols];
+        swap(pivotInd, right, matrix);
+        int swapInd = left;
+        for(int i = left; i <= right; i++) {
+            if(matrix[i / cols][i % cols] < pivotValue) swap(swapInd++, i, matrix);
         }
-
-        swap(swapIndex, right);
-        return swapIndex;
+        swap(swapInd, right, matrix);
+        return swapInd;
     }
 
-    private void swap(int i, int j){
+    private void swap(int i, int j, int[][] matrix) {
         int tmp = matrix[i / cols][i % cols];
         matrix[i / cols][i % cols] = matrix[j / cols][j % cols];
         matrix[j / cols][j % cols] = tmp;
