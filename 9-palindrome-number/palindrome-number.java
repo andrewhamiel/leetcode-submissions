2b300e8ea8881deed2bfad8993321dcd7bc64501
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-26 08:00:12 -0600

    Time: 5 ms (72.56%) | Memory: 43.9 MB (88.03%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,13 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0|| (x != 0 && x % 10 == 0)) return false;
+        if(x < 0 || (x != 0 && x % 10 == 0)) return false;
+
         int reversedNum = 0;
         while(reversedNum < x) {
             reversedNum*= 10;
             reversedNum+= (x % 10);
             x/= 10;
         }
-        //Even length exact same, odd length one digit away
+        //Even length or odd length remove one digit
         return reversedNum == x || reversedNum / 10 == x;
     }
 }
