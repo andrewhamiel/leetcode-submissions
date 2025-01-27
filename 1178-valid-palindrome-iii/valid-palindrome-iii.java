ba73e21a43fa178ad91bd76af28ac55ce0d14c2d
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-27 06:54:56 -0600

    Time: 16 ms (20.34%) | Memory: 45.4 MB (84.28%) - LeetSync

--- 1178-valid-palindrome-iii/valid-palindrome-iii.java
+++ 1178-valid-palindrome-iii/valid-palindrome-iii.java
@@ -4,18 +4,14 @@ class Solution {
     }
 
     private int longestCommonSubsequence(String num1, String num2) {
-        if(num1.length() > num2.length()) return longestCommonSubsequence(num2, num1);
-
-        int[] prev = new int[num2.length() + 1];
+        int[][] dp = new int[num1.length() + 1][num2.length() + 1];
 
         for(int row = num1.length() - 1; row >= 0; row--) {
-            int[] curr = new int[num2.length() + 1];
             for(int col = num2.length() - 1; col >= 0; col--) {
-                if(num1.charAt(row) == num2.charAt(col)) curr[col] = 1 + prev[col + 1];
-                else curr[col] = Math.max(prev[col], curr[col + 1]);
+                if(num1.charAt(row) == num2.charAt(col)) dp[row][col] = 1 + dp[row + 1][col + 1];
+                else dp[row][col] = Math.max(dp[row][col + 1], dp[row + 1][col]);
             }
-            prev = curr;
         }
-        return prev[0];
+        return dp[0][0];
     }
 }
