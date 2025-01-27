048a83dfa46c21e5d7471fb165e26486e310bfe2
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-27 07:44:26 -0600

    Time: 81 ms (20.87%) | Memory: 54.1 MB (91.13%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.java
+++ 493-reverse-pairs/reverse-pairs.java
@@ -10,15 +10,13 @@ class Solution {
         int pairs = mergeSort(start, mid, nums) + mergeSort(mid + 1, end, nums) + countPairs(start, end, nums);
 
         merge(start, end, nums);
-
         return pairs;
     }
 
     private int countPairs(int start, int end, int[] nums) {
-        int mid = start + (end - start)/2, count = 0, right = mid + 1;
+        int mid = start + (end - start)/2, right = mid + 1, count = 0;
         for(int left = start; left <= mid; left++) {
-            while(right <= end && nums[left] > 2 * (long) nums[right]) right++;
-            //Right now at first index where condition does not hold true
+            while(right <= end && nums[left] >  2 * (long)nums[right]) right++;
             count+= right - mid - 1;
         }
         return count;
