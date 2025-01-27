a6cebede0783c67c721bccae72a5ba85caca0aec
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-27 07:32:24 -0600

    Time: 0 ms (100.00%) | Memory: 42.1 MB (17.65%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,16 +1,18 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int carry = 0;
         digits[digits.length - 1]++;
+        int carry = 0;
         for(int i = digits.length - 1; i >= 0; i--) {
-            int sum = digits[i] + carry;
+            int sum  = digits[i] + carry;
+            
             digits[i] = sum % 10;
-            if(sum < 10) return digits;
             carry = sum / 10;
+            if(sum < 10) return digits;
         }
-        //All numbers '9' if we reach this point
+
+        //If at this point, you have all 9s. New array adds a prefix 1 then returns all 0s after
         int[] result = new int[digits.length + 1];
-        result[0] = 1; 
+        result[0] = 1;
         return result;
     }
 }
