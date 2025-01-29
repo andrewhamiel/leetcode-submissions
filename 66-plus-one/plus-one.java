3935a11c008e2b016aaa48028e1b6eb2eca7cca4
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-28 19:34:48 -0600

    Time: 0 ms (100.00%) | Memory: 41.5 MB (96.51%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -3,14 +3,12 @@ class Solution {
         digits[digits.length - 1]++;
         int carry = 0;
         for(int i = digits.length - 1; i >= 0; i--) {
-            int sum  = digits[i] + carry;
-            
+            int sum = digits[i] + carry;
             digits[i] = sum % 10;
             carry = sum / 10;
             if(sum < 10) return digits;
         }
-
-        //If at this point, you have all 9s. New array adds a prefix 1 then returns all 0s after
+        //If we are here, all digits were 9. new array with first digit 1
         int[] result = new int[digits.length + 1];
         result[0] = 1;
         return result;
