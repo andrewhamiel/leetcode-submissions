18fdb047d4c894a82cfa1e16e1df2386c2eaa956
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-28 19:30:28 -0600

    Time: 3 ms (41.99%) | Memory: 42 MB (22.57%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,10 +2,11 @@ class Solution {
     public boolean isValid(String s) {
         Map<Character, Character> rightMapping = Map.of(')', '(', '}', '{', ']', '[');
         Deque<Character> stack = new ArrayDeque<>();
-        for(char c : s.toCharArray()) {
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
             if(rightMapping.containsKey(c)) {
                 if(stack.isEmpty() || stack.removeFirst() != rightMapping.get(c)) return false;
-            }else stack.addFirst(c);
+            } else stack.addFirst(c);
         }
         return stack.isEmpty();
     }
