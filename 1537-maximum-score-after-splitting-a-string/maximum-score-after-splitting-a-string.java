8f5f7711adceb68dc499db76ed489d2cc2535469
Andrew Hamiel <42918502+andrewhamiel@users.noreply.github.com>
2025-01-28 18:50:45 -0600

    Time: 1 ms (98.59%) | Memory: 41.8 MB (19.68%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -1,15 +1,15 @@
 class Solution {
     public int maxScore(String s) {
-        int ones = 0, zeros = 0, best = Integer.MIN_VALUE;       
-        for (int i = 0; i < s.length() - 1; i++) {
-            if (s.charAt(i) == '1') ones++;
-            else zeros++;
+        int ones = 0, zeroes = 0, best = Integer.MIN_VALUE;
+        for(int i = 0; i < s.length() - 1; i++) {
+            if(s.charAt(i) == '1') ones++;
+            else zeroes++;
 
-            best = Math.max(best, zeros - ones);
+            best = Math.max(best, zeroes - ones);
         }
-        
-        if (s.charAt(s.length() - 1) == '1') ones++;
-    
+
+        if(s.charAt(s.length() - 1) == '1') ones++;
+
         return best + ones;
     }
 }
